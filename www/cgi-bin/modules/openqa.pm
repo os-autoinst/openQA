7592911080d35650fc8c98d2fed99183eb7b23c7
Dominik Heidler <dheidler@suse.de>
2011-12-01 11:53:23 +0100

    cleanup openqa cgi module

--- www/cgi-bin/modules/openqa.pm
+++ www/cgi-bin/modules/openqa.pm
@@ -8,7 +8,7 @@ $VERSION = sprintf "%d.%03d", q$Revision: 1.12 $ =~ /(\d+)/g;
 @ISA = qw(Exporter);
 @EXPORT = qw(
 $prj $basedir $perldir $perlurl
-&parse_log &parse_log_to_stats &parse_log_to_hash &log_to_scriptpath &path_to_url &split_filename &get_header_footer &resultname_to_log &resultname_to_url &is_authorized_rw &get_testimgs &get_waitimgs &get_clickimgs testimg &get_testwavs &running_log &clickimg &path_to_testname &cycle &sortkeys &syntax_highlight &first_run &data_name &parse_refimg_path &parse_refimg_name
+&parse_log &parse_log_to_stats &parse_log_to_hash &log_to_scriptpath &path_to_url &split_filename &resultname_to_log &resultname_to_url &is_authorized_rw &get_testimgs &get_waitimgs &get_clickimgs testimg &get_testwavs &running_log &clickimg &path_to_testname &cycle &sortkeys &syntax_highlight &first_run &data_name &parse_refimg_path &parse_refimg_name
 );
 use lib "/srv/www/cgi-bin/modules";
 use awstandard;
@@ -90,35 +90,13 @@ sub imgdir($) { my $fn=shift;
 	$fn=~s%\.ogv$%%;
 	"$basedir/$prj/testresults/$fn";
 }
+
 sub path_to_url($) { my($fn)=@_;
 	my $url=$fn;
 	$url=~s%^$basedir%%; # strip path to make relative URL
 	return $url;
 }
 
-sub path_to_ogvlink($) { my($fn)=@_;
-	my $url=path_to_url($fn);
-	$url=~s%\.autoinst\.txt$%%;
-	$url=~s%^(.*)%<a href="$1"><img width="23" height="23" src="/images/video.png" alt="ogv" title="ogg/theora video of this testrun"/></a>%;
-	return $url;
-}
-sub path_to_detailurl($) { my($fn)=@_;
-	my $url=path_to_url($fn);
-	$url=~s%^/$prj/video(.*).ogv.autoinst.txt%/results$1%;
-	return $url;
-}
-sub path_to_detaillink($) { my($fn)=@_;
-	my $url=path_to_detailurl($fn);
-	$url=qq(<a href="$url"><img width="23" height="23" src="/images/details.png" alt="details" title="test result details"/></a>);
-	return $url;
-}
-sub path_to_loglink($) { my($fn)=@_;
-	my $url=path_to_url($fn);
-	return qq%<a href="$url"><img width="23" height="23" src="/images/log.png" alt="log" title="complete log of this testrun"/></a>%;
-}
-sub test_to_logslink($) { my($fn)=@_;
-	return qq%<a href="/$prj/logs/$fn.tar.bz2"><img width="23" height="23" src="/images/download.png" alt="logs" title="download var/log.tar.bz2"/></a>%;
-}
 sub split_filename($) { my($fn)=@_;
 	my $origfn=$fn;
 	$fn=~s%\.autoinst\.txt$%%; # strip suffix
@@ -133,19 +111,7 @@ sub split_filename($) { my($fn)=@_;
 	my @a=split("-",$fn);
 	$a[3]=~s/Build//;
 	$a[4]||=""; # extrainfo is optional
-	my $links=path_to_detaillink($origfn)." ".path_to_ogvlink($origfn);
-	return ($links, @a);
-}
-
-sub get_header_footer(;$)
-{	my ($title)=@_;
-	my $templatedir="/srv/www/htdocs/template";
-	my $header=file_content("$templatedir/header.html");
-	$title=($title? " &gt; $title" : "");
-	$header=~s{<!-- CURRENTLOCATION -->}{$title};
-	$header=file_content("$templatedir/header0.html").$header.file_content("$templatedir/header-cgi.html");
-	my $footer=file_content("$templatedir/footer.html");
-	return ($header,$footer);
+	return (@a);
 }
 
 sub resultname_to_log($)
