d97b5110b57d9f09563b0fd89dbcaa560c0197f9
Ludwig Nussel <ludwig.nussel@suse.de>
2013-08-21 14:33:20 +0200

    fix log upload

--- www/cgi-bin/modules/openqa.pm
+++ www/cgi-bin/modules/openqa.pm
@@ -8,10 +8,12 @@ $VERSION = sprintf "%d.%03d", q$Revision: 1.12 $ =~ /(\d+)/g;
 @ISA = qw(Exporter);
 @EXPORT = qw(
 $prj $basedir $perldir $perlurl $resultdir $scheduledir $app_title $app_subtitle @runner $res_css $res_display
+$loguploaddir
 &parse_log &parse_log_to_stats &parse_log_to_hash &parse_iso &log_to_scriptpath &path_to_url &split_filename &resultname_to_log &resultname_to_url &is_authorized_rw &is_scheduled &get_testimgs &get_waitimgs &get_clickimgs testimg &get_testwavs &running_log &clickimg &path_to_testname &cycle &sortkeys &syntax_highlight &first_run &data_name &parse_refimg_path &parse_refimg_name &back_log &running_state &get_running_modinfo &match_title &needle_info
 &test_result &test_result_stats &test_result_hash &test_result_module &test_resultfile_list &testresultdir &test_uploadlog_list
 $localstatedir $dbfile
 &get_failed_needles
+&sanitize_testname
 );
 #use lib "/usr/share/openqa/cgi-bin/modules";
 use awstandard;
@@ -24,6 +26,7 @@ our $perlurl="$prj/perl/autoinst";
 our $perldir="$basedir/$perlurl";
 our $resultdir="$basedir/$prj/testresults";
 our $scheduledir="$basedir/$prj/schedule.d";
+our $loguploaddir="$basedir/$prj/logupload";
 our $hostname=$ENV{'SERVER_NAME'};
 our $app_title = 'openQA test instance';
 our $app_subtitle = 'openSUSE automated testing';
@@ -507,6 +510,14 @@ sub get_failed_needles(;$)
 		}
 	}
 	return $failures;
+}
+
+sub sanitize_testname($)
+{
+	my $name = shift;
+	$name =~ s/[^a-zA-Z0-9._+-]//g;
+	return undef unless $name =~ /^[a-zA-Z]/;
+	return $name;
 }
 
 1;
