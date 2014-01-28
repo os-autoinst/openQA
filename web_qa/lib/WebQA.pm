34165430b9e9af5a5740210cd83ec99efa240863
Ancor Gonzalez Sosa <ancor@suse.de>
2014-01-28 18:12:03 +0100

    More routes

--- web_qa/lib/WebQA.pm
+++ web_qa/lib/WebQA.pm
@@ -12,6 +12,15 @@ sub startup {
   my $r = $self->routes;
 
   $r->get('/tests')->name('tests')->to('test#list');
+  my $test_r = $r->get('/tests/#testid');
+  $test_r->get('/')->name('test')->to('test#show');
+
+  my $asset_r = $test_r->get('/modules/:moduleid/assets/:assetid');
+  $asset_r->get('/view')->to(action => 'view');
+  $asset_r->get('/edit')->name('edit_asset')->to(action => 'edit');
+  $asset_r->get('/src')->name('src_asset')->to(action => 'src');
+  $asset_r->get('/')->name('asset')->to(controller => 'asset', action => 'view', assetid => 1);
+
   # Default route
   $r->get('/')->to('test#list');
 }
