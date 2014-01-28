7a28bb34b5e4741222347db84304c858533616ae
Ancor Gonzalez Sosa <ancor@suse.de>
2014-01-28 22:55:18 +0100

    All needed routes already defined (hopefully)

--- web_qa/lib/WebQA.pm
+++ web_qa/lib/WebQA.pm
@@ -14,12 +14,28 @@ sub startup {
   $r->get('/tests')->name('tests')->to('test#list');
   my $test_r = $r->get('/tests/#testid');
   $test_r->get('/')->name('test')->to('test#show');
+  $test_r->get('/currentstep')->name('currentstep')->to('test#currentstep');
+  $test_r->get('/modlist')->name('modlist')->to('test#modlist');
+  $test_r->get('/modstat')->name('modstat')->to('test#modstat');
+  $test_r->get('/status')->name('status')->to('test#status');
+  $test_r->get('/livelog')->name('livelog')->to('test#livelog');
+  $test_r->get('/streaming')->name('streaming')->to('streaming#show');
+  $test_r->post('/cancel')->name('cancel')->to('schedule#cancel');
+  $test_r->post('/restart')->name('restart')->to('schedule#restart');
+  $test_r->post('/setpriority/:priority')->name('setpriority')->to('schedule#setpriority');
+  $test_r->post('/uploadlog/#filename')->name('uploadlog')->to('test#uploadlog');
+  $test_r->get('/images/#filename')->name('image')->to('image#show');
+  $test_r->get('/diskimages/:imageid')->name('diskimage')->to('diskimage#show');
 
   my $asset_r = $test_r->get('/modules/:moduleid/assets/:assetid');
   $asset_r->get('/view')->to(action => 'view');
   $asset_r->get('/edit')->name('edit_asset')->to(action => 'edit');
   $asset_r->get('/src')->name('src_asset')->to(action => 'src');
   $asset_r->get('/')->name('asset')->to(controller => 'asset', action => 'view', assetid => 1);
+
+  $r->get('/builds/:buildid')->name('build')->to('build#show');
+  $r->post('/rpc')->name('rpc')->to('rpc#call');
+  $r->post('/jsonrpc/:method')->name('jsonrpc')->to('jsonrpc#call');
 
   # Default route
   $r->get('/')->to('test#list');
