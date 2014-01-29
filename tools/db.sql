ef0cb988d9658e454af14f2e3b000b8589eb4156
Alberto Planas <aplanas@suse.com>
2014-01-29 16:09:26 +0100

    Add id column in job_settings. Needed for the ORM.

--- tools/db.sql
+++ tools/db.sql
@@ -7,33 +7,34 @@ INSERT INTO "job_state" VALUES(3,'stopped');
 INSERT INTO "job_state" VALUES(4,'waiting');
 INSERT INTO "job_state" VALUES(5,'done');
 CREATE TABLE worker(
-	id INTEGER PRIMARY KEY,
-	host TEXT,
-	instance INTEGER,
-	backend TEXT,
-	seen TIMESTAMP,
-	UNIQUE(host, instance)
+       id INTEGER PRIMARY KEY,
+       host TEXT,
+       instance INTEGER,
+       backend TEXT,
+       seen TIMESTAMP,
+       UNIQUE(host, instance)
 );
 INSERT INTO worker (id) VALUES(0);
 CREATE TABLE jobs (
-	id INTEGER PRIMARY KEY,
-	name TEXT,
-	state INTEGER DEFAULT 1 REFERENCES job_state(id),
-	priority INTEGER DEFAULT 50, -- 0-99
-	result TEXT,
-	worker INTEGER DEFAULT 0 REFERENCES worker(id) ON DELETE SET DEFAULT,
-	start_date TIMESTAMP,
-	finish_date TIMESTAMP,
-	UNIQUE(name)
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       state INTEGER DEFAULT 1 REFERENCES job_state(id),
+       priority INTEGER DEFAULT 50, -- 0-99
+       result TEXT,
+       worker INTEGER DEFAULT 0 REFERENCES worker(id) ON DELETE SET DEFAULT,
+       start_date TIMESTAMP,
+       finish_date TIMESTAMP,
+       UNIQUE(name)
 );
 CREATE TABLE job_settings(
-	jobid INTEGER REFERENCES jobs(id),
-	key TEXT,
-	value TEXT
+       id INTEGER PRIMARY KEY,
+       jobid INTEGER REFERENCES jobs(id),
+       key TEXT,
+       value TEXT
 );
 CREATE TABLE commands(
-	id INTEGER PRIMARY KEY,
-	worker INTEGER REFERENCES worker(id),
-	command TEXT
+       id INTEGER PRIMARY KEY,
+       worker INTEGER REFERENCES worker(id),
+       command TEXT
 );
 COMMIT;
