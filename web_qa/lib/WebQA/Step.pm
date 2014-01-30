7d10844bbd6d67b5d71039d64a6af037ea7ddf2a
Ancor Gonzalez Sosa <ancor@suse.de>
2014-01-30 17:37:06 +0100

    WIP: needle editor (still cannot save)

--- web_qa/lib/WebQA/Step.pm
+++ web_qa/lib/WebQA/Step.pm
@@ -61,10 +61,124 @@ sub view {
   }
 }
 
+# Needle editor
 sub edit {
   my $self = shift;
+  return 0 unless $self->init();
 
-  # Needle editor
+  my $testindex = $self->stash('stepid');
+  my $module_detail = $self->stash('module')->{'details'}->[$testindex-1];
+  my $imgname = $module_detail->{'screenshot'};
+  my $results = $self->stash('results');
+  my $testname = $self->param('testid');
+  my $info_msg;
+  my $error_msg;
+
+  # Each object in $needles will contain the name, both the url and the local path
+  # of the image and 2 lists of areas: 'area' and 'matches'.
+  # The former refers to the original definitions and the later shows the position
+  # found (best try) in the actual screenshot.
+  # The first element of the needles array is the screenshot itself, with an empty
+  # 'areas' (there is no needle associated to the screenshot) and with all matching
+  # areas in 'matches'.
+  my $needles = [];
+  # All tags (from all needles)
+  my $tags = [];
+  $tags = $module_detail->{'tags'} if ($module_detail->{'tags'});
+  if ($module_detail->{'needle'}) {
+
+    # First position: the screenshot with all the matching areas (in result)
+    push(@$needles, {'name' => 'screenshot',
+        'imageurl' => $self->url_for('test_img', filename => $module_detail->{'screenshot'}),
+        'imagepath' => "$basedir/$prj/testresults/$testname/$imgname",
+        'area' => [], 'matches' => [], 'tags' => []});
+    for my $tag (@$tags) {
+      push(@{$needles->[0]->{'tags'}}, $tag);
+    }
+    for my $area (@{$module_detail->{'area'}}) {
+      push(@{$needles->[0]->{'matches'}},
+        {'xpos' => int $area->{'x'}, 'width' => int $area->{'w'},
+          'ypos' => int $area->{'y'}, 'height' => int $area->{'h'},
+          'type' => 'match'});
+    }
+    # Second position: the only needle (with the same matches)
+    my $needle = needle_info($module_detail->{'needle'}, $results->{'distribution'});
+    push(@$needles, {'name' => $module_detail->{'needle'},
+        'imageurl' => $self->url_for('needle_file', distri => $results->{'distribution'}, name => $module_detail->{'needle'}.'.png'),
+        'imagepath' => $needle->{'image'}, 'area' => $needle->{'area'},
+        'tags' => $needle->{'tags'}, 'matches' => $needles->[0]->{'matches'}});
+    for my $t (@{$needle->{'tags'}}) {
+      push(@$tags, $t) unless grep(/^$t$/, @$tags);
+    }
+
+
+  } elsif ($module_detail->{'needles'}) {
+
+    # First position: the screenshot
+    push(@$needles, {'name' => 'screenshot',
+        'imagepath' => "$basedir/$prj/testresults/$testname/$imgname",
+        'imageurl' => $self->url_for('test_img', filename => $module_detail->{'screenshot'}),
+        'area' => [], 'matches' => [], 'tags' => []});
+    for my $tag (@$tags) {
+      push(@{$needles->[0]->{'tags'}}, $tag);
+    }
+    # Afterwards, all the candidate needles
+    my $needleinfo;
+    my $needlename;
+    my $area;
+    # For each candidate we will use theee variables:
+    # $needle: needle information from result, in which 'areas' refers to the best matches
+    # $needlename: read from the above
+    # $needleinfo: actual definition of the needle, with the original areas
+    # We also use $area for transforming the match information intro a real area
+    for my $needle (@{$module_detail->{'needles'}}) {
+      $needlename = $needle->{'name'};
+      $needleinfo  = needle_info($needlename, $results->{'distribution'});
+      push(@$needles, {'name' => $needlename,
+          'imageurl' => $self->url_for('needle_file', distri => $results->{'distribution'}, name => "$needlename.png"),
+          'imagepath' => $needleinfo->{'image'},
+          'tags' => $needleinfo->{'tags'},
+          'area' => $needleinfo->{'area'}, 'matches' => []});
+      for my $match (@{$needle->{'area'}}) {
+        $area = {'xpos' => int $match->{'x'}, 'width' => int $match->{'w'},
+          'ypos' => int $match->{'y'}, 'height' => int $match->{'h'},
+          'type' => 'match'};
+        push(@{$needles->[0]->{'matches'}}, $area);
+        push(@{$needles->[scalar(@$needles)-1]->{'matches'}}, $area);
+      }
+      for my $t (@{$needleinfo->{'tags'}}) {
+        push(@$tags, $t) unless grep(/^$t$/, @$tags);
+      }
+    }
+  } else {
+    # Failing with not a single candidate needle
+    push(@$needles, {'name' => 'screenshot',
+        'imageurl' => $self->url_for('test_img', filename => $module_detail->{'screenshot'}),
+        'imagepath' => "$basedir/$prj/testresults/$testname/$imgname",
+        'area' => [], 'matches' => [], 'tags' => $tags});
+  }
+
+  # Default values
+  #  - area: matches from best candidate
+  #  - tags: tags from the screenshot
+  my $default_needle = {};
+  my $default_name;
+  $default_needle->{'tags'} = $needles->[0]->{'tags'};
+  if (scalar(@$needles) > 1) {
+    $default_needle->{'area'} = $needles->[1]->{'matches'};
+    $default_name = $needles->[1]->{'name'};
+  } else {
+    $default_needle->{'area'} = [];
+    $default_name = $self->param('moduleid');
+  }
+  $default_name = $default_name."-".time;
+
+  $self->stash('error_msg', $error_msg);
+  $self->stash('info_msg', $info_msg);
+  $self->stash('needles', $needles);
+  $self->stash('tags', $tags);
+  $self->stash('default_needle', $default_needle);
+  $self->stash('needlename', $default_name);
 }
 
 sub src {
