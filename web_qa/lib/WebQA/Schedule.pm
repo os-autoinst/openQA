6379f3568100c0405b943bd0bd7aa360b95653ae
Ludwig Nussel <ludwig.nussel@suse.de>
2014-02-04 16:35:40 +0100

    fix scheduling links in buildview

--- web_qa/lib/WebQA/Schedule.pm
+++ web_qa/lib/WebQA/Schedule.pm
@@ -2,55 +2,67 @@ package WebQA::Schedule;
 use Mojo::Base 'Mojolicious::Controller';
 use openqa;
 
-sub cancel
+sub _stash_back
 {
     my $self = shift;
     my $back = $self->param('back');
+
+    if ($back eq 'results') {
+        $back = $self->url_for('tests');
+    } elsif ($back eq 'details') {
+        $back = $self->url_for('test', 'testid' => $self->param('testid'));
+    } else {
+        $back = $self->req->headers->referrer;
+    }
+    $self->stash('back', $back);
+}
+
+sub cancel
+{
+    my $self = shift;
     my $name = $self->param('testid');
 
-    $self->stash('back', $back);
-
     if(!is_authorized_rw()) {
-	#return $self->render(text => "forbidden", status => 403);
-	$self->stash('denied', 1);
+        #return $self->render(text => "forbidden", status => 403);
+        $self->stash('denied', 1);
     } else {
         Scheduler::job_stop($name);
-	$self->stash('denied', 0);
+        $self->stash('denied', 0);
     }
+
+    _stash_back($self);
 }
 
 sub restart
 {
     my $self = shift;
-    my $back = $self->param('back');
     my $name = $self->param('testid');
 
-    $self->stash('back', $back);
-
     if(!is_authorized_rw()) {
-	$self->stash('denied', 1);
+        $self->stash('denied', 1);
     } else {
         Scheduler::job_restart($name);
-	$self->stash('denied', 0);
+        $self->stash('denied', 0);
     }
+
+    _stash_back($self);
 }
 
 sub setpriority
 {
     my $self = shift;
-    my $back = $self->param('back');
     my $name = $self->param('testid');
     my $priority = $self->param('priority');
 
-    $self->stash('back', $back);
-
     if(!is_authorized_rw()) {
-	$self->stash('denied', 1);
+        $self->stash('denied', 1);
     } else {
         my $job = Scheduler::job_get($name);
         Scheduler::job_set_prio( prio=>$priority, jobid=>$job->{id} );
-	$self->stash('denied', 0);
+        $self->stash('denied', 0);
     }
+
+    _stash_back($self);
 }
 
 1;
