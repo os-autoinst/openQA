064f7b151459dd48c8ca19c4306421e4cfdb697a
Ludwig Nussel <ludwig.nussel@suse.de>
2014-02-07 08:39:57 +0100

    adjust setup script a bit

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+# script to prepare a development environment for openqa
 
-dst="$1"
-if [ -z "$dst" ]; then
-	echo "please specify where you want to run openqa from" >&2
-	exit 1
-fi
+dst=/var/lib/openqa
 
 run()
 {
@@ -13,29 +10,16 @@ run()
 	"$@"
 }
 
-echo "1. setting up directory structure ..."
-
 for i in perl testresults pool/1 factory/iso backlog; do
-	run mkdir -p "$dst/$i"
+	run sudo install -d -m 755 "$dst/$i"
 done
-run ln -s $PWD/tools $dst/tools
+run sudo ln -s $PWD/tools $dst/tools
+run sudo install -d -m 755 -o $USER /var/lib/openqa/db
+run tools/initdb
 
-cat <<EOF
-2. please run:
+run git clone git://gitorious.org/os-autoinst/os-autoinst.git $dst/perl/autoinst
 
-sudo tools/initdb
+run $dst/perl/autoinst/tools/fetchneedles
 
-sudo mkdir /usr/share/openqa
-sudo ln -s $PWD/www/* /usr/share/openqa
-
-3. copy and adjust etc/apache2 to /etc/apache2
-
-4. setup os-autoinst
-
-git clone git://gitorious.org/os-autoinst/os-autoinst.git $dst/perl/autoinst
-
-git clone git@github.com:openSUSE-Team/os-autoinst-needles.git
-ln -s os-autoinst-needles/distri/opensuse $dst/perl/autoinst/distri/opensuse/needles
-sudo chown -R wwwrun os-autoinst-needles
-
-EOF
+echo "+++ done. now start the web ui:"
+echo "morbo script/openqa"
