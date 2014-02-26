52d568fd99c0c9b66351a7a1709924721c87db06
Ancor Gonzalez Sosa <ancor@suse.de>
2014-02-26 11:37:30 +0100

    Schema for API keys

---
+++ lib/OpenQA/modules/Schema/Result/ApiKeys.pm
@@ -0,0 +1,61 @@
+# Copyright (C) 2014 SUSE Linux Products GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package Schema::Result::ApiKeys;
+use base qw/DBIx::Class::Core/;
+
+use db_helpers;
+
+__PACKAGE__->table('api_keys');
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'integer',
+        is_auto_increment => 1,
+    },
+    key => {
+        data_type => 'text',
+    },
+    secret => {
+        data_type => 'text',
+    },
+    user_id => {
+        data_type => 'integer',
+        is_foreign_key => 1,
+    },
+    t_expiration => {
+        data_type => 'timestamp',
+        is_nullable => 1,
+    },
+    t_created => {
+        data_type => 'timestamp',
+        is_nullable => 1,
+    },
+    t_updated => {
+        data_type => 'timestamp',
+        is_nullable => 1,
+    },
+);
+__PACKAGE__->set_primary_key('id');
+__PACKAGE__->add_unique_constraint(constraint_name => [ qw/key/ ]);
+__PACKAGE__->belongs_to(user => 'Schema::Result::Users', 'user_id');
+
+sub sqlt_deploy_hook {
+    my ($self, $sqlt_table) = @_;
+
+    db_helpers::create_auto_timestamps($sqlt_table->schema, __PACKAGE__->table);
+}
+
+1;
