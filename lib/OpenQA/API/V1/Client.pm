f4812c5e65f86251db02e29434cb65250225f9ff
Ludwig Nussel <ludwig.nussel@suse.de>
2014-04-01 17:42:23 +0200

    Revert "Add emacs local variables for cperl mode"
    need to revert that for reverting the vim commit too
    
    This reverts commit 2773bdcafa3e76637c3f0f2e172b08cc0594744d.

--- lib/OpenQA/API/V1/Client.pm
+++ lib/OpenQA/API/V1/Client.pm
@@ -160,13 +160,4 @@ the api.
 L<Mojo::UserAgent>, L<Config::IniFiles>
 
 =cut
-# Local Variables:
-# mode: cperl
-# cperl-close-paren-offset: -4
-# cperl-continued-statement-offset: 4
-# cperl-indent-level: 4
-# cperl-indent-parens-as-block: t
-# cperl-tab-always-indent: t
-# indent-tabs-mode: nil
-# End:
 # vim: set ts=4 sw=4 sts=4 et:
--- script/upgrade3_4_example.pl
+++ script/upgrade3_4_example.pl
@@ -42,13 +42,4 @@ my $dh = DH->new(
 $dh->prepare_deploy;
 $dh->prepare_upgrade({ from_version => 3, to_version => 4});
 $dh->upgrade;
-# Local Variables:
-# mode: cperl
-# cperl-close-paren-offset: -4
-# cperl-continued-statement-offset: 4
-# cperl-indent-level: 4
-# cperl-indent-parens-as-block: t
-# cperl-tab-always-indent: t
-# indent-tabs-mode: nil
-# End:
 # vim: set ts=4 sw=4 sts=4 et:
