269b763ef1ad958e498add02b685ea1974f812e4
Antonio Larrosa <alarrosa@suse.com>
2014-04-09 15:42:08 +0200

    Moved the execute_sql_file and the initial fixture loading to the Schema class so that just a call to deploy_fixtures deploys all of them.

--- lib/OpenQA/modules/Schema/Schema.pm
+++ lib/OpenQA/modules/Schema/Schema.pm
@@ -16,10 +16,37 @@
 
 package Schema;
 use base qw/DBIx::Class::Schema/;
+use IO::Dir;
+use SQL::SplitStatement;
+use Fcntl ':mode';
 
 our $VERSION = '5';
 
 __PACKAGE__->load_namespaces;
+
+sub execute_sql_file {
+    my ( $class, $sqlfile ) = @_;
+    print "Executing SQL statements from: $sqlfile\n";
+
+    open my $fh, '<', $sqlfile or die "error opening $sqlfile: $!";
+    my $contents = do { local $/; <$fh> };
+
+    my $sql_splitter = SQL::SplitStatement->new;
+    my @statements = $sql_splitter->split($contents);
+
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$openqa::dbfile") or die $DBI::errstr;
+    $dbh->do("BEGIN TRANSACTION");
+    foreach (@statements) {
+        print "$_\n";
+
+        unless ($dbh->do($_)) {
+            print "ROLLBACK;\n";
+            $dbh->do("ROLLBACK");
+            die;
+        }
+    }
+    $dbh->do("COMMIT");
+}
 
 sub deploy_fixtures {
     my ( $class, $attrs ) = @_;
@@ -54,6 +81,17 @@ sub deploy_fixtures {
             $dbh->do("INSERT INTO workers (id, host, instance, backend) VALUES(0, 'NONE', 0, 'NONE');");
         }
     );
+
+    # Deploy fixtures specified statically in the fixtures directory
+    my $script_directory = "$FindBin::Bin/../dbicdh";
+    my %fixture_deploy_dir;
+    tie %fixture_deploy_dir, 'IO::Dir', "$script_directory/fixtures/deploy/$VERSION";
+
+    foreach (keys %fixture_deploy_dir) {
+        if ( S_ISREG($fixture_deploy_dir{$_}->mode) ) {
+            $class->execute_sql_file("$script_directory/fixtures/deploy/$VERSION/$_");
+        }
+    }
 
     return 0;
 }
