fd43e0b7b2ba110e207b4eaf08df6871e6721586
Antonio Larrosa <alarrosa@suse.com>
2014-04-11 12:46:40 +0200

    Removed these files that are no longer needed with the latests simplifications of the code

--- t/fixtures/sql/01-workers.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "workers" VALUES(1,'localhost',1,'qemu',datetime('now'),datetime('now'));
--- t/fixtures/sql/03-users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "users" VALUES(99901,'https://openid.camelot.uk/arthur',1,1,datetime('now'),datetime('now'));
-INSERT INTO "users" VALUES(99902,'https://openid.camelot.uk/lancelot',0,0,datetime('now'),datetime('now'));
-INSERT INTO "users" VALUES(99903,'https://openid.camelot.uk/percival',1,0,datetime('now'),datetime('now'));
