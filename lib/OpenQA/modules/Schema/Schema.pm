5fec97f8b1c5b3172c89b840d2074e84ec14ae32
Ludwig Nussel <ludwig.nussel@suse.de>
2014-04-29 17:56:57 +0200

    use deploymenthandler method for deploying seeds

--- lib/OpenQA/modules/Schema/Schema.pm
+++ lib/OpenQA/modules/Schema/Schema.pm
@@ -24,42 +24,5 @@ our $VERSION = '8';
 
 __PACKAGE__->load_namespaces;
 
-sub deploy_seeds {
-    my $class        = shift;
-
-    # insert pre-defined values to job_states
-    $class->storage->dbh_do(
-        sub {
-            my ($storage, $dbh, @values) = @_;
-            for my $i (0 .. $#values) {
-                $dbh->do(sprintf("INSERT INTO job_states VALUES(%s, '%s');", $i, $values[$i]));
-            }
-        },
-        (qw/scheduled running cancelled waiting done/)
-    );
-
-    # insert pre-defined values to job_results
-    $class->storage->dbh_do(
-        sub {
-            my ($storage, $dbh, @values) = @_;
-            for my $i (0 .. $#values) {
-                $dbh->do(sprintf("INSERT INTO job_results VALUES(%s, '%s');", $i, $values[$i]));
-            }
-        },
-        (qw/none passed failed incomplete/)
-    );
-
-    # prepare worker table
-    # XXX: get rid of worker zero at some point
-    $class->storage->dbh_do(
-        sub {
-            my ($storage, $dbh, @values) = @_;
-            $dbh->do("INSERT INTO workers (id, host, instance, backend) VALUES(0, 'NONE', 0, 'NONE');");
-        }
-    );
-
-    return 0;
-}
-
 1;
 # vim: set sw=4 et:
