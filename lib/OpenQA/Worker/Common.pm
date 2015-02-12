64cf1cf57d450d598f761225fdf003c4e4ccde37
Ondrej Holecek <oholecek@suse.com>
2015-02-12 14:27:54 +0100

    Refactor worker scripts and add locking support

---
+++ lib/OpenQA/Worker/Common.pm
@@ -0,0 +1,267 @@
+# Copyright (C) 2015 SUSE Linux Products GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package OpenQA::Worker::Common;
+use strict;
+use warnings;
+
+use Carp;
+use POSIX qw/uname/;
+
+use base qw/Exporter/;
+our @EXPORT = qw/$job $workerid $verbose $instance $worker_settings $pooldir $nocleanup $worker_caps $testresults
+  OPENQA_BASE OPENQA_SHARE RESULTS_DIR ISO_DIR HDD_DIR STATUS_UPDATES_SLOW STATUS_UPDATES_FAST
+  add_timer remove_timer change_timer
+  api_call verify_workerid/;
+
+# Exported variables
+our $job;
+our $workerid;
+our $verbose = 0;
+our $instance = 'manual';
+our $worker_settings;
+our $pooldir;
+our $nocleanup = 0;
+our $testresults;
+our $worker_caps;
+
+# package global variables
+my $url;
+my $ua;
+my $ws;
+my ($sysname, $hostname, $release, $version, $machine) = POSIX::uname();
+
+# global constants
+use constant OPENQA_BASE => '/var/lib/openqa';
+use constant OPENQA_SHARE => OPENQA_BASE . '/share';
+use constant ASSET_DIR => OPENQA_SHARE . '/factory';
+use constant {
+    ISO_DIR => ASSET_DIR . '/iso',
+    HDD_DIR => ASSET_DIR . '/hdd',
+    RESULTS_DIR => OPENQA_SHARE . '/testresults'
+};
+use constant {
+    STATUS_UPDATES_SLOW => 5,
+    STATUS_UPDATES_FAST => 0.5,
+};
+
+## Mojo timers ids
+my $timers = {
+    # check for commands from scheduler
+    'ws_keepalive' => undef,
+    # check for new job
+    'check_job'       => undef,
+    # update status of running job
+    'update_status'   => undef,
+    # check for crashed backend and its running status
+    'check_backend'   => undef,
+    # trigger stop_job if running for > $max_job_time
+    'job_timeout'     => undef
+};
+
+sub add_timer {
+    my ($timer, $timeout, $callback, $nonrecurring) = @_;
+    return unless ($timer && $timeout && $callback);
+    return if ($timers->{$timer});
+    print "## adding timer $timer\n" if $verbose;
+    my $timerid;
+    if ($nonrecurring) {
+        $timerid = Mojo::IOLoop->timer( $timeout => $callback);
+    }
+    else {
+        $timerid = Mojo::IOLoop->recurring( $timeout => $callback);
+    }
+    $timers->{$timer} = [$timerid, $callback];
+    return $timerid;
+}
+
+sub remove_timer {
+    my ($timer) = @_;
+    return unless ($timer && $timers->{$timer});
+    print "## removing timer $timer\n" if $verbose;
+    Mojo::IOLoop->remove($timers->{$timer}->[0]);
+    $timers->{$timer} = undef;
+}
+
+sub change_timer {
+    my ($timer, $newtimeout, $callback) = @_;
+    return unless ($timer && $timers->{$timer});
+    print "## changing timer $timer\n" if $verbose;
+    $callback = $timers->{$timer}->[1] unless $callback;
+    remove_timer($timer);
+    add_timer($timer, $newtimeout, $callback);
+}
+
+## prepare UA and URL for OpenQA-scheduler connection
+sub api_init {
+    my ($options) = @_;
+    my $host = $options->{'host'};
+
+    if ($host !~ '/') {
+        $url = Mojo::URL->new();
+        $url->host($host);
+        $url->scheme('http');
+    }
+    else {
+        $url = Mojo::URL->new($host);
+    }
+    # Relative paths are appended to the existing one
+    $url->path('/api/v1/');
+
+    my ($apikey, $apisecret) = ($options->{'apikey'}, $options->{'apisecret'});
+    $ua = OpenQA::Client->new(
+        api => $url->host,
+        apikey => $apikey,
+        apisecret => $apisecret
+    );
+
+    unless ($ua->apikey && $ua->apisecret) {
+        unless ($apikey && $apisecret) {
+            die "API key and secret are needed for the worker connecting " . $url->host . "\n";
+        }
+        $ua->apikey($apikey);
+        $ua->apisecret($apisecret);
+    }
+}
+
+# send a command to openQA API
+sub api_call {
+    my ($method, $path, $params, $json_data, $ignore_errors) = @_;
+    $method = lc $method;
+    my $ua_url = $url->clone;
+
+    $ua_url->path($path =~ s/^\///r);
+    $ua_url->query($params) if $params;
+
+    my $tries = 3;
+    while (1) {
+        print uc($method) . " $ua_url\n" if $verbose;
+        my $tx;
+        if ($json_data) {
+            $tx = $ua->$method($ua_url => json => $json_data);
+        }
+        else {
+            $tx = $ua->$method($ua_url);
+        }
+        if ($tx->success) {
+            return $tx->success->json;
+        }
+        elsif ($ignore_errors) {
+            return;
+        }
+        --$tries;
+        my ($err, $code) = $tx->error;
+        my $msg = $code ? "$tries: $code response: $err" : "$tries: Connection error: $err->{message}";
+        carp "$msg";
+        if (!$tries) {
+            # abort the current job, we're in trouble - but keep running to grab the next
+            stop_job('api-failure');
+            return;
+        }
+        sleep 5;
+    }
+}
+
+sub _get_capabilities {
+    my $caps = {};
+    my $query_cmd;
+
+    if ($worker_settings->{ARCH}) {
+        $caps->{cpu_arch} = $worker_settings->{ARCH};
+    }
+    else {
+        open(LSCPU, "LC_ALL=C lscpu|");
+        while (<LSCPU>) {
+            chomp;
+            if (m/Model name:\s+(.+)$/) {
+                $caps->{cpu_modelname} = $1;
+            }
+            if (m/Architecture:\s+(.+)$/) {
+                $caps->{cpu_arch} = $1;
+            }
+            if (m/CPU op-mode\(s\):\s+(.+)$/) {
+                $caps->{cpu_opmode} = $1;
+            }
+        }
+        close(LSCPU);
+    }
+    open(MEMINFO, "/proc/meminfo");
+    while (<MEMINFO>) {
+        chomp;
+        if (m/MemTotal:\s+(\d+).+kB/) {
+            my $mem_max = $1 ? $1 : '';
+            $caps->{mem_max} = int($mem_max/1024) if $mem_max;
+        }
+    }
+    close(MEMINFO);
+    return $caps;
+}
+
+sub verify_workerid {
+    if (!$workerid) {
+        $worker_caps = _get_capabilities;
+        $worker_caps->{host} = $hostname;
+        $worker_caps->{instance} = $instance;
+        $worker_caps->{backend} = $worker_settings->{'BACKEND'};
+
+        my $res = api_call('post','workers', $worker_caps);
+        return unless $res;
+        $ENV{'WORKERID'} = $workerid = $res->{id};
+        # recreate WebSocket connection, our id may have changed
+        if ($ws) {
+            $ws->finish;
+            $ws = undef;
+        }
+    }
+    if (!$ws) {
+        my $ua_url = $url->clone();
+        if ($url->scheme eq 'http') {
+            $ua_url->scheme('ws');
+        }
+        else {
+            $ua_url->scheme('wss');
+        }
+        $ua_url->path("workers/$workerid/ws");
+        print "WEBSOCKET $ua_url\n" if $verbose;
+        $ua->websocket(
+            $ua_url => sub {
+                my ($ua, $tx) = @_;
+                if ($tx->is_websocket) {
+                    $ws = $tx;
+                    $tx->on(message => \&OpenQA::Worker::Commands::websocket_commands);
+                    $tx->on(
+                        finish => sub {
+                            print "closing websocket connection\n" if $verbose;
+                            $ws = undef;
+                        }
+                    );
+                }
+                else {
+                    my $err = $tx->error;
+                    carp "Unable to upgrade connection to WebSocket: ".$err->{code}.". proxy_wstunnel enabled?" if defined $err;
+                    $ws = undef;
+                }
+            }
+        );
+    }
+    return $workerid;
+}
+
+sub ws_keepalive {
+    #send ok keepalive so WebSocket connection is not inactive
+    return unless $ws;
+    $ws->send('ok');
+}
+1;
---
+++ lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -0,0 +1,189 @@
+# Copyright (C) 2015 SUSE Linux Products GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package OpenQA::Worker::Engines::isotovideo;
+use strict;
+use warnings;
+
+use OpenQA::Worker::Common;
+use OpenQA::Utils qw//;
+
+use POSIX qw/:sys_wait_h strftime SIGTERM SIGKILL uname/;
+use JSON qw/to_json/;
+use Fcntl;
+
+my $isotovideo = "/usr/bin/isotovideo";
+my $workerpid;
+
+require Exporter;
+our (@ISA, @EXPORT);
+@ISA = qw(Exporter);
+@EXPORT = qw/engine_workit engine_check/;
+
+sub set_engine_exec {
+    my ($path) = @_;
+    $isotovideo = $path;
+}
+
+sub _kill($) {
+    my ($pid) = @_;
+    my $n = kill(SIGTERM, $pid);
+    for (my $i = 0; $n && $i < 5; ++$i) {
+        sleep 1;
+        $n = kill(SIGTERM, $pid);
+    }
+    if ($n) {
+        warn "pid $pid didn't die, sending KILL";
+        kill(SIGKILL, $pid);
+    }
+}
+
+sub _save_vars($) {
+    my $vars = shift;
+    die "cannot get environment variables!\n" unless $vars;
+    my $fn = $pooldir . "/vars.json";
+    unlink "$pooldir/vars.json" if -e "$pooldir/vars.json";
+    open( my $fd, ">", $fn ) or die "can not write vars.json: $!\n";
+    fcntl( $fd, F_SETLKW, pack( 'ssqql', F_WRLCK, 0, 0, 0, $$ ) ) or die "cannot lock vars.json: $!\n";
+    truncate( $fd, 0 ) or die "cannot truncate vars.json: $!\n";
+
+    print $fd to_json( \%$vars, { pretty => 1 } );
+    close($fd);
+}
+
+sub engine_workit($){
+    my $job = shift;
+
+    # XXX: this should come from the worker table. Only included
+    # here for convenience when looking at the pool of
+    # debugging.
+    for my $i (qw/QEMUPORT VNC OPENQA_HOSTNAME/) {
+        $job->{settings}->{$i} = $ENV{$i};
+    }
+    if (open(my $fh, '>', 'job.json')) {
+        print $fh to_json($job, { pretty => 1 });
+        close $fh;
+    }
+
+    # always set proper TAPDEV for os-autoinst when using tap network mode
+    # ensure MAC addresses differ, tap devices may be bridged together
+    # and allow MAC addresses for more than 256 workers (up to 65535)
+    if (($job->{'settings'}->{'NICTYPE'}//'') eq 'tap') {
+        $job->{'settings'}->{'TAPDEV'} = 'tap' . ($instance - 1);
+        $job->{'settings'}->{'NICMAC'} = sprintf("52:54:00:12:%02x:%02x", int($instance / 256), $instance % 256);
+    }
+
+    if (my $iso = $job->{'settings'}->{'ISO'}) {
+        $iso = join('/', ISO_DIR, $iso);
+        unless (-e $iso) {
+            warn "$iso does not exist!\n";
+            return;
+        }
+        $job->{'settings'}->{'ISO'} = $iso;
+    }
+
+    my $nd = $job->{'settings'}->{'NUMDISKS'} || 2;
+    for my $i (1..$nd) {
+        my $hdd = $job->{'settings'}->{"HDD_$i"} || undef;
+        if ($hdd) {
+            $hdd = join('/', HDD_DIR, $hdd);
+            unless (-e $hdd) {
+                warn "$hdd does not exist!\n";
+                return;
+            }
+            $job->{'settings'}->{"HDD_$i"} = $hdd;
+        }
+    }
+
+    my %vars = ();
+    while (my ($k, $v) = each %{$job->{'settings'}}) {
+        print "setting $k=$v\n" if $verbose;
+        $vars{$k} = $v;
+    }
+    $vars{CASEDIR} = OpenQA::Utils::testcasedir($vars{DISTRI}, $vars{VERSION});
+    _save_vars(\%vars);
+
+    # create tmpdir for qemu to write here
+    my $tmpdir = "$pooldir/tmp";
+    mkdir($tmpdir) unless (-d $tmpdir);
+
+    my $child = fork();
+    die "failed to fork: $!\n" unless defined $child;
+
+    unless ($child) {
+        $ENV{'TMPDIR'} = $tmpdir;
+        printf "$$: WORKING %d\n", $job->{'id'};
+        if (open(my $log, '>', "autoinst-log.txt")) {
+            print $log "+++ worker notes +++\n";
+            printf $log "start time: %s\n", strftime("%F %T", gmtime);
+            my ($sysname, $hostname, $release, $version, $machine) = POSIX::uname();
+            printf $log "running on $hostname:%d ($sysname $release $version $machine)\n", $instance;
+            close($log);
+        }
+        open STDOUT, ">>", "autoinst-log.txt";
+        open STDERR, ">&STDOUT";
+        exec "$isotovideo", '-d';
+        die "exec failed: $!\n";
+    }
+    else {
+        $workerpid = $child;
+        return { pid => $child };
+    }
+
+}
+
+sub engine_check {
+    # abort job if backend crashed and reschedule it
+    if(-e "$pooldir/backend.crashed") {
+        unlink("$pooldir/backend.crashed");
+        print STDERR "backend crashed ...\n";
+        if (open(my $fh, '<', "$pooldir/qemu.pid")) {
+            local $/;
+            my $pid = <$fh>;
+            close $fh;
+            if ($pid =~ /(\d+)/) {
+                print STDERR "killing qemu $1\n";
+                _kill($1);
+            }
+        }
+        if (open(my $fh, '<', "$pooldir/os-autoinst.pid")) {
+            local $/;
+            my $pid = <$fh>;
+            close $fh;
+            if ($pid =~ /(\d+)/) {
+                print STDERR "killing os-autoinst $1\n";
+                _kill($1);
+            }
+        }
+        return 'crashed';
+    }
+
+    # check if the worker is still running
+    my $pid = waitpid($workerpid, WNOHANG);
+    if ($verbose) {
+        printf "waitpid %d returned %d\n", $workerpid, $pid;
+    }
+
+    if ($pid == $workerpid) {
+        if ($?) {
+            warn "child $pid died with exit status $?\n";
+            return 'died';
+        }
+        else {
+            return 'done';
+        }
+    }
+    return;
+}
