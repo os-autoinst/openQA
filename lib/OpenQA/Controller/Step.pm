os-autoinst/openQA

commit f0226cecb9e2a519be32bfcfbca81ddcf3f0a368

Import more of results.json into the database
I had to squash several commits into one to avoid a huge merge mess (as
the whole branch took too much time):

- parse flags into JobModules
- remove unused test_branch from Jobs
- add backend_infos to Jobs
- various fixes for /tests route
- transfer results.json in status call to get info about running job

lib/OpenQA/Controller/Step.pm
@@ -23,37 +23,29 @@ use OpenQA::Scheduler;
 use POSIX qw/strftime/;
 use Try::Tiny;
 use JSON;
+use Data::Dumper;
 
 sub init {
     my $self = shift;
 
     my $testindex = $self->param('stepid');
 
-
     my $job = OpenQA::Scheduler::job_get($self->param('testid'));
     $self->stash('testname', $job->{'name'});
     my $testdirname = $job->{'settings'}->{'NAME'};
-    my $results = test_result($testdirname);
+    my $testresultdir = OpenQA::Utils::testresultdir($testdirname);
 
-    unless ($results) {
-        $self->reply->not_found;
-        return 0;
-    }
-    $self->stash('results', $results);
+    my $module = OpenQA::Schema::Result::JobModules::job_module($job, $self->param('moduleid'));
+    my $details = $module->details($testresultdir);
+    $self->stash('job', $job);
+    $self->stash('module',  $module);
+    $self->stash('imglist', $details);
 
-    my $module = test_result_module($results->{'testmodules'}, $self->param('moduleid'));
-    unless ($module) {
-        $self->reply->not_found;
-        return 0;
-    }
-    $self->stash('module', $module);
-    $self->stash('imglist', $module->{'details'});
-
-    my $modinfo = get_running_modinfo($results);
+    my $modinfo = OpenQA::Schema::Result::JobModules::running_modinfo($job);
     $self->stash('modinfo', $modinfo);
 
     my $tabmode = 'screenshot'; # Default
-    if ($testindex > @{$module->{'details'}}) {
+    if ($testindex > @$details) {
         # This means that the module have no details at all
         if ($testindex == 1) {
             if ($self->stash('action') eq 'src') {
@@ -71,7 +63,7 @@ sub init {
         }
     }
     else {
-        my $module_detail = $module->{'details'}->[$testindex-1];
+        my $module_detail = $details->[$testindex-1];
         $tabmode = 'audio' if ($module_detail->{'audio'});
         $self->stash('module_detail', $module_detail);
     }
@@ -82,10 +74,7 @@ sub init {
 
 # Helper function to generate the needle url, with an optional version
 sub needle_url {
-    my $self = shift;
-    my $distri = shift;
-    my $name = shift;
-    my $version = shift;
+    my ($self, $distri, $name, $version) = @_;
 
     if (defined($version) && $version) {
         $self->url_for('needle_file', distri => $distri, name => $name)->query(version => $version);
@@ -97,7 +86,7 @@ sub needle_url {
 
 # Call to viewimg or viewaudio
 sub view {
-    my $self = shift;
+    my ($self) = @_;
     return 0 unless $self->init();
 
     if ('audio' eq $self->stash('tabmode')) {
@@ -110,8 +99,7 @@ sub view {
 
 # Needle editor
 sub edit {
-    my $self = shift;
-    my ($ow_overwrite, $ow_json, $ow_imagename, $ow_imagedistri, $ow_imageversion, $ow_needlename) = @_;
+    my ($self, $ow_overwrite, $ow_json, $ow_imagename,$ow_imagedistri, $ow_imageversion, $ow_needlename) = @_;
     return 0 unless $self->init();
 
     my $module_detail = $self->stash('module_detail');
@@ -335,14 +323,15 @@ sub edit {
 }
 
 sub src {
-    my $self = shift;
+    my ($self) = @_;
     return 0 unless $self->init();
 
-    my $results = $self->stash('results');
+    my $job = $self->stash('job');
     my $module = $self->stash('module');
 
-    my $testcasedir = testcasedir($results->{distribution}, $results->{version});
-    my $scriptpath = "$testcasedir/$module->{'script'}";
+    my $testcasedir = testcasedir($job->{settings}->{DISTRI}, $job->{settings}->{VERSION});
+    my $scriptpath = "$testcasedir/" . $module->script;
+    print "S $scriptpath\n";
     if(!$scriptpath || !-e $scriptpath) {
         $scriptpath||="";
         return $self->reply->not_found;
@@ -379,8 +368,7 @@ sub _commit_git {
 
 # Adds a timestamp to a needle name or replace the already present timestamp
 sub _timestamp {
-    my $self = shift;
-    my $name = shift;
+    my ($self, $name) = @_;
     my $today = strftime("%Y%m%d", gmtime(time));
 
     if ( $name =~ /(.*)-\d{8}$/ ) {
@@ -392,7 +380,7 @@ sub _timestamp {
 }
 
 sub save_needle {
-    my $self = shift;
+    my ($self) = @_;
     return 0 unless $self->init();
 
     my $validation = $self->validation;
@@ -423,7 +411,7 @@ sub save_needle {
     my $imageversion = $validation->param('imageversion');
     my $needlename = $validation->param('needlename');
     my $overwrite = $validation->param('overwrite');
-    my $needledir = needledir($results->{distribution}, $results->{version});
+    my $needledir = needledir($job->{'settings'}->{DISTRI}, $job->{'settings'}->{VERSION});
     my $success = 1;
 
     my $imagepath;
@@ -525,30 +513,36 @@ sub calc_min_similarity($$) {
 sub viewimg {
     my $self = shift;
     my $module_detail = $self->stash('module_detail');
-    my $results = $self->stash('results');
+    my $job = $self->stash('job');
+    my $distribution = $job->{settings}->{DISTRI};
+    my $dversion = $job->{settings}->{VERSION} || '';
+
+    print Dumper($module_detail);
 
     my @needles;
     if ($module_detail->{'needle'}) {
-        my $needle = needle_info($module_detail->{'needle'}, $results->{'distribution'}, $results->{'version'}||'');
-        my $info = {
-            'name' => $module_detail->{'needle'},
-            'image' => $self->needle_url($results->{'distribution'}, $module_detail->{'needle'}.'.png', $results->{'version'}),
-            'areas' => $needle->{'area'},
-            'matches' => []
-        };
-        calc_matches($info, $module_detail->{'area'});
-        push(@needles, $info);
+        my $needle = needle_info($module_detail->{'needle'}, $distribution, $dversion);
+        if ($needle) { # possibly missing/broken file
+            my $info = {
+                'name' => $module_detail->{'needle'},
+                'image' => $self->needle_url($distribution, $module_detail->{'needle'}.'.png', $dversion),
+                'areas' => $needle->{'area'},
+                'matches' => []
+            };
+            calc_matches($info, $module_detail->{'area'});
+            push(@needles, $info);
+        }
     }
     elsif ($module_detail->{'needles'}) {
         my $needlename;
         my $needleinfo;
         for my $needle (@{$module_detail->{'needles'}}) {
             $needlename = $needle->{'name'};
-            $needleinfo  = needle_info($needlename, $results->{'distribution'}, $results->{'version'}||'');
+            $needleinfo  = needle_info($needlename, $distribution, $dversion);
             next unless $needleinfo;
             my $info = {
                 'name' => $needlename,
-                'image' => $self->needle_url($results->{'distribution'}, "$needlename.png", $results->{'version'}),
+                'image' => $self->needle_url($distribution, "$needlename.png", $dversion),
                 'areas' => $needleinfo->{'area'},
                 'matches' => []
             };

dbicdh/MySQL/deploy/24/001-auto-__VERSION.sql
@@ -1,0 +1,18 @@
+-- 
+-- Created by SQL::Translator::Producer::MySQL
+-- Created on Thu Feb  5 11:28:30 2015
+-- 
+;
+SET foreign_key_checks=0;
+--
+-- Table: `dbix_class_deploymenthandler_versions`
+--
+CREATE TABLE `dbix_class_deploymenthandler_versions` (
+  `id` integer NOT NULL auto_increment,
+  `version` varchar(50) NOT NULL,
+  `ddl` text NULL,
+  `upgrade_sql` text NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `dbix_class_deploymenthandler_versions_version` (`version`)
+);
+SET foreign_key_checks=1;

dbicdh/MySQL/deploy/24/001-auto.sql
@@ -1,0 +1,285 @@
+-- 
+-- Created by SQL::Translator::Producer::MySQL
+-- Created on Thu Feb  5 11:28:29 2015
+-- 
+;
+SET foreign_key_checks=0;
+--
+-- Table: `assets`
+--
+CREATE TABLE `assets` (
+  `id` integer NOT NULL auto_increment,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `assets_type_name` (`type`, `name`)
+) ENGINE=InnoDB;
+--
+-- Table: `job_modules`
+--
+CREATE TABLE `job_modules` (
+  `id` integer NOT NULL auto_increment,
+  `job_id` integer NOT NULL,
+  `name` text NOT NULL,
+  `script` text NOT NULL,
+  `category` text NOT NULL,
+  `soft_failure` integer NOT NULL DEFAULT 0,
+  `milestone` integer NOT NULL DEFAULT 0,
+  `important` integer NOT NULL DEFAULT 0,
+  `fatal` integer NOT NULL DEFAULT 0,
+  `result` varchar(255) NOT NULL DEFAULT 'none',
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `job_modules_idx_job_id` (`job_id`),
+  INDEX `idx_job_modules_result` (`result`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `job_modules_fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `job_settings`
+--
+CREATE TABLE `job_settings` (
+  `id` integer NOT NULL auto_increment,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  `job_id` integer NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `job_settings_idx_job_id` (`job_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `job_settings_fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `machine_settings`
+--
+CREATE TABLE `machine_settings` (
+  `id` integer NOT NULL auto_increment,
+  `machine_id` integer NOT NULL,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `machine_settings_idx_machine_id` (`machine_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE `machine_settings_machine_id_key` (`machine_id`, `key`),
+  CONSTRAINT `machine_settings_fk_machine_id` FOREIGN KEY (`machine_id`) REFERENCES `machines` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `machines`
+--
+CREATE TABLE `machines` (
+  `id` integer NOT NULL auto_increment,
+  `name` text NOT NULL,
+  `backend` text NOT NULL,
+  `variables` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `machines_name` (`name`)
+) ENGINE=InnoDB;
+--
+-- Table: `product_settings`
+--
+CREATE TABLE `product_settings` (
+  `id` integer NOT NULL auto_increment,
+  `product_id` integer NOT NULL,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `product_settings_idx_product_id` (`product_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE `product_settings_product_id_key` (`product_id`, `key`),
+  CONSTRAINT `product_settings_fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `products`
+--
+CREATE TABLE `products` (
+  `id` integer NOT NULL auto_increment,
+  `name` text NOT NULL,
+  `distri` text NOT NULL,
+  `version` text NOT NULL DEFAULT '',
+  `arch` text NOT NULL,
+  `flavor` text NOT NULL,
+  `variables` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `products_distri_version_arch_flavor` (`distri`, `version`, `arch`, `flavor`)
+) ENGINE=InnoDB;
+--
+-- Table: `secrets`
+--
+CREATE TABLE `secrets` (
+  `id` integer NOT NULL auto_increment,
+  `secret` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `secrets_secret` (`secret`)
+);
+--
+-- Table: `test_suite_settings`
+--
+CREATE TABLE `test_suite_settings` (
+  `id` integer NOT NULL auto_increment,
+  `test_suite_id` integer NOT NULL,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `test_suite_settings_idx_test_suite_id` (`test_suite_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE `test_suite_settings_test_suite_id_key` (`test_suite_id`, `key`),
+  CONSTRAINT `test_suite_settings_fk_test_suite_id` FOREIGN KEY (`test_suite_id`) REFERENCES `test_suites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `test_suites`
+--
+CREATE TABLE `test_suites` (
+  `id` integer NOT NULL auto_increment,
+  `name` text NOT NULL,
+  `variables` text NOT NULL,
+  `prio` integer NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `test_suites_name` (`name`)
+) ENGINE=InnoDB;
+--
+-- Table: `users`
+--
+CREATE TABLE `users` (
+  `id` integer NOT NULL auto_increment,
+  `username` text NOT NULL,
+  `email` text NULL,
+  `fullname` text NULL,
+  `nickname` text NULL,
+  `is_operator` integer NOT NULL DEFAULT 0,
+  `is_admin` integer NOT NULL DEFAULT 0,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `users_username` (`username`)
+) ENGINE=InnoDB;
+--
+-- Table: `worker_properties`
+--
+CREATE TABLE `worker_properties` (
+  `id` integer NOT NULL auto_increment,
+  `key` text NOT NULL,
+  `value` text NOT NULL,
+  `worker_id` integer NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `worker_properties_idx_worker_id` (`worker_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `worker_properties_fk_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `workers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `workers`
+--
+CREATE TABLE `workers` (
+  `id` integer NOT NULL auto_increment,
+  `host` text NOT NULL,
+  `instance` integer NOT NULL,
+  `backend` text NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `workers_host_instance` (`host`, `instance`)
+) ENGINE=InnoDB;
+--
+-- Table: `api_keys`
+--
+CREATE TABLE `api_keys` (
+  `id` integer NOT NULL auto_increment,
+  `key` text NOT NULL,
+  `secret` text NOT NULL,
+  `user_id` integer NOT NULL,
+  `t_expiration` timestamp NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `api_keys_idx_user_id` (`user_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE `api_keys_key` (`key`),
+  CONSTRAINT `api_keys_fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `jobs`
+--
+CREATE TABLE `jobs` (
+  `id` integer NOT NULL auto_increment,
+  `slug` text NULL,
+  `state` varchar(255) NOT NULL DEFAULT 'scheduled',
+  `priority` integer NOT NULL DEFAULT 50,
+  `result` varchar(255) NOT NULL DEFAULT 'none',
+  `worker_id` integer NOT NULL DEFAULT 0,
+  `test` text NOT NULL,
+  `clone_id` integer NULL,
+  `retry_avbl` integer NOT NULL DEFAULT 3,
+  `backend_info` text NULL,
+  `t_started` timestamp NULL,
+  `t_finished` timestamp NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `jobs_idx_clone_id` (`clone_id`),
+  INDEX `jobs_idx_worker_id` (`worker_id`),
+  INDEX `idx_jobs_state` (`state`),
+  INDEX `idx_jobs_result` (`result`),
+  PRIMARY KEY (`id`),
+  UNIQUE `jobs_slug` (`slug`),
+  CONSTRAINT `jobs_fk_clone_id` FOREIGN KEY (`clone_id`) REFERENCES `jobs` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `jobs_fk_worker_id` FOREIGN KEY (`worker_id`) REFERENCES `workers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `job_dependencies`
+--
+CREATE TABLE `job_dependencies` (
+  `child_job_id` integer NOT NULL,
+  `parent_job_id` integer NOT NULL,
+  `dependency` integer NOT NULL,
+  INDEX `job_dependencies_idx_child_job_id` (`child_job_id`),
+  INDEX `job_dependencies_idx_parent_job_id` (`parent_job_id`),
+  INDEX `idx_job_dependencies_dependency` (`dependency`),
+  PRIMARY KEY (`child_job_id`, `parent_job_id`, `dependency`),
+  CONSTRAINT `job_dependencies_fk_child_job_id` FOREIGN KEY (`child_job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `job_dependencies_fk_parent_job_id` FOREIGN KEY (`parent_job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `job_templates`
+--
+CREATE TABLE `job_templates` (
+  `id` integer NOT NULL auto_increment,
+  `product_id` integer NOT NULL,
+  `machine_id` integer NOT NULL,
+  `test_suite_id` integer NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `job_templates_idx_machine_id` (`machine_id`),
+  INDEX `job_templates_idx_product_id` (`product_id`),
+  INDEX `job_templates_idx_test_suite_id` (`test_suite_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE `job_templates_product_id_machine_id_test_suite_id` (`product_id`, `machine_id`, `test_suite_id`),
+  CONSTRAINT `job_templates_fk_machine_id` FOREIGN KEY (`machine_id`) REFERENCES `machines` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `job_templates_fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `job_templates_fk_test_suite_id` FOREIGN KEY (`test_suite_id`) REFERENCES `test_suites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+--
+-- Table: `jobs_assets`
+--
+CREATE TABLE `jobs_assets` (
+  `job_id` integer NOT NULL,
+  `asset_id` integer NOT NULL,
+  `t_created` timestamp NOT NULL,
+  `t_updated` timestamp NOT NULL,
+  INDEX `jobs_assets_idx_asset_id` (`asset_id`),
+  INDEX `jobs_assets_idx_job_id` (`job_id`),
+  UNIQUE `jobs_assets_job_id_asset_id` (`job_id`, `asset_id`),
+  CONSTRAINT `jobs_assets_fk_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `jobs_assets_fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+SET foreign_key_checks=1;

dbicdh/MySQL/upgrade/23-24/001-auto.sql
@@ -1,0 +1,19 @@
+-- Convert schema '/home/coolo/prod/openQA/script/../dbicdh/_source/deploy/23/001-auto.yml' to '/home/coolo/prod/openQA/script/../dbicdh/_source/deploy/24/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE job_modules ADD COLUMN soft_failure integer NOT NULL DEFAULT 0,
+                        ADD COLUMN milestone integer NOT NULL DEFAULT 0,
+                        ADD COLUMN important integer NOT NULL DEFAULT 0,
+                        ADD COLUMN fatal integer NOT NULL DEFAULT 0;
+
+;
+ALTER TABLE jobs DROP COLUMN test_branch,
+                 ADD COLUMN backend_info text NULL;
+
+;
+
+COMMIT;
+

dbicdh/PostgreSQL/deploy/24/001-auto-__VERSION.sql
@@ -1,0 +1,18 @@
+-- 
+-- Created by SQL::Translator::Producer::PostgreSQL
+-- Created on Thu Feb  5 11:28:30 2015
+-- 
+;
+--
+-- Table: dbix_class_deploymenthandler_versions.
+--
+CREATE TABLE "dbix_class_deploymenthandler_versions" (
+  "id" serial NOT NULL,
+  "version" character varying(50) NOT NULL,
+  "ddl" text,
+  "upgrade_sql" text,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "dbix_class_deploymenthandler_versions_version" UNIQUE ("version")
+);
+
+;

commit 0b39afe35d902db1d5a1ee0b86e67d417e0835b7

stash('results') no longer exists
this is apparently broken since f0226cecb9e2a

lib/OpenQA/Controller/Step.pm
@@ -103,9 +103,10 @@ sub edit {
 
     my $module_detail = $self->stash('module_detail');
     my $imgname = $module_detail->{'screenshot'};
-    my $results = $self->stash('results');
     my $job = OpenQA::Scheduler::job_get($self->param('testid'));
     my $testdirname = $job->{'settings'}->{'NAME'};
+    my $distribution = $job->{settings}->{DISTRI};
+    my $dversion = $job->{settings}->{VERSION} || '';
 
     # Each object in $needles will contain the name, both the url and the local path
     # of the image and 2 lists of areas: 'area' and 'matches'.
@@ -151,14 +152,14 @@ sub edit {
             );
         }
         # Second position: the only needle (with the same matches)
-        my $needle = needle_info($module_detail->{'needle'}, $results->{'distribution'}, $results->{'version'}||'');
+        my $needle = needle_info($module_detail->{'needle'}, $distribution, $dversion);
 
-        $self->app->log->error(sprintf("Could not find needle: %s for %s %s",$module_detail->{'needle'},$results->{'distribution'},$results->{'version'})) if !defined $needle;
+        $self->app->log->error(sprintf("Could not find needle: %s for %s %s",$module_detail->{'needle'},$distribution,$dversion)) if !defined $needle;
 
         my $matched = {
             'name' => $module_detail->{'needle'},
             'suggested_name' => $self->_timestamp($module_detail->{'needle'}),
-            'imageurl' => $self->needle_url($results->{'distribution'}, $module_detail->{'needle'}.'.png',$results->{'version'}),
+            'imageurl' => $self->needle_url($distribution, $module_detail->{'needle'}.'.png',$dversion),
             'imagename' => basename($needle->{'image'}),
             'imagedistri' => $needle->{'distri'},
             'imageversion' => $needle->{'version'},
@@ -201,10 +202,10 @@ sub edit {
         # We also use $area for transforming the match information intro a real area
         for my $needle (@{$module_detail->{'needles'}}) {
             $needlename = $needle->{'name'};
-            $needleinfo = needle_info($needlename, $results->{'distribution'}, $results->{'version'}||'');
+            $needleinfo = needle_info($needlename, $distribution, $dversion||'');
 
             if( !defined $needleinfo ) {
-                $self->app->log->error(sprintf("Could not parse needle: %s for %s %s",$needlename,$results->{'distribution'},$results->{'version'} || ''));
+                $self->app->log->error(sprintf("Could not parse needle: %s for %s %s",$needlename,$distribution,$dversion || ''));
 
                 $needleinfo->{'image'} = [];
                 $needleinfo->{'tags'} = [];
@@ -217,7 +218,7 @@ sub edit {
                 {
                     'name' => $needlename,
                     'suggested_name' => $self->_timestamp($needlename),
-                    'imageurl' => $self->needle_url($results->{'distribution'}, "$needlename.png", $results->{'version'}),
+                    'imageurl' => $self->needle_url($distribution, "$needlename.png", $dversion),
                     'imagename' => basename($needleinfo->{'image'}),
                     'imagedistri' => $needleinfo->{'distri'},
                     'imageversion' => $needleinfo->{'version'},
@@ -430,8 +431,9 @@ sub save_needle {
         return $self->edit;
     }
 
-    my $results = $self->stash('results');
     my $job = OpenQA::Scheduler::job_get($self->param('testid'));
+    my $distribution = $job->{settings}->{DISTRI};
+    my $dversion = $job->{settings}->{VERSION} || '';
     my $testdirname = $job->{'settings'}->{'NAME'};
     my $json = $validation->param('json');
     my $imagename = $validation->param('imagename');