c3bbd003e34a8d5bfe5afaf3eb523487ac9deb6d
Stephan Kulow <coolo@suse.de>
2015-03-06 15:28:36 +0100

    port the running route to jquery

--- public/javascripts/running.js
+++ public/javascripts/running.js
@@ -12,155 +12,135 @@ var testStatus = {
 
 // Update global variable testStatus
 function updateTestStatus(newStatus) {
-    window.testStatus.workerid = newStatus.workerid;
-    if (window.testStatus.interactive != newStatus.interactive
-        || window.testStatus.interactive_requested != newStatus.interactive_requested) {
-        window.testStatus.interactive = newStatus.interactive;
-        window.testStatus.interactive_requested = newStatus.interactive_requested;
-        window.updateInteractiveIndicator();
+    testStatus.workerid = newStatus.workerid;
+    if (testStatus.interactive != newStatus.interactive
+        || testStatus.interactive_requested != newStatus.interactive_requested) {
+        testStatus.interactive = newStatus.interactive;
+        testStatus.interactive_requested = newStatus.interactive_requested;
+        updateInteractiveIndicator();
     }
-    if (window.testStatus.needinput != newStatus.needinput
-        || window.testStatus.stop_waitforneedle_requested != newStatus.stop_waitforneedle_requested) {
-        window.testStatus.needinput = newStatus.needinput;
-        window.testStatus.stop_waitforneedle_requested = newStatus.stop_waitforneedle_requested;
-        window.updateNeedinputIndicator();
+    if (testStatus.needinput != newStatus.needinput
+        || testStatus.stop_waitforneedle_requested != newStatus.stop_waitforneedle_requested) {
+        testStatus.needinput = newStatus.needinput;
+        testStatus.stop_waitforneedle_requested = newStatus.stop_waitforneedle_requested;
+        updateNeedinputIndicator();
     }
     if (newStatus.state != 'running' && newStatus.state != 'waiting') {
           setTimeout(function() {location.reload();}, 2000);
           return;
     }
     // If a new module have been started, redraw module list
-    if (window.testStatus.modlist_initialized == 0 || window.testStatus.running != newStatus.running) {
-        window.testStatus.running = newStatus.running;
-        new Ajax.Request("/tests/" + window.testStatus.testname + "/modlist", {
-            method: "get",
-            dataType: 'json',
-            onSuccess: function(resp) {
-                var modlist = resp.responseJSON;
+    if (testStatus.modlist_initialized == 0 || testStatus.running != newStatus.running) {
+        testStatus.running = newStatus.running;
+        $.ajax("/tests/" + testStatus.testname + "/modlist").
+            done(function(modlist) {
                 if (modlist.length > 0) {
-                    window.prototype_updateModuleslist(modlist, window.testStatus.testname, window.testStatus.running);
-                    window.testStatus.modlist_initialized = 1;
+                    updateModuleslist(modlist, testStatus.testname, testStatus.running);
+                    testStatus.modlist_initialized = 1;
                 }
-            }
-        });
+            });
     }
-}
-
-// Helper functions to show/hide elements
-
-function hide(id) {
-    if ($(id)) {
-        $(id).hide();
-    }
-    return true;
-}
-
-function show(id) {
-    if ($(id)) {
-        $(id).show();
-    }
-    return true;
 }
 
 function updateInteractiveIndicator() {
     var indicator = $("interactive_indicator");
-    console.log("i r "+window.testStatus.interactive+" "+window.testStatus.interactive_requested);
-    if (window.testStatus.interactive == null) {
-        indicator.innerHTML = "Unknown";
-        window.hide("interactive_spinner");
-        window.hide("interactive0_button");
-        window.hide("interactive1_button");
+    //console.log("i r "+testStatus.interactive+" "+testStatus.interactive_requested);
+    if (testStatus.interactive == null) {
+        indicator.html("Unknown");
+        $("#interactive_spinner").hide();
+        $("#interactive0_button").hide();
+        $("#interactive1_button").hide();
     }
-    else if (window.testStatus.interactive == 1) {
-        indicator.innerHTML = "Yes";
-        if (window.testStatus.interactive_requested == 0) {
-            window.show("interactive_spinner");
-            window.hide("interactive0_button");
-            window.hide("interactive1_button");
+    else if (testStatus.interactive == 1) {
+        indicator.text("Yes");
+        if (testStatus.interactive_requested == 0) {
+            $("#interactive_spinner").show();
+            $("#interactive0_button").hide();
+            $("#interactive1_button").hide();
         }
         else {
-            window.hide("interactive_spinner");
-            window.hide("interactive0_button");
-            window.show("interactive1_button");
-            if (!window.testStatus.needinput && !window.testStatus.stop_waitforneedle_requested) {
- //               window.show("stop_button");
+            $("#interactive_spinner").hide();
+            $("#interactive0_button").hide();
+            $("#interactive1_button").show();
+            if (!testStatus.needinput && !testStatus.stop_waitforneedle_requested) {
+                //  $("#stop_button").show();
             }
         }
     }
     else {
-        indicator.innerHTML = "No";
-        if (window.testStatus.interactive_requested == 1) {
-            window.show("interactive_spinner");
-            window.hide("interactive0_button");
-            window.hide("interactive1_button");
+        indicator.text("No");
+        if (testStatus.interactive_requested == 1) {
+            $("#interactive_spinner").show();
+            $("#interactive0_button").hide();
+            $("#interactive1_button").hide();
         }
         else {
-            window.hide("interactive_spinner");
-            window.show("interactive0_button");
-            window.hide("interactive1_button");
-//            window.hide("stop_button");
+            $("#interactive_spinner").hide();
+            $("#interactive0_button").show();
+            $("#interactive1_button").hide();
+            // $("#stop_button").hide();
         }
     }
-    indicator.highlight();
+    //indicator.highlight();
     updateNeedinputIndicator();
 }
 
 function updateNeedinputIndicator() {
-    console.log("n r "+window.testStatus.needinput+" "+window.testStatus.stop_waitforneedle_requested);
+    console.log("n r "+testStatus.needinput+" "+testStatus.stop_waitforneedle_requested);
     var indicator = $("needinput_indicator");
-    if (window.testStatus.interactive != 1 || window.testStatus.needinput == null) {
-        indicator.innerHTML = "N/A";
-        window.hide("crop_button");
-        window.hide("continue_button");
-        window.hide("retry_button");
-        window.hide("stop_button");
-        if (window.testStatus.needinput == null) {
-            window.show("stop_waitforneedle_spinner");
+    if (testStatus.interactive != 1 || testStatus.needinput == null) {
+        indicator.text("N/A");
+        $("#crop_button").hide();
+        $("#continue_button").hide();
+        $("#retry_button").hide();
+        $("#stop_button").hide();
+        if (testStatus.needinput == null) {
+            $("#stop_waitforneedle_spinner").show();
         }
         else {
-            window.hide("stop_waitforneedle_spinner");
+            $("#stop_waitforneedle_spinner").hide();
         }
     }
-    else if (window.testStatus.needinput == 1) {
-        indicator.innerHTML = "Yes";
-        if (window.testStatus.stop_waitforneedle_requested == 0) {
-            window.show("stop_waitforneedle_spinner");
-            window.hide("crop_button");
-            window.hide("continue_button");
-            window.hide("retry_button");
-            window.hide("stop_button");
+    else if (testStatus.needinput == 1) {
+        indicator.text("Yes");
+        if (testStatus.stop_waitforneedle_requested == 0) {
+            $("#stop_waitforneedle_spinner").show();
+            $("#crop_button").hide();
+            $("#continue_button").hide();
+            $("#retry_button").hide();
+            $("#stop_button").hide();
         }
         else {
-            window.hide("stop_waitforneedle_spinner");
-            window.show("crop_button");
-            window.show("continue_button");
-            window.show("retry_button");
-            window.hide("stop_button");
+            $("#stop_waitforneedle_spinner").hide();
+            $("#crop_button").show();
+            $("#continue_button").show();
+            $("#retry_button").show();
+            $("#stop_button").hide();
         }
     }
     else {
-        indicator.innerHTML = "No";
-        if (window.testStatus.stop_waitforneedle_requested == 1) {
-            window.show("stop_waitforneedle_spinner");
-            window.hide("crop_button");
-            window.hide("continue_button");
-            window.hide("retry_button");
-            window.hide("stop_button");
+        indicator.text("No");
+        if (testStatus.stop_waitforneedle_requested == 1) {
+            $("#stop_waitforneedle_spinner").show();
+            $("#crop_button").hide();
+            $("#continue_button").hide();
+            $("#retry_button").hide();
+            $("#stop_button").hide();
         }
         else {
-            window.hide("stop_waitforneedle_spinner");
-            window.hide("crop_button");
-            window.hide("continue_button");
-            window.hide("retry_button");
-            if (window.testStatus.interactive && window.testStatus.interactive == window.testStatus.interactive_requested) {
-                window.show("stop_button");
+            $("#stop_waitforneedle_spinner").hide();
+            $("#crop_button").hide();
+            $("#continue_button").hide();
+            $("#retry_button").hide();
+            if (testStatus.interactive && testStatus.interactive == testStatus.interactive_requested) {
+                $("#stop_button").show();
             }
             else {
-                window.hide("stop_button");
+                $("#stop_button").hide();
             }
         }
     }
-    indicator.highlight();
+    //indicator.highlight();
 }
 
 function enableInteractive() {
@@ -168,65 +148,60 @@ function enableInteractive() {
 }
 
 function disableInteractive() {
-    window.hide("stop_button");
+    $("#stop_button").hide();
     sendCommand("disable_interactive_mode");
 }
 
 function sendCommand(command) {
-    var wid = window.testStatus.workerid;
+    var wid = testStatus.workerid;
     if (wid == null) return false;
-    new Ajax.Request("/api/v1/workers/" + wid + "/commands", {
-        method: "post",
-        parameters: { command: command },
-        onSuccess: function(resp) {
-            window.setTimeout("updateStatus()", 0);
-        }});
+    $.ajax({url: "/api/v1/workers/" + wid + "/commands",
+            type: 'POST',
+            data: { command: command },
+            success: function(resp) {
+                setTimeout("updateStatus()", 0);
+            }});
 }
 
 function updateStatus() {
-    new Ajax.Request("/tests/" + window.testStatus.testname + "/status", {
-        method: "get",
-        dataType: 'json',
-        onSuccess: function(resp) {
-            var status = resp.responseJSON;
-            window.updateTestStatus(status);
-            window.setTimeout("updateStatus()", 3000);
-        },
-        on404: function(resp) { // no status? probably not running anymore
-          window.setTimeout(function() {location.reload();}, 1000);
-        }
-    });
+    $.ajax("/tests/" + testStatus.testname + "/status").
+        done(function(status) {
+            updateTestStatus(status);
+            setTimeout("updateStatus()", 3000);
+        }).fail(function() {
+            setTimeout(function() {location.reload();}, 1000);
+        });
 }
 
-function init_status(tname) {
-    window.testStatus.testname = tname;
+function initStatus(tname) {
+    testStatus.testname = tname;
     updateStatus();
 }
 
 /********* LIVE LOG *********/
 
 // global vars for livelog
-var livelog, scrolldown;
+var scrolldown;
 
 // checkbox callback
-function set_scrolldown(newval) {
-    window.scrolldown = newval;
-    if (window.scrolldown) {
-        window.livelog.scrollTop = window.livelog.scrollHeight;
+function setScrolldown(newval) {
+    scrolldown = newval;
+    if (scrolldown) {
+        $('#livelog')[0].scrollTop = livelog.scrollHeight;
     }
 }
 
-function init_livelog(url) {
-    window.scrolldown = true;
-    window.livelog = document.getElementById('livelog');
-    document.getElementById('scrolldown').checked = true;
-
+function initLivelog() {
+    scrolldown = true;
+    $('#scrolldown').attr('checked', true);
+    
     // start stream
-    var events = new EventSource(url);
+    var events = new EventSource(livelog.data('url'));
     events.addEventListener('message', function(event) {
-        window.livelog.innerHTML += JSON.parse(event.data)[0];
-        if (window.scrolldown) {
-            window.livelog.scrollTop = window.livelog.scrollHeight;
+        var livelog = $('#livelog');
+        livelog[0].innerHTML += JSON.parse(event.data)[0];
+        if (scrolldown) {
+            livelog[0].scrollTop = livelog[0].scrollHeight;
         }
     }, false);
 }
@@ -237,72 +212,95 @@ function init_livelog(url) {
 /********* LIVE STREAM *********/
 
 // global vars for livestream
-var livestream, last_event, image_size_x, image_size_y;
+var last_event;
 
 // loads a data-url img into a canvas
 function load_canvas(canvas, dataURL) {
-    var context = canvas.getContext('2d');
+    var context = canvas[0].getContext('2d');
 
     // load image from data url
     var scrn = new Image();
     scrn.onload = function() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(this, 0, 0, width=canvas.width, height=canvas.height);
-        window.image_size_x.innerHTML = scrn.width;
-        window.image_size_y.innerHTML = scrn.height;
+        context.clearRect(0, 0, canvas.width(), canvas.height());
+        context.drawImage(this, 0, 0, width=canvas.width(), height=canvas.height());
+        $('#image_size_x').text(canvas.width());
+        $('#image_size_y').text(canvas.height());
     };
     scrn.src = dataURL;
 }
 
 // canvas size
-function resize_livestream(arg) {
-    window.livestream.width  = arg.split("x")[0];
-    window.livestream.height = arg.split("x")[1];
+function resizeLivestream(arg) {
+    var livestream = $('#livestream');
+    livestream.attr('width', arg.split("x")[0]);
+    livestream.attr('height', arg.split("x")[1]);
     if (last_event) {
-        load_canvas(window.livestream, window.last_event.data);
+        load_canvas(livestream, last_event.data);
     }
 }
 
 // select callback
-function set_resolution(arg) {
+function setResolution(arg) {
     if (arg == "auto") {
-        set_cookie("livestream_size", arg, -5);
-        if (document.getElementById('canholder').clientWidth >= 1024) {
-            resize_livestream("1024x768");
+        setCookie("livestream_size", arg, -5);
+        if ($('#canholder').innerWidth() >= 1024) {
+            resizeLivestream("1024x768");
         }
         else {
-            resize_livestream("800x600");
+            resizeLivestream("800x600");
         }
     }
     else {
-        set_cookie("livestream_size", arg, 365);
-        resize_livestream(arg);
+        setCookie("livestream_size", arg, 365);
+        resizeLivestream(arg);
     }
 }
 
-function init_livestream(url) {
-    window.livestream = document.getElementById('livestream');
-    window.image_size_x = document.getElementById('image_size_x');
-    window.image_size_y = document.getElementById('image_size_y');
-    var sel_resolution = document.getElementById('sel_resolution');
-
+function initLivestream() {
+    var sel_resolution = $('#sel_resolution');
+    
     // initially set canvas size
-    var livestream_size = get_cookie("window.livestream_size");
+    var livestream_size = getGookie("livestream_size");
     if (! livestream_size) {
         livestream_size = "auto";
     }
-    set_resolution(livestream_size);
+    setResolution(livestream_size);
     sel_resolution.value = livestream_size;
-
+    
     // start stream
-    var events = new EventSource(url);
+    var livestream = $('#livestream');
+    var events = new EventSource(livestream.data('url'));
     events.addEventListener('message', function(event) {
-        load_canvas(window.livestream, event.data);
+        load_canvas(livestream, event.data);
         last_event = event;
     }, false);
 }
 
 /********* LIVE STREAM END *********/
 
+function setupRunning() {
+    initLivelog();
+    initLivestream();
+    
+    $('#interactive0_button').click(enableInteractive);
+    $('#interactive1_button').click(disableInteractive);
+
+    $('#continue_button').click(function() {
+        sendCommand('continue_waitforneedle');
+    });
+    $('#retry_button').click(function() {
+        sendCommand('reload_needles_and_retry');
+    });
+    $('#stop_button').click(function() {
+        sendCommand('stop_waitforneedle');
+    });
+
+    $('#sel_resolution').change(function() {
+        setResolution($(this).val());
+    });
+    $('#scrolldown').change(function() {
+        setScrolldown($(this).attr('checked') ? true : false);
+    });
+}
 
 // vim: set sw=4 et:
