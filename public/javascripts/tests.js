71ad8a44c82b709aafab0263822269f2b172ae7c
Ondrej Holecek <oholecek@suse.com>
2015-03-12 14:19:23 +0100

    add tooltips to icons, fix deps detecting
    - because row['deps'] is now always passed (as empty array) check
    for length instead

--- public/javascripts/tests.js
+++ public/javascripts/tests.js
@@ -43,27 +43,27 @@ function renderTestResult( data, type, row ) {
     if (type === 'display') {
 	var html = '' 
 	if (row['state'] === 'done') {
-	    html += data['passed'] + "<i class='fa module_passed fa-star'></i>";
+	    html += data['passed'] + "<i class='fa module_passed fa-star' title='modules passed'></i>";
 	    if (data['dents']) {
-		html +=  " " + data['dents'] + "<i class='fa module_softfail fa-star-half-empty'></i> ";
+		html +=  " " + data['dents'] + "<i class='fa module_softfail fa-star-half-empty' title='modules with warnings'></i> ";
 	    }
 	    if (data['failed']) {
-		html +=  " " + data['failed'] + "<i class='fa module_failed fa-star-o'></i> ";
+		html +=  " " + data['failed'] + "<i class='fa module_failed fa-star-o' title='modules failed'></i> ";
 	    }
 	    if (data['none']) {
-		html +=  " " + data['none'] + "<i class='fa module_none fa-ban'></i> ";
+		html +=  " " + data['none'] + "<i class='fa module_none fa-ban' title='modules skipped'></i> ";
 	    }
 	}
 	if (row['state'] === 'cancelled') {
-	    html += "<i class='fa fa-times'></i>";
+	    html += "<i class='fa fa-times' title='canceled'></i>";
 	}
-	if (row['deps']) {
+	if (row['deps'].length > 0) {
 	    if (row['result'] === 'skipped' ||
 		row['result'] === 'parallel_failed') {
-		html += "<i class='fa fa-chain-broken'></i>";
+		html += "<i class='fa fa-chain-broken' title='dependency failed'></i>";
 	    }
 	    else {
-		html += "<i class='fa fa-link'></i>";
+		html += "<i class='fa fa-link' title='dependency passed'></i>";
 	    }
 	}
         return '<a href="/tests/' + row['id'] + '">' + html + '</a>';
