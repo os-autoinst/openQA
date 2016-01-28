346dc6c0ac6cacdee18ab0863755e5c91c0b4db1
Ondrej Holecek <oholecek@suse.com>
2016-01-28 22:08:41 +0100

    Convert AuditEvents event data from perl to JSON
    DB version bumped to 37 to trigger update. Actual perl data are parsed
    by Safe feature with limited opcodes to bare minimum needed to prevent
    evaling and executing possibly unwanted code.

---
+++ dbicdh/PostgreSQL/upgrade/36-37/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/home/openQA/openQA/script/../dbicdh/_source/deploy/36/001-auto.yml' to '/home/openQA/openQA/script/../dbicdh/_source/deploy/37/001-auto.yml':;
+
+;
+-- No differences found;
+
