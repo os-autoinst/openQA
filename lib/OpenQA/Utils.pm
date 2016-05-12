os-autoinst/openQA

commit 2b8ac3c859e6db1296fbf7f6ff71d0306d9501e8

Make sure needledir is in testcasedir

lib/OpenQA/Utils.pm
@@ -2,6 +2,7 @@ package OpenQA::Utils;
 use strict;
 require 5.002;
 
+use Cwd;
 use Carp;
 use IPC::Run();
 use Mojo::URL;
@@ -102,6 +103,12 @@ sub needle_info {
     }
     else {
         $needledir = dirname($fn);
+    }
+
+    # make sure needledir is a subdir of testcasedir
+    if (index(Cwd::realpath($needledir), Cwd::realpath($testcasedir)) != 0) {
+        warn "$needledir is not a subdir of $testcasedir";
+        return;
     }
 
     my $JF;

lib/OpenQA/WebAPI.pm
@@ -358,7 +358,7 @@ sub startup {
     $step_auth->post('/')->name('save_needle')->to('step#save_needle');
     $step_r->get('/')->name('step')->to(action => 'view');
 
-    $r->get('/needles/:distri/*name')->name('needle_file')->to('file#needle');
+    $r->get('/needles/:distri/#name')->name('needle_file')->to('file#needle');
     $r->get('/image/:md5_dirname/.thumbs/#md5_basename')->name('thumb_image')->to('file#thumb_image');
 
     $r->get('/group_overview/:groupid')->name('group_overview')->to('main#group_overview');

commit 95e3b3ebdc35740cf23c21403d268f5bbe51540b

Fix use of symlinked needle dirs from outside of basedir
This fixes a problem introduced with 2b8ac3c859e6db1296fbf7f6ff71d0306d9501e8.
The check for the needledir has been moved where an external file path is used
coming from a REST request where additional care should be taken to ensure a
valid and secure directory. For a "local" definition of the needledir the
check was removed changing the behaviour back to the old way supporting
symlinked needle dirs, e.g. when /local/needles is symlinked into
/var/lib/openqa/share/tests/... instead of being a real subdirectory.

For the "new" feature of an external definition of needle dirs test cases are
(still) missing and should be added before another related change is done.

Fixes gh #667.

lib/OpenQA/Utils.pm
@@ -105,12 +105,13 @@ sub needle_info {
     }
     else {
         $needledir = dirname($fn);
-    }
-
-    # make sure needledir is a subdir of testcasedir
-    if (index(Cwd::realpath($needledir), Cwd::realpath($testcasedir)) != 0) {
-        warn "$needledir is not a subdir of $testcasedir";
-        return;
+        # make sure the directory of the file parameter is a real subdir of
+        # testcasedir when applying it as needledir to prevent access
+        # outside of the zoo
+        if (index(Cwd::realpath($needledir), Cwd::realpath($testcasedir)) != 0) {
+            warn "$needledir is not a subdir of $testcasedir";
+            return;
+        }
     }
 
     my $JF;