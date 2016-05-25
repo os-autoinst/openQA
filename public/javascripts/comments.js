07a5f5ebbcce0c612fa183c78ddc8a326d858ca2
Marius Kittler <mkittler@suse.de>
2016-05-25 15:01:18 +0200

    Fix showing editing buttons
    Editing buttons are now shown correctly when not logged-in as an admin.

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -1,7 +1,10 @@
 function showCommentEditor(commentId, form) {
     form.text.style.display = "block";
     form.editComment.style.display = "none";
-    document.getElementById("removeComment_" + commentId).style.display = "none";
+    var removeCommentElement = document.getElementById("removeComment_" + commentId);
+    if(removeCommentElement) {
+        removeCommentElement.style.display = "none";
+    }
     form.applyChanges.style.display = "inline";
     form.discardChanges.style.display = "inline";
     document.getElementById("commentMd_" + commentId).style.display = "none";
@@ -10,7 +13,10 @@ function showCommentEditor(commentId, form) {
 function hideCommentEditor(commentId, form) {
     form.text.style.display = "none";
     form.editComment.style.display = "inline";
-    document.getElementById("removeComment_" + commentId).style.display = "inline";
+    var removeCommentElement = document.getElementById("removeComment_" + commentId);
+    if(removeCommentElement) {
+        removeCommentElement.style.display = "inline";
+    }
     form.applyChanges.style.display = "none";
     form.discardChanges.style.display = "none";
     document.getElementById("commentMd_" + commentId).style.display = "block";
