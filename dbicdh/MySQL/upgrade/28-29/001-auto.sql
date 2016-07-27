72b00051ad7c10386408e6fddcfd73423ffe5452
Stephan Kulow <coolo@suse.de>
2016-07-27 11:07:00 +0200

    Remove MySQL from deployments, we never test migrations or deployments

--- dbicdh/MySQL/upgrade/28-29/001-auto.sql
+++
@@ -1,5 +0,0 @@
--- Convert schema '/space/lnussel/git/openQA/script/../dbicdh/_source/deploy/28/001-auto.yml' to '/space/lnussel/git/openQA/script/../dbicdh/_source/deploy/29/001-auto.yml':;
-
-;
--- No differences found;
-
