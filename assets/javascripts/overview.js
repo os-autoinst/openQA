0b08882c4272189902186d4d44d57801e89aacc1
Marius Kittler <mkittler@suse.de>
2016-09-12 09:24:54 +0200

    Add filter for all states and results

--- assets/javascripts/overview.js
+++ assets/javascripts/overview.js
@@ -42,6 +42,7 @@ function setupOverview() {
     // find specified results
     var varPairs = window.location.search.substring(1).split('&');
     var results = {};
+    var states = {};
     for (var j = 0; j < varPairs.length; ++j) {
         var pair = varPairs[j].split('=');
         if(pair.length > 1) {
@@ -49,6 +50,8 @@ function setupOverview() {
             var val = pair[1];
             if (key === 'result') {
                 results[val] = true;
+            } else if (key === 'state') {
+                states[val] = true;
             } else if (key === 'todo') {
                 $('#filter-todo').prop('checked', val !== '0');
             } else if (key === 'arch') {
@@ -64,12 +67,16 @@ function setupOverview() {
     }
     
     // set enabled/disabled state of checkboxes (according to current filter)
-    var badges = ['passed', 'incomplete', 'softfailed', 'failed', 'scheduled', 'running', 'none', 'unknown'];
-    for(var i = 0; i < badges.length; ++i) {
-        var badge = badges[i];
-        var filterEnabled = results[badge];
-        var badgeElement = $('#filter-' + badge);
-        badgeElement.prop('checked', filterEnabled);
+    var resultNames = ['none', 'skipped', 'obsoleted', 'parallel_failed', 'parallel_restarted', 'user_cancelled', 'user_restarted', 'passed', 'incomplete', 'softfailed', 'failed', 'scheduled', 'running', 'none', 'unknown'];
+    for(var i = 0; i < resultNames.length; ++i) {
+        var result = resultNames[i];
+        $('#filter-' + result).prop('checked', results[result]);
+    }
+    
+    var stateNames = ['scheduled', 'running', 'cancelled', 'waiting', 'done', 'uploading'];
+    for(var i = 0; i < stateNames.length; ++i) {
+        var state = stateNames[i];
+        $('#filter-' + state).prop('checked', states[state]);
     }
 }
 
