c1550d6fa9caf2025282026828f035fa70985710
Dominik Heidler <dominik@heidler.eu>
2016-10-12 14:54:14 +0200

    Fix test result info spacing (#938)
    On /tests there is a space between the stars and the following number.
    But the space was missing between the green star and the
    chained-dependency icon.

--- assets/javascripts/tests.js
+++ assets/javascripts/tests.js
@@ -103,13 +103,13 @@ function renderTestResult( data, type, row ) {
         if (row['state'] === 'done') {
             html += data['passed'] + "<i class='fa module_passed fa-star' title='modules passed'></i>";
             if (data['softfailed']) {
-                html +=  " " + data['softfailed'] + "<i class='fa module_softfail fa-star-half-empty' title='modules with warnings'></i> ";
+                html += " " + data['softfailed'] + "<i class='fa module_softfail fa-star-half-empty' title='modules with warnings'></i>";
             }
             if (data['failed']) {
-                html +=  " " + data['failed'] + "<i class='fa module_failed fa-star-o' title='modules failed'></i> ";
+                html += " " + data['failed'] + "<i class='fa module_failed fa-star-o' title='modules failed'></i>";
             }
             if (data['none']) {
-                html +=  " " + data['none'] + "<i class='fa module_none fa-ban' title='modules skipped'></i> ";
+                html += " " + data['none'] + "<i class='fa module_none fa-ban' title='modules skipped'></i>";
             }
         }
         if (row['state'] === 'cancelled') {
@@ -118,10 +118,10 @@ function renderTestResult( data, type, row ) {
         if (row['deps']['parents']['Parallel'].length + row['deps']['parents']['Chained'].length > 0) {
             if (row['result'] === 'skipped' ||
                 row['result'] === 'parallel_failed') {
-                html += "<i class='fa fa-chain-broken' title='dependency failed'></i>";
+                html += " <i class='fa fa-chain-broken' title='dependency failed'></i>";
             }
             else {
-                html += "<i class='fa fa-link' title='dependency passed'></i>";
+                html += " <i class='fa fa-link' title='dependency passed'></i>";
             }
         }
         return '<a href="/tests/' + row['id'] + '">' + html + '</a>';
