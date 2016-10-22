os-autoinst/openQA

commit 0b78fc5f04c27cc44b5ee1f8200ee4f46493f10d

Try harder to avoid long running pre-processing (#897)
If many workers grab a job at the same time, they all go through the
scheduler bottleneck, wo will assign every worker a job one by one.
But the last couple of job_grab requests will actually get a timeout
and the worker will believe it didn't get a job.

if it then later grabs another job, we need to do something with the
previously assigned job. Just saying no is a problem as both the worker
keeps being unused and the job stays in running (pre-processing).

So now I set the previous job to incomplete and duplicate it. Beside
that I also made sure that cancelling a job kills the worker reference,
so it can grab another one.

lib/OpenQA/Schema/Result/Jobs.pm
@@ -1416,25 +1416,16 @@ test modules
 =cut
 sub done {
     my ($self, %args) = @_;
-    my $newbuild = 0;
-    $newbuild = int($args{newbuild}) if defined $args{newbuild};
+    my $newbuild = int($args{newbuild} // 0);
     $args{result} = OBSOLETED if $newbuild;
-
-    # cleanup
-    $self->set_property('JOBTOKEN');
-    $self->release_networks();
-    $self->owned_locks->delete;
-    $self->locked_locks->update({locked_by => undef});
-    if ($self->worker) {
-        # free the worker
-        $self->worker->update({job_id => undef});
-    }
 
     # update result if not provided
     my $result = $args{result} || $self->calculate_result();
     my %new_val = (state => DONE);
     # for cancelled jobs the result is already known
     $new_val{result} = $result if $self->result eq NONE;
+
+    $self->_cleanup;
 
     $self->update(\%new_val);
 
@@ -1446,6 +1437,20 @@ sub done {
     $self->carry_over_labels;
 
     return $result;
+}
+
+# free the worker and remove token and locks
+sub _cleanup {
+    my ($self) = @_;
+
+    $self->set_property('JOBTOKEN');
+    $self->release_networks();
+    $self->owned_locks->delete;
+    $self->locked_locks->update({locked_by => undef});
+    if ($self->worker) {
+        # free the worker
+        $self->worker->update({job_id => undef});
+    }
 }
 
 sub cancel {
@@ -1466,6 +1471,8 @@ sub cancel {
         $count += $self->_job_skip_children;
         $count += $self->_job_stop_children;
     }
+
+    $self->_cleanup;
     return $count;
 }
 1;

lib/OpenQA/Scheduler/Scheduler.pm
@@ -145,9 +145,10 @@ sub job_grab {
 
     my $worker = _validate_workerid($workerid);
     if ($worker->job) {
-        # TROUBLE ahead - do not call seen, to get into dead worker detection first
-        log_warning($worker->name . " still has a job, but wants to grab new one: " . $worker->job->id);
-        return {};
+        my $job = $worker->job;
+        log_warning($worker->name . " wants to grab a new job - killing the old one: " . $job->id);
+        $job->done(result => 'incomplete');
+        $job->auto_duplicate;
     }
     $worker->seen($workercaps);
 

t/04-scheduler.t
@@ -316,9 +316,6 @@ $result = OpenQA::Scheduler::Scheduler::job_set_running($job_id);
 $job    = job_get($job_id);
 ok($result == 0 && $job->state eq "running", "Retry job_set_running");
 
-# impossible to grab new job while old one running
-is_deeply({}, OpenQA::Scheduler::Scheduler::job_grab(%args), "not being able to grab twice");
-
 sleep 1;
 # Testing job_set_done
 $job = job_get($job_id);
@@ -360,6 +357,24 @@ $schema->resultset('Jobs')->find($job_id)->set_prio(100);
 $job = job_get($job_id);
 is($job->priority, 100, "job->set_prio");
 
+$result = $schema->resultset('Jobs')->find($job_id)->delete;
+my $no_job_id = job_get($job_id);
+ok($result && !defined $no_job_id, "job_delete");
+
+# Testing double grab
+%args   = (workerid => $worker->{id},);
+$job    = OpenQA::Scheduler::Scheduler::job_grab(%args);
+$job_id = $job->{id};
+$job    = job_get($job_id);
+is($job->state, 'running', 'grabed job runs');
+
+my $job4 = OpenQA::Scheduler::Scheduler::job_grab(%args);
+isnt($job4->{id}, $job_id, "Grabed another job");
+$job->discard_changes;
+is($job4->{state}, 'running',    'grabed job 4 runs');
+is($job->state,    'done',       'job 2 no longer runs');
+is($job->result,   'incomplete', 'first job set to incomplete');
+
 # Testing job_restart
 # TBD
 
@@ -369,17 +384,16 @@ is($job->priority, 100, "job->set_prio");
 # Testing job_fill_settings
 # TBD
 
-
-$result = $schema->resultset('Jobs')->find($job_id)->delete;
-my $no_job_id = job_get($job_id);
-ok($result && !defined $no_job_id, "job_delete");
-
 $result    = $schema->resultset('Jobs')->find($job2->id)->delete;
 $no_job_id = job_get($job2->id);
 ok($result && !defined $no_job_id, "job_delete");
 
 $result    = $schema->resultset('Jobs')->find($job3_id)->delete;
 $no_job_id = job_get($job3_id);
+ok($result && !defined $no_job_id, "job_delete");
+
+$result    = $schema->resultset('Jobs')->find($job4->{id})->delete;
+$no_job_id = job_get($job4->{id});
 ok($result && !defined $no_job_id, "job_delete");
 
 $current_jobs = list_jobs();

commit 97fb37f1c24f024959cee77bc4cee11b87c4298b

Don't free worker before artefacts are uploaded (POO #14378)
0b78fc5f broke artefact uploading on job cancellation. Because
the job `cancel()` method now frees the worker (so far as the
schema is concerned) immediately after telling the worker to
cancel the job, by the time the worker process reaches the
artefact upload bits of `_stop_job()`, the schema job instance
has no worker, and when the worker process tries to upload the
artefacts, the schema job instance `create_artefact()` method
throws a 404 because so far as it knows, it no longer has a
worker.

The change was intended to mitigate a problem caused by the
dbus deadlock issue, where the server never actually handled
the `set_done` API call the worker process sends at the end
of `_stop_job`, so the worker was never freed - but as the dbus
deadlock issue has now been addressed, we shouldn't need to
worry about that any more.

lib/OpenQA/Schema/Result/Jobs.pm
@@ -1402,13 +1402,21 @@ sub done {
     my $newbuild = int($args{newbuild} // 0);
     $args{result} = OBSOLETED if $newbuild;
 
+    # cleanup
+    $self->set_property('JOBTOKEN');
+    $self->release_networks();
+    $self->owned_locks->delete;
+    $self->locked_locks->update({locked_by => undef});
+    if ($self->worker) {
+        # free the worker
+        $self->worker->update({job_id => undef});
+    }
+
     # update result if not provided
     my $result = $args{result} || $self->calculate_result();
     my %new_val = (state => DONE);
     # for cancelled jobs the result is already known
     $new_val{result} = $result if $self->result eq NONE;
-
-    $self->_cleanup;
 
     $self->update(\%new_val);
 
@@ -1420,20 +1428,6 @@ sub done {
     $self->carry_over_labels;
 
     return $result;
-}
-
-# free the worker and remove token and locks
-sub _cleanup {
-    my ($self) = @_;
-
-    $self->set_property('JOBTOKEN');
-    $self->release_networks();
-    $self->owned_locks->delete;
-    $self->locked_locks->update({locked_by => undef});
-    if ($self->worker) {
-        # free the worker
-        $self->worker->update({job_id => undef});
-    }
 }
 
 sub cancel {
@@ -1454,8 +1448,6 @@ sub cancel {
         $count += $self->_job_skip_children;
         $count += $self->_job_stop_children;
     }
-
-    $self->_cleanup;
     return $count;
 }
 1;