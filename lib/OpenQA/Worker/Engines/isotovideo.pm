os-autoinst/openQA

commit baac24bcae7618c9fd6014cc6f3e6f1dc2609159

Add 'fixed' asset subdirectories for fixed assets (#928) (#945)
This adjusts all the places that look for asset files to check
both in the current location and in a new possible location,
which is in a 'fixed/' subdirectory of the normal asset folder
for that asset type. It also adjusts the `limit_assets` gru
task to ignore files in the 'fixed' subdirectories. This
replaces the current special-casing of disk image assets created
by user '0'. That approach has become problematic now both SUSE
and Fedora are testing disk images that come from the distro
composes; these disk images are never getting cleaned up and
that causes disk space exhaustion. This also allows for 'fixed'
ISO and other assets, which the current approach does not.

As part of this change, we do a bit of refactoring to avoid
duplicating the code for constructing the possible file paths,
and have the worker code use the asset path definitions from
Utils rather than duplicating them.

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -18,7 +18,7 @@ use strict;
 use warnings;
 
 use OpenQA::Worker::Common;
-use OpenQA::Utils qw//;
+use OpenQA::Utils qw/locate_asset/;
 
 use POSIX qw/:sys_wait_h strftime uname/;
 use JSON qw/to_json/;
@@ -75,8 +75,8 @@ sub engine_workit($) {
 
     for my $isokey (qw/ISO/, map { "ISO_$_" } (1 .. 9)) {
         if (my $iso = $job->{settings}->{$isokey}) {
-            $iso = join('/', ISO_DIR, $iso);
-            unless (-e $iso) {
+            $iso = locate_asset('iso', $iso, 1);
+            unless ($iso) {
                 my $error = "$iso does not exist!";
                 return {error => $error};
             }
@@ -86,8 +86,8 @@ sub engine_workit($) {
 
     for my $otherkey (qw/KERNEL INITRD/) {
         if (my $file = $job->{settings}->{$otherkey}) {
-            $file = join('/', OTHER_DIR, $file);
-            unless (-e $file) {
+            $file = locate_asset('other', $file, 1);
+            unless ($file) {
                 my $error = "$file does not exist!";
                 return {error => $error};
             }
@@ -99,8 +99,8 @@ sub engine_workit($) {
     for my $i (1 .. $nd) {
         my $hdd = $job->{settings}->{"HDD_$i"} || undef;
         if ($hdd) {
-            $hdd = join('/', HDD_DIR, $hdd);
-            unless (-e $hdd) {
+            $hdd = locate_asset('hdd', $hdd, 1);
+            unless ($hdd) {
                 my $error = "$hdd does not exist!";
                 return {error => $error};
             }
@@ -116,7 +116,7 @@ sub engine_workit($) {
         print "setting $k=$v\n" if $verbose;
         $vars{$k} = $v;
     }
-    $vars{ASSETDIR}   = ASSET_DIR;
+    $vars{ASSETDIR}   = $OpenQA::Utils::assetdir;
     $vars{CASEDIR}    = OpenQA::Utils::testcasedir($vars{DISTRI}, $vars{VERSION});
     $vars{PRODUCTDIR} = OpenQA::Utils::productdir($vars{DISTRI}, $vars{VERSION});
     _save_vars(\%vars);

docs/GettingStarted.asciidoc
@@ -442,7 +442,7 @@ For Fedora, similarly, you can call:
 --------------------------------------------------------------------------------
 
 Some Fedora tests require special hard disk images to be present in
-+/var/lib/openqa/share/factory/hdd+. The +createhdds.py+ script in the
++/var/lib/openqa/share/factory/hdd/fixed+. The +createhdds.py+ script in the
 https://bitbucket.org/rajcze/openqa_fedora_tools.git[openqa_fedora_tools]
 repository can be used to create these. See the documentation in that repo
 for more information.

docs/Installing.asciidoc
@@ -375,13 +375,21 @@ directories, most of which must be owned by the user that runs openQA
 * +images+ is where the server stores test screenshots and thumbnails
 * +share+ contains shared directories for remote workers, can be owned by root
 * +share/factory+ contains test assets and temp directory, can be owned by root but sysadmin must create subdirs
-* +share/factory/iso+ contains ISOs for tests
-* +share/factory/hdd+ contains hard disk images for tests
-* +share/factory/repo+ contains repositories for tests
-* +share/factory/other+ contains miscellaneous test assets (e.g. kernels and initrds)
+* +share/factory/iso+ and +share/factory/iso/fixed+ contain ISOs for tests
+* +share/factory/hdd+ and +share/factory/hdd/fixed+ contain hard disk images for tests
+* +share/factory/repo+ and +share/factory/repo/fixed+ contain repositories for tests
+* +share/factory/other+ and +share/factory/other/fixed+ contain miscellaneous test assets (e.g. kernels and initrds)
 * +share/factory/tmp+ is used as a temporary directory (openQA will create it if it owns +share/factory+)
 * +share/tests+ contains the tests themselves
 * +testresults+ is where the server stores test logs and test-generated assets
+
+Each of the asset directories (+factory/iso+, +factory/hdd+, +factory/repo+ and
++factory/other+) may contain a +fixed/+ subdirectory, and assets of the same
+type may be placed in that directory. Placing an asset in the +fixed/+
+subdirectory indicates that it should not be deleted to save space: the GRU
+task which removes old assets when the size of all assets for a given job
+group is above a specified size will ignore assets in the +fixed/+
+subdirectories.
 
 It also contains several symlinks which are necessary due to various things
 moving around over the course of openQA's development. All the symlinks

lib/OpenQA/Scheduler/Scheduler.pm
@@ -30,7 +30,6 @@ use Data::Dump qw/dd pp/;
 use Date::Format qw/time2str/;
 use DBIx::Class::Timestamps qw/now/;
 use DateTime;
-use File::Spec::Functions qw/catfile catdir/;
 use File::Temp qw/tempdir/;
 use Mojo::URL;
 use Try::Tiny;

lib/OpenQA/Schema/Result/Assets.pm
@@ -23,7 +23,7 @@ use OpenQA::IPC;
 use Date::Format;
 use Archive::Extract;
 use File::Basename;
-use File::Spec::Functions 'catdir';
+use File::Spec::Functions 'catfile';
 use Mojo::UserAgent;
 use File::Spec::Functions 'splitpath';
 use Try::Tiny;
@@ -81,7 +81,12 @@ sub _getDirSize {
 
 sub disk_file {
     my ($self) = @_;
-    sprintf("%s/%s/%s", $OpenQA::Utils::assetdir, $self->type, $self->name);
+    return locate_asset($self->type, $self->name, 0);
+}
+
+sub is_fixed {
+    my ($self) = @_;
+    return (index($self->disk_file, catfile('fixed', $self->name)) > -1);
 }
 
 sub remove_from_disk {
@@ -196,7 +201,7 @@ sub download_asset {
         my $ae = Archive::Extract->new(archive => $dlpath);
         # Remove last extension from the end of filename
         my ($filename, $dirs, $suffix) = fileparse($dlpath, qr/\.[^.]*/);
-        $filename = catdir($dirs, $filename);
+        $filename = catfile($dirs, $filename);
 
         OpenQA::Utils::log_debug("Extracting to $filename");
 
@@ -258,8 +263,8 @@ sub limit_assets {
         for my $a (@job_assets) {
             my $asset = $assets{$a->asset_id};
             OpenQA::Utils::log_debug(sprintf "Group %s: %s/%s %s->%s", $g->name, $asset->type, $asset->name, human_readable_size($asset->size // 0), human_readable_size($sizelimit));
-            # ignore predefined images
-            next if ($asset->type eq 'hdd' && $a->created_by == 0);
+            # ignore fixed assets
+            next if ($asset->is_fixed);
             $seen_asset{$asset->id} = $g->id;
             my $size = $asset->ensure_size;
             if ($size > 0 && $reduceto > 0) {
@@ -294,24 +299,38 @@ sub limit_assets {
         my $dh;
         if (opendir($dh, $OpenQA::Utils::assetdir . "/$type")) {
             my %assets;
+            my @paths;
             while (readdir($dh)) {
-                # very specific to our external syncing
-                next if $_ =~ m/CURRENT/;
-                next if -l "$OpenQA::Utils::assetdir/$type/$_";
-                # ignore files not owned by us
-                next unless -o "$OpenQA::Utils::assetdir/$type/$_";
-                if ($type eq 'repo') {
-                    next unless -d "$OpenQA::Utils::assetdir/$type/$_";
+                unless ($_ eq 'fixed' or $_ eq '.' or $_ eq '..') {
+                    push(@paths, "$OpenQA::Utils::assetdir/$type/$_");
                 }
-                else {
-                    next unless -f "$OpenQA::Utils::assetdir/$type/$_";
-                    if ($type eq 'iso') {
-                        next unless $_ =~ m/\.iso$/;
-                    }
-                }
-                $assets{$_} = 0;
             }
             closedir($dh);
+            if (opendir($dh, $OpenQA::Utils::assetdir . "/$type" . "/fixed")) {
+                while (readdir($dh)) {
+                    unless ($_ eq 'fixed' or $_ eq '.' or $_ eq '..') {
+                        push(@paths, "$OpenQA::Utils::assetdir/$type/fixed/$_");
+                    }
+                }
+                closedir($dh);
+            }
+            for my $path (@paths) {
+                # very specific to our external syncing
+                next if basename($path) =~ m/CURRENT/;
+                next if -l $path;
+                # ignore files not owned by us
+                next unless -o $path;
+                if ($type eq 'repo') {
+                    next unless -d $path;
+                }
+                else {
+                    next unless -f $path;
+                    if ($type eq 'iso') {
+                        next unless $path =~ m/\.iso$/;
+                    }
+                }
+                $assets{basename($path)} = 0;
+            }
             $assets = $app->db->resultset('Assets')->search({type => $type, name => {in => [keys %assets]}});
             while (my $a = $assets->next) {
                 $assets{$a->name} = $a->id;

lib/OpenQA/Schema/ResultSet/Assets.pm
@@ -17,7 +17,7 @@
 package OpenQA::Schema::ResultSet::Assets;
 use strict;
 use base qw/DBIx::Class::ResultSet/;
-use OpenQA::Utils qw/log_warning/;
+use OpenQA::Utils qw/log_warning locate_asset/;
 
 sub register {
     my ($self, $type, $name) = @_;
@@ -27,8 +27,12 @@ sub register {
         log_warning "asset type '$type' invalid";
         return;
     }
-    unless ($name && $name =~ /^[0-9A-Za-z+-._]+$/ && -e join('/', $OpenQA::Utils::assetdir, $type, $name)) {
-        log_warning "asset name '$name' invalid or does not exist";
+    unless ($name && $name =~ /^[0-9A-Za-z+-._]+$/) {
+        log_warning "asset name '$name' invalid";
+        return;
+    }
+    unless (locate_asset $type, $name, 1) {
+        log_warning "no file found for asset '$name' type '$type'";
         return;
     }
     my $asset = $self->find_or_create(

lib/OpenQA/Utils.pm
@@ -14,6 +14,7 @@ $VERSION = sprintf "%d.%03d", q$Revision: 1.12 $ =~ /(\d+)/g;
 @EXPORT  = qw(
   $prj
   $basedir
+  $prjdir
   $resultdir
   &data_name
   &needle_info
@@ -39,6 +40,7 @@ $VERSION = sprintf "%d.%03d", q$Revision: 1.12 $ =~ /(\d+)/g;
   &check_download_whitelist
   &notify_workers
   &human_readable_size
+  &locate_asset
 );
 
 
@@ -50,16 +52,20 @@ if ($0 =~ /\.t$/) {
 
 #use lib "/usr/share/openqa/cgi-bin/modules";
 use File::Basename;
+use File::Spec::Functions qw/catfile/;
 use Fcntl;
 use JSON "decode_json";
 our $basedir     = $ENV{OPENQA_BASEDIR} || "/var/lib";
 our $prj         = "openqa";
-our $resultdir   = "$basedir/$prj/testresults";
-our $assetdir    = "$basedir/$prj/factory";
+our $prjdir      = "$basedir/$prj";
+our $resultdir   = "$prjdir/testresults";
+our $assetdir    = "$prjdir/factory";
 our $isodir      = "$assetdir/iso";
-our $imagesdir   = "$basedir/$prj/images";
+our $hdddir      = "$assetdir/hdd";
+our $otherdir    = "$assetdir/other";
+our $imagesdir   = "$prjdir/images";
 our $hostname    = $ENV{SERVER_NAME};
-our $testcasedir = "$basedir/openqa/share/tests";
+our $testcasedir = "$prjdir/share/tests";
 our $app;
 
 # the desired new folder structure is
@@ -325,6 +331,17 @@ sub parse_assets_from_settings {
     }
 
     return $assets;
+}
+
+# find the actual disk location of a given asset.
+sub locate_asset {
+    my ($type, $name, $mustexist) = @_;
+    $mustexist //= 0;
+    my $trans = catfile($assetdir, $type, $name);
+    my $fixed = catfile($assetdir, $type, 'fixed', $name);
+    return $trans if (-e $trans);
+    return $fixed if (-e $fixed);
+    $mustexist ? return : return $trans;
 }
 
 my %bugrefs = (

lib/OpenQA/WebAPI.pm
@@ -28,7 +28,6 @@ use Mojolicious::Commands;
 use DateTime;
 use Cwd qw/abs_path/;
 use File::Path qw/make_path/;
-use OpenQA::Worker::Common qw/ASSET_DIR/;
 
 # reinit pseudo random number generator in every child to avoid
 # starting off with the same state.
@@ -78,7 +77,7 @@ sub startup {
     OpenQA::ServerStartup::setup_logging($self);
 
     unless ($ENV{MOJO_TMPDIR}) {
-        $ENV{MOJO_TMPDIR} = ASSET_DIR . '/tmp';
+        $ENV{MOJO_TMPDIR} = $OpenQA::Utils::assetdir . '/tmp';
         # Try to create tmpdir if it doesn't exist but don't die if failed to create
         if (!-e $ENV{MOJO_TMPDIR}) {
             eval { make_path($ENV{MOJO_TMPDIR}); };

commit fbb0df396427d2d78294498dda092df5130c4637

Fix worker error reporting when asset cannot be found
I introduced a problem in baac24bc by using `locate_asset` here.
Previously when the asset wasn't found, the variable (`$iso` or
whatever) would be the path where we were looking for it; now,
it's undef. So the error message won't tell us what asset it was
looking for, and additionally we get a (non-fatal) perl error
constructing the error message. So let's keep the asset filename
around for the purpose of creating the error message, and tweak
the wording a bit.

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -74,10 +74,10 @@ sub engine_workit($) {
     }
 
     for my $isokey (qw/ISO/, map { "ISO_$_" } (1 .. 9)) {
-        if (my $iso = $job->{settings}->{$isokey}) {
-            $iso = locate_asset('iso', $iso, 1);
+        if (my $isoname = $job->{settings}->{$isokey}) {
+            my $iso = locate_asset('iso', $isoname, 1);
             unless ($iso) {
-                my $error = "$iso does not exist!";
+                my $error = "Cannot find ISO asset $isoname!";
                 return {error => $error};
             }
             $job->{settings}->{$isokey} = $iso;
@@ -85,10 +85,10 @@ sub engine_workit($) {
     }
 
     for my $otherkey (qw/KERNEL INITRD/) {
-        if (my $file = $job->{settings}->{$otherkey}) {
-            $file = locate_asset('other', $file, 1);
+        if (my $filename = $job->{settings}->{$otherkey}) {
+            my $file = locate_asset('other', $filename, 1);
             unless ($file) {
-                my $error = "$file does not exist!";
+                my $error = "Cannot find OTHER asset $filename!";
                 return {error => $error};
             }
             $job->{settings}->{$otherkey} = $file;
@@ -97,11 +97,11 @@ sub engine_workit($) {
 
     my $nd = $job->{settings}->{NUMDISKS} || 2;
     for my $i (1 .. $nd) {
-        my $hdd = $job->{settings}->{"HDD_$i"} || undef;
-        if ($hdd) {
-            $hdd = locate_asset('hdd', $hdd, 1);
+        my $hddname = $job->{settings}->{"HDD_$i"} || undef;
+        if ($hddname) {
+            my $hdd = locate_asset('hdd', $hddname, 1);
             unless ($hdd) {
-                my $error = "$hdd does not exist!";
+                my $error = "Cannot find HDD asset $hddname!";
                 return {error => $error};
             }
             $job->{settings}->{"HDD_$i"} = $hdd;