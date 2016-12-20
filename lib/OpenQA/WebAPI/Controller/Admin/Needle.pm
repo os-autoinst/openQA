os-autoinst/openQA

commit 2440dc9190793ee4322622c867d9c2e63e31a49e

Fix a few formatting mistakes and remove repetitive method calls (#1020)
There were a few spots left from #1017 that still needed manual formatting
fixes. And since the method "list" in
OpenQA::WebAPI::Controller::API::V1::JobTemplate and the helper
"current_user" in OpenQA::WebAPI::Plugin::Helpers were affected by this
anyway, i've cleaned them up as well.

lib/OpenQA/WebAPI/Controller/Admin/Needle.pm
@@ -63,17 +63,9 @@ sub ajax {
     my @order_by_params;
     my $index = 0;
     while (1) {
-        my $column_index = $self->param("order[$index][column]");
+        my $column_index = $self->param("order[$index][column]") // @columns;
         my $column_order = $self->param("order[$index][dir]");
-        if (
-            !(
-                   defined($column_index)
-                && $column_index < scalar(@columns)
-                && ($column_order eq 'asc' || $column_order eq 'desc')))
-        {
-            last;
-        }
-        push(@order_by_params, {'-' . $column_order => $columns[$column_index]});
+        last unless $column_index < @columns && grep { $column_order eq $_ } qw(asc desc);
         ++$index;
     }
     push(@order_by_params, {-asc => 'filename'}) unless @order_by_params;

lib/OpenQA/Schema/Result/Jobs.pm
@@ -688,9 +688,8 @@ sub auto_duplicate {
             $args->{retry_avbl} = int($self->retry_avbl) - 1;
         }
         else {
-            log_warning(
-"Could not auto-duplicated! The job are auto-duplicated too many times. Please restart the job manually."
-            );
+            log_warning('Could not auto-duplicated! The job are auto-duplicated too many times.'
+                  . ' Please restart the job manually.');
             return;
         }
     }

lib/OpenQA/Schema/Result/Workers.pm
@@ -172,11 +172,8 @@ sub send_command {
     return if (!defined $args{command});
 
     if (!grep { /^$args{command}$/ } COMMANDS) {
-        log_error(
-            sprintf(
-                'Trying to issue unknown command "%s" for worker "%s:%n"',
-                $args{command}, $self->host, $self->instance
-            ));
+        my $msg = 'Trying to issue unknown command "%s" for worker "%s:%n"';
+        log_error(sprintf($msg, $args{command}, $self->host, $self->instance));
         return;
     }
 

lib/OpenQA/Schema/ResultSet/Jobs.pm
@@ -233,7 +233,8 @@ sub complex_query {
                 'me.result' => {    # these results should be hidden by default
                     -not_in => [
                         OpenQA::Schema::Result::Jobs::OBSOLETED,
-             # OpenQA::Schema::Result::Jobs::USER_CANCELLED  I think USER_CANCELLED jobs should be available for restart
+                        # OpenQA::Schema::Result::Jobs::USER_CANCELLED
+                        # I think USER_CANCELLED jobs should be available for restart
                     ]}});
     }
     if ($scope eq 'current') {

lib/OpenQA/WebAPI.pm
@@ -130,10 +130,7 @@ sub startup {
             }
             if (my $days = $c->app->config->{global}->{hsts}) {
                 $c->res->headers->header(
-                    'Strict-Transport-Security',
-                    sprintf 'max-age=%d; includeSubDomains',
-                    $days * 24 * 60 * 60
-                );
+                    'Strict-Transport-Security' => sprintf('max-age=%d; includeSubDomains', $days * 24 * 60 * 60));
             }
             $c->stash('job_groups_and_parents', job_groups_and_parents);
         });

lib/OpenQA/WebAPI/Controller/API/V1/Job.pm
@@ -25,11 +25,11 @@ sub list {
     my $self = shift;
 
     my %args;
-    for my $arg (
-        qw/build iso distri version flavor maxage scope group groupid limit page before after arch hdd_1 test machine/)
-    {
-        next unless defined $self->param($arg);
-        $args{$arg} = $self->param($arg);
+    my @args
+      = qw/build iso distri version flavor maxage scope group groupid limit page before after arch hdd_1 test machine/;
+    for my $arg (@args) {
+        next unless defined(my $value = $self->param($arg));
+        $args{$arg} = $value;
     }
     # For these, we accept a single value, a single instance of the arg
     # with a comma-separated list of values, or multiple instances of the

lib/OpenQA/WebAPI/Controller/API/V1/JobTemplate.pm
@@ -22,72 +22,65 @@ sub list {
 
     my @templates;
     eval {
-        if ($self->param("job_template_id")) {
-            @templates = $self->db->resultset("JobTemplates")->search({id => $self->param("job_template_id")});
+        if (my $id = $self->param('job_template_id')) {
+            @templates = $self->db->resultset("JobTemplates")->search({id => $id});
         }
-        elsif ($self->param('machine_name')
-            || $self->param('machine_id')
-            || $self->param('test_suite_name')
-            || $self->param('test_suite_id')
-            || $self->param('group_id')
-            || $self->param('arch') && $self->param('distri') && $self->param('flavor') && $self->param('version')
-            || $self->param('product_id'))
-        {
 
-            my %params;
-
-            $params{'machine.name'}    = $self->param('machine_name')    if $self->param('machine_name');
-            $params{'test_suite.name'} = $self->param('test_suite_name') if $self->param('test_suite_name');
-            $params{'product.arch'}    = $self->param('arch')            if $self->param('arch');
-            $params{'product.distri'}  = $self->param('distri')          if $self->param('distri');
-            $params{'product.flavor'}  = $self->param('flavor')          if $self->param('flavor');
-            $params{'product.version'} = $self->param('version')         if $self->param('version');
-            for my $id (qw/machine_id test_suite_id product_id group_id/) {
-                $params{$id} = $self->param($id) if $self->param($id);
-            }
-            @templates
-              = $self->db->resultset("JobTemplates")
-              ->search(\%params,
-                {join => ['machine', 'test_suite', 'product'], prefetch => [qw/machine test_suite product/]});
-        }
         else {
-            @templates
-              = $self->db->resultset("JobTemplates")->search({}, {prefetch => [qw/machine test_suite product/]});
+
+            my %cond;
+            if (my $value = $self->param('machine_name'))    { $cond{'machine.name'}    = $value }
+            if (my $value = $self->param('test_suite_name')) { $cond{'test_suite.name'} = $value }
+            for my $id (qw(arch distri flavor version)) {
+                if (my $value = $self->param($id)) { $cond{"product.$id"} = $value }
+            }
+            for my $id (qw(machine_id test_suite_id product_id group_id)) {
+                if (my $value = $self->param($id)) { $cond{$id} = $value }
+            }
+
+            my $has_query = grep { $cond{$_} } (
+                qw(machine_name machine_id test_suite.name test_suite_id group_id product.arch product.distri),
+                qw(product.flavor product.version product_id)
+            );
+
+            if ($has_query) {
+                my $attrs
+                  = {join => ['machine', 'test_suite', 'product'], prefetch => [qw/machine test_suite product/]};
+                @templates = $self->db->resultset("JobTemplates")->search(\%cond, $attrs);
+            }
+            else {
+                @templates
+                  = $self->db->resultset("JobTemplates")->search({}, {prefetch => [qw/machine test_suite product/]});
+            }
         }
     };
-    my $error = $@;
 
-    if ($error) {
-        $self->render(json => {error => $error}, status => 404);
-        return;
-    }
+    if (my $error = $@) { return $self->render(json => {error => $error}, status => 404) }
 
-    $self->render(
-        json => {
-            JobTemplates => [
-                map {
-                    {
-                        id         => $_->id,
-                        prio       => $_->prio,
-                        group_name => $_->group ? $_->group->name : '',
-                        product    => {
-                            id      => $_->product_id,
-                            arch    => $_->product->arch,
-                            distri  => $_->product->distri,
-                            flavor  => $_->product->flavor,
-                            group   => $_->product->mediagroup,
-                            version => $_->product->version
-                        },
-                        machine => {
-                            id   => $_->machine_id,
-                            name => $_->machine ? $_->machine->name : ''
-                        },
-                        test_suite => {
-                            id   => $_->test_suite_id,
-                            name => $_->test_suite->name
-                        }}
-                } @templates
-            ]});
+    @templates = map {
+        {
+            id         => $_->id,
+            prio       => $_->prio,
+            group_name => $_->group ? $_->group->name : '',
+            product    => {
+                id      => $_->product_id,
+                arch    => $_->product->arch,
+                distri  => $_->product->distri,
+                flavor  => $_->product->flavor,
+                group   => $_->product->mediagroup,
+                version => $_->product->version
+            },
+            machine => {
+                id   => $_->machine_id,
+                name => $_->machine ? $_->machine->name : ''
+            },
+            test_suite => {
+                id   => $_->test_suite_id,
+                name => $_->test_suite->name
+            }}
+    } @templates;
+
+    $self->render(json => {JobTemplates => \@templates});
 }
 
 sub create {

lib/OpenQA/WebAPI/Controller/File.pm
@@ -88,28 +88,18 @@ sub test_asset {
 
     my $jobid = $self->param('testid');
     my %cond = ('me.id' => $jobid);
-    if ($self->param('assetid')) {
-        $cond{'asset.id'} = $self->param('assetid');
-    }
+    if ($self->param('assetid')) { $cond{'asset.id'} = $self->param('assetid') }
     elsif ($self->param('assettype') and $self->param('assetname')) {
         $cond{name} = $self->param('assetname');
         $cond{type} = $self->param('assettype');
     }
-    else {
-        return $self->render(text => 'Missing or wrong parameters provided', status => 400);
-    }
+    else { return $self->render(text => 'Missing or wrong parameters provided', status => 400) }
 
     my %asset;
-    my $res
-      = $self->db->resultset('Jobs')
-      ->search(\%cond,
-        {join => {jobs_assets => 'asset'}, +select => [qw(asset.name asset.type)], +as => [qw(name type)]});
-    if ($res and $res->first) {
-        %asset = $res->first->get_columns;
-    }
-    else {
-        return $self->reply->not_found;
-    }
+    my $attrs = {join => {jobs_assets => 'asset'}, +select => [qw(asset.name asset.type)], +as => [qw(name type)]};
+    my $res = $self->db->resultset('Jobs')->search(\%cond, $attrs);
+    if ($res and $res->first) { %asset = $res->first->get_columns }
+    else                      { return $self->reply->not_found }
 
     # find the asset path
     my $path = locate_asset($asset{type}, $asset{name}, relative => 1);

commit 78a2da75e735bfaef7260e61e30b80a278446314

Fix sorting of needle table
This regression has been introduced with commit 2440dc9

lib/OpenQA/WebAPI/Controller/Admin/Needle.pm
@@ -66,6 +66,7 @@ sub ajax {
         my $column_index = $self->param("order[$index][column]") // @columns;
         my $column_order = $self->param("order[$index][dir]");
         last unless $column_index < @columns && grep { $column_order eq $_ } qw(asc desc);
+        push(@order_by_params, {'-' . $column_order => $columns[$column_index]});
         ++$index;
     }
     push(@order_by_params, {-asc => 'filename'}) unless @order_by_params;