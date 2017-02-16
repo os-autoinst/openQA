os-autoinst/openQA

commit f127654aaa0186038ea8afb571a193e402f7d4c2

Enhance jobs list in worker page (#1212)
* Don't show job ID
* Allow retriggering jobs

See https://progress.opensuse.org/issues/16316

assets/javascripts/admin_worker.js
@@ -1,30 +1,20 @@
 function setupWorkerNeedles() {
 
     var table = $('#previous_jobs').DataTable(
-        { "ajax": $('#previous_jobs').data('ajax-url'),
+        {ajax: $('#previous_jobs').data('ajax-url'),
             deferRender: true,
             "columns": [
-                {"data": "id"},
-                {"data": "name", "orderable": false},
-                {"data": "result"},
-                {"data": "created"}
+                {data: "name", orderable: false},
+                {data: "result_stats"},
+                {data: "finished"}
             ],
-            "processing": true,
-            "serverSide": true,
-            "order": [[3, "desc"], [0, "asc"]],
-            "columnDefs": [
-            {  "targets": [0],
-                "className": "time",
-                "render": function (data, type, row) {
-                    return "<a href=\"/tests/" + data + "\">" + data + "</a>";
-                }
-            },
-            {  "targets": [3],
-                "className": "time",
-                "render": function (data, type, row) {
-                    return jQuery.timeago(new Date(data));
-                }
-            }
+            processing: true,
+            serverSide: true,
+            order: [[2, "desc"]],
+            columnDefs: [
+                {targets: 0, render: renderTestName},
+                {targets: 1, render: renderTestResult},
+                {targets: 2, render: renderFinishedTimeAgo}
             ]
         });
 

assets/javascripts/tests.js
@@ -72,6 +72,14 @@ function renderTestName ( data, type, row ) {
     }
 }
 
+function renderFinishedTimeAgo(data, type, row) {
+    if(type === 'display') {
+        return data ? jQuery.timeago(data) : 'not finished yet';
+    } else {
+        return data ? data : 0;
+    }
+}
+
 function renderTestResult( data, type, row ) {
     if (type === 'display') {
         var html = '';
@@ -154,12 +162,7 @@ function renderTestsList(jobs) {
               "render": renderTestName
             },
             { targets: 3,
-              "render": function ( data, type, row ) {
-                  if (type === 'display')
-                      return jQuery.timeago(data + " UTC");
-                  else
-                      return data;
-              }
+              "render": renderFinishedTimeAgo
             },
             { targets: 2,
               "render": renderTestResult

lib/OpenQA/WebAPI/Controller/Test.pm
@@ -129,37 +129,25 @@ sub list_ajax {
         })->all;
     # need to use all as the order is too complex for a cursor
     for my $job (@jobs) {
-        # job dependencies
-        my %deps = (
-            parents  => {Chained => [], Parallel => []},
-            children => {Chained => [], Parallel => []});
-        my $jp = $job->parents;
-        while (my $s = $jp->next) {
-            push(@{$deps{parents}->{$s->to_string}}, $s->parent_job_id);
-        }
-        my $jc = $job->children;
-        while (my $s = $jc->next) {
-            push(@{$deps{children}->{$s->to_string}}, $s->child_job_id);
-        }
-
-        my $data = {
-            DT_RowId     => "job_" . $job->id,
-            id           => $job->id,
-            result_stats => $stats->{$job->id},
-            deps         => \%deps,
-            clone        => $job->clone_id,
-            test         => $job->TEST . "@" . ($job->MACHINE // ''),
-            distri  => $job->DISTRI  // '',
-            version => $job->VERSION // '',
-            flavor  => $job->FLAVOR  // '',
-            arch    => $job->ARCH    // '',
-            build   => $job->BUILD   // '',
-            testtime => $job->t_finished,
-            result   => $job->result,
-            group    => $job->group_id,
-            state    => $job->state
-        };
-        push @list, $data;
+        push(
+            @list,
+            {
+                DT_RowId     => "job_" . $job->id,
+                id           => $job->id,
+                result_stats => $stats->{$job->id},
+                deps         => $job->dependencies,
+                clone        => $job->clone_id,
+                test         => $job->TEST . "@" . ($job->MACHINE // ''),
+                distri  => $job->DISTRI  // '',
+                version => $job->VERSION // '',
+                flavor  => $job->FLAVOR  // '',
+                arch    => $job->ARCH    // '',
+                build   => $job->BUILD   // '',
+                testtime => $job->t_finished,
+                result   => $job->result,
+                group    => $job->group_id,
+                state    => $job->state
+            });
     }
     $self->render(json => {data => \@list});
 }

lib/OpenQA/Schema/Result/Jobs.pm
@@ -1537,6 +1537,27 @@ sub cancel {
     return $count;
 }
 
+sub dependencies {
+    my ($self) = @_;
+
+    my %parents  = (Chained => [], Parallel => []);
+    my %children = (Chained => [], Parallel => []);
+
+    my $jp = $self->parents;
+    while (my $s = $jp->next) {
+        push(@{${parents}{$s->to_string}}, $s->parent_job_id);
+    }
+    my $jc = $self->children;
+    while (my $s = $jc->next) {
+        push(@{${children}{$s->to_string}}, $s->child_job_id);
+    }
+
+    return {
+        parents  => \%parents,
+        children => \%children
+    };
+}
+
 1;
 
 # vim: set sw=4 et:

lib/OpenQA/WebAPI/Controller/Admin/Workers.pm
@@ -63,7 +63,7 @@ sub previous_jobs_ajax {
     my $total_count = $worker->previous_jobs->count;
 
     # Parameter for order
-    my @columns = qw(id id result t_created);
+    my @columns = qw(id result t_finished);
     my @order_by_params;
     my $index = 0;
     while (1) {
@@ -73,29 +73,37 @@ sub previous_jobs_ajax {
         push(@order_by_params, {'-' . $column_order => $columns[$column_index]});
         ++$index;
     }
-    my $params = {order_by => \@order_by_params};
+    my %params = (order_by => \@order_by_params);
 
     # Determine number of needles with all filters applied except paging
-    my $filtered_count = $worker->previous_jobs({}, $params)->count;
+    my $filtered_count = $worker->previous_jobs({}, \%params)->count;
 
     # Parameter for paging
     my $first_row = $self->param('start');
-    $params->{offset} = $first_row if $first_row;
+    $params{offset} = $first_row if $first_row;
     my $row_limit = $self->param('length');
-    $params->{rows} = $row_limit if $row_limit;
+    $params{rows} = $row_limit if $row_limit;
+    $params{prefetch} = [qw(children parents)];
 
-    my $jobs = $worker->previous_jobs({}, $params);
+    my @jobs = $worker->previous_jobs({}, \%params)->all;
+    my @ids = map { $_->id } @jobs;
+    my $stats = OpenQA::Schema::Result::JobModules::job_module_stats(\@ids);
 
     my @data;
     my %modules;
-    while (my $j = $jobs->next) {
-        my $hash = {
-            id      => $j->id,
-            name    => $j->name,
-            result  => $j->result,
-            created => $j->t_created->datetime() . 'Z'
-        };
-        push(@data, $hash);
+    for my $job (@jobs) {
+        push(
+            @data,
+            {
+                id           => $job->id,
+                name         => $job->name,
+                deps         => $job->dependencies,
+                result       => $job->result,
+                result_stats => $stats->{$job->id},
+                state        => $job->state,
+                clone        => $job->clone_id,
+                finished     => $job->t_finished ? $job->t_finished->datetime() . 'Z' : undef,
+            });
     }
     $self->render(
         json => {

t/ui/15-admin-workers.t
@@ -26,7 +26,7 @@ use Test::More;
 use Test::Mojo;
 use Test::Warnings;
 use OpenQA::Test::Case;
-use Data::Dumper;
+use Date::Format 'time2str';
 
 use OpenQA::WebSockets;
 use OpenQA::Scheduler;
@@ -89,14 +89,15 @@ wait_for_ajax;
 my $table = $driver->find_element_by_id('previous_jobs');
 ok($table, 'previous jobs table found');
 my @entries = map { $_->get_text() } $driver->find_child_elements($table, 'tbody/tr/td', 'xpath');
-is(scalar @entries, 8, 'two previous jobs shown (4 cols per row)');
+is(scalar @entries, 6, 'two previous jobs shown (3 cols per row)');
 is_deeply(
     \@entries,
     [
-        '99926',      'opensuse-Factory-staging_e-x86_64-Build87.5011-minimalx@32bit',
-        'incomplete', 'about 2 hours ago',
-        '99928',      'opensuse-13.1-DVD-i586-Build0091-RAID1@32bit',
-        'none',       'about 2 hours ago',
+        'opensuse-Factory-staging_e-x86_64-Build87.5011-minimalx@32bit',
+        '0',
+        'about an hour ago',
+        'opensuse-13.1-DVD-i586-Build0091-RAID1@32bit',
+        '', 'not finished yet',
     ],
     'correct entries shown'
 );

templates/admin/workers/show.html.ep
@@ -2,6 +2,8 @@
 % title "Worker " . $worker->{name};
 
 % content_for 'ready_function' => begin
+    is_operator = <%= (is_operator) ? 'true' : 'false' %>;
+    restart_url = "<%= url_for("apiv1_restart", jobid => 'REPLACEIT') %>";
     $('.timeago').timeago();
     setupResultButtons();
     setupWorkerNeedles();
@@ -37,12 +39,12 @@
     </table>
 
     <h3>Previous jobs</h3>
-    <table id="previous_jobs" class="table table-striped" data-ajax-url="<%= url_for('admin_worker_previous_jobs_ajax', worker_id => $worker->{id}) %>">
+    <table id="previous_jobs" class="table table-striped"
+      data-ajax-url="<%= url_for('admin_worker_previous_jobs_ajax', worker_id => $worker->{id}) %>">
         <thead>
-            <th>Id</th>
-            <th>Name</th>
+            <th>Test</th>
             <th>Result</th>
-            <th>Created</th>
+            <th>Finished</th>
         </thead>
     </table>
 </div>

commit a02bd07f6a5fa8f9802a8f9134b37dac36296d99

Fix time display regression introduced by f127654

assets/javascripts/admin_worker.js
@@ -14,7 +14,7 @@ function setupWorkerNeedles() {
             columnDefs: [
                 {targets: 0, render: renderTestName},
                 {targets: 1, render: renderTestResult},
-                {targets: 2, render: renderFinishedTimeAgo}
+                {targets: 2, render: renderTimeAgo}
             ]
         });
 

assets/javascripts/tests.js
@@ -72,7 +72,7 @@ function renderTestName ( data, type, row ) {
     }
 }
 
-function renderFinishedTimeAgo(data, type, row) {
+function renderTimeAgo(data, type, row) {
     if(type === 'display') {
         return data ? jQuery.timeago(data) : 'not finished yet';
     } else {
@@ -162,7 +162,7 @@ function renderTestsList(jobs) {
               "render": renderTestName
             },
             { targets: 3,
-              "render": renderFinishedTimeAgo
+              "render": renderTimeAgo
             },
             { targets: 2,
               "render": renderTestResult

lib/OpenQA/WebAPI/Controller/Test.pm
@@ -143,7 +143,7 @@ sub list_ajax {
                 flavor  => $job->FLAVOR  // '',
                 arch    => $job->ARCH    // '',
                 build   => $job->BUILD   // '',
-                testtime => $job->t_finished,
+                testtime => $job->t_finished . 'Z',
                 result   => $job->result,
                 group    => $job->group_id,
                 state    => $job->state