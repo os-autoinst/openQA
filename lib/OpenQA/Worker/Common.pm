d91357027a05ca6b43b662b5606f35d67b2f798f
Santiago Zarate <santiago.zarate@suse.com>
2017-03-15 11:06:53 +0100

    Allow api_call to be called without callback
    To allow the caching to work correctly, the timers need to be avoided
    when using api_call, since they are added to the event loop and by the
    nature of how the caching is working (blocking), the event loop will
    only keep running after the cache has finished downloading the assets.

--- lib/OpenQA/Worker/Common.pm
+++ lib/OpenQA/Worker/Common.pm
@@ -202,61 +202,67 @@ sub api_call {
 
     my $tx = $ua->build_tx(@args);
     my $cb;
-    $cb = sub {
-        my ($ua, $tx, $tries) = @_;
-        if ($tx->success && $tx->success->json) {
-            my $res = $tx->success->json;
-            return $callback->($res);
-        }
-        elsif ($ignore_errors) {
-            return $callback->();
-        }
-
-        # handle error case
-        --$tries;
-        my $err = $tx->error;
-        my $msg;
-
-        # format error message for log
-        if ($tx->res && $tx->res->json) {
-            # JSON API might provide error message
-            $msg = $tx->res->json->{error};
-        }
-        $msg //= $err->{message};
-        if ($err->{code}) {
-            $msg = "$err->{code} response: $err->{message}";
-            if ($err->{code} == 404) {
-                # don't retry on 404 errors (in this case we can't expect different
-                # results on further attempts)
-                $tries = 0;
+    if ($callback eq "no") {
+        $ua->start($tx);
+    }
+    else {
+        $cb = sub {
+            my ($ua, $tx, $tries) = @_;
+            if ($tx->success && $tx->success->json) {
+                my $res = $tx->success->json;
+                return $callback->($res);
             }
-        }
-        else {
-            $msg = "Connection error: $err->{message}";
-        }
-        OpenQA::Utils::log_error($msg . " (remaining tries: $tries)");
+            elsif ($ignore_errors) {
+                return $callback->();
+            }
 
-        if (!$tries) {
-            # abort the current job, we're in trouble - but keep running to grab the next
-            OpenQA::Worker::Jobs::stop_job('api-failure');
-            $hosts->{$host}{workerid} = undef;
-            $host = undef;
-            add_timer('register_worker', 10, \&register_worker, 1);
-            $callback->();
-            return;
-        }
+            # handle error case
+            --$tries;
+            my $err = $tx->error;
+            my $msg;
 
-        $tx = $ua->build_tx(@args);
-        add_timer(
-            '', 5,
-            sub {
-                $ua->start($tx => sub { $cb->(@_, $tries) });
-            },
-            1
-        );
-    };
-    $ua->start($tx => sub { $cb->(@_, $tries) });
+            # format error message for log
+            if ($tx->res && $tx->res->json) {
+                # JSON API might provide error message
+                $msg = $tx->res->json->{error};
+            }
+            $msg //= $err->{message};
+            if ($err->{code}) {
+                $msg = "$err->{code} response: $err->{message}";
+                if ($err->{code} == 404) {
+                    # don't retry on 404 errors (in this case we can't expect different
+                    # results on further attempts)
+                    $tries = 0;
+                }
+            }
+            else {
+                $msg = "Connection error: $err->{message}";
+            }
+            OpenQA::Utils::log_error($msg . " (remaining tries: $tries)");
+
+            if (!$tries) {
+                # abort the current job, we're in trouble - but keep running to grab the next
+                OpenQA::Worker::Jobs::stop_job('api-failure');
+                $hosts->{$host}{workerid} = undef;
+                $host = undef;
+                add_timer('register_worker', 10, \&register_worker, 1);
+                $callback->();
+                return;
+            }
+
+            $tx = $ua->build_tx(@args);
+            add_timer(
+                '', 5,
+                sub {
+                    $ua->start($tx => sub { $cb->(@_, $tries) });
+                },
+                1
+            );
+        };
+        $ua->start($tx => sub { $cb->(@_, $tries) });
+    }
 }
+
 
 sub ws_call {
     my ($type, $data) = @_;
