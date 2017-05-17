13f97916fe61552becd19692477d019463b6666e
Lukas Krause <lkrause@suse.de>
2017-05-17 13:03:48 +0200

    Fix navbar to not hide if dropdown selected

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -44,19 +44,21 @@ function hideNavbar() {
     var navbarHeight = $(".navbar").outerHeight();
     navbarHeight -= 30;
 
-    if ($('#filter-fullscreen').is(':checked')){
+    if ($('#filter-fullscreen').is(':checked')) {
         $("#content").attr('id', 'content_fullscreen');
         $(".navbar").addClass('hidden');
         $(".footer").addClass('hidden');
         $(".jumbotron").addClass('hidden');
         document.addEventListener('mousemove', function(e){
-          mouseY = e.clientY || e.pageY;
+            mouseY = e.clientY || e.pageY;
             if (mouseY < navbarHeight) {
-              $(".navbar").removeClass('hidden').addClass('show');
+                $(".navbar").removeClass('hidden').addClass('show');
             }
             else if (mouseY > navbarHeight) {
-              $(".navbar").removeClass('show').addClass('hidden');
+                if (!$("li").hasClass('dropdown open')) {
+                    $(".navbar").removeClass('show').addClass('hidden');
+                }
             }
         }, false);
-      }
+    }
 };
