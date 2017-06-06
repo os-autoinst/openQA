c18aaa3fac73c735b4a3d20a506024c5dcd3d8ee
Ettore Di Giacinto <edigiacinto@suse.com>
2017-06-06 09:48:00 +0200

    Extend sorting by time to parent group overview
    See related progress issue: https://progress.opensuse.org/issues/18082

--- lib/OpenQA/Schema.pm
+++ lib/OpenQA/Schema.pm
@@ -30,7 +30,7 @@ use OpenQA::Utils ();
 
 # after bumping the version please look at the instructions in the docs/Contributing.asciidoc file
 # on what scripts should be run and how
-our $VERSION   = 55;
+our $VERSION   = 56;
 our @databases = qw(SQLite PostgreSQL);
 
 __PACKAGE__->load_namespaces;
