afa0d770b43e5697c49dff836125827237185ee7
Lukas Krause <lkrause@suse.de>
2017-07-12 15:07:30 +0200

    Add prototyp of the feature jquery code

--- assets/javascripts/feature.js
+++ assets/javascripts/feature.js
@@ -1,1 +1,77 @@
-TODO
+function newFeature() {
+
+    var version = "";
+    var currentFeature = 1;
+
+    var feature01 = new Tour({
+        //Enable to save progress local
+        storage: false
+    });
+
+    feature01.addSteps([
+        {
+          element: ".jumbotron",
+          title: "Interested in new features?",
+          content: "Just click next to see what's new in openQA",
+          placement: "bottom",
+          backdrop: false,
+          template: changeTemplate(),
+        },
+        {
+          element: "#filter-fullscreen",
+          title: "Full screen mode",
+          content: "Check out the new full screen view option",
+          placement: "top",
+          backdrop: false,
+          onShown: function(){return endTour()}
+        }
+    ]);
+
+    function changeTemplate() {
+        return ("<div class='popover tour'>"+
+                "<div class='arrow'></div>"+
+                "<h3 class='popover-title'></h3>"+
+                "<div class='popover-content'></div>"+
+                "<div class='popover-navigation'>"+
+                "<div class='checkbox'><label><input type='checkbox'>Don't show again</label></div>"+
+                "<button class='btn btn-default' data-role='prev'>« Prev</button>"+
+                "<span data-role='separator'>|</span>"+
+                "<button class='btn btn-default' data-role='next'>Next »</button>"+
+                "<button class='btn btn-default' data-role='end'>End tour</button>"+
+                "</div>"+
+                "</div>")
+    };
+
+    function checkVersion() {
+        $.ajax({
+            dataType: "json",
+            url: '/api/v1/feature',
+            method: 'GET',
+            success: function(data){
+                getResult(data);
+            }
+        });
+    };
+
+    function getResult(data, version) {
+        version = data.version;
+    };
+
+    if (currentFeature > version) {
+        //Initialize the tour
+        feature01.init();
+        //Start the tour
+        feature01.start();
+    };
+
+    function endTour(currentFeature) {
+        $.ajax({
+            url: '/api/v1/feature',
+            method: 'POST',
+            data:
+                {
+                  'version' : currentFeature,
+                }
+        });
+    };
+};
