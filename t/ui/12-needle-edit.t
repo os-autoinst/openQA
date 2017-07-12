os-autoinst/openQA

commit 6ce9f3accf1a8370b28d369ac406ec663a9f9615

Show exclude areas in needle editor always
Previously exclude areas were only shown and taken
over when 'take matches' was checked.

See https://progress.opensuse.org/issues/16058

assets/javascripts/needleeditor.js
@@ -294,9 +294,15 @@ function loadTagsAndName() {
 function loadAreas() {
   var needle = window.needles[$('#area_select option:selected').val()];
   if ($('#take_matches').prop('checked')) {
-    nEditor.LoadAreas(needle['matches']);
+    var matchesAndExcludes = needle.matches.slice();
+    $.each(needle.area, function(index, area) {
+      if (area.type === 'exclude') {
+        matchesAndExcludes.push(area);
+      }
+    });
+    nEditor.LoadAreas(matchesAndExcludes);
   } else {
-    nEditor.LoadAreas(needle['area']);
+    nEditor.LoadAreas(needle.area);
   }
 }
 

t/ui/12-needle-edit.t
@@ -130,22 +130,23 @@ sub editpage_layout_check() {
     $elem            = $driver->find_element_by_id('needleeditor_textarea');
     $decode_textarea = decode_json($elem->get_value());
     # the value already defined in $default_json
-    is(@{$decode_textarea->{area}},           1,       'exclude is not present when take matches selected');
-    is($decode_textarea->{area}[0]->{xpos},   0,       'xpos correct');
-    is($decode_textarea->{area}[0]->{ypos},   0,       'ypos correct');
-    is($decode_textarea->{area}[0]->{width},  384,     'width correct');
-    is($decode_textarea->{area}[0]->{height}, 217,     'height correct');
-    is($decode_textarea->{area}[0]->{type},   'match', 'type correct');
-
-    # unselect "take matches": exclude area should show up
-    $driver->find_element_by_id('take_matches')->click();
-    $decode_textarea = decode_json($elem->get_value());
-    is(@{$decode_textarea->{area}},           2,         'both areas present when take matches not selected');
+    is(@{$decode_textarea->{area}},           2,         'exclude areas always present');
+    is($decode_textarea->{area}[0]->{xpos},   0,         'xpos correct');
+    is($decode_textarea->{area}[0]->{ypos},   0,         'ypos correct');
+    is($decode_textarea->{area}[0]->{width},  384,       'width correct');
+    is($decode_textarea->{area}[0]->{height}, 217,       'height correct');
+    is($decode_textarea->{area}[0]->{type},   'match',   'type correct');
     is($decode_textarea->{area}[1]->{xpos},   175,       'xpos correct');
     is($decode_textarea->{area}[1]->{ypos},   45,        'ypos correct');
     is($decode_textarea->{area}[1]->{width},  160,       'width correct');
     is($decode_textarea->{area}[1]->{height}, 60,        'height correct');
     is($decode_textarea->{area}[1]->{type},   'exclude', 'type correct');
+
+    # toggling 'take matches' has no effect
+    $driver->find_element_by_xpath('//input[@value="inst-timezone"]')->click();
+    is(@{decode_json($elem->get_value())->{area}}, 2, 'exclude areas always present');
+    $driver->find_element_by_xpath('//input[@value="inst-timezone"]')->click();
+    is(@{decode_json($elem->get_value())->{area}}, 2, 'no duplicated exclude areas present');
 }
 
 sub add_needle_tag(;$) {

commit d9af0996fa5809c37d2341fd3494bfd46784c577

Fix saving needle when taking matches
* Regression introduced in 6ce9f3accf1a8370b28d369ac406ec663a9f9615
* See https://progress.opensuse.org/issues/17666

assets/javascripts/needleeditor.js
@@ -294,13 +294,17 @@ function loadTagsAndName() {
 function loadAreas() {
   var needle = window.needles[$('#area_select option:selected').val()];
   if ($('#take_matches').prop('checked')) {
-    var matchesAndExcludes = needle.matches.slice();
-    $.each(needle.area, function(index, area) {
-      if (area.type === 'exclude') {
-        matchesAndExcludes.push(area);
-      }
-    });
-    nEditor.LoadAreas(matchesAndExcludes);
+    // merge exclude areas into matches if not done yet
+    var matches = needle.matches;
+    if (!matches.hasIncludes) {
+      $.each(needle.area, function(index, area) {
+        if (area.type === 'exclude') {
+          matches.push(area);
+        }
+      });
+      matches.hasIncludes = true;
+    }
+    nEditor.LoadAreas(needle.matches);
   } else {
     nEditor.LoadAreas(needle.area);
   }

t/ui/12-needle-edit.t
@@ -332,17 +332,34 @@ my $xoffset = my $yoffset = 200;
 change_needle_value($xoffset, $yoffset);    # xoffset and yoffset 200 for new area
 overwrite_needle($needlename);
 
-subtest 'Saving needle without taking matches' => sub {
-    $driver->get('/tests/99938/modules/logpackages/steps/1/edit');
-    $driver->find_element_by_id('tag_ENV-DESKTOP-kde')->click();
-    $driver->find_element_by_id('take_matches')->click();
-    create_needle(20, 50);
-    $driver->find_element_by_id('save')->click();
+# load needle editor for 'logpackages-before-package-selection', removing animation from modal again
+$driver->get('/tests/99938/modules/logpackages/steps/1/edit');
+$driver->execute_script('$(\'#modal-overwrite\').removeClass(\'fade\');');
+
+sub check_flash_for_saving_logpackages {
+    wait_for_ajax();
     like(
         $driver->find_element('#flash-messages span')->get_text(),
         qr/Needle logpackages-before-package-selection-\d{8} created\/updated - restart job/,
         'highlight appears correct'
     );
+    $driver->find_element('#flash-messages .close')->click();
+}
+
+subtest 'Saving needle when "taking matches" selected but no matches present' => sub {
+    $driver->find_element_by_id('tag_ENV-DESKTOP-kde')->click();
+    create_needle(100, 120);
+    $driver->find_element_by_id('save')->click();
+    check_flash_for_saving_logpackages();
+};
+
+subtest 'Saving needle when "taking matches" not selected' => sub {
+    $driver->find_element_by_id('take_matches')->click();
+    create_needle(200, 220);
+    $driver->find_element_by_id('save')->click();
+    wait_for_ajax();
+    $driver->find_element_by_id('modal-overwrite-confirm')->click();
+    check_flash_for_saving_logpackages();
 };
 
 # parse new needle json