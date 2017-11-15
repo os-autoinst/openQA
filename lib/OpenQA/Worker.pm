166e7e491130bc1adca7ac8ca409fd6726244f79
Santiago Zarate <santiago.zarate@suse.com>
2017-11-15 12:30:20 +0100

    Print project directory upon worker's init failure
    Ensure that upon failure to setup the worker's project dir (due to
    missing directory) print what where the attempts via debug, and
    send to the error the reason for the failure.

--- lib/OpenQA/Worker.pm
+++ lib/OpenQA/Worker.pm
@@ -70,11 +70,12 @@ sub main {
         my @dirs = ($host_settings->{$h}{SHARE_DIRECTORY}, catdir($OpenQA::Utils::prjdir, 'share'));
         ($dir) = grep { $_ && -d } @dirs;
         unless ($dir) {
-            log_error("Can not find working directory for host $h. Ignoring host");
+            map { log_debug("Found possible working directory for $h: $_") if $_ } @dirs;
+            log_error("Ignoring host '$h': Working directory does not exist.");
             next;
         }
 
-        log_debug("Using dir $dir for host $h");
+        log_info("Project dir for host $h is $dir");
         Mojo::IOLoop->next_tick(
             sub {
                 OpenQA::Worker::Common::register_worker($h, $dir, $host_settings->{$h}{TESTPOOLSERVER}, $shared_cache);
