c06358b093accfc868305b9383458acf7d141d24
Marius Kittler <mkittler@suse.de>
2017-11-21 13:52:34 +0100

    Show asset size

--- assets/javascripts/assets.js
+++ assets/javascripts/assets.js
@@ -2,17 +2,36 @@ function setup_asset_table()
 {
     $('#assets').DataTable(
         {
-            "columnDefs": [
+            columnDefs: [
                 { targets: 3,
-                  "render": function ( data, type, row ) {
+                  render: function(data, type, row) {
                       if (type === 'display') {
                           return jQuery.timeago(data + "Z");
-                      } else
-                          return data;
+                      }
+                      return data;
+                  }
+                },
+                { targets: 4,
+                  render: function(data, type, row) {
+                      if (type === 'display') {
+                          var unitFactor = 1073741824; // one GiB
+                          var dataWithUnit;
+                          $.each([' GiB', ' MiB', ' KiB', ' byte'], function(index, unit) {
+                              if (!unitFactor || data >= unitFactor) {
+                                  dataWithUnit = (data / unitFactor) + unit;
+                                  return false;
+                              }
+                              unitFactor >>= 10;
+                          });
+                          if (dataWithUnit) {
+                            return dataWithUnit;
+                          }
+                      }
+                      return data;
                   }
                 }
             ],
-            "order": [[3, 'desc']]
+            order: [[3, 'desc']]
         }
     );
 }
