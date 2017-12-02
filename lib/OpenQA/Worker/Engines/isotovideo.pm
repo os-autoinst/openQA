os-autoinst/openQA

commit 00108e5813187e9616612130d4174b1947df59b0

Refactor to allow CACHEDIRECTORY to replace share directory
Changes applied in this commit will alow the openQA to replace
share directory settings comming from the webUI with the CACHEDIRECTORY
value combined with the host, so multiple webUIs are supported now.

Assets still need to get a random filename, but they're at least separately
stored in the db as per webUI now.

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -23,6 +23,8 @@ use OpenQA::Utils qw(locate_asset log_error log_info log_debug);
 use POSIX qw(:sys_wait_h strftime uname);
 use JSON 'to_json';
 use Fcntl;
+use File::Spec::Functions 'catdir';
+use File::Basename;
 use Errno;
 use Cwd 'abs_path';
 use OpenQA::Cache;
@@ -68,9 +70,28 @@ sub cache_assets {
     #cache ISO and HDD
     foreach my $this_asset (sort grep { /^(ISO|HDD)[_]*\d?$/ } keys %{$job->{settings}}) {
         log_debug("Found $this_asset, caching " . $job->{settings}->{$this_asset});
-        $job->{settings}{$this_asset} =~ s/share\///;
-        get_asset($job, $this_asset, $job->{settings}{$this_asset});
+        my $asset = get_asset($job, $this_asset, $job->{settings}{$this_asset});
+        symlink($asset, basename($asset)) or die "cannot create link: $asset, $pooldir";
     }
+}
+
+sub cache_tests {
+
+    my ($shared_cache, $testpoolserver) = @_;
+    no autodie 'system';
+
+    my $start = time;
+    #Do an flock to ensure only one worker is trying to synchronize at a time.
+    my @cmd = qw(flock -E 999);
+    push @cmd, "$shared_cache/needleslock";
+    push @cmd, (qw(rsync -aP), $testpoolserver, qw(--delete --exclude=.git));
+    push @cmd, "$shared_cache/tests";
+
+    my $res = system(@cmd);
+
+    die "Failed to rsync tests: '$@' " if $res;
+    log_debug(sprintf("RSYNC: Synchronization of tests directory took %.2f seconds", time - $start));
+    return $shared_cache;
 }
 
 sub engine_workit($) {
@@ -98,50 +119,6 @@ sub engine_workit($) {
         close $fh;
     }
 
-    for my $isokey (qw(ISO), map { "ISO_$_" } (1 .. 9)) {
-        if (my $isoname = $job->{settings}->{$isokey}) {
-            my $iso = locate_asset('iso', $isoname, mustexist => 1);
-            unless ($iso) {
-                my $error = "Cannot find ISO asset $isoname!";
-                return {error => $error};
-            }
-            $job->{settings}->{$isokey} = $iso;
-        }
-    }
-
-    for my $otherkey (qw(KERNEL INITRD)) {
-        if (my $filename = $job->{settings}->{$otherkey}) {
-            my $file = locate_asset('other', $filename, mustexist => 1);
-            unless ($file) {
-                my $error = "Cannot find OTHER asset $filename!";
-                return {error => $error};
-            }
-            $job->{settings}->{$otherkey} = $file;
-        }
-    }
-
-    my $nd = $job->{settings}->{NUMDISKS} || 2;
-    for my $i (1 .. $nd) {
-        my $hddname = $job->{settings}->{"HDD_$i"};
-        if ($hddname) {
-            my $hdd = locate_asset('hdd', $hddname, mustexist => 1);
-            unless ($hdd) {
-                my $error = "Cannot find HDD asset $hddname!";
-                return {error => $error};
-            }
-            $job->{settings}->{"HDD_$i"} = $hdd;
-        }
-    }
-
-    # for now the only condition to enable syncing is $hosts->{$current_host}{dir}
-    if (!-e $hosts->{$current_host}{dir}) {
-        OpenQA::Cache::init($current_host);
-        cache_assets($job);
-    }
-    else {
-        log_info("CACHE: share directory found, asset caching is not enabled");
-    }
-
     # pass worker instance and worker id to isotovideo
     # both used to create unique MAC and TAP devices if needed
     # workerid is also used by libvirt backend to identify VMs
@@ -153,10 +130,31 @@ sub engine_workit($) {
         $vars{$k} = $v;
     }
 
+    $vars{PRJDIR} = $OpenQA::Utils::sharedir;
+    my $shared_cache;
+
+    # for now the only condition to enable syncing is $hosts->{$current_host}{dir}
+    if ($worker_settings->{CACHEDIRECTORY} && $hosts->{$current_host}{testpoolserver}) {
+        my $host_to_cache = Mojo::URL->new($current_host)->host;
+        $shared_cache = catdir($worker_settings->{CACHEDIRECTORY}, $host_to_cache);
+        $vars{PRJDIR} = $shared_cache;
+        OpenQA::Cache::init($current_host, $worker_settings->{CACHEDIRECTORY});
+        cache_assets($job);
+        cache_tests($shared_cache, $hosts->{$current_host}{testpoolserver});
+        $shared_cache = catdir($shared_cache, 'tests');
+
+    }
+    else {
+        locate_local_assets();
+        log_info("CACHE: share directory found, asset caching is not enabled");
+    }
+
+
+
     $vars{ASSETDIR}   = $OpenQA::Utils::assetdir;
-    $vars{PRJDIR}     = $OpenQA::Utils::sharedir;
-    $vars{CASEDIR}    = OpenQA::Utils::testcasedir($vars{DISTRI}, $vars{VERSION});
-    $vars{PRODUCTDIR} = OpenQA::Utils::productdir($vars{DISTRI}, $vars{VERSION});
+    $vars{CASEDIR}    = OpenQA::Utils::testcasedir($vars{DISTRI}, $vars{VERSION}, $shared_cache);
+    $vars{PRODUCTDIR} = OpenQA::Utils::productdir($vars{DISTRI}, $vars{VERSION}, $shared_cache);
+
     _save_vars(\%vars);
 
     # os-autoinst's commands server
@@ -191,6 +189,43 @@ sub engine_workit($) {
         return {pid => $child};
     }
 
+}
+
+sub locate_local_assets {
+    for my $isokey (qw(ISO), map { "ISO_$_" } (1 .. 9)) {
+        if (my $isoname = $job->{settings}->{$isokey}) {
+            my $iso = locate_asset('iso', $isoname, mustexist => 1);
+            unless ($iso) {
+                my $error = "Cannot find ISO asset $isoname!";
+                return {error => $error};
+            }
+            $job->{settings}->{$isokey} = $iso;
+        }
+    }
+
+    for my $otherkey (qw(KERNEL INITRD)) {
+        if (my $filename = $job->{settings}->{$otherkey}) {
+            my $file = locate_asset('other', $filename, mustexist => 1);
+            unless ($file) {
+                my $error = "Cannot find OTHER asset $filename!";
+                return {error => $error};
+            }
+            $job->{settings}->{$otherkey} = $file;
+        }
+    }
+
+    my $nd = $job->{settings}->{NUMDISKS} || 2;
+    for my $i (1 .. $nd) {
+        my $hddname = $job->{settings}->{"HDD_$i"};
+        if ($hddname) {
+            my $hdd = locate_asset('hdd', $hddname, mustexist =>);
+            unless ($hdd) {
+                my $error = "Cannot find HDD asset $hddname!";
+                return {error => $error};
+            }
+            $job->{settings}->{"HDD_$i"} = $hdd;
+        }
+    }
 }
 
 sub engine_check {

lib/OpenQA/Cache.pm
@@ -8,6 +8,7 @@ use Mojo::UserAgent;
 use OpenQA::Utils qw(log_error log_info log_debug);
 use OpenQA::Worker::Common;
 use List::MoreUtils;
+use File::Spec::Functions 'catdir';
 use Data::Dumper;
 use JSON;
 
@@ -18,6 +19,7 @@ our (@ISA, @EXPORT);
 
 my $cache;
 my $host;
+my $location;
 my $limit   = 50;
 my $remove  = 1;
 my $db_file = "../cache.db";
@@ -33,14 +35,14 @@ sub get {
 sub set {
     my ($asset) = @_;
     unshift @{$cache->{$host}}, $asset;
-    expire_asset();
+    get($asset);
 }
 
 sub init {
     my $class;
-    $host = shift;
+    ($host, $location) = @_;
     @{$cache->{$host}} = read_db();
-    log_debug(__PACKAGE__ . ": Initialized with $host");
+    log_debug(__PACKAGE__ . ": Initialized with $host at $location");
 }
 
 sub update_setup_status {
@@ -93,16 +95,20 @@ sub download_asset {
 
     $tx = $ua->start($tx);
     print $log "CACHE: " . basename($asset) . " download sucessful";
+    $asset = $tx->res->content->asset->move_to($asset);
     close($log);
-    $tx->res->content->asset->move_to($asset);
+    return $asset;
 
 }
 
 sub get_asset {
     my ($job, $asset_type, $asset) = @_;
     my $type;
+
     # repo, kernel, and initrd should be also accepted here.
     $asset_type = (split /^(ISO|HDD)/, $asset_type)[1];
+    $asset = catdir($location, $asset);
+
     while () {
         read_db();
         log_debug "CACHE: Aquiring lock";
@@ -134,6 +140,7 @@ sub get_asset {
 
     unlink($asset . ".lock") or log_debug("Lock file for " . basename($asset) . " is not present");
     log_debug "$type for: " . basename($asset);
+    return $asset;
 }
 
 sub purge_cache {

lib/OpenQA/Utils.pm
@@ -92,23 +92,22 @@ our $app;
 # testrepository name" and "old/new folder structure" within the
 # testrepository.
 sub productdir {
-    my ($distri, $version) = @_;
+    my ($distri, $version, $rootfortests) = @_;
 
-    my $dir = testcasedir($distri, $version);
+    my $dir = testcasedir($distri, $version, $rootfortests);
     return $dir . "/products/$distri" if -e "$dir/products/$distri";
     return $dir;
 }
 
 sub testcasedir {
-    my ($distri, $version) = @_;
-
-    my @dirs = (catdir($prjdir, 'share', 'tests', $distri), catdir($prjdir, 'tests', $distri));
-    if ($version) {
-        unshift @dirs, catdir($prjdir, 'share', 'tests', "$distri-$version");
+    my ($distri, $version, $rootfortests) = @_;
+    for my $dir (catdir($prjdir, 'share', 'tests'), catdir($prjdir, 'tests')) {
+        $rootfortests ||= $dir if -d $dir;
     }
     # TODO actually "distri" is misused here. It should rather be something
     # like the name of the repository with all tests
-    my ($dir) = grep { -d } @dirs;
+    my $dir = catdir($rootfortests, $distri);
+    $dir .= "-$version" if $version && -e "$dir-$version";
     return $dir;
 }
 

lib/OpenQA/Worker.pm
@@ -31,42 +31,56 @@ use OpenQA::Worker::Pool qw(lockit clean_pool);
 use OpenQA::Worker::Jobs;
 
 sub init {
-    my ($worker_options, $host_settings, %options) = @_;
-    $worker_settings = $worker_options;
-    $instance        = $options{instance} if defined $options{instance};
-    $pooldir         = $OpenQA::Utils::prjdir . '/pool/' . $instance;
-    $nocleanup       = $options{"no-cleanup"};
-    $verbose         = $options{verbose} if defined $options{verbose};
+    my ($host_settings, $options) = @_;
+    $instance  = $options->{instance} if defined $options->{instance};
+    $pooldir   = $OpenQA::Utils::prjdir . '/pool/' . $instance;
+    $nocleanup = $options->{"no-cleanup"};
+    $verbose   = $options->{verbose} if defined $options->{verbose};
 
-    OpenQA::Worker::Common::api_init($host_settings, \%options);
-    OpenQA::Worker::Engines::isotovideo::set_engine_exec($options{isotovideo}) if $options{isotovideo};
+    OpenQA::Worker::Common::api_init($host_settings, $options);
+    OpenQA::Worker::Engines::isotovideo::set_engine_exec($options->{isotovideo}) if $options->{isotovideo};
 }
 
 sub main {
     my ($host_settings) = @_;
     my $lockfd = lockit();
-
+    my $dir;
     clean_pool();
     ## register worker at startup to all webuis
     for my $h (@{$host_settings->{HOSTS}}) {
         # check if host`s working directory exists
-        my @dirs = (
-            $host_settings->{$h}{SHARE_DIRECTORY},
-            catdir($OpenQA::Utils::prjdir, $h),
-            catdir($OpenQA::Utils::prjdir, 'share'));
-        my ($dir) = grep { $_ && -d } @dirs;
-        unless ($dir) {
-            log_error("Can not find working directory for host $h. Ignoring host");
-            next;
+        # if caching is not enabled
+
+        if ($host_settings->{$h}{TESTPOOLSERVER}) {
+            $dir = prepare_cache_directory($h, $worker_settings->{CACHEDIRECTORY});
         }
+        else {
+            my @dirs = ($host_settings->{$h}{SHARE_DIRECTORY}, catdir($OpenQA::Utils::prjdir, 'share'));
+            ($dir) = grep { $_ && -d } @dirs;
+            unless ($dir) {
+                log_error("Can not find working directory for host $h. Ignoring host");
+                next;
+            }
+        }
+
         log_debug("Using dir $dir for host $h") if $verbose;
-        Mojo::IOLoop->next_tick(sub { OpenQA::Worker::Common::register_worker($h, $dir) });
+        Mojo::IOLoop->next_tick(
+            sub { OpenQA::Worker::Common::register_worker($h, $dir, $host_settings->{$h}{TESTPOOLSERVER}) });
     }
 
     # start event loop - this will block until stop is called
     Mojo::IOLoop->start;
 
     return 0;
+}
+
+sub prepare_cache_directory {
+    my ($current_host, $cachedirectory) = @_;
+    my $host_to_cache = Mojo::URL->new($current_host)->host;
+    my $shared_cache = File::Spec->catdir($cachedirectory, $host_to_cache);
+    File::Path::make_path($shared_cache) if (!-e $shared_cache);
+    log_info("CACHE: caching is enabled, setting up $shared_cache");
+    return $shared_cache;
 }
 
 sub catch_exit {

lib/OpenQA/Worker/Common.pm
@@ -139,9 +139,7 @@ sub get_timer {
 ## prepare UA and URL for OpenQA-scheduler connection
 sub api_init {
     my ($host_settings, $options) = @_;
-    my @hosts = @{$host_settings->{HOSTS}};
-
-    for my $host (@hosts) {
+    for my $host (@{$host_settings->{HOSTS}}) {
         my ($ua, $url);
         if ($host !~ '/') {
             $url = Mojo::URL->new->scheme('http')->host($host);
@@ -391,7 +389,7 @@ sub call_websocket {
 }
 
 sub register_worker {
-    my ($host, $dir) = @_;
+    my ($host, $dir, $testpoolserver) = @_;
     die unless $host;
 
     $worker_caps             = _get_capabilities;
@@ -437,6 +435,7 @@ sub register_worker {
         }
         return;
     }
+    $hosts->{$host}{testpoolserver} = $testpoolserver;
     my $newid    = $tx->success->json->{id};
     my $workerid = $hosts->{$host}{workerid};
     my $ws       = $hosts->{$host}{ws};

script/worker
@@ -86,7 +86,6 @@ L<OpenQA::Client>
 use strict;
 use warnings;
 
-
 BEGIN {
     #prepare for large files
     $ENV{MOJO_MAX_MESSAGE_SIZE}   = 1024 * 1024 * 1024 * 20;
@@ -106,6 +105,7 @@ use Getopt::Long;
 Getopt::Long::Configure("no_ignore_case");
 
 use OpenQA::Worker;
+use OpenQA::Worker::Common;
 
 my %options;
 
@@ -161,12 +161,12 @@ sub read_worker_config {
 $options{instance} ||= 1;
 
 my ($worker_settings, $host_settings) = read_worker_config($options{instance}, $options{host});
-
+$OpenQA::Worker::Common::worker_settings = $worker_settings;
 # XXX: this should be sent to the scheduler to be included in the worker's table
 $ENV{QEMUPORT} = ($options{instance}) * 10 + 20002;
 $ENV{VNC}      = ($options{instance}) + 90;
 
-OpenQA::Worker::init($worker_settings, $host_settings, %options);
+OpenQA::Worker::init($host_settings, \%options);
 OpenQA::Worker::main($host_settings);
 
 # vim: set sw=4 sts=4 et:

commit 2ae3d9fdff5ddf9d815da195003145dfcb3eadf0

Fix caching of assets without caching of tests
This got broken by 00108e58 . Since then, if CACHEDIRECTORY is
set but TESTPOOLSERVER is not, asset caching will not happen,
because *all* the caching code is inside this conditional which
checks for both.

This should make things work right if you want to cache assets
but not tests.

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -155,38 +155,42 @@ sub engine_workit {
 
     my $assetkeys = detect_asset_keys(\%vars);
 
-    # for now the only condition to enable syncing is $hosts->{$current_host}{dir}
-    if ($worker_settings->{CACHEDIRECTORY} && $hosts->{$current_host}{testpoolserver}) {
+    # do asset caching if CACHEDIRECTORY is set
+    if ($worker_settings->{CACHEDIRECTORY}) {
         my $host_to_cache = Mojo::URL->new($current_host)->host;
-        $shared_cache = catdir($worker_settings->{CACHEDIRECTORY}, $host_to_cache);
-        $vars{PRJDIR} = $shared_cache;
         OpenQA::Worker::Cache::init($current_host, $worker_settings->{CACHEDIRECTORY});
         my $error = cache_assets(\%vars, $assetkeys);
         return $error if $error;
 
-        # my attempts to use ioloop::subprocess failed, so go back to blocking
-        my $sync_child = fork();
-        if (!$sync_child) {
-            cache_tests($shared_cache, $hosts->{$current_host}{testpoolserver});
+        # do test caching if TESTPOOLSERVER is set
+        if ($hosts->{$current_host}{testpoolserver}) {
+            # my attempts to use ioloop::subprocess failed, so go back to blocking
+            my $sync_child = fork();
+            if (!$sync_child) {
+                cache_tests($shared_cache, $hosts->{$current_host}{testpoolserver});
+            }
+            else {
+                my $last_update = time;
+                while (waitpid($sync_child, WNOHANG) == 0) {
+                    log_info "Waiting for subprocess";
+                    if (time - $last_update > 5) {    # do not spam the webui
+                        update_setup_status;
+                        $last_update = time;
+                    }
+                    sleep .5;
+                }
+                if ($?) {
+                    return {error => "Failed to rsync tests: exit $?"};
+                }
+            }
+            $vars{PRJDIR} = catdir($worker_settings->{CACHEDIRECTORY}, $host_to_cache);
+            $shared_cache = catdir($vars{PRJDIR}, 'tests');
         }
         else {
-            my $last_update = time;
-            while (waitpid($sync_child, WNOHANG) == 0) {
-                log_info "Waiting for subprocess";
-                if (time - $last_update > 5) {    # do not spam the webui
-                    update_setup_status;
-                    $last_update = time;
-                }
-                sleep .5;
-            }
-            if ($?) {
-                return {error => "Failed to rsync tests: exit $?"};
-            }
+            $vars{PRJDIR} = $OpenQA::Utils::sharedir;
         }
-        $shared_cache = catdir($shared_cache, 'tests');
     }
     else {
-        $vars{PRJDIR} = $OpenQA::Utils::sharedir;
         my $error = locate_local_assets(\%vars, $assetkeys);
         return $error if $error;
     }