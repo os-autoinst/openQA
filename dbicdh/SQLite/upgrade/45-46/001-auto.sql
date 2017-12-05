40bdcd902ce8535001200d8b8858d6c773a9f99d
Stephan Kulow <coolo@suse.de>
2017-12-05 06:27:31 +0100

    Remove SQL files for SQlite and for 2016

--- dbicdh/SQLite/upgrade/45-46/001-auto.sql
+++
@@ -1,5 +0,0 @@
--- Convert schema '/space/prod/openQA/script/../dbicdh/_source/deploy/45/001-auto.yml' to '/space/prod/openQA/script/../dbicdh/_source/deploy/46/001-auto.yml':;
-
-;
--- No differences found;
-
