5a0cb6c2c53e1f40d1d6a0a1e5885ac24392c793
Marius Kittler <mkittler@suse.de>
2018-03-19 16:29:14 +0100

    Simplify save_needle_ajax code

--- lib/OpenQA/WebAPI/Controller/Step.pm
+++ lib/OpenQA/WebAPI/Controller/Step.pm
@@ -449,6 +449,7 @@ sub save_needle_ajax {
     my ($self) = @_;
     return 0 unless $self->init();
 
+    # validate parameter
     my $validation = $self->validation;
     $validation->required('json');
     $validation->required('imagename')->like(qr/^[^.\/][^\/]{3,}\.png$/);
@@ -456,7 +457,6 @@ sub save_needle_ajax {
     $validation->optional('imageversion')->like(qr/^(?!.*([.])\1+).*$/);
     $validation->optional('imageversion')->like(qr/^[^\/]+$/);
     $validation->required('needlename')->like(qr/^[^.\/][^\/]{3,}$/);
-
     if ($validation->has_error) {
         my $error = 'wrong parameters:';
         for my $k (qw(json imagename imagedistri imageversion needlename)) {
@@ -466,6 +466,7 @@ sub save_needle_ajax {
         return $self->render(json => {error => "Error creating/updating needle: $error"});
     }
 
+    # read parameter
     my $job          = find_job($self, $self->param('testid')) or return;
     my $distribution = $job->DISTRI;
     my $dversion     = $job->VERSION || '';
@@ -477,6 +478,7 @@ sub save_needle_ajax {
     my $needlename   = $validation->param('needlename');
     my $needledir    = needledir($job->DISTRI, $job->VERSION);
 
+    # read JSON data
     my $json_data;
     eval { $json_data = $self->_json_validation($json); };
     if ($@) {
@@ -485,6 +487,7 @@ sub save_needle_ajax {
         return $self->render(json => {error => $message});
     }
 
+    # determine imagepath
     my $success = 1;
     my $imagepath;
     if ($imagedir) {
@@ -501,19 +504,19 @@ sub save_needle_ajax {
         return $self->render(json => {error => "Image $imagename could not be found!"});
     }
 
-    my $baseneedle = "$needledir/$needlename";
     # do not overwrite the exist needle if disallow to overwrite
+    my $baseneedle = "$needledir/$needlename";
     if (-e "$baseneedle.png" && !$self->param('overwrite')) {
         $success = 0;
         my $returned_data = $self->req->params->to_hash;
         $returned_data->{requires_overwrite} = 1;
         return $self->render(json => $returned_data);
     }
-    unless ($imagepath eq "$baseneedle.png") {
-        unless (copy($imagepath, "$baseneedle.png")) {
-            $self->app->log->error("Copy $imagepath -> $baseneedle.png failed: $!");
-            $success = 0;
-        }
+
+    # copy image
+    if (!($imagepath eq "$baseneedle.png") && !copy($imagepath, "$baseneedle.png")) {
+        $self->app->log->error("Copy $imagepath -> $baseneedle.png failed: $!");
+        $success = 0;
     }
     if ($success) {
         open(my $J, ">", "$baseneedle.json") or $success = 0;
@@ -525,39 +528,34 @@ sub save_needle_ajax {
             $self->app->log->error("Writing needle $baseneedle.json failed: $!");
         }
     }
-
-    if ($success) {
-        $self->app->gru->enqueue('scan_needles');
-        if (($self->app->config->{global}->{scm} || '') eq 'git') {
-            if ($needledir && -d "$needledir/.git") {
-                try {
-                    $self->_commit_git($job, $needledir, $needlename);
-                }
-                catch {
-                    $self->app->log->error($_);
-                    return $self->render(json => {error => $_});
-                };
-            }
-            else {
-                return $self->render(json => {error => "$needledir is not a git repo"});
-            }
-        }
-        $self->emit_event('openqa_needle_modify',
-            {needle => "$baseneedle.png", tags => $json_data->{tags}, update => 0});
-        my $info = {info => "Needle $needlename created/updated"};
-        if ($job->worker_id && $job->worker->get_property('INTERACTIVE')) {
-            $info->{interactive_job} = $job->id;
-        }
-        if ($job->can_be_duplicated) {
-            $info->{restart} = $self->url_for('apiv1_restart', jobid => $job->id);
-        }
-        return $self->render(json => $info);
-    }
-    else {
+    if (!$success) {
         return $self->render(json => {error => "Error creating/updating needle: $!."});
     }
-    # not reached
-    return;
+
+    # commit needle in Git repository
+    $self->app->gru->enqueue('scan_needles');
+    if (($self->app->config->{global}->{scm} || '') eq 'git') {
+        if (!$needledir || !(-d "$needledir/.git")) {
+            return $self->render(json => {error => "$needledir is not a git repo"});
+        }
+        try {
+            $self->_commit_git($job, $needledir, $needlename);
+        }
+        catch {
+            $self->app->log->error($_);
+            return $self->render(json => {error => $_});
+        };
+    }
+
+    $self->emit_event('openqa_needle_modify', {needle => "$baseneedle.png", tags => $json_data->{tags}, update => 0});
+    my $info = {info => "Needle $needlename created/updated"};
+    if ($job->worker_id && $job->worker->get_property('INTERACTIVE')) {
+        $info->{interactive_job} = $job->id;
+    }
+    if ($job->can_be_duplicated) {
+        $info->{restart} = $self->url_for('apiv1_restart', jobid => $job->id);
+    }
+    return $self->render(json => $info);
 }
 
 sub map_error_to_avg {
