aa52c8fde71b3298306b610c7e6c87dd8e3a66a5
Marius Kittler <mkittler@suse.de>
2018-05-23 17:33:11 +0200

    Add web socket console for isotovideo commands
    * Accessible via /tests/:testid/developer/ws-console
      (admin-only)
    * See
        * https://progress.opensuse.org/issues/36442#note-2
        * https://progress.opensuse.org/issues/36454

---
+++ assets/javascripts/ws_console.js
@@ -0,0 +1,48 @@
+function setupWebSocketContosle(url) {
+    var form = $('#ws_console_form');
+    var url = form.data('url');
+    if (!url.length) {
+        return;
+    }
+
+    var msg = $('#msg');
+    var log = $('#log');
+    var followLogCheckBox = $('#follow_log');
+
+    // setup logging
+    var followLog = function() {
+        if (!followLogCheckBox.prop('checked')) {
+            return;
+        }
+        log[0].scrollTop = log[0].scrollHeight;
+    };
+    followLogCheckBox.change(followLog);
+    var logLine = function(msg) {
+        log.append(msg + "\n");
+        followLog();
+    };
+
+    // establish and handle web socket connection
+    var ws = new WebSocket(url);
+    logLine('Connecting to ' + url);
+    ws.onopen = function () {
+        logLine('Connection opened');
+    };
+    ws.onerror = function(error) {
+        logLine('Connection error: ' + error.type + " (check JavaScript console for details)");
+    }
+    ws.onclose = function() {
+        logLine('Connection closed');
+    }
+    ws.onmessage = function(msg) {
+        logLine(msg.data);
+    };
+
+    // send command when user presses return
+    form.submit(function(event) {
+        event.preventDefault();
+        ws.send(msg.val());
+        msg.val('');
+    });
+    msg.focus();
+}
