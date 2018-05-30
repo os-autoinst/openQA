540e9b3a6f9d11f7af7b0c3657bb68a3126a1f6a
Marius Kittler <mkittler@suse.de>
2018-05-30 16:12:20 +0200

    Make the websocket proxy route basically work
    * Establish web socket connection to os-autoinst
      command server
    * Allow to passing and receive commands to/from
      os-autoinst command server
    
    still TODO:
    * Implement session management
    * Validate commands before passing to os-autoinst
    * Handle timeouts
    * Integrate development mode into the real GUI

--- lib/OpenQA/WebAPI/Controller/Developer.pm
+++ lib/OpenQA/WebAPI/Controller/Developer.pm
@@ -16,9 +16,12 @@
 
 package OpenQA::WebAPI::Controller::Developer;
 use strict;
+use Try::Tiny;
+use Mojo::URL;
 use Mojo::Base 'Mojolicious::Controller';
 use OpenQA::Utils;
 use OpenQA::Schema::Result::Jobs;
+use JSON qw(decode_json);
 
 # returns the isotovideo web socket URL for the given job or undef if not available
 sub determine_web_socket_url {
@@ -67,29 +70,53 @@ sub ws_console {
 # provides a web socket connection acting as a proxy to interact with os-autoinst indirectly
 sub ws_proxy {
     my ($self) = @_;
-    my $job  = $self->find_job()     or return $self->reply->not_found;
-    my $user = $self->current_user() or return $self->reply->not_found;
-    my $app  = $self->app;
-    my $client_id = sprintf '%s', $self->tx;
 
-    $app->log->debug('Client connected: ' . $client_id);
+    # determine basic variables
+    my $job  = $self->find_current_job() or return $self->reply->not_found;
+    my $user = $self->current_user()     or return $self->reply->not_found;
+    my $app  = $self->app;
+
+    $app->log->debug('ws_proxy: client connected: ' . $user->name);
+
+    # define variables for transactions
+    my $java_script_tx = $self->tx;    # for connection from browser/JavaScript to this server
+    my $cmd_srv_tx;                    # for connection from this server to os-autoinst command server
 
     # TODO: register development session, ensure only one development session is opened per job
     # my $development_sessions = $app->schema->resultset('DevelopmentSessions');
     # ...
 
-    # TODO: define functions to push information to the JavaScript client
+    # define functions to push information to the JavaScript client
     my $send_message_to_java_script = sub {
-
+        my ($type, $what, $data) = @_;
+        $java_script_tx->send(
+            {
+                json => {
+                    type => $type,
+                    what => $what,
+                    data => $data,
+                }});
+        OpenQA::Utils::log_debug("ws_proxy: $type: $what");
     };
-    my $close_connection_to_java_script = sub {
-
+    my $quit_development_session = sub {
+        $java_script_tx->finish();
+        # TODO: unregister development session
     };
 
-    # start opening a websocket connection to os-autoinst
-    my $cmd_srv_url        = Mojo::URL::new(determine_web_socket_url($job));
-    my $cmd_srv_tx;
-    my $connect_to_cmd_srv = sub {
+    # determine url to os-autoinst command server
+    my $cmd_srv_raw_url = determine_web_socket_url($job);
+    if (!$cmd_srv_raw_url) {
+        $app->log->debug('ws_proxy: attempt to open for job ' . $job->name . ' (' . $job->id . ')');
+        $send_message_to_java_script->(error => 'os-autoinst command server not available, job is likely not running');
+        $quit_development_session->();
+    }
+
+    # define function to start a websocket connection to os-autoinst for this developer session
+    my $cmd_srv_url = Mojo::URL->new($cmd_srv_raw_url);
+    my $connect_to_cmd_srv;
+    $connect_to_cmd_srv = sub {
+        $send_message_to_java_script->(info => 'connecting to os-autuinst command server at ' . $cmd_srv_raw_url);
+
         return $app->ua->websocket(
             $cmd_srv_url => {'Sec-WebSocket-Extensions' => 'permessage-deflate'} => sub {
                 my ($ua, $tx) = @_;
@@ -99,47 +126,84 @@ sub ws_proxy {
                 if (!$tx->is_websocket) {
                     my $location_header = ($tx->completed ? $tx->res->headers->location : undef);
                     if (!$location_header) {
-                        $send_message_to_java_script->('Unable to upgrade ws to command server');
+                        $send_message_to_java_script->(error => 'unable to upgrade ws to command server');
                         return;
                     }
-                    OpenQA::Utils::log_debug('ws_proxy: Following ws redirection to: ' . $location_header);
-                    $cmd_srv_url = $ua_url->parse($cmd_srv_url);
+                    OpenQA::Utils::log_debug('ws_proxy: following ws redirection to: ' . $location_header);
+                    $cmd_srv_url = $cmd_srv_url->parse($location_header);
                     $connect_to_cmd_srv->();
                     return;
                 }
 
-                # TODO: handle messages from os-autoinst command server
+                # handle messages from os-autoinst command server
+                $send_message_to_java_script->(info => 'connected to os-autoinst command server');
                 $tx->on(
                     json => sub {
                         my ($tx, $json) = @_;
+                        $send_message_to_java_script->(info => 'cmdsrvmsg', $json);
                     });
 
-                # TODO: handle connection to os-autoinst command server being quit
+                # handle connection to os-autoinst command server being quit
                 $tx->on(
                     finish => sub {
                         my (undef, $code, $reason) = @_;
+                        # inform the JavaScript client
+                        $send_message_to_java_script->(
+                            error => 'connection to os-autoinst command server lost',
+                            {
+                                reason => $reason,
+                                code   => $code,
+                            });
+                        # quit the development session; the user can just reopen the session to try again
+                        $quit_development_session->();
                     });
             });
     };
 
-    # TODO: define functions to push information to the os-autoinst command server
+    # start opening a websocket connection to os-autoinst instantly
+    $connect_to_cmd_srv->();
+
+    # define function to push information to the os-autoinst command server
     my $send_message_to_os_autoinst = sub {
-
-    };
-    my $close_connection_to_os_autoinst = sub {
-
+        my ($msg) = @_;
+        if (!$cmd_srv_tx) {
+            $send_message_to_java_script->(
+                error => 'failed to pass message to os-autoinst command server because not connected yet');
+            return;
+        }
+        $cmd_srv_tx->send({json => $msg});
     };
 
     # TODO: handle messages from the JavaScript
     $self->on(
         message => sub {
+            my ($tx, $msg) = @_;
+            try {
+                my $json = decode_json($msg);
+                # return a simple echo
+                $send_message_to_java_script->(info => 'echo', $json);
 
+                # TODO: handle some internal messages, eg. to quit the development session
+
+                # TODO: validate the messages before passing to command server
+                $send_message_to_os_autoinst->($json);
+            }
+            catch {
+                $send_message_to_java_script->(
+                    warning => 'ignoring invalid json',
+                    {
+                        msg => $msg,
+                    });
+            };
         });
 
     # TODO: handle development session being quit from the JavaScript-side
     $self->on(
         finish => sub {
-
+            $app->log->debug('ws_proxy: client disconnected: ' . $user->name);
+            # TODO: mark development session as inactive
+            # note: it is likely not useful to quit the development session instantly because the user
+            #       might just have pressed the reload button
         });
 }
 
