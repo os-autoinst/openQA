3a96a5b261e80d0474a46f6163db0cc35580cb6d
Marius Kittler <mkittler@suse.de>
2018-06-15 16:54:03 +0200

    Let the web socket console automatically reconnect

--- assets/javascripts/ws_console.js
+++ assets/javascripts/ws_console.js
@@ -1,3 +1,38 @@
+function followLog() {
+    if (!document.followLogCheckBox.prop('checked')) {
+        return;
+    }
+    document.logElement[0].scrollTop = document.logElement[0].scrollHeight;
+}
+
+function logLine(msg) {
+    document.logElement.append(msg + "\n");
+    followLog();
+}
+
+function establishWebSocketConnection() {
+    var ws = new WebSocket(window.wsUrl);
+    logLine('Connecting to ' + window.wsUrl);
+    ws.onopen = function () {
+        logLine('Connection opened');
+        window.ws = ws;
+        ws.send('{"cmd":"status"}');
+    };
+    ws.onerror = function(error) {
+        logLine('Connection error: ' + error.type + " (check JavaScript console for details)");
+    }
+    ws.onclose = function() {
+        logLine('Connection closed, trying to reconnect in 2 seconds');
+        window.ws = undefined;
+        setTimeout(function() {
+            establishWebSocketConnection();
+        }, 2000);
+    }
+    ws.onmessage = function(msg) {
+        logLine(msg.data);
+    };
+}
+
 function setupWebSocketContosle(url) {
     var form = $('#ws_console_form');
     var url = form.data('url');
@@ -19,43 +54,21 @@ function setupWebSocketContosle(url) {
         url = 'ws://' + window.location.hostname + port + (url.indexOf('/') !== 0 ? '/' : '') + url;
     }
 
-    var msg = $('#msg');
-    var log = $('#log');
-    var followLogCheckBox = $('#follow_log');
-
-    // setup logging
-    var followLog = function() {
-        if (!followLogCheckBox.prop('checked')) {
-            return;
-        }
-        log[0].scrollTop = log[0].scrollHeight;
-    };
-    followLogCheckBox.change(followLog);
-    var logLine = function(msg) {
-        log.append(msg + "\n");
-        followLog();
-    };
-
     // establish and handle web socket connection
-    var ws = new WebSocket(url);
-    logLine('Connecting to ' + url);
-    ws.onopen = function () {
-        logLine('Connection opened');
-    };
-    ws.onerror = function(error) {
-        logLine('Connection error: ' + error.type + " (check JavaScript console for details)");
-    }
-    ws.onclose = function() {
-        logLine('Connection closed');
-    }
-    ws.onmessage = function(msg) {
-        logLine(msg.data);
-    };
+    window.wsUrl = url;
+    document.logElement = $('#log');
+    document.followLogCheckBox = $('#follow_log');
+    establishWebSocketConnection();
 
     // send command when user presses return
+    var msg = $('#msg');
     form.submit(function(event) {
+        if (!window.ws) {
+            logLine("Can't send command, no ws connection opened!");
+            return;
+        }
         event.preventDefault();
-        ws.send(msg.val());
+        window.ws.send(msg.val());
         msg.val('');
     });
     msg.focus();
