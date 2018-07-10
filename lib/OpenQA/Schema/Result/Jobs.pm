fe6344f292edd1c4e536543f40e8db2b3b7fab36
Stephan Kulow <coolo@suse.de>
2018-07-10 08:27:49 +0200

    Fix blocked_by for chained parallel jobs

--- lib/OpenQA/Schema/Result/Jobs.pm
+++ lib/OpenQA/Schema/Result/Jobs.pm
@@ -1817,10 +1817,10 @@ sub search_for {
 sub blocked_by_parent_job {
     my ($self) = @_;
 
-    my $parents = $self->parents->search(
+    my $parents = $self->result_source->schema->resultset('JobDependencies')->search(
         {
-            dependency => OpenQA::Schema::Result::JobDependencies->CHAINED,
-        });
+            dependency   => OpenQA::Schema::Result::JobDependencies->CHAINED,
+            child_job_id => {-in => [keys %{$self->cluster_jobs}]}});
     while (my $pd = $parents->next) {
         my $p     = $pd->parent;
         my $state = $p->state;
