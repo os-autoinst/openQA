8310815eefb3f3c9115e05e6762f703197028739
Stephan Kulow <coolo@suse.de>
2018-07-11 07:06:30 +0200

    blocked_by calculation: Don't block on self
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- lib/OpenQA/Schema/Result/Jobs.pm
+++ lib/OpenQA/Schema/Result/Jobs.pm
@@ -1834,8 +1834,9 @@ sub blocked_by_parent_job {
 
     my $parents = $self->result_source->schema->resultset('JobDependencies')->search(
         {
-            dependency   => OpenQA::Schema::Result::JobDependencies->CHAINED,
-            child_job_id => {-in => [keys %{$self->cluster_jobs}]}});
+            dependency    => OpenQA::Schema::Result::JobDependencies->CHAINED,
+            parent_job_id => {'!=' => $self->id},
+            child_job_id  => {-in => [keys %{$self->cluster_jobs}]}});
     while (my $pd = $parents->next) {
         my $p     = $pd->parent;
         my $state = $p->state;
