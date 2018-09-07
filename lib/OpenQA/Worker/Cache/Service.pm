ebb1307ad3c8a37b70a8ce11b20453896b7ee02f
Ettore Di Giacinto <edigiacinto@suse.com>
2018-09-07 12:46:48 +0200

    Create separate class for Minion Server and worker

---
+++ lib/OpenQA/Worker/Cache/Service.pm
@@ -0,0 +1,87 @@
+# Copyright (C) 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package OpenQA::Worker::Cache::Service;
+
+use Mojolicious::Lite;
+use OpenQA::Worker::Cache::Task::Asset;
+use Mojolicious::Plugin::Minion;
+
+use constant ASSET_STATUS_PROCESSED   => 001;
+use constant ASSET_STATUS_ENQUEUED    => 002;
+use constant ASSET_STATUS_DOWNLOADING => 003;
+use constant ASSET_STATUS_IGNORE      => 004;
+use constant ASSET_STATUS_ERROR       => 005;
+use constant SESSION_TOKEN            => int(rand(999999999999));
+
+plugin 'OpenQA::Worker::Cache::Task::Asset';
+
+get '/session_token' => sub {
+    my $c = shift;
+    $c->render(json => {session_token => SESSION_TOKEN()});
+};
+
+get '/info' => sub {
+    my $c = shift;
+    $c->render(json => $c->minion->stats);
+};
+
+sub _gen_guard_name { join('.', SESSION_TOKEN, pop) }
+
+sub _exists { !!defined $_[0] && exists $_[0]->{total} && $_[0]->{total} > 0 }
+
+sub enqueued { _exists(app->minion->backend->list_jobs(0, 1, {states => ['inactive'], notes => {token => shift}})) }
+
+sub active { _exists(app->minion->backend->list_jobs(0, 1, {states => ['active'], notes => {token => shift}})) }
+
+post '/download' => sub {
+    my $c = shift;
+
+    my $data = $c->req->json;
+
+    my $id    = $data->{'id'};
+    my $type  = $data->{'type'};
+    my $asset = $data->{'asset'};
+    my $host  = $data->{'host'};
+    # Specific error cases for missing fields
+    return $c->render(json => {status => ASSET_STATUS_ERROR, error => "No ID defined"})          unless defined $id;
+    return $c->render(json => {status => ASSET_STATUS_ERROR, error => "No Asset defined"})       unless defined $asset;
+    return $c->render(json => {status => ASSET_STATUS_ERROR, error => "No Asset  type defined"}) unless defined $type;
+    return $c->render(json => {status => ASSET_STATUS_ERROR, error => "No Host defined"})        unless defined $host;
+    app->log->debug("Requested: ID: $id Type: $type Asset: $asset Host: $host ");
+
+    my $lock = _gen_guard_name($asset);
+
+    return $c->render(json => {status => ASSET_STATUS_DOWNLOADING}) if active($lock);
+    return $c->render(json => {status => ASSET_STATUS_IGNORE})      if enqueued($lock);
+
+    $c->minion->enqueue(cache_asset => [$id, $type, $asset, $host] => {notes => {token => $lock}});
+    $c->render(json => {status => ASSET_STATUS_ENQUEUED});
+};
+
+get '/status/#asset' => sub {
+    my $c     = shift;
+    my $asset = $c->param('asset');
+    my $lock  = _gen_guard_name($asset);
+
+    return $c->render(json => {status => ASSET_STATUS_DOWNLOADING}) if active($lock);
+    return $c->render(json => {status => ASSET_STATUS_IGNORE})      if enqueued($lock);
+    $c->render(json => {status => ASSET_STATUS_PROCESSED});
+};
+
+# Start the Mojolicious command system
+app->start;
+
+!!42;
---
+++ lib/OpenQA/Worker/Cache/Task/Asset.pm
@@ -0,0 +1,65 @@
+# Copyright (C) 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package OpenQA::Worker::Cache::Task::Asset;
+
+use Mojo::Base 'Mojolicious::Plugin';
+use constant LOCK_RETRY_DELAY   => 30;
+use constant MINION_LOCK_EXPIRE => 999999999;
+
+use OpenQA::Worker::Cache;
+use OpenQA::Worker::Common;    # FIXME: This import needs to disappear.
+has ua => sub { Mojo::UserAgent->new };
+has cache => sub {
+    my ($worker_settings, undef) = OpenQA::Worker::Common::read_worker_config(undef, undef);
+    OpenQA::Worker::Cache->new(
+        host     => 'localhost',
+        location => ($ENV{CACHE_DIR} || $worker_settings->{CACHEDIRECTORY}));
+};
+
+#has token => sub { int(rand(999999999999)) };
+
+sub token {
+    shift->ua->get('http://localhost:3000/session_token')->result->json->{session_token};
+}
+
+sub _gen_guard_name { join('.', shift->token, pop) }
+
+sub register {
+    my ($self, $app) = @_;
+    $app->plugin(Minion => {SQLite => 'sqlite:' . $self->cache->db_file});
+
+    # Make it a helper
+    $app->helper(asset_task => sub { $self });
+
+    $app->minion->add_task(
+        cache_asset => sub {
+            my ($job, $id, $type, $asset_name, $host) = @_;
+            my $guard_name = $self->_gen_guard_name($asset_name);
+            return $job->remove unless defined $asset_name && defined $type && defined $host;
+
+            return $job->retry({delay => LOCK_RETRY_DELAY})
+              unless my $guard = $app->minion->guard($guard_name, MINION_LOCK_EXPIRE);
+            $app->log->debug("[$$] [Job #" . $job->id . "] Guard: $guard_name Download: $asset_name");
+
+            # Do the real download
+            $self->cache->host($host);
+            $self->cache->get_asset({id => $id}, $type, $asset_name);
+            $app->log->debug("[$$] [Job #" . $job->id . "] Finished");
+            $job->remove;
+        });
+}
+
+1;
