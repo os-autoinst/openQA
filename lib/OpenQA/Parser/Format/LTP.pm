4d8cf50a02ded9c1454d180632d4c66eaf556502
Sebastian Riedel <sri@cpan.org>
2018-11-23 15:30:22 +0100

    Convert a few cases back to Cpanel::JSON::XS that require special configurations (mostly for tests, but there might be underlying protocol requirements too)

--- lib/OpenQA/Parser/Format/LTP.pm
+++ lib/OpenQA/Parser/Format/LTP.pm
@@ -20,6 +20,7 @@ package OpenQA::Parser::Format::LTP;
 use Mojo::Base 'OpenQA::Parser::Format::JUnit';
 use Carp qw(croak confess);
 use Mojo::JSON 'from_json';
+use Cpanel::JSON::XS ();
 use Storable 'dclone';
 has include_results => 1;
 
@@ -29,7 +30,7 @@ sub _add_single_result { shift->results->add(OpenQA::Parser::Result::LTP::Test->
 sub parse {
     my ($self, $json) = @_;
     confess "No JSON given/loaded" unless $json;
-    my $decoded_json = from_json($json);
+    my $decoded_json = Cpanel::JSON::XS->new->utf8(0)->decode($json);
 
     # may be optional since format result_array:v2
     $self->generated_tests_extra->add(OpenQA::Parser::Result::LTP::Environment->new($decoded_json->{environment}))
