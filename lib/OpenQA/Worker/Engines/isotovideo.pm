os-autoinst/openQA

commit c973569911bd4f55265a99e5b83636ef8db8ec15

worker: Fix two perl warnings
Observed on power8:

```
Jan 17 20:23:54 power8 worker[15039]: Use of uninitialized value in concatenation (.) or string at /usr/share/openqa/script/../lib/OpenQA/Worker/Engines/isotovideo.pm line 245.
Jan 17 20:23:54 power8 worker[15039]: Use of uninitialized value $exit in numeric eq (==) at /usr/share/openqa/script/../lib/OpenQA/Worker/Engines/isotovideo.pm line 246.
```

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -242,8 +242,8 @@ sub engine_workit {
             sleep 5 and update_setup_status until $rsync_request->processed;
             my $exit = $rsync_request->result;
 
-            log_info("rsync: " . $rsync_request->output, channels => 'autoinst');
-            return {error => "Failed to rsync tests: exit " . $exit} unless $exit == 0;
+            if (my $output = $rsync_request->output) { log_info("rsync: " . $output, channels => 'autoinst') }
+            return {error => "Failed to rsync tests: exit " . $exit} unless ($exit && $exit == 0);
 
             $shared_cache = catdir($shared_cache, 'tests');
         }

commit 2a9fc00928ed7e965836888295699535f11d2bfa

Fix regression of test caching failing introduced by c9735699
The exit code of "0" must not be evaluated in boolean context in perl.

Related progress issue: https://progress.opensuse.org/issues/46658

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -243,7 +243,7 @@ sub engine_workit {
             my $exit = $rsync_request->result;
 
             if (my $output = $rsync_request->output) { log_info("rsync: " . $output, channels => 'autoinst') }
-            return {error => "Failed to rsync tests: exit " . $exit} unless ($exit && $exit == 0);
+            return {error => "Failed to rsync tests: exit " . $exit} unless (defined $exit && $exit == 0);
 
             $shared_cache = catdir($shared_cache, 'tests');
         }