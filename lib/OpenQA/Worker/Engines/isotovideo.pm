os-autoinst/openQA

commit c973569911bd4f55265a99e5b83636ef8db8ec15

worker: Fix two perl warnings
Observed on power8:

```
Jan 17 20:23:54 power8 worker[15039]: Use of uninitialized value in concatenation (.) or string at /usr/share/openqa/script/../lib/OpenQA/Worker/Engines/isotovideo.pm line 245.
Jan 17 20:23:54 power8 worker[15039]: Use of uninitialized value $exit in numeric eq (==) at /usr/share/openqa/script/../lib/OpenQA/Worker/Engines/isotovideo.pm line 246.
```

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -242,8 +242,8 @@ sub engine_workit {
             sleep 5 and update_setup_status until $rsync_request->processed;
             my $exit = $rsync_request->result;
 
-            log_info("rsync: " . $rsync_request->output, channels => 'autoinst');
-            return {error => "Failed to rsync tests: exit " . $exit} unless $exit == 0;
+            if (my $output = $rsync_request->output) { log_info("rsync: " . $output, channels => 'autoinst') }
+            return {error => "Failed to rsync tests: exit " . $exit} unless ($exit && $exit == 0);
 
             $shared_cache = catdir($shared_cache, 'tests');
         }

commit 4176c1a1edae205669cd1e240528832f06c72ea5

Fix another regression from c9735699 when no sync is needed
Related progress issue: https://progress.opensuse.org/issues/46658

lib/OpenQA/Worker/Engines/isotovideo.pm
@@ -240,10 +240,11 @@ sub engine_workit {
             return {error => "Failed to send rsync cache request"} unless $rsync_request->enqueue;
 
             sleep 5 and update_setup_status until $rsync_request->processed;
-            my $exit = $rsync_request->result;
+            # treat "no sync necessary" as success as well
+            my $exit = $rsync_request->result // 0;
 
             if (my $output = $rsync_request->output) { log_info("rsync: " . $output, channels => 'autoinst') }
-            return {error => "Failed to rsync tests: exit " . $exit} unless (defined $exit && $exit == 0);
+            return {error => "Failed to rsync tests: exit code: $exit"} unless (defined $exit && $exit == 0);
 
             $shared_cache = catdir($shared_cache, 'tests');
         }