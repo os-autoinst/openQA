334f53ac011ba2bda98a9d8db6be7e36cd284491
Sebastian Riedel <sri@cpan.org>
2019-02-01 11:54:33 +0100

    Remove SQL files for 2017

--- dbicdh/PostgreSQL/upgrade/49-50/001-auto.sql
+++
@@ -1,5 +0,0 @@
--- Convert schema '/home/openQA/openQA/script/../dbicdh/_source/deploy/49/001-auto.yml' to '/home/openQA/openQA/script/../dbicdh/_source/deploy/50/001-auto.yml':;
-
-;
--- No differences found;
-
