8c0a2b7e80f8fa33b384598cb103f74cab763f8e
Marius Kittler <mkittler@suse.de>
2019-03-07 16:17:58 +0100

    Log job group events via audit log

--- lib/OpenQA/WebAPI/Plugin/AuditLog.pm
+++ lib/OpenQA/WebAPI/Plugin/AuditLog.pm
@@ -23,7 +23,7 @@ use OpenQA::Events;
 my @table_events = qw(table_create table_update table_delete);
 my @job_events   = qw(job_create job_delete job_cancel job_duplicate job_restart jobs_restart job_update_result
   job_done job_grab job_cancel_by_settings);
-my @jobgroup_events    = qw(jobgroup_create jobgroup_connect);
+my @jobgroup_events    = qw(jobgroup_create jobgroup_update jobgroup_delete jobgroup_connect);
 my @jobtemplate_events = qw(jobtemplate_create jobtemplate_delete);
 my @user_events        = qw(user_update user_login user_new_comment user_update_comment user_delete_comment);
 my @asset_events       = qw(asset_register asset_delete);
