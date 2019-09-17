1a8f130aa61410d67e70f0dc483e6b34079088ab
Andrii Nikitin <anikitin@suse.de>
2019-09-17 17:26:21 +0200

    Add circleci

---
+++ .circleci/build_local_docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) 2019 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t localtest -f- "$thisdir"<<EOF
+FROM registry.opensuse.org/devel/openqa/ci/containers/base:latest
+
+RUN sudo zypper ar -f -p 80 https://download.opensuse.org/repositories/devel:/openQA:/Leap:/15.1/openSUSE_Leap_15.1 openQA
+RUN sudo zypper ar -f -p 81 http://download.opensuse.org/repositories/devel:openQA/openSUSE_Leap_15.1 devel
+
+RUN sudo zypper --gpg-auto-import-keys ref
+
+RUN sudo zypper -n install $(echo $(cat $thisdir/dependencies.txt | sed -e 's/\r//' |sort))
+
+COPY build_autoinst.sh .
+RUN sudo mkdir '../os-autoinst'
+RUN sudo chown 1000 '../os-autoinst'
+RUN pwd && ls -la
+RUN ./build_autoinst.sh '../os-autoinst' $(cat $thisdir/autoinst.sha)
+RUN sudo chown -R 1000 '/opt/testing_area'
+WORKDIR /opt/testing_area
+USER squamata
+EOF
