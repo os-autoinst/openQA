2041cb1af6a54828007d4cc4f06a8c355e1ec294
Andrii Nikitin <anikitin@suse.de>
2019-11-24 01:30:22 +0100

    Stabilize t/api/14-plugin_obs_rsync_async.t
    Control of timing of mock rsync.sh by presence of predefined file

--- t/data/openqa-trigger-from-obs/script/rsync.sh
+++ t/data/openqa-trigger-from-obs/script/rsync.sh
@@ -1,4 +1,15 @@
-[ "$1" != MockProjectLongProcessing ] || { echo "Sleeping $1"; sleep 4; }
-[ "$1" != MockProjectLongProcessing1 ] || { echo "Sleeping $1"; sleep 4; }
+#!/bin/bash
+
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function sleep_until_file_created {
+    for ((i = 0; i < 20; i += 1)); do
+        [ ! -f "$thisdir/.$1-ready" ] || return 0
+        sleep 1
+    done
+    return 1
+}
+
+[[ "$1" =~ MockProjectLongProcessing* ]] && { sleep_until_file_created "$1" || exit 1; }
 [ "$1" != MockProjectError ] || { >&2 echo "Mock Error";  exit 1; }
 echo MOCK OK $1
