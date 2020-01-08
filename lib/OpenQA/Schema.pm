9513e82145237581342257fdbdc2bbd788ecb13c
Marius Kittler <mkittler@suse.de>
2020-01-08 17:50:58 +0100

    Fix db migration for size_limit_db column of job_group_parents table
    * Add a new migration with the missing bits
    * Move the manual migration script to the new migration
    * Fix the YAML file for the previous migration for consistency and to
      be actually able to generate the new migration

--- lib/OpenQA/Schema.pm
+++ lib/OpenQA/Schema.pm
@@ -32,7 +32,7 @@ use OpenQA::Utils ();
 
 # after bumping the version please look at the instructions in the docs/Contributing.asciidoc file
 # on what scripts should be run and how
-our $VERSION = 85;
+our $VERSION = 86;
 
 __PACKAGE__->load_namespaces;
 
