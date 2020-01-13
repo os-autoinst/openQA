c6b4308b255adc60f1eee22f95c73a2c376ca564
Marius Kittler <mkittler@suse.de>
2020-01-13 14:46:42 +0100

    Test asset download of openqa-clone-job script
    * See https://progress.opensuse.org/issues/62021
    * Moves the asset and job download into the OpenQA::Scripts
      module to ease testing

--- lib/OpenQA/Script.pm
+++ lib/OpenQA/Script.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 SUSE LLC
+# Copyright (C) 2018-2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,10 +17,14 @@ package OpenQA::Script;
 use strict;
 use warnings;
 
+use Cpanel::JSON::XS;
 use Exporter 'import';
+use Mojo::File 'path';
 
 our @EXPORT = qw(
   clone_job_apply_settings
+  clone_job_get_job
+  clone_job_download_assets
 );
 
 use constant GLOBAL_SETTINGS => ('WORKER_CLASS');
@@ -59,6 +63,72 @@ sub clone_job_apply_settings {
         $settings->{$key} = $value;
         if (my $override = JOB_SETTING_OVERRIDES->{$key}) {
             delete $settings->{$override};
+        }
+    }
+}
+
+sub clone_job_get_job {
+    my ($jobid, $remote, $remote_url, $options) = @_;
+
+    my $job;
+    my $url = $remote_url->clone;
+    $url->path("jobs/$jobid");
+    my $tx = $remote->max_redirects(3)->get($url);
+    if (!$tx->error) {
+        if ($tx->res->code == 200) {
+            $job = $tx->res->json->{job};
+        }
+        else {
+            warn sprintf("unexpected return code: %s %s", $tx->res->code, $tx->res->message);
+            exit 1;
+        }
+    }
+    else {
+        my $err = $tx->error;
+        # there is no code for some error reasons, e.g. 'connection refused'
+        $err->{code} //= '';
+        warn "failed to get job '$jobid': $err->{code} $err->{message}";
+        exit 1;
+    }
+
+    print Cpanel::JSON::XS->new->pretty->encode($job) if $options->{verbose};
+    return $job;
+}
+
+sub clone_job_download_assets {
+    my ($jobid, $job, $remote, $remote_url, $ua, $options) = @_;
+    my @parents = map { clone_job_get_job($_, $remote, $remote_url, $options) } @{$job->{parents}->{Chained}};
+  ASSET:
+    for my $type (keys %{$job->{assets}}) {
+        next if $type eq 'repo';    # we can't download repos
+        for my $file (@{$job->{assets}->{$type}}) {
+            my $dst = $file;
+            # skip downloading published assets assuming we are also cloning
+            # the generation job or if the only cloned job *is* the generation
+            # job.
+            my $nr_parents = @parents;
+            if ((!$options->{'skip-deps'} && !$options->{'skip-chained-deps'}) || ($nr_parents == 0)) {
+                for my $j (@parents, $job) {
+                    next ASSET if $j->{settings}->{PUBLISH_HDD_1} && $file eq $j->{settings}->{PUBLISH_HDD_1};
+                }
+            }
+            $dst =~ s,.*/,,;
+            $dst = join('/', $options->{dir}, $type, $dst);
+            my $from = $remote_url->clone;
+            $from->path(sprintf '/tests/%d/asset/%s/%s', $jobid, $type, $file);
+            $from = $from->to_string;
+
+            die "can't write $options->{dir}/$type\n" unless -w "$options->{dir}/$type";
+
+            print "downloading\n$from\nto\n$dst\n";
+            my $r = $ua->mirror($from, $dst);
+            unless ($r->is_success || $r->code == 304) {
+                die "$jobid failed: ", $r->status_line, "\n";
+            }
+
+            # ensure the asset cleanup preserves the asset the configured amount of days starting from the time
+            # it has been cloned (otherwise old assets might be cleaned up directly again after cloning)
+            path($dst)->touch;
         }
     }
 }
