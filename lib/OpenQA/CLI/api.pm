45a4055c9adec5c4f9d6f57c6570cb50d9a85648
Sebastian Riedel <sri@cpan.org>
2020-03-24 17:15:59 +0100

    Use the new license header

--- lib/OpenQA/CLI/api.pm
+++ lib/OpenQA/CLI/api.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 SUSE Linux Products GmbH
+# Copyright (C) 2020 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,8 +11,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# with this program; if not, see <http://www.gnu.org/licenses/>.
 
 package OpenQA::CLI::api;
 use Mojo::Base 'OpenQA::Command';
