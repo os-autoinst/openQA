472b097a0d836848f65f5561eee0673fe47f5e0d
Christian Dywan <cdywan@suse.de>
2020-04-24 17:25:05 +0200

    Rename YAMLRenderer plugin to YAML
    - The plugin contains a renderer.
    - And, too, the helper for validation.
    - Other plugins have short base names as well.

--- lib/OpenQA/WebAPI/Plugin/YAML.pm
+++ lib/OpenQA/WebAPI/Plugin/YAML.pm
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
-package OpenQA::WebAPI::Plugin::YAMLRenderer;
+package OpenQA::WebAPI::Plugin::YAML;
 use Mojo::Base 'Mojolicious::Plugin';
 use OpenQA::YAML 'validate_data';
 
