os-autoinst/openQA

commit 5c9068a3d4431f0cc9d15f439403dbe4b23e1408

Extend client test for JSON/YAML output and error handling

lib/OpenQA/Client.pm
@@ -52,7 +52,8 @@ sub handle_result {
         my $body         = $res->body;
         if ($options->{'json-output'}) {
             if ($content_type =~ m{text/yaml}) {
-                print Cpanel::JSON::XS->new->pretty->encode(load_yaml(string => $body));
+                my $yaml = load_yaml(string => $body);
+                print Cpanel::JSON::XS->new->pretty->encode($yaml);
             }
             else {
                 print Cpanel::JSON::XS->new->allow_nonref->pretty->encode($json);
@@ -83,7 +84,7 @@ sub handle_result {
             dd($res->json || $res->body);
         }
     }
-    exit(1);
+    return 1;
 }
 
 # prepend the API-base if the specified path is relative
@@ -163,8 +164,8 @@ sub run {
     my $client
       = OpenQA::Client->new(apikey => $options->{apikey}, apisecret => $options->{apisecret}, api => $url->host);
 
-    exit handle_result($options, $client->$method($url, form => \%params)->res) if $options->{form};
-    exit handle_result($options,
+    return handle_result($options, $client->$method($url, form => \%params)->res) if $options->{form};
+    return handle_result($options,
         $client->$method($url, {'Content-Type' => 'application/json'} => $options->{'json-data'})->res)
       if $options->{'json-data'};
 
@@ -188,10 +189,10 @@ sub run {
         $url->query(Mojo::Parameters->new);
         $url->query(jobs => \@job_ids);
         print("$url\n");
-        handle_result($options, $client->post($url, {Accept => $accept_header})->res);
+        return handle_result($options, $client->post($url, {Accept => $accept_header})->res);
     }
     else {
-        handle_result($options, $client->$method($url, {Accept => $accept_header})->res);
+        return handle_result($options, $client->$method($url, {Accept => $accept_header})->res);
     }
 }
 

script/client
@@ -181,6 +181,6 @@ if ($options{form} && $options{'json-data'}) {
     exit(2);
 }
 
-OpenQA::Client::run(\%options, @ARGV);
+exit OpenQA::Client::run(\%options, @ARGV);
 
 1;

t/31-client.t
@@ -54,15 +54,40 @@ throws_ok sub { OpenQA::Client::run }, qr/Need \@args/, 'needs arguments parsed 
 my %options      = (verbose => 1);
 my $client_mock  = Test::MockModule->new('OpenQA::UserAgent');
 my $code         = 200;
-my $headers_mock = Test::MockObject->new()->set_always(content_type => 'application/json');
-my $code_mock    = Test::MockObject->new()->mock(code => sub { $code })->mock(headers => sub { $headers_mock })
-  ->set_always(json => 'my_json')->set_always(body => 'body');
+my $content_type = 'application/json';
+my $headers_mock = Test::MockObject->new()->set_bound(content_type => \$content_type);
+my $json         = {my => 'json'};
+my $code_mock    = Test::MockObject->new()->set_bound(code => \$code)->mock(headers => sub { $headers_mock })
+  ->set_always(json => $json)->set_always(body => 'my: yaml');
 my $res = Test::MockObject->new()->mock(res => sub { $code_mock });
 $client_mock->redefine(
     new => sub {
         Test::MockObject->new()->mock(get => sub { $res });
     });
 
-is OpenQA::Client::run(\%options, qw(jobs)), 'my_json', 'returns job data';
+is OpenQA::Client::run(\%options, qw(jobs)),     $json, 'returns job data';
+is OpenQA::Client::run(\%options, qw(jobs GeT)), $json, 'method can be passed (case in-sensitive)';
+
+is OpenQA::Client::run({%options, 'json-output' => 1}, qw(jobs)), $json, 'returns job data in json mode';
+is OpenQA::Client::run({%options, 'yaml-output' => 1}, qw(jobs)), $json, 'returns job data in yaml mode';
+$content_type = 'text/yaml';
+Test::MockModule->new('OpenQA::Client')->redefine(load_yaml => undef);
+is OpenQA::Client::run(\%options, qw(jobs)), $json, 'returns job data for YAML';
+is OpenQA::Client::run({%options, 'json-output' => 1}, qw(jobs)), $json, 'returns job data in json mode for YAML';
+is OpenQA::Client::run({%options, 'yaml-output' => 1}, qw(jobs)), $json, 'returns job data in yaml mode for YAML';
+
+$code = 201;
+$code_mock->{error} = {message => 'created'};
+my $ret;
+stderr_like sub { $ret = OpenQA::Client::run(\%options, qw(jobs post test=foo)) }, qr/$code.*created/, 'Codes reported';
+is $ret, $json, 'can create job';
+$code = 404;
+$code_mock->{error} = {message => 'Not Found'};
+sub wrong_call { $ret = OpenQA::Client::run(\%options, qw(unknown)) }
+stderr_like \&wrong_call, qr/$code.*Not Found/, 'Error reported';
+is $ret, 1, 'exit code shows error';
+$options{json} = 1;
+stderr_like \&wrong_call, qr/$code.*Not Found/, 'Error reported for undocumented "json" parameter';
+is $ret, 1, 'exit code shows error';
 
 done_testing();

commit a84531171dd15ff0cb3ea6cc7ecc3f98db5cd363

Fix exit code returned by client
Fixes a problem introduced with 5c9068a3d.

lib/OpenQA/Client.pm
@@ -84,7 +84,7 @@ sub handle_result {
             dd($res->json || $res->body);
         }
     }
-    return 1;
+    return undef;
 }
 
 # prepend the API-base if the specified path is relative

script/client
@@ -181,6 +181,7 @@ if ($options{form} && $options{'json-data'}) {
     exit(2);
 }
 
-exit OpenQA::Client::run(\%options, @ARGV);
+my $ret = OpenQA::Client::run(\%options, @ARGV);
+exit(defined $ret ? 0 : 1);
 
 1;

t/31-client.t
@@ -85,9 +85,9 @@ $code = 404;
 $code_mock->{error} = {message => 'Not Found'};
 sub wrong_call { $ret = OpenQA::Client::run(\%options, qw(unknown)) }
 stderr_like \&wrong_call, qr/$code.*Not Found/, 'Error reported';
-is $ret, 1, 'exit code shows error';
+is $ret, undef, 'undef shows error';
 $options{json} = 1;
 stderr_like \&wrong_call, qr/$code.*Not Found/, 'Error reported for undocumented "json" parameter';
-is $ret, 1, 'exit code shows error';
+is $ret, undef, 'undef shows error for undocumented parameter';
 
 done_testing();