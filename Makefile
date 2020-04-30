os-autoinst/openQA

commit be80ae65fbeb158b46a04547aeaacee718442720

Move all circleci test groups as convenience targets to Makefile

Makefile
@@ -12,6 +12,7 @@ KEEP_DB ?= 0
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))
 docker_env_file := "$(current_dir)/docker.env"
+unstables := $(cat .circleci/unstable_tests.txt | tr '\n' :)
 
 .PHONY: help
 help:
@@ -116,6 +117,39 @@ else
 test: checkstyle test-with-database
 endif
 endif
+
+.PHONY: test-checkstyle
+test-checkstyle: checkstyle
+	$(MAKE) test-unit-and-integration CHECKSTYLE=1 PROVE_ARGS="$$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$(unstables)"
+
+.PHONY: test-t
+test-t:
+	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+
+.PHONY: test-ui
+test-ui:
+	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+
+.PHONY: test-api
+test-api:
+	$(MAKE) test-with-database PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:$(unstables)"
+
+# put unstable tests in unstable_tests.txt and uncomment in circle CI to handle unstables with retries
+.PHONY: test-unstable
+test-unstable:
+	for f in $$(cat .circleci/unstable_tests.txt); do $(MAKE) test-with-database PROVE_ARGS="$$HARNESS $f" RETRY=3 || break; done
+
+.PHONY: test-fullstack
+test-fullstack:
+	$(MAKE) test-with-database FULLSTACK=1 PROVE_ARGS="$$HARNESS t/full-stack.t" RETRY=3
+
+.PHONY: test-scheduler
+test-scheduler:
+	$(MAKE) test-with-database SCHEDULER_FULLSTACK=1 PROVE_ARGS="$$HARNESS t/05-scheduler-full.t" RETRY=3
+
+.PHONY: test-developer
+test-developer:
+	$(MAKE) test-with-database DEVELOPER_FULLSTACK=1 PROVE_ARGS="$$HARNESS t/33-developer_mode.t" RETRY=3
 
 .PHONY: test-with-database
 test-with-database:

.circleci/config.yml
@@ -88,18 +88,7 @@ aliases:
     command: |
       export PERL5OPT="$COVEROPT,-db,cover_db_${CIRCLE_JOB}"
       echo PERL5OPT="$PERL5OPT"
-      export unstables=$(cat .circleci/unstable_tests.txt | tr '\n' :)
-      case "$CIRCLE_JOB" in
-      checkstyle) make test CHECKSTYLE=1 PROVE_ARGS="$HARNESS t/*{tidy,compile}*.t" GLOBIGNORE="$unstables";;
-      t)          make test CHECKSTYLE=0 PROVE_ARGS="$HARNESS t/*.t"               GLOBIGNORE="t/*tidy*:t/*compile*:$unstables";;
-      ui)         make test CHECKSTYLE=0 PROVE_ARGS="$HARNESS t/ui/*.t"            GLOBIGNORE="t/*tidy*:t/*compile*:$unstables";;
-      api)        make test CHECKSTYLE=0 PROVE_ARGS="$HARNESS t/api/*.t"           GLOBIGNORE="t/*tidy*:t/*compile*:$unstables";;
-      # put unstable tests in unstable_tests.txt and uncomment to handle with retries
-      #unstable)   for f in $(cat .circleci/unstable_tests.txt); do make test CHECKSTYLE=0 PROVE_ARGS="$HARNESS $f" RETRY=3 || break; done;;
-      fullstack)  make test CHECKSTYLE=0 FULLSTACK=1           PROVE_ARGS="$HARNESS t/full-stack.t" RETRY=3;;
-      scheduler)  make test CHECKSTYLE=0 SCHEDULER_FULLSTACK=1 PROVE_ARGS="$HARNESS t/05-scheduler-full.t" RETRY=3;;
-      developer)  make test CHECKSTYLE=0 DEVELOPER_FULLSTACK=1 PROVE_ARGS="$HARNESS t/33-developer_mode.t" RETRY=3;;
-      esac
+      make test-$CIRCLE_JOB
 
   - &git_token_authentication
     name: "Prepare auth token"

commit a8537863d3a3eaeb734770aabd1d807afdd351ae

Makefile: Fix evaluation of 'unstables'
Fixes regression introduced in be80ae65f

Makefile
@@ -24,7 +24,7 @@ TIMEOUT_RETRIES ?= $$((${TIMEOUT_M} * (${RETRY} + 1) ))m
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 current_dir := $(patsubst %/,%,$(dir $(mkfile_path)))
 docker_env_file := "$(current_dir)/docker.env"
-unstables := $(cat .circleci/unstable_tests.txt | tr '\n' :)
+unstables := $(shell cat .circleci/unstable_tests.txt | tr '\n' :)
 
 # tests need these environment variables to be unset
 OPENQA_BASEDIR =