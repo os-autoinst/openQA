a752bb7d4e86047b73bbab70609d6f17987c384b
Sebastian Riedel <sri@cpan.org>
2020-05-05 13:38:16 +0200

    Remove an old workaround for Mojolicious 6

--- lib/OpenQA/WebAPI/Auth/OpenID.pm
+++ lib/OpenQA/WebAPI/Auth/OpenID.pm
@@ -75,14 +75,7 @@ sub auth_login {
 sub auth_response {
     my ($self) = @_;
 
-    # FIXME: Mojo6 hack, remove after version bump
-    my %params;
-    if ($self->req->query_params->can('params')) {
-        %params = @{$self->req->query_params->params};
-    }
-    else {
-        %params = @{$self->req->query_params->pairs};
-    }
+    my %params = @{$self->req->query_params->params};
 
     my $url = $self->app->config->{global}->{base_url} || $self->req->url->base;
 
