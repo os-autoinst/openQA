77bc64c220510f9119d6ad59d0c14dda645c95f3
Ivan Lausuch <ilausuch@suse.com>
2020-06-05 14:50:01 +0200

    Add autorefresh in the index page (#3141)
    * Autorefresh in the index page
    
    https://progress.opensuse.org/issues/17886
    
    This new feature allows to autorefresh the index page with 30, 60,
    120 and 180 seconds. The user controls the interval with the
    filter control panel.
    
    * Autorefresh in the index page when fullscreen
    
    The autorefresh filter is only visible and applicable when
    fullscreen is selected
    
    * Index filter ajust top margin to allow the openning
    
    Problem: When this page is in fullmode and the filter window is
    closed (for instance if we comeback keeping the fullscreen=1
    query value in url) is impossible to open the filter panel
    because the header appears when the cursor enters in the filter
    panel area to be clicked
    
    Solution: Add additional margin to the top of the filter panel
    
    * Do autorefresh independent of fullscreen mode

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -41,7 +41,10 @@ function setupIndexPage() {
             filterFullScreenCheckBox.prop('checked', val !== '0');
             return 'fullscreen';
         } else if (key === 'interval') {
-            window.autoreload = val;
+            if (val !== 0)
+                window.autoreload = val;
+            else
+                window.autoreload = undefined;
             return 'interval';
         } else if (key === 'default_expanded') {
             defaultExpanedCheckBox.prop('checked', val !== '0');
@@ -61,14 +64,12 @@ function setupIndexPage() {
             parseFilterArguments(handleFilterParams);
         }
         toggleFullscreenMode($('#filter-fullscreen').is(':checked'));
+        autoRefreshRestart();
         event.preventDefault();
     });
 
     toggleFullscreenMode(filterFullScreenCheckBox.is(':checked'));
-
-    if (window.autoreload) {
-        window.setInterval(loadBuildResults, window.autoreload*1000);
-    }
+    autoRefreshRestart();
 }
 
 function loadBuildResults(queryParams) {
@@ -112,4 +113,12 @@ function loadBuildResults(queryParams) {
             window.buildResultStatus = 'error: ' + thrownError;
         },
     });
+}
+
+function autoRefreshRestart() {
+    if (window.autoreloadIntervalId)
+        clearInterval(window.autoreloadIntervalId);
+
+    if (window.autoreload > 0)
+        window.autoreloadIntervalId = setInterval(loadBuildResults, window.autoreload * 1000);
 }
