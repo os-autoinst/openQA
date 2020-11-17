os-autoinst/openQA

commit b3bc8ebfafe80513fcc36be433144931aaac8eed

Add gru download assets by using job settings
Failed to create assets download lists when trigger jobs by using 'isos
post' without '_URL=' arguments, even though the test suites or others
already have a setting '_URL'. This is because the assets download lists
is created by using arguments.
Change this behavior to use job settings.

See: https://progress.opensuse.org/issues/62159

lib/OpenQA/Schema/Result/ScheduledProducts.pm
@@ -224,10 +224,7 @@ sub _schedule_iso {
     my $onlysame           = delete $args->{_ONLY_OBSOLETE_SAME_BUILD} // 0;
     my $skip_chained_deps  = delete $args->{_SKIP_CHAINED_DEPS} // 0;
 
-    # Any arg name ending in _URL is special: it tells us to download
-    # the file at that URL before running the job
-    my $downloads = create_downloads_list($args);
-    my $result    = $self->_generate_jobs($args, \@notes, $skip_chained_deps);
+    my $result = $self->_generate_jobs($args, \@notes, $skip_chained_deps);
     return {error => $result->{error_message}} if defined $result->{error_message};
     my $jobs = $result->{settings_result};
 
@@ -264,6 +261,7 @@ sub _schedule_iso {
     # define function to create jobs in the database; executed as transaction
     my @successful_job_ids;
     my @failed_job_info;
+    my %tmp_downloads;
     my $create_jobs_in_database = sub {
         my $jobs_resultset = $schema->resultset('Jobs');
         my @created_jobs;
@@ -274,6 +272,23 @@ sub _schedule_iso {
         for my $settings (@{$jobs || []}) {
             my $prio = delete $settings->{PRIO};
             $settings->{_GROUP_ID} = delete $settings->{GROUP_ID};
+
+            # Any setting name ending in _URL is special: it tells us to download
+            # the file at that URL before running the job
+            my $download_list = create_downloads_list($settings);
+            foreach my $url (keys %$download_list) {
+                my $download_parameters = $download_list->{$url};
+
+        # caveat: The extraction parameter is currently not processed per destination.
+        # If multiple destinations for the same download have a different 'do_extract' parameter the first one will win.
+                unless ($tmp_downloads{$url}) {
+                    $tmp_downloads{$url}
+                      = {destination => {$download_parameters->[0] => 1}, do_extract => $download_parameters->[1]};
+                    next;
+                }
+                $tmp_downloads{$url}->{destination}->{$download_parameters->[0]} = 1
+                  unless ($tmp_downloads{$url}->{destination}->{$download_parameters->[0]});
+            }
 
             # create a new job with these parameters and count if successful, do not send job notifies yet
             try {
@@ -329,7 +344,9 @@ sub _schedule_iso {
         for my $job (@created_jobs) {
             $job->calculate_blocked_by;
         }
-        $gru->enqueue_download_jobs($downloads, \@successful_job_ids);
+        my %downloads = map { $_ => [[keys %{$tmp_downloads{$_}->{destination}}], $tmp_downloads{$_}->{do_extract}] }
+          keys %tmp_downloads;
+        $gru->enqueue_download_jobs(\%downloads, \@successful_job_ids);
     };
 
     try {

t/api/02-iso-download.t
@@ -49,7 +49,9 @@ $t->ua(
     OpenQA::Client->new(apikey => 'PERCIVALKEY02', apisecret => 'PERCIVALSECRET02')->ioloop(Mojo::IOLoop->singleton));
 $t->app($app);
 
-my $gru_tasks = $t->app->schema->resultset('GruTasks');
+my $gru_tasks        = $t->app->schema->resultset('GruTasks');
+my $gru_dependencies = $t->app->schema->resultset('GruDependencies');
+my $test_suites      = $t->app->schema->resultset('TestSuites');
 
 sub schedule_iso {
     my ($args, $status, $query_params) = @_;
@@ -111,7 +113,7 @@ sub check_job_setting {
 
 sub job_gru {
     my $job_id = shift;
-    return $t->app->schema->resultset('GruDependencies')->search({job_id => $job_id})->single->gru_task->id;
+    return $gru_dependencies->search({job_id => $job_id})->single->gru_task->id;
 }
 
 my $expected_job_count = 10;
@@ -129,20 +131,20 @@ my %params = (DISTRI => 'opensuse', VERSION => '13.1', FLAVOR => 'DVD', ARCH => 
 $rsp = schedule_iso({%params, ISO_URL => 'http://localhost/nonexistent.iso'});
 is($rsp->json->{count}, $expected_job_count, 'a regular ISO post creates the expected number of jobs');
 check_download_asset('non-existent ISO',
-    ['http://localhost/nonexistent.iso', locate_asset('iso', 'nonexistent.iso', mustexist => 0), 0]);
+    ['http://localhost/nonexistent.iso', [locate_asset('iso', 'nonexistent.iso', mustexist => 0)], 0]);
 check_job_setting($t, $rsp, 'ISO', 'nonexistent.iso', 'parameter ISO is correctly set from ISO_URL');
 
 # Schedule download and uncompression of a non-existing HDD
 $rsp = schedule_iso({%iso, HDD_1_DECOMPRESS_URL => 'http://localhost/nonexistent.hda.xz'});
 is($rsp->json->{count}, $expected_job_count, 'a regular ISO post creates the expected number of jobs');
 check_download_asset('non-existent HDD (with uncompression)',
-    ['http://localhost/nonexistent.hda.xz', locate_asset('hdd', 'nonexistent.hda', mustexist => 0), 1]);
+    ['http://localhost/nonexistent.hda.xz', [locate_asset('hdd', 'nonexistent.hda', mustexist => 0)], 1]);
 check_job_setting($t, $rsp, 'HDD_1', 'nonexistent.hda', 'parameter HDD_1 correctly set from HDD_1_DECOMPRESS_URL');
 
 # Schedule download of a non-existing ISO with a custom target name
 $rsp = schedule_iso({%iso, ISO_URL => 'http://localhost/nonexistent2.iso', ISO => 'callitthis.iso'});
 check_download_asset('non-existent ISO (with custom name)',
-    ['http://localhost/nonexistent2.iso', locate_asset('iso', 'callitthis.iso', mustexist => 0), 0]);
+    ['http://localhost/nonexistent2.iso', [locate_asset('iso', 'callitthis.iso', mustexist => 0)], 0]);
 check_job_setting($t, $rsp, 'ISO', 'callitthis.iso', 'parameter ISO is not overwritten when ISO_URL is set');
 
 # Schedule download and uncompression of a non-existing kernel with a custom target name
@@ -154,7 +156,7 @@ $rsp = schedule_iso(
     });
 is($rsp->json->{count}, $expected_job_count, 'a regular ISO post creates the expected number of jobs');
 check_download_asset('non-existent kernel (with uncompression, custom name',
-    ['http://localhost/nonexistvmlinuz', locate_asset('other', 'callitvmlinuz', mustexist => 0), 1]);
+    ['http://localhost/nonexistvmlinuz', [locate_asset('other', 'callitvmlinuz', mustexist => 0)], 1]);
 check_job_setting($t, $rsp, 'KERNEL', 'callitvmlinuz',
     'parameter KERNEL is not overwritten when KERNEL_DECOMPRESS_URL is set');
 
@@ -205,5 +207,85 @@ foreach my $j (@{$rsp->json->{ids}}) {
     is $ret->{state}, 'done';
     like $ret->{result}, qr/incomplete|skipped/, 'Job skipped/incompleted';
 }
+
+sub get_gru_tasks {
+    my $job_ids = shift;
+    my %gru_task_ids;
+    foreach my $job_id (@$job_ids) {
+        my @gru_dependencies = $gru_dependencies->search({job_id => $job_id});
+        foreach my $gru_dependency (@gru_dependencies) {
+            my $gru_id = $gru_dependency->gru_task->id;
+            if ($gru_task_ids{$gru_id}) {
+                push @{$gru_task_ids{$gru_id}}, $job_id;
+                next;
+            }
+            $gru_task_ids{$gru_id} = [$job_id];
+        }
+    }
+    return \%gru_task_ids;
+}
+
+subtest 'handle _URL in job settings' => sub {
+    $test_suites->find({name => 'kde'})->settings->create({key => 'HDD_1_URL', value => 'http://localhost/test.qcow2'});
+    $rsp = schedule_iso({%iso, TEST => 'kde'}, 200);
+    is $rsp->json->{count}, 1, 'one job was scheduled';
+    $gru = job_gru($rsp->json->{ids}->[0]);
+    check_download_asset('download asset when _URL in test suite settings',
+        ['http://localhost/test.qcow2', [locate_asset('hdd', 'test.qcow2', mustexist => 0)], 0]);
+};
+
+subtest 'create one download task when there is HDD_1_URL in isos post command' => sub {
+    $rsp = schedule_iso({%iso, HDD_1_URL => 'http://localhost/test.qcow2'}, 200);
+    is $rsp->json->{count}, $expected_job_count, 'ten job were scheduled';
+    my $gru_task_ids = get_gru_tasks($rsp->json->{ids});
+    is scalar(keys %$gru_task_ids), 1, 'only one download task was created';
+    my @value = values %$gru_task_ids;
+    is scalar(@{$value[0]}), $expected_job_count, 'ten job were blocked by the same download task';
+    check_download_asset('download asset was created',
+        ['http://localhost/test.qcow2', [locate_asset('hdd', 'test.qcow2', mustexist => 0)], 0]);
+};
+
+subtest 'create many download tasks when many test suites have different _URL' => sub {
+    $test_suites->find({name => 'textmode'})
+      ->settings->create({key => 'HDD_1_URL', value => 'http://localhost/test_textmode.qcow2'});
+    $test_suites->find({name => 'server'})
+      ->settings->create({key => 'HDD_1_URL', value => 'http://localhost/test_server.qcow2'});
+    $rsp = schedule_iso(\%iso, 200);
+    is $rsp->json->{count}, $expected_job_count, 'ten job was scheduled';
+    my $gru_task_ids = get_gru_tasks($rsp->json->{ids});
+    is scalar(keys %$gru_task_ids), 3, 'three download tasks were created';
+    my $expected_download_tasks = {
+        'http://localhost/test.qcow2'          => [locate_asset('hdd', 'test.qcow2',          mustexist => 0)],
+        'http://localhost/test_textmode.qcow2' => [locate_asset('hdd', 'test_textmode.qcow2', mustexist => 0)],
+        'http://localhost/test_server.qcow2'   => [locate_asset('hdd', 'test_server.qcow2',   mustexist => 0)],
+    };
+    my %created_download_tasks;
+    foreach my $gru_id (keys %$gru_task_ids) {
+        my $args = $gru_tasks->find($gru_id)->args;
+        $created_download_tasks{$args->[0]} = $args->[1];
+    }
+    is_deeply \%created_download_tasks, $expected_download_tasks, 'the download tasks were created correctly';
+};
+
+subtest 'create one download task when test suites have different destinations' => sub {
+    $test_suites->find({name => 'textmode'})->settings->create({key => 'HDD_1', value => 'test_textmode.qcow2'});
+    $test_suites->find({name => 'server'})->settings->create({key => 'HDD_1', value => 'test_server.qcow2'});
+    $test_suites->find({name => 'kde'})->settings->create({key => 'HDD_1', value => 'test_kde.qcow2'});
+    $rsp = schedule_iso({%iso, HDD_1_URL => 'http://localhost/test.qcow2'}, 200);
+    is $rsp->json->{count}, $expected_job_count, 'ten job was scheduled';
+    my @gru_task_ids = keys %{get_gru_tasks($rsp->json->{ids})};
+    is scalar(@gru_task_ids), 1, 'only one download task was created';
+    my $args = $gru_tasks->find($gru_task_ids[0])->args;
+    is $args->[0], 'http://localhost/test.qcow2', 'the url was correct';
+    my @destinations = sort @{$args->[1]};
+    is_deeply \@destinations,
+      [
+        locate_asset('hdd', 'test.qcow2',          mustexist => 0),
+        locate_asset('hdd', 'test_kde.qcow2',      mustexist => 0),
+        locate_asset('hdd', 'test_server.qcow2',   mustexist => 0),
+        locate_asset('hdd', 'test_textmode.qcow2', mustexist => 0)
+      ],
+      'one download task has 4 destinations';
+};
 
 done_testing();

lib/OpenQA/Task/Asset/Download.pm
@@ -26,10 +26,18 @@ sub register {
 }
 
 sub _download {
-    my ($job, $url, $assetpath, $do_extract) = @_;
+    my ($job, $url, $assetpaths, $do_extract) = @_;
 
     my $app    = $job->app;
     my $job_id = $job->id;
+
+    # deal with one download task has many destinations
+    my $assetpath          = $assetpaths;
+    my $other_destinations = [];
+    if (ref($assetpaths) eq 'ARRAY') {
+        $assetpath          = shift @$assetpaths;
+        $other_destinations = $assetpaths;
+    }
 
     # Prevent multiple asset download tasks for the same asset to run in parallel
     return $job->retry({delay => 30})
@@ -70,7 +78,15 @@ sub _download {
     };
     unless ($downloader->download($url, $assetpath, $options)) {
         $ctx->error(my $msg = qq{Downloading "$url" failed because of too many download errors});
-        $job->fail($msg);
+        return $job->fail($msg);
+    }
+
+    my @error_message;
+    symlink($assetpath, $_) || push @error_message, "Cannot create symlink from $assetpath to $_ : $!"
+      for (@$other_destinations);
+    if (scalar(@error_message) > 0) {
+        $ctx->error(my $msg = join('\n', @error_message));
+        return $job->fail($msg);
     }
 }
 

t/14-grutasks.t
@@ -38,6 +38,7 @@ use Mojo::File qw(path tempdir);
 use Mojo::Log;
 use Storable qw(store retrieve);
 use Mojo::IOLoop;
+use Cwd qw(getcwd);
 use utf8;
 
 # mock asset deletion
@@ -601,6 +602,28 @@ subtest 'download assets with correct permissions' => sub {
     combined_like { run_gru_job($t->app, 'download_asset' => [$assetsource, $assetpath, 0]) }
     qr/Skipping download of "$assetsource" to "$assetpath" because file already exists/, 'everything logged';
     ok -f $assetpath, 'asset downloaded';
+
+    my $cwd          = getcwd;
+    my @destinations = (
+        "$cwd/t/data/openqa/share/factory/iso/test1.iso",
+        "$cwd/t/data/openqa/share/factory/iso/test2.iso",
+        "$cwd/t/data/openqa/share/factory/iso/test3.iso"
+    );
+    combined_like { run_gru_job($t->app, 'download_asset' => [$assetsource, \@destinations, 0]) }
+    qr /Download of "$destinations[0]" successful/, 'download task successed';
+    my $destination = shift @destinations;
+    ok -f $destination, 'asset download successfully';
+    foreach (@destinations) {
+        ok -f $_, "$_ symlink successfully";
+        is readlink($_), $destination, "the source file is correct";
+    }
+    path("$cwd/t/data/openqa/share/factory/iso/test3.iso")->remove;
+    path($destination)->remove;
+    combined_like { run_gru_job($t->app, 'download_asset' => [$assetsource, [$destination, @destinations], 0]) }
+    qr/Cannot create symlink from $destination to .*: File exists/, 'cannot create symlink';
+
+    path($_)->remove for @destinations;
+    path($destination)->remove;
 };
 
 subtest 'finalize job results' => sub {
@@ -667,7 +690,6 @@ subtest 'finalize job results' => sub {
         }
     };
 };
-
 
 $webapi->signal('TERM');
 $webapi->finish;

commit f0ed31fc758034648e85c420d4c050297e1e9a52

Re-do handling of placeholders and + after _URL variables parsed
b3bc8ebf caused some problems by moving the parsing of `_URL`
settings to after the call to `JobSettings::generate_settings`,
which is where handling of placeholders (%FOO%) and +-overrides
(+FOO), usually from the templates, is done. This broke the case
where e.g. a template sets `+ISO`, or uses `%ISO%` in a value,
and the job is posted with `ISO_URL` but no explicit `ISO` set.
In that case the derived `ISO` setting should be overridden or
used in the placeholder expansion, but this was broken.

This fixes the problems by handling placeholder expansion and
+-overrides *again* after the parsing of `_URL` settings. They
are pretty simple operations anyway so it shouldn't cause much
overhead. To facilitate this we have to set up destructive and
non-destructive modes for `handle_plus_in_settings` so it can be
run non-destructively at first and then destructively later, and
similarly set up definitive and non-definitive modes for
`expand_placeholders`, so on the first run if the setting a
placeholder refers to is not (yet?) set, we leave the placeholder
rather than replacing it with an empty string.

Signed-off-by: Adam Williamson <awilliam@redhat.com>

lib/OpenQA/JobSettings.pm
@@ -18,7 +18,9 @@ use strict;
 use warnings;
 
 sub generate_settings {
-    my ($params) = @_;
+    my ($params, $destructive, $definitive) = @_;
+    $destructive //= 1;
+    $definitive //= 1;
     my $settings = $params->{settings};
     my @worker_class;
     for my $entity (qw (product machine test_suite job_template)) {
@@ -53,20 +55,24 @@ sub generate_settings {
         $settings->{JOB_DESCRIPTION} = $job_template->description if length $job_template->description;
     }
 
-    handle_plus_in_settings($settings);
-    return expand_placeholders($settings);
+    handle_plus_in_settings($settings, $destructive);
+    return expand_placeholders($settings, $definitive);
 }
 
-# replace %NAME% with $settings{NAME}
+# replace %NAME% with $settings{NAME}. if "definitive" is true, then
+# if $settings{NAME} is not set, we replace with an empty string. If
+# "definitive" is false, then if it is not set, we leave the
+# placeholder in place.
 sub expand_placeholders {
-    my ($settings) = @_;
+    my ($settings, $definitive) = @_;
+    $definitive //= 1;
 
     for my $value (values %$settings) {
         next unless defined $value;
 
         my %visited_top_level_placeholders;
 
-        eval { $value =~ s/%(\w+)%/_expand_placeholder($settings, $1, \%visited_top_level_placeholders)/eg; };
+        eval { $value =~ s/%(\w+)%/_expand_placeholder($settings, $definitive, $1, \%visited_top_level_placeholders)/eg; };
         if ($@) {
             return "Error: $@";
         }
@@ -75,9 +81,12 @@ sub expand_placeholders {
 }
 
 sub _expand_placeholder {
-    my ($settings, $key, $visited_placeholders_in_parent_scope) = @_;
+    my ($settings, $definitive, $key, $visited_placeholders_in_parent_scope) = @_;
 
-    return '' unless defined $settings->{$key};
+    unless (defined $settings->{$key}) {
+        return '' if $definitive;
+        return '%' . $key . '%';
+    }
 
     my %visited_placeholders = %$visited_placeholders_in_parent_scope;
     if ($visited_placeholders{$key}++) {
@@ -85,7 +94,7 @@ sub _expand_placeholder {
     }
 
     my $value = $settings->{$key};
-    $value =~ s/%(\w+)%/_expand_placeholder($settings, $1, \%visited_placeholders)/eg;
+    $value =~ s/%(\w+)%/_expand_placeholder($settings, $definitive, $1, \%visited_placeholders)/eg;
 
     return $value;
 }
@@ -96,10 +105,12 @@ sub _expand_placeholder {
 # if *multiple* things set +VARIABLE, whichever comes highest in
 # the usual precedence order wins.
 sub handle_plus_in_settings {
-    my ($settings) = @_;
+    my ($settings, $destructive) = @_;
+    $destructive //= 1;
     for (keys %$settings) {
         if (substr($_, 0, 1) eq '+') {
-            $settings->{substr($_, 1)} = delete $settings->{$_};
+            $settings->{substr($_, 1)} = $settings->{$_};
+            delete $settings->{$_} if $destructive;
         }
     }
 }

lib/OpenQA/Schema/Result/ScheduledProducts.pm
@@ -282,7 +282,12 @@ sub _schedule_iso {
                 # Any setting name ending in _URL is special: it tells us to download
                 # the file at that URL before running the job
                 my $download_list = create_downloads_list($settings);
-                my $job           = $jobs_resultset->create_from_settings($settings, $self->id);
+                # Re-do placeholder expansion and override-by-+-prefix
+                # now any FOOs have been created from FOO_URLs, now in
+                # destructive and definitive modes
+                OpenQA::JobSettings::handle_plus_in_settings($settings, 1);
+                die "$_" if OpenQA::JobSettings::expand_placeholders($settings, 1);
+                my $job = $jobs_resultset->create_from_settings($settings, $self->id);
                 push @created_jobs, $job;
                 my $j_id = $job->id;
                 $job_ids_by_test_machine{_settings_key($settings)} //= [];
@@ -558,7 +563,10 @@ sub _generate_jobs {
                 test_suite   => $job_template->test_suite,
                 job_template => $job_template,
             );
-            my $error = OpenQA::JobSettings::generate_settings(\%params);
+            # we use non-destructive and non-definitive modes here so
+            # the +-prefixed vars and placeholders remain for a second
+            # pass later
+            my $error = OpenQA::JobSettings::generate_settings(\%params, 0, 0);
             $error_message .= $error if defined $error;
 
             $settings{PRIO}     = defined($priority) ? $priority : $job_template->prio;

lib/OpenQA/WebAPI/Controller/API/V1/Job.pm
@@ -261,7 +261,12 @@ sub create {
     }
     try {
         my $downloads = create_downloads_list($job_settings);
-        my $schema    = $self->schema;
+        # Re-do placeholder expansion and override-by-+-prefix
+        # now any FOOs have been created from FOO_URLs, now in
+        # destructive and definitive modes
+        OpenQA::JobSettings::handle_plus_in_settings($job_settings, 1);
+        die "$_" if OpenQA::JobSettings::expand_placeholders($job_settings, 1);
+        my $schema = $self->schema;
         $schema->txn_do(
             sub {
                 my $job    = $schema->resultset('Jobs')->create_from_settings($job_settings);
@@ -896,7 +901,10 @@ sub _generate_job_setting {
         $params{test_suite} = $test_suite;
     }
 
-    my $error_message = OpenQA::JobSettings::generate_settings(\%params);
+    # we use non-destructive and non-definitive mode here so
+    # the +-prefixed vars and placeholders remain for a second
+    # pass later
+    my $error_message = OpenQA::JobSettings::generate_settings(\%params, 0, 0);
     return {error_message => $error_message, settings_result => \%settings};
 }
 

t/40-job_settings.t
@@ -121,8 +121,32 @@ subtest 'handle_plus_in_settings' => sub {
         '+ARCH'  => 'x86_64',
         'DISTRI' => 'opensuse',
     };
+    OpenQA::JobSettings::handle_plus_in_settings($settings, 0);
+    is_deeply(
+        $settings,
+        {ISO => 'bar.iso', '+ISO' => 'bar.iso', ARCH => 'x86_64', '+ARCH' => 'x86_64', DISTRI => 'opensuse'},
+        'handle the plus correctly (explicitly not destructive)'
+    );
+
+    OpenQA::JobSettings::handle_plus_in_settings($settings, 1);
+    is_deeply(
+        $settings,
+        {ISO => 'bar.iso', ARCH => 'x86_64', DISTRI => 'opensuse'},
+        'handle the plus correctly (explicitly destructive)'
+    );
+
+    $settings = {
+        'ISO'    => 'foo.iso',
+        '+ISO'   => 'bar.iso',
+        '+ARCH'  => 'x86_64',
+        'DISTRI' => 'opensuse',
+    };
     OpenQA::JobSettings::handle_plus_in_settings($settings);
-    is_deeply($settings, {ISO => 'bar.iso', ARCH => 'x86_64', DISTRI => 'opensuse'}, 'handle the plus correctly');
+    is_deeply(
+        $settings,
+        {ISO => 'bar.iso', ARCH => 'x86_64', DISTRI => 'opensuse'},
+        'handle the plus correctly (destructive by default)'
+    );
 };
 
 done_testing;

t/api/02-iso-download.t
@@ -298,4 +298,28 @@ subtest 'download task only blocks the related job when test suites have differe
     is scalar(@{$gru_dep_tasks->{$gru_task_ids[0]}}), 3, 'one download task was created and it blocked 3 jobs';
 };
 
+subtest 'placeholder expansions work with _URL-derived settings' => sub {
+    $test_suites->find({name => 'kde'})->settings->create({key => 'FOOBAR', value => '%ISO%'});
+    $rsp
+      = schedule_iso({%params, ISO_URL => 'http://localhost/openSUSE-13.1-DVD-i586-Build0091-Media.iso', TEST => 'kde'},
+        200);
+    is $rsp->json->{count}, 1, 'one job was scheduled';
+    my $expanderjob = get_job($rsp->json->{ids}->[0]);
+    is(
+        $expanderjob->{settings}->{FOOBAR},
+        'openSUSE-13.1-DVD-i586-Build0091-Media.iso',
+        '%ISO% in template is expanded by posted ISO_URL'
+    );
+};
+
+subtest 'plus-overrides work with _URL-derived settings' => sub {
+    $test_suites->find({name => 'kde'})->settings->create({key => '+ISO', value => 'templateoverride.iso'});
+    $rsp
+      = schedule_iso({%params, ISO_URL => 'http://localhost/openSUSE-13.1-DVD-i586-Build0091-Media.iso', TEST => 'kde'},
+        200);
+    is $rsp->json->{count}, 1, 'one job was scheduled';
+    my $overriddenjob = get_job($rsp->json->{ids}->[0]);
+    is($overriddenjob->{settings}->{ISO}, 'templateoverride.iso', '+ISO in template overrides posted ISO_URL');
+};
+
 done_testing();