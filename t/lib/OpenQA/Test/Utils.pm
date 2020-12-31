826918b3f1412c34fea0f9751c3b834867c18d86
Christian Dywan <cdywan@suse.de>
2020-12-31 20:08:55 +0100

    Unify blocking loop and sleep codepaths

--- t/lib/OpenQA/Test/Utils.pm
+++ t/lib/OpenQA/Test/Utils.pm
@@ -449,7 +449,9 @@ sub create_worker {
 sub unstable_worker {
     my ($apikey, $apisecret, $host, $instance, $ticks, $sleep) = @_;
     note("Starting unstable worker. Instance: $instance for host $host");
-    $ticks = 1 unless defined $ticks;
+    # Block the event loop for the specified number of events or indefinitely if -1 is specified
+    $ticks = $ticks == -1 ? ~0 >> 1 : ($ticks // 1);
+    $sleep //= 0;
 
     my $h = _setup_sigchld_handler 'openqa-worker-unstable', start sub {
         _setup_sub_process 'openqa-worker-unstable';
@@ -462,18 +464,11 @@ sub unstable_worker {
             });
         setup_worker($worker, $host);
         $worker->init();
-        if ($ticks < 0) {
-            Mojo::IOLoop->singleton->start;
-        }
-        else {
-            Mojo::IOLoop->singleton->one_tick for (0 .. $ticks);
-        }
+        Mojo::IOLoop->singleton->one_tick for (0 .. $ticks);
+        sleep $sleep;
         Devel::Cover::report() if Devel::Cover->can('report');
-        if ($sleep) {    # uncoverable statement
-            1 while sleep $sleep;    # uncoverable statement
-        }    # uncoverable statement
     };
-    sleep $sleep if $sleep;
+    sleep $sleep;
     return $h;
 }
 
