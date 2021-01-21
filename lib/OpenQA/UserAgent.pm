3fea15291cec1c52f25dc6c9fed4aaaf80ed8fcb
Tina Müller <tina.mueller@suse.com>
2021-01-21 17:57:28 +0100

    Revert "client: Log what config file is being read"
    This reverts commit b3ee9d2139dd77d6ad8ddebfb597fd81af7ad9f8.
    
    Issue: https://progress.opensuse.org/issues/88053

--- lib/OpenQA/UserAgent.pm
+++ lib/OpenQA/UserAgent.pm
@@ -16,8 +16,6 @@
 package OpenQA::UserAgent;
 use Mojo::Base 'Mojo::UserAgent';
 
-use OpenQA::Log 'log_info';
-
 use Mojo::Util 'hmac_sha1_sum';
 use Config::IniFiles;
 use Scalar::Util ();
@@ -40,7 +38,6 @@ sub new {
         for my $path (@cfgpaths) {
             my $file = $path . '/client.conf';
             next unless $file && -r $file;
-            log_info("Reading config from $file");
             my $cfg = Config::IniFiles->new(-file => $file) || last;
             last unless $cfg->SectionExists($args{api});
             for my $i (qw(key secret)) {
