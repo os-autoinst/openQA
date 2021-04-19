745ae7387c2eb9afaae33719776ca5a04e19c58c
Sebastian Riedel <sri@cpan.org>
2021-04-19 15:46:36 +0200

    Use form validation in Main controller

--- lib/OpenQA/WebAPI/Controller/Main.pm
+++ lib/OpenQA/WebAPI/Controller/Main.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2019 SUSE LLC
+# Copyright (C) 2015-2021 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,18 +14,16 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
 package OpenQA::WebAPI::Controller::Main;
-use Mojo::Base 'Mojolicious::Controller';
+use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 use Date::Format;
 use OpenQA::Jobs::Constants;
 use OpenQA::Schema::Result::Jobs;
 use OpenQA::BuildResults;
 use OpenQA::Utils;
-use Scalar::Util 'looks_like_number';
+use Mojo::File qw(path);
 
-sub _map_tags_into_build {
-    my ($results, $tags) = @_;
-
+sub _map_tags_into_build ($results, $tags) {
     for my $res (@$results) {
         if (my $full_tag = $tags->{$res->{key}}) {
             $res->{tag} = $full_tag;
@@ -37,21 +35,26 @@ sub _map_tags_into_build {
             $res->{tag} = $build_only_tag;
         }
     }
-    return;
 }
 
-sub dashboard_build_results {
-    my ($self) = @_;
+sub dashboard_build_results ($self) {
+    my $validation = $self->validation;
+    $validation->optional('limit_builds')->num;
+    $validation->optional('time_limit_days')->like(qr/^[0-9.]+$/);
+    $validation->optional('only_tagged');
+    $validation->optional('default_expanded');
+    $validation->optional('show_tags');
+    $validation->optional('group');
+    return $self->reply->validation_error({format => $self->accepts('html', 'json')}) if $validation->has_error;
 
-    my $limit_builds = $self->param('limit_builds');
-    $limit_builds = 3 unless looks_like_number($limit_builds);
-    my $time_limit_days = $self->param('time_limit_days');
-    $time_limit_days = 14 unless looks_like_number($time_limit_days);
-    my $only_tagged      = $self->param('only_tagged')      // 0;
-    my $default_expanded = $self->param('default_expanded') // 0;
-    my $show_tags        = $self->param('show_tags')        // $only_tagged;
-    my $group_params     = $self->every_param('group');
-    my $groups           = $self->stash('job_groups_and_parents');
+    my $limit_builds     = $validation->param('limit_builds')     // 3;
+    my $time_limit_days  = $validation->param('time_limit_days')  // 14;
+    my $only_tagged      = $validation->param('only_tagged')      // 0;
+    my $default_expanded = $validation->param('default_expanded') // 0;
+    my $show_tags        = $validation->param('show_tags')        // $only_tagged;
+    my $group_params     = $validation->every_param('group');
+
+    my $groups = $self->stash('job_groups_and_parents');
 
     my @results;
     for my $group (@$groups) {
@@ -65,10 +68,8 @@ sub dashboard_build_results {
             $group_params);
 
         my $build_results_for_group = $build_results->{build_results};
-        if ($show_tags) {
-            _map_tags_into_build($build_results_for_group, $tags);
-        }
-        push(@results, $build_results) if @{$build_results_for_group};
+        _map_tags_into_build($build_results_for_group, $tags) if $show_tags;
+        push(@results, $build_results)                        if @{$build_results_for_group};
     }
 
     $self->stash(
@@ -78,37 +79,34 @@ sub dashboard_build_results {
 
     $self->respond_to(
         json => {json     => {results => \@results}},
-        html => {template => 'main/dashboard_build_results'},
-    );
+        html => {template => 'main/dashboard_build_results'});
 }
 
-sub group_overview {
-    my ($self, $resultset, $template) = @_;
+sub _group_overview ($self, $resultset, $template) {
+    my $validation = $self->validation;
+    $validation->optional('limit_builds')->num;
+    $validation->optional('time_limit_days')->like(qr/^[0-9.]+$/);
+    $validation->optional('only_tagged');
+    $validation->optional('fullscreen');
+    $validation->optional('interval');
+    $validation->optional('comments_page')->num;
+    $validation->optional('comments_limit')->num;
+    return $self->reply->validation_error({format => $self->accepts('html', 'json')}) if $validation->has_error;
 
-    my $limit_builds = $self->param('limit_builds');
-    $limit_builds = 10 unless looks_like_number($limit_builds);
-    my $time_limit_days = $self->param('time_limit_days');
-    $time_limit_days = 0 unless looks_like_number($time_limit_days);
-
+    my $limit_builds    = $validation->param('limit_builds')    // 10;
+    my $time_limit_days = $validation->param('time_limit_days') // 0;
     $self->app->log->debug("Retrieving results for up to $limit_builds builds up to $time_limit_days days old");
-    my $only_tagged = $self->param('only_tagged') // 0;
-    my $group_id    = $self->param('groupid');
-    return $self->reply->not_found unless looks_like_number($group_id);
-    my $group = $self->schema->resultset($resultset)->find($group_id);
-    return $self->reply->not_found unless $group;
-    $self->stash('fullscreen', $self->param('fullscreen') // 0);
-    my $interval = $self->param('interval') // 60;
-    $self->stash('interval', $interval);
+    my $only_tagged = $validation->param('only_tagged') // 0;
 
-    my @comments;
-    my @pinned_comments;
-    my $tags;
+    my $group_id = $self->stash('groupid');
+    return $self->reply->not_found unless my $group = $self->schema->resultset($resultset)->find($group_id);
 
-    # read paging parameter
-    my $page       = int($self->param('comments_page')  // 1);
-    my $page_limit = int($self->param('comments_limit') // 5);
-    return $self->respond_to(json => sub { html => 'Invalid paging parameter specified.' })
-      unless $page && $page_limit;
+    my $fullscreen = $validation->param('fullscreen') // 0;
+    my $interval   = $validation->param('interval')   // 60;
+    $self->stash(fullscreen => $fullscreen, interval => $interval);
+
+    my $page       = $validation->param('comments_page')  // 1;
+    my $page_limit = $validation->param('comments_limit') // 5;
 
     # find comments
     my $comments = $group->comments->search(
@@ -119,24 +117,17 @@ sub group_overview {
             order_by => {-desc => 't_created'},
         });
     $self->stash('comments_pager', $comments->pager());
-    @comments = $comments->all;
+    my @comments = $comments->all;
 
     # find "pinned descriptions" (comments by operators with the word 'pinned-description' in it)
     # FIXME: use a join with the user table here to do the check for operator via the database
-    for my $comment (
-        $group->comments->search(
-            {
-                text => {like => '%pinned-description%'},
-            }
-        )->all
-      )
-    {
-        push(@pinned_comments, $comment) if ($comment->user->is_operator);
+    my @pinned_comments;
+    for my $comment ($group->comments->search({text => {like => '%pinned-description%'}})->all) {
+        push(@pinned_comments, $comment) if $comment->user->is_operator;
     }
 
-    $tags = $group->tags;
-
-    my $cbr = OpenQA::BuildResults::compute_build_results(
+    my $tags = $group->tags;
+    my $cbr  = OpenQA::BuildResults::compute_build_results(
         $group, $limit_builds, $time_limit_days,
         $only_tagged ? $tags : undef,
         $self->every_param('group'));
@@ -145,8 +136,7 @@ sub group_overview {
     $self->stash(children => $cbr->{children});
 
     _map_tags_into_build($build_results, $tags);
-    $self->stash('build_results', $build_results);
-    $self->stash('max_jobs',      $max_jobs);
+    $self->stash(build_results => $build_results, max_jobs => $max_jobs);
 
     my $is_parent_group              = $group->can('children');
     my $comment_context              = $is_parent_group ? 'parent_group' : 'group';
@@ -155,24 +145,24 @@ sub group_overview {
         id                   => $group->id,
         name                 => $group->name,
         is_parent            => $is_parent_group,
-        rendered_description => $group->rendered_description // undef,
+        rendered_description => $group->rendered_description
     };
-    if (!$is_parent_group) {
-        if (my $parent = $group->parent) {
-            $group_hash->{parent_id}   = $parent->id;
-            $group_hash->{parent_name} = $parent->name;
-        }
+    if (!$is_parent_group && (my $parent = $group->parent)) {
+        $group_hash->{parent_id}   = $parent->id;
+        $group_hash->{parent_name} = $parent->name;
     }
-    $self->stash('group',                 $group_hash);
-    $self->stash('limit_builds',          $limit_builds);
-    $self->stash('only_tagged',           $only_tagged);
-    $self->stash('comments',              \@comments);
-    $self->stash('pinned_comments',       \@pinned_comments);
-    $self->stash('child_groups',          [$group->children->all]) if ($is_parent_group);
-    $self->stash('comment_context',       $comment_context);
-    $self->stash('comment_post_action',   'apiv1_post_' . $comment_context_route_suffix);
-    $self->stash('comment_put_action',    'apiv1_put_' . $comment_context_route_suffix);
-    $self->stash('comment_delete_action', 'apiv1_delete_' . $comment_context_route_suffix);
+    $self->stash(
+        group                 => $group_hash,
+        limit_builds          => $limit_builds,
+        only_tagged           => $only_tagged,
+        comments              => \@comments,
+        pinned_comments       => \@pinned_comments,
+        comment_context       => $comment_context,
+        comment_post_action   => 'apiv1_post_' . $comment_context_route_suffix,
+        comment_put_action    => 'apiv1_put_' . $comment_context_route_suffix,
+        comment_delete_action => 'apiv1_delete_' . $comment_context_route_suffix
+    );
+    $self->stash(child_groups => [$group->children->all]) if $is_parent_group;
 
     $self->respond_to(
         json => sub {
@@ -191,28 +181,13 @@ sub group_overview {
         html => {template => $template});
 }
 
-sub job_group_overview {
-    my ($self) = @_;
-    $self->group_overview('JobGroups', 'main/group_overview');
-}
+sub job_group_overview    ($self) { $self->_group_overview('JobGroups',       'main/group_overview') }
+sub parent_group_overview ($self) { $self->_group_overview('JobGroupParents', 'main/parent_group_overview') }
 
-sub parent_group_overview {
-    my ($self) = @_;
-    $self->group_overview('JobGroupParents', 'main/parent_group_overview');
-}
-
-sub changelog {
-    my ($self) = @_;
-
-    my $changelog;
-    if (open(my $changelog_file, '<', $self->app->config->{global}->{changelog_file})) {
-        read($changelog_file, $changelog, -s $changelog_file);
-        close($changelog_file);
-    }
-    else {
-        $changelog = 'No changelog available.';
-    }
-    $self->stash(changelog => $changelog);
+sub changelog ($self) {
+    my $file      = path($self->app->config->{global}->{changelog_file});
+    my $changelog = -r $file ? $file->slurp : 'No changelog available.';
+    $self->render(changelog => $changelog);
 }
 
 1;
