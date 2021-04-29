os-autoinst/openQA

commit a34d89c4a2a78d874c84596257ac28f27f20d892

Treat result uploads for non-existant modules as error
The worker reads the test order and updates it before each upload attempt
so the web UI should be aware of all test modules even if the test schedule
changes in the middle. So it should be safe to treat this situation as an
error.

This should improve the error feedback in cases like
https://progress.opensuse.org/issues/90152 where the logs tell us currently
that the result upload worked but in the end nothing is there. Of course
this just improves the symptom. The cause is likely that the test order
could not be uploaded in the first place.

lib/OpenQA/Schema/Result/Jobs.pm
@@ -1310,6 +1310,7 @@ sub update_module {
     }
 
     $mod->save_results($raw_result, $known_md5_sums, $known_file_names);
+    return 1;
 }
 
 # computes the progress info for the current job
@@ -1537,13 +1538,16 @@ sub update_status {
     $self->insert_test_modules($status->{test_order}) if $status->{test_order};
     my %known_image;
     my %known_files;
+    my @failed_modules;
     if (my $result = $status->{result}) {
         for my $name (sort keys %$result) {
-            $self->update_module($name, $result->{$name}, \%known_image, \%known_files);
+            push @failed_modules, $name
+              unless $self->update_module($name, $result->{$name}, \%known_image, \%known_files);
         }
     }
     $ret->{known_images} = [sort keys %known_image];
     $ret->{known_files}  = [sort keys %known_files];
+    $ret->{error}        = 'Failed modules: ' . join ', ', @failed_modules if @failed_modules;
 
     # update info used to compose the URL to os-autoinst command server
     if (my $assigned_worker = $self->assigned_worker) {

t/api/04-jobs.t
@@ -578,32 +578,29 @@ subtest 'update job status' => sub {
         }
         path($result_dir, $_)->spurt('fake result') for @known_files;
 
-        $t->post_ok(
+        my @details = (
+            {screenshot => {name => 'known-screenshot.png',   md5 => '098f6bcd4621d373cade4e832627b4f6'}},
+            {screenshot => {name => 'unknown-screenshot.png', md5 => 'ad0234829205b9033196ba818f7a872b'}},
+            {text       => 'known-text.txt'},
+            {text       => 'unknown-text.txt'},
+            {audio      => 'known-audio.wav'},
+            {audio      => 'unknown-audio.wav'},
+        );
+        my @post_args = (
             '/api/v1/jobs/99963/status',
             json => {
                 status => {
                     worker_id => 1,
                     result    => {
-                        foo_module => {
-                            result  => 'running',
-                            details => [
-                                {
-                                    screenshot =>
-                                      {name => 'known-screenshot.png', md5 => '098f6bcd4621d373cade4e832627b4f6'}
-                                },
-                                {
-                                    screenshot =>
-                                      {name => 'unknown-screenshot.png', md5 => 'ad0234829205b9033196ba818f7a872b'}
-                                },
-                                {text  => 'known-text.txt'},
-                                {text  => 'unknown-text.txt'},
-                                {audio => 'known-audio.wav'},
-                                {audio => 'unknown-audio.wav'},
-                            ],
-                        },
-                        bar_module => {result => 'none'},    # supposed to be ignored
+                        foo_module => {result => 'running', details => \@details},
+                        bar_module => {result => 'none'},                            # supposed to be ignored
                     },
-                }})->status_is(200);
+                }});
+        $t->post_ok(@post_args)->status_is(400, 'result upload returns error code if module does not exist');
+        is $t->tx->res->json->{error}, 'Failed modules: bar_module', 'error specifies problematic module';
+
+        $job->modules->create({name => 'bar_module', category => 'selftests', script => 'bar_module.pm'});
+        $t->post_ok(@post_args)->status_is(200, 'result upload for existing module succeeds');
         my $response = $t->tx->res->json;
         my %expected_response
           = (job_result => 'failed', known_files => \@known_files, known_images => \@known_images, result => 1);

commit b6ab87371ff881b9fdc6661cb6f1bd931ead3a64

Let the worker retry uploading when updating module results failed
* Avoid incompletes with the reason "Failed modules: …"
    * Weaken error handling introduced in
      a34d89c4a2a78d874c84596257ac28f27f20d892 so the worker would try the
      upload again
    * Affected tests actually have the failing test modules so it is likely
      that a retry would help
* See https://progress.opensuse.org/issues/91902

lib/OpenQA/Schema/Result/Jobs.pm
@@ -1547,7 +1547,10 @@ sub update_status {
     }
     $ret->{known_images} = [sort keys %known_image];
     $ret->{known_files}  = [sort keys %known_files];
-    $ret->{error}        = 'Failed modules: ' . join ', ', @failed_modules if @failed_modules;
+    if (@failed_modules) {
+        $ret->{error}        = 'Failed modules: ' . join ', ', @failed_modules;
+        $ret->{error_status} = 490;    # let the worker do its usual retries (see poo#91902)
+    }
 
     # update info used to compose the URL to os-autoinst command server
     if (my $assigned_worker = $self->assigned_worker) {

lib/OpenQA/Worker/WebUIConnection.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2020 SUSE LLC
+# Copyright (C) 2019-2021 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -311,7 +311,7 @@ sub send {
         $msg //= $err->{message};
         if (my $error_code = $err->{code}) {
             $msg = "$error_code response: $msg";
-            if (400 <= $error_code && $error_code < 500 && $error_code != 408 && $error_code != 425) {
+            if ($error_code < 500 && $error_code != 408 && $error_code != 425 && $error_code != 490) {
                 # don't retry on most 4xx errors (in this case we can't expect
                 # different results on further attempts)
                 $tries = 0;

t/api/04-jobs.t
@@ -596,7 +596,7 @@ subtest 'update job status' => sub {
                         bar_module => {result => 'none'},                            # supposed to be ignored
                     },
                 }});
-        $t->post_ok(@post_args)->status_is(400, 'result upload returns error code if module does not exist');
+        $t->post_ok(@post_args)->status_is(490, 'result upload returns error code if module does not exist');
         is $t->tx->res->json->{error}, 'Failed modules: bar_module', 'error specifies problematic module';
 
         $job->modules->create({name => 'bar_module', category => 'selftests', script => 'bar_module.pm'});