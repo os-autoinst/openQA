91a24feda4af15fd91fa5bffd48567b7a9da5a37
Adam Williamson <awilliam@redhat.com>
2021-07-02 09:18:45 -0700

    Fix module source content negotiation for Mojo < 9
    The 'fix' in 30571b5 breaks things for Mojo < 9 (which we still
    have compatibility with elsewhere in this file).
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/OpenQA/WebAPI.pm
+++ lib/OpenQA/WebAPI.pm
@@ -192,7 +192,12 @@ sub startup ($self) {
     my $step_auth = $test_auth->any('/modules/:moduleid/steps/<stepid:step>');
     $step_r->get('/view')->to(action => 'view');
     $step_r->get('/edit')->name('edit_step')->to(action => 'edit');
-    $step_r->get('/src', [format => ['txt']])->name('src_step')->to(action => 'src', format => undef);
+    if ($Mojolicious::VERSION > 9.10) {
+        $step_r->get('/src', [format => ['txt']])->name('src_step')->to(action => 'src', format => undef);
+    }
+    elsif ($Mojolicious::VERSION < 9) {
+        $step_r->get('/src')->name('src_step')->to(action => 'src');
+    }
     $step_auth->post('/')->name('save_needle_ajax')->to('step#save_needle_ajax');
     $step_r->get('/')->name('step')->to(action => 'view');
 
