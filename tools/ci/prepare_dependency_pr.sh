75b34be430da2765be5b7357b47ff8d92d09a8d9
Oliver Kurz <okurz@suse.de>
2021-07-08 09:28:31 +0200

    Generalize CI scripts location
    Move more scripts from circleci to allow a potential easier migration to
    other CI services.

--- tools/ci/prepare_dependency_pr.sh
+++ tools/ci/prepare_dependency_pr.sh
@@ -5,17 +5,17 @@ depid=$(date -I)
 # more specific branch name if the same exists
 ! git ls-remote --exit-code -- origin "dependency_$depid" || depid=$(date +%Y-%m-%dT%H%M%S)
 git checkout -b dependency_"$depid"
-bash .circleci/build_dependencies.sh
+bash tools/ci/build_dependencies.sh
 
 git status
-git --no-pager diff .circleci/ci-packages.txt
-git diff --quiet .circleci/ci-packages.txt || (
+git --no-pager diff tools/ci/ci-packages.txt
+git diff --quiet tools/ci/ci-packages.txt || (
     set +o pipefail
     sudo zypper -n install git hub curl
     hub --version
-    curl -s https://api.github.com/repos/os-autoinst/os-autoinst/commits | grep sha | head -n 1 | grep -o -E '[0-9a-f]{40}' > .circleci/autoinst.sha
-    git add .circleci/ci-packages.txt .circleci/autoinst.sha
-    # these might have changed by tidy in .circleci/build_dependencies.sh
+    curl -s https://api.github.com/repos/os-autoinst/os-autoinst/commits | grep sha | head -n 1 | grep -o -E '[0-9a-f]{40}' > tools/ci/autoinst.sha
+    git add tools/ci/ci-packages.txt tools/ci/autoinst.sha
+    # these might have changed by tidy in tools/ci/build_dependencies.sh
     git add cpanfile dbicdh dependencies.yaml tools/* script/* lib/* t/*
     git commit -m "Dependency cron $depid"
     git push -q -f https://token@github.com/openqabot/openQA.git dependency_"$depid"
