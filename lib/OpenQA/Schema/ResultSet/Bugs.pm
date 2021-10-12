56176a28b3fe59888eb1ee356f6d75042f3824e7
Oliver Kurz <okurz@suse.de>
2021-10-12 22:12:49 +0200

    OpenQA::Schema::ResultSet::Bugs: Use signatures

--- lib/OpenQA/Schema/ResultSet/Bugs.pm
+++ lib/OpenQA/Schema/ResultSet/Bugs.pm
@@ -3,26 +3,16 @@
 
 package OpenQA::Schema::ResultSet::Bugs;
 
-
-use Mojo::Base 'DBIx::Class::ResultSet';
-
+use Mojo::Base 'DBIx::Class::ResultSet', -signatures;
 use OpenQA::App;
 
 # inserts the bug if it is new, returns the bug if it has been refreshed, undef otherwise
-sub get_bug {
-    my ($self, $bugid, %attrs) = @_;
+sub get_bug ($self, $bugid = undef, %attrs) {
     return unless $bugid;
-
     my $bug = $self->find_or_new({bugid => $bugid, %attrs});
-
-    if (!$bug->in_storage) {
-        $bug->insert;
-        OpenQA::App->singleton->emit_event(openqa_bug_create => {id => $bug->id, bugid => $bug->bugid, implicit => 1});
-    }
-    elsif ($bug->refreshed && $bug->existing) {
-        return $bug;
-    }
-
+    return ($bug->refreshed && $bug->existing) ? $bug : undef if $bug->in_storage;
+    $bug->insert;
+    OpenQA::App->singleton->emit_event(openqa_bug_create => {id => $bug->id, bugid => $bug->bugid, implicit => 1});
     return undef;
 }
 
