9ef5d494d69dbbf324212da7f0443b5ad03c2c9f
Moritz Kodytek <moritz.kodytek@suse.com>
2022-01-24 16:49:33 +0100

    Add some more perl signatures
    https://progress.opensuse.org/issues/105127

--- lib/OpenQA/CacheService/Controller/Influxdb.pm
+++ lib/OpenQA/CacheService/Controller/Influxdb.pm
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 package OpenQA::CacheService::Controller::Influxdb;
-use Mojo::Base 'Mojolicious::Controller';
+use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub minion {
     my $self = shift;
@@ -23,8 +23,7 @@ sub minion {
     $self->render(text => $text);
 }
 
-sub _output_measure {
-    my ($url, $key, $states) = @_;
+sub _output_measure ($url, $key, $states) {
     my $line = "$key,url=$url ";
     $line .= join(',', map { "$_=$states->{$_}i" } sort keys %$states);
     return $line . "\n";
