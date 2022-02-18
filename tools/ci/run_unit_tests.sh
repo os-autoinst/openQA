2850133d19f79ec54c1a1975d1f6fe7636f57d2a
Cris Dywan <cdywan@suse.de>
2022-02-18 18:13:43 +0100

    ci: Retain fullstack temp folder in artifacts
    Fixes: https://progress.opensuse.org/issues/107002

--- tools/ci/run_unit_tests.sh
+++ tools/ci/run_unit_tests.sh
@@ -19,4 +19,5 @@ sudo chown -R $USER test-results
 # circleCI can be particularly slow sometimes since some time around 2021-06
 export OPENQA_TEST_TIMEOUT_SCALE_CI=3
 export EXTRA_PROVE_ARGS="-v"
+export OPENQA_FULLSTACK_TEMP_DIR=$PWD/test-results/fullstack
 make test-$target
