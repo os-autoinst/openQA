bcb68dffd85dc5a71476dd4bd1f517f4b27c4492
Dependency bot CI <skynet@open.qa>
2022-02-22 02:26:51 +0000

    Dependency cron 2022-02-22

--- lib/OpenQA/Parser/Format/Base.pm
+++ lib/OpenQA/Parser/Format/Base.pm
@@ -12,8 +12,9 @@ use OpenQA::Parser::Result::OpenQA;
 
 has include_results => 1;
 has generated_tests => sub { OpenQA::Parser::Result::OpenQA::Results->new };    #testsuites
-has generated_tests_results =>
-  sub { OpenQA::Parser::Result::OpenQA::Results->new }; #testsuites results - when include_result is set it includes also the test.
+has generated_tests_results => sub {
+    OpenQA::Parser::Result::OpenQA::Results->new;
+};    #testsuites results - when include_result is set it includes also the test.
 has generated_tests_output => sub { OpenQA::Parser::Result::OpenQA::Results->new };    #testcase results
 has generated_tests_extra => sub { OpenQA::Parser::Result::OpenQA::Results->new };    # tests extra data.
 
