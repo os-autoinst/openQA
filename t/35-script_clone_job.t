os-autoinst/openQA

commit e591fd06c52e7ea97239021a32e1f4a0d5c94fca

openqa-clone-job: Create jobs atomically to avoid dependency problems
* Non-atomic job creation means that the scheduler might see a half-created
  dependency tree and thus already starts half of it regardless of further
  parallel or directly chained dependencies (which therefore breaks
  parallel and directly chained dependencies).
* This change posts all jobs atomically (using a single API call) to avoid
  the problem described in the previous point.
* See https://progress.opensuse.org/issues/103962

lib/OpenQA/Script/CloneJob.pm
@@ -15,7 +15,7 @@ use Mojo::URL;
 use Mojo::JSON;    # booleans
 
 our @EXPORT = qw(
-  clone_job
+  clone_jobs
   clone_job_apply_settings
   clone_job_get_job
   clone_job_download_assets
@@ -34,7 +34,6 @@ sub is_global_setting ($key) { grep /^$key$/, GLOBAL_SETTINGS }
 
 sub clone_job_apply_settings ($argv, $depth, $settings, $options) {
     delete $settings->{NAME};    # usually autocreated
-    $settings->{is_clone_job} = 1;    # used to figure out if this is a clone operation
 
     for my $arg (@$argv) {
         # split arg into key and value
@@ -60,10 +59,10 @@ sub clone_job_apply_settings ($argv, $depth, $settings, $options) {
     }
 }
 
-sub clone_job_get_job ($jobid, $remote, $remote_url, $options) {
-    my $url = $remote_url->clone;
+sub clone_job_get_job ($jobid, $url_handler, $options) {
+    my $url = $url_handler->{remote_url}->clone;
     $url->path("jobs/$jobid");
-    my $tx = $remote->max_redirects(3)->get($url);
+    my $tx = $url_handler->{remote}->max_redirects(3)->get($url);
     if ($tx->error) {
         my $err = $tx->error;
         # there is no code for some error reasons, e.g. 'connection refused'
@@ -84,11 +83,11 @@ sub _job_setting_is ($job, $key, $expected_value) {
     return $actual_value && $actual_value eq $expected_value;
 }
 
-sub _get_chained_parents ($job, $remote, $remote_url, $options, $parents = [], $parent_ids = {}) {
+sub _get_chained_parents ($job, $url_handler, $options, $parents = [], $parent_ids = {}) {
     next if $parent_ids->{$job->{id}}++;
-    my @direct_parents = map { clone_job_get_job($_, $remote, $remote_url, $options) } @{$job->{parents}->{Chained}};
+    my @direct_parents = map { clone_job_get_job($_, $url_handler, $options) } @{$job->{parents}->{Chained}};
     push @$parents, @direct_parents;
-    _get_chained_parents($_, $remote, $remote_url, $options, $parents, $parent_ids) for @direct_parents;
+    _get_chained_parents($_, $url_handler, $options, $parents, $parent_ids) for @direct_parents;
     return $parents;
 }
 
@@ -106,8 +105,10 @@ sub _job_publishes_uefi_vars ($job, $file) {
     $job->{settings}->{UEFI} && _job_setting_is $job, PUBLISH_PFLASH_VARS => $file;
 }
 
-sub clone_job_download_assets ($jobid, $job, $remote, $remote_url, $ua, $options) {
-    my $parents = _get_chained_parents($job, $remote, $remote_url, $options);
+sub clone_job_download_assets ($jobid, $job, $url_handler, $options) {
+    my $parents = _get_chained_parents($job, $url_handler, $options);
+    my $ua = $url_handler->{ua};
+    my $remote_url = $url_handler->{remote_url};
     for my $type (keys %{$job->{assets}}) {
         next if $type eq 'repo';    # we can't download repos
         for my $file (@{$job->{assets}->{$type}}) {
@@ -176,7 +177,7 @@ sub create_url_handler ($options) {
     $remote_url->path('/api/v1/jobs');
     my $remote = OpenQA::Client->new(api => $options->{host});
 
-    return ($ua, $local, $local_url, $remote, $remote_url);
+    return {ua => $ua, local => $local, local_url => $local_url, remote => $remote, remote_url => $remote_url};
 }
 
 sub openqa_baseurl ($local_url) {
@@ -204,14 +205,18 @@ sub get_deps ($job, $options, $job_type) {
     return ($chained // [], $directly_chained // [], $parallel // []);
 }
 
-sub handle_tx ($tx, $jobid, $job, $base_url, $options, $clone_map, $depth, $child_list) {
+sub handle_tx ($tx, $url_handler, $options, $jobs) {
     my $res = $tx->res;
     my $json = $res->json;
-    if (!$tx->error && ref $json eq 'HASH' && $json->{id}) {
-        my $clone_id = $clone_map->{$jobid} = $json->{id};
-        print "Created job #$clone_id: $job->{name} -> $base_url/t$clone_id\n";
-        clone_job($_, $options, $clone_map, $depth + 1, $clone_id) for @$child_list;    # clone children if needed
-        return $clone_id;
+    if (!$tx->error && ref $json eq 'HASH' && ref $json->{ids} eq 'HASH') {
+        my $cloned_jobs = $json->{ids};
+        my $base_url = openqa_baseurl($url_handler->{local_url});
+        for my $orig_job_id (keys %$cloned_jobs) {
+            my $orig_job = $jobs->{$orig_job_id};
+            my $cloned_job_id = $cloned_jobs->{$orig_job_id};
+            print "Created job #$cloned_job_id: $orig_job->{name} -> $base_url/t$cloned_job_id\n";
+        }
+        return $cloned_jobs;
     }
     elsif (my $body = $res->body) {
         die 'Failed to create job, server replied: ', pp(ref $json ? $json : $body), "\n";
@@ -221,43 +226,42 @@ sub handle_tx ($tx, $jobid, $job, $base_url, $options, $clone_map, $depth, $chil
     }
 }
 
-sub clone_job ($jobid, $options, $clone_map = {}, $depth = 0, $parent_jobid = 0) {
-    return $clone_map->{$jobid} if defined $clone_map->{$jobid};
+sub clone_jobs ($jobid, $options) {
+    my $url_handler = create_url_handler($options);
+    clone_job($jobid, $url_handler, $options, my $post_params = {}, my $jobs = {});
+    my $tx = post_jobs($post_params, $url_handler, $options);
+    handle_tx($tx, $url_handler, $options, $jobs);
+}
 
-    my ($ua, $local, $local_url, $remote, $remote_url) = create_url_handler($options);
-    my $job = clone_job_get_job($jobid, $remote, $remote_url, $options);
+sub clone_job ($jobid, $url_handler, $options, $post_params = {}, $jobs = {}, $depth = 0, $parent_jobid = 0) {
+    return $post_params if defined $post_params->{$jobid};
+
+    my $job = $jobs->{$jobid} = clone_job_get_job($jobid, $url_handler, $options);
     my @job_types = ('parents');
     push @job_types, 'children' if $options->{'clone-children'} && $depth == 0;
     my $child_list;
     for my $job_type (@job_types) {
         if ($job->{$job_type}) {
             my ($chained, $directly_chained, $parallel) = get_deps($job, $options, $job_type);
-
-            print "Cloning dependencies of $job->{name}\n" if (@$chained || @$directly_chained || @$parallel);
+            print "Cloning $job_type of $job->{name}\n" if (@$chained || @$directly_chained || @$parallel);
             push @$child_list, @$parallel if (@$parallel && $job_type eq 'children');
             for my $dependencies ($chained, $directly_chained, $parallel) {
                 # don't clone parallel jobs yet if children job type
                 next if $dependencies == $parallel && $job_type eq 'children';
-                clone_job($_, $options, $clone_map, $depth + 1) for @$dependencies;
+                clone_job($_, $url_handler, $options, $post_params, $jobs, $depth + 1) for @$dependencies;
                 # abort here if the job has already been cloned as part of the preceding recursive clone_job call
-                return $clone_map->{$jobid} if defined $clone_map->{$jobid};
+                return $post_params if defined $post_params->{$jobid};
             }
-
-            my @new_chained = map { $clone_map->{$_} } @$chained;
-            my @new_directly_chained = map { $clone_map->{$_} } @$directly_chained;
-            my @new_parallel = map { $clone_map->{$_} } @$parallel;
-
-            $job->{settings}->{_PARALLEL_JOBS} = join(',', @new_parallel) if @new_parallel && !defined $child_list;
-            $job->{settings}->{_PARALLEL_JOBS} = join(',', $parent_jobid) if $parent_jobid > 0;
-            $job->{settings}->{_START_AFTER_JOBS} = join(',', @new_chained) if @new_chained;
-            $job->{settings}->{_START_DIRECTLY_AFTER_JOBS} = join(',', @new_directly_chained) if @new_directly_chained;
+            $job->{settings}->{_PARALLEL} = join(',', @$parallel) if @$parallel && !defined $child_list;
+            $job->{settings}->{_PARALLEL} = join(',', $parent_jobid) if $parent_jobid > 0;
+            $job->{settings}->{_START_AFTER} = join(',', @$chained) if @$chained;
+            $job->{settings}->{_START_DIRECTLY_AFTER} = join(',', @$directly_chained) if @$directly_chained;
         }
     }
 
-    clone_job_download_assets($jobid, $job, $remote, $remote_url, $ua, $options) unless $options->{'skip-download'};
+    clone_job_download_assets($jobid, $job, $url_handler, $options) unless $options->{'skip-download'};
 
-    my $url = $local_url->clone;
-    my $source_url = $remote_url->clone;
+    my $source_url = $url_handler->{remote_url}->clone;
     $source_url->path("/tests/$jobid");
     my %settings = %{$job->{settings}};
     $settings{CLONED_FROM} = $source_url->to_string;
@@ -265,10 +269,22 @@ sub clone_job ($jobid, $options, $clone_map = {}, $depth = 0, $parent_jobid = 0)
         $settings{_GROUP_ID} = $group_id;
     }
     clone_job_apply_settings($options->{args}, $depth, \%settings, $options);
-    print Cpanel::JSON::XS->new->pretty->encode(\%settings) if $options->{verbose};
+    $post_params->{$jobid} = \%settings;
 
-    my $tx = $local->max_redirects(3)->post($url, form => \%settings);
-    handle_tx($tx, $jobid, $job, openqa_baseurl($local_url), $options, $clone_map, $depth, $child_list);
+    # clone children if needed
+    clone_job($_, $url_handler, $options, $post_params, $jobs, $depth + 1, $jobid) for @$child_list;
+}
+
+sub post_jobs ($post_params, $url_handler, $options) {
+    my %composed_params = map {
+        my $job_id = $_;
+        my $params_for_job = $post_params->{$job_id};
+        map { my $key = "$_:$job_id"; $key => $params_for_job->{$_} } keys %$params_for_job
+    } keys %$post_params;
+    $composed_params{is_clone_job} = 1;    # used to figure out if this is a clone operation
+    print Cpanel::JSON::XS->new->pretty->encode(\%composed_params) if $options->{verbose};
+    my ($local, $local_url) = ($url_handler->{local}, $url_handler->{local_url}->clone);
+    return $local->max_redirects(3)->post($local_url, form => \%composed_params);
 }
 
 1;

t/35-script_clone_job.t
@@ -61,14 +61,12 @@ subtest 'clone job apply settings tests' => sub {
     $test_settings{HDD_1} = 'new.qcow2';
     $test_settings{HDDSIZEGB} = 40;
     $test_settings{WORKER_CLASS} = 'local';
-    $test_settings{is_clone_job} = 1;
     delete $test_settings{NAME};
     clone_job_apply_settings(\@argv, 0, \%child_settings, \%options);
     is_deeply(\%child_settings, \%test_settings, 'cloned child job with correct global setting and new settings');
 
     %test_settings = %parent_settings;
     $test_settings{WORKER_CLASS} = 'local';
-    $test_settings{is_clone_job} = 1;
     delete $test_settings{NAME};
     clone_job_apply_settings(\@argv, 1, \%parent_settings, \%options);
     is_deeply(\%parent_settings, \%test_settings, 'cloned parent job only take global setting');
@@ -77,23 +75,22 @@ subtest 'clone job apply settings tests' => sub {
 subtest '_GROUP and _GROUP_ID override each other' => sub {
     my %settings = ();
     clone_job_apply_settings([qw(_GROUP=foo _GROUP_ID=bar)], 0, \%settings, \%options);
-    is_deeply(\%settings, {_GROUP_ID => 'bar', is_clone_job => 1}, '_GROUP_ID overrides _GROUP');
+    is_deeply(\%settings, {_GROUP_ID => 'bar'}, '_GROUP_ID overrides _GROUP');
     %settings = ();
     clone_job_apply_settings([qw(_GROUP_ID=bar _GROUP=foo)], 0, \%settings, \%options);
-    is_deeply(\%settings, {_GROUP => 'foo', is_clone_job => 1}, '_GROUP overrides _GROUP_ID');
+    is_deeply(\%settings, {_GROUP => 'foo'}, '_GROUP overrides _GROUP_ID');
 };
 
 subtest 'delete empty setting' => sub {
     my %settings = ();
     clone_job_apply_settings([qw(ISO_1= ADDONS=)], 0, \%settings, \%options);
-    is_deeply(\%settings, {is_clone_job => 1}, 'all empty settings removed');
+    is_deeply(\%settings, {}, 'all empty settings removed');
 };
 
 subtest 'asset download' => sub {
     my $temp_assetdir = tempdir;
     my $fake_ua = Test::FakeLWPUserAgent->new;
-    my $remote = undef;    # should not be used here (there are no parents)
-    my $remote_url = Mojo::URL->new('http://foo');
+    my %url_handler = (remote_url => Mojo::URL->new('http://foo'), ua => $fake_ua);
     my %options = (dir => $temp_assetdir);
     my $job_id = 1;
     my %job = (
@@ -108,7 +105,7 @@ subtest 'asset download' => sub {
     );
     my $clone_mock = Test::MockModule->new('OpenQA::Script::CloneJob');
     $clone_mock->redefine(
-        clone_job_get_job => sub ($job_id, $remote, $remove_url, $options) {
+        clone_job_get_job => sub ($job_id, $url_handler, $options) {
             return {id => 2, settings => {}, parents => {Chained => [3, 4]}} if $job_id eq 2;
             return {id => 3, settings => {PUBLISH_HDD_1 => 'some.qcow2'}} if $job_id eq 3;
             return {id => 4, settings => {UEFI => 1, PUBLISH_PFLASH_VARS => 'uefi-vars.qcow2'}} if $job_id eq 4;
@@ -121,23 +118,23 @@ subtest 'asset download' => sub {
         "http://foo/tests/$job_id/asset/hdd/another.qcow2" => "$temp_assetdir/hdd/another.qcow2",
     );
 
-    throws_ok { clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options) }
+    throws_ok { clone_job_download_assets($job_id, \%job, \%url_handler, \%options) }
     qr/can't write $temp_assetdir/, 'error because folder does not exist';
 
     $temp_assetdir->child($_)->make_path for qw(iso hdd);
     $fake_ua->missing(1);
     throws_ok {
-        combined_like { clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options) }
+        combined_like { clone_job_download_assets($job_id, \%job, \%url_handler, \%options) }
         qr/downloading.*foo.*to.*failed.*some status/s, 'download error logged';
     }
     qr/Can't clone due to missing assets: some status/, 'error if asset does not exist';
 
     $options{'ignore-missing-assets'} = 1;
-    combined_like { clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options) }
+    combined_like { clone_job_download_assets($job_id, \%job, \%url_handler, \%options) }
     qr/downloading.*foo.*to.*failed.*some status.*downloading.*bar.*failed/s, 'download error logged but ignored';
 
     $fake_ua->mirrored({})->missing(0);
-    combined_like { clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options) }
+    combined_like { clone_job_download_assets($job_id, \%job, \%url_handler, \%options) }
     qr{downloading.*http://.*foo.iso.*to.*foo.iso.*downloading.*http://.*bar.iso.*to.*bar.iso}s, 'download logged';
     is_deeply($fake_ua->mirrored, \%expected_downloads,
         'assets downloadeded except HDDs which are generated by parent job anyways')
@@ -149,7 +146,7 @@ subtest 'asset download' => sub {
     $options{'skip-deps'} = 1;
     $expected_downloads{"http://foo/tests/$job_id/asset/hdd/some.qcow2"} = "$temp_assetdir/hdd/some.qcow2";
     $expected_downloads{"http://foo/tests/$job_id/asset/hdd/uefi-vars.qcow2"} = "$temp_assetdir/hdd/uefi-vars.qcow2";
-    clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options);
+    clone_job_download_assets($job_id, \%job, \%url_handler, \%options);
     is_deeply($fake_ua->mirrored, \%expected_downloads,
         'assets downloadeded including HDDs because we skip cloning the parent job')
       or diag explain $fake_ua->mirrored;
@@ -157,7 +154,7 @@ subtest 'asset download' => sub {
     $fake_ua->mirrored({});
     $job{parents} = {Chained => [3, 5]};
     delete $expected_downloads{"http://foo/tests/$job_id/asset/hdd/uefi-vars.qcow2"};
-    clone_job_download_assets($job_id, \%job, $remote, $remote_url, $fake_ua, \%options);
+    clone_job_download_assets($job_id, \%job, \%url_handler, \%options);
     is_deeply($fake_ua->mirrored, \%expected_downloads,
         'assets downloadeded except uefi-vars because no parent produces it anyways')
       or diag explain $fake_ua->mirrored;
@@ -222,14 +219,15 @@ subtest 'get 2 nodes HA cluster with get_deps' => sub {
 
 subtest 'error handling' => sub {
     my $tx = Mojo::Transaction->new;
-    my $test = sub { OpenQA::Script::CloneJob::handle_tx($tx, 42, {name => 'testjob'}, 'base-url', {}, {}, 0, []) };
+    my %url_handler = (local_url => Mojo::URL->new('https://base-url/foo/bar'));
+    my $test = sub { OpenQA::Script::CloneJob::handle_tx($tx, \%url_handler, {}, {42 => {name => 'testjob'}}) };
     throws_ok { $test->() } qr/Failed to create job, empty response/, 'empty response handled';
     $tx->res->body('foobar');
     throws_ok { $test->() } qr/Failed to create job, server replied: "foobar"/, 'unexpected non-JSON handled';
     $tx->res->code(200)->body('{"foo": "bar"}');
     throws_ok { $test->() } qr/Failed to create job, server replied: \{ foo => "bar" \}/, 'unexpected JSON handled';
-    ($tx = Mojo::Transaction->new)->res->code(200)->body('{"id": 43}');
-    combined_like { $test->() } qr|Created job #43: testjob -> base-url/t43|, 'expected response';
+    ($tx = Mojo::Transaction->new)->res->code(200)->body('{"ids": {"42": 43}}');
+    combined_like { $test->() } qr|Created job #43: testjob -> https://base-url/t43|, 'expected response';
 };
 
 subtest 'overall cloning with parallel and chained dependencies' => sub {
@@ -237,9 +235,9 @@ subtest 'overall cloning with parallel and chained dependencies' => sub {
     my $ua_mock = Test::MockModule->new('Mojo::UserAgent');
     my $clone_mock = Test::MockModule->new('OpenQA::Script::CloneJob');
     my @post_args;
+    my $tx_handled = 0;
     $ua_mock->redefine(post => sub { push @post_args, [@_] });
-    $clone_mock->redefine(handle_tx =>
-          sub ($tx, $job_id, $job, $base_url, $options, $clone_map, @args) { $clone_map->{$job_id} = 100 + $job_id });
+    $clone_mock->redefine(handle_tx => sub ($tx, $url_handler, $options, $jobs) { $tx_handled = 1 });
 
     # fake the jobs to be cloned
     my %fake_jobs = (
@@ -256,25 +254,30 @@ subtest 'overall cloning with parallel and chained dependencies' => sub {
     $clone_mock->redefine(clone_job_get_job => sub ($job_id, @args) { $fake_jobs{$job_id} });
 
     my %options = (host => 'foo', from => 'bar', 'clone-children' => 1, 'skip-download' => 1, verbose => 1, args => []);
-    throws_ok { OpenQA::Script::CloneJob::clone_job(42, \%options) } qr|API key/secret missing|,
+    throws_ok { OpenQA::Script::CloneJob::clone_jobs(42, \%options) } qr|API key/secret missing|,
       'dies on missing API credentials';
 
-    my %clone_map;
     $options{apikey} = $options{apisecret} = 'bar';
-    combined_like { OpenQA::Script::CloneJob::clone_job(42, \%options, \%clone_map) } qr|parent.*main.*child|s,
+    combined_like { OpenQA::Script::CloneJob::clone_jobs(42, \%options) } qr|parent.*main.*child|s,
       'verbose output printed';
-    cmp_ok scalar @post_args, '>=', 3, 'clones for all dependencies posted' or diag explain \@post_args;
+    ok $tx_handled, 'transaction handled';
 
     # note: We'd actually only expect 3 clones to be posted. The 4th job is wrong. This is a limitation of the clone-job
     #       script when mixing parallel and chained dependencies in the way this test does.
 
-    is_deeply \%clone_map, {41 => 141, 42 => 142, 43 => 143}, 'jobs cloned as expected' or diag explain \%clone_map;
-    is $post_args[0]->[3]->{TEST}, 'parent', 'parent job 41 cloned (as 141)';
-    is $post_args[1]->[3]->{TEST}, 'main', 'main job 42 cloned (as 142)';
-    is $post_args[1]->[3]->{_PARALLEL_JOBS}, '141', 'main job cloned to start parallel with parent job 141';
-    is $post_args[2]->[3]->{TEST}, 'child', 'child job 43 cloned (as 143)';
-    is $post_args[2]->[3]->{_START_AFTER_JOBS}, '142', 'child job cloned to start after main job 142';
-    is scalar @post_args, 3, 'exactly 3 jobs posted';
+    subtest 'post args' => sub {
+        is scalar @post_args, 1, 'exactly one post call made' or return;
+        my $params = $post_args[0]->[3] // {};
+        is delete $params->{is_clone_job}, 1, 'is_clone_job-flag set';
+        is delete $params->{'TEST:41'}, 'parent', 'parent job 41 cloned';
+        is delete $params->{'TEST:42'}, 'main', 'main job 42 cloned';
+        is delete $params->{'_PARALLEL:42'}, '41', 'main job cloned to start parallel with parent job 41';
+        is delete $params->{'TEST:43'}, 'child', 'child job 43 cloned';
+        is delete $params->{'_START_AFTER:43'}, '42', 'child job cloned to start after main job 42';
+        is delete $params->{'group_id:42'}, 21, 'group of 42 preserved';
+        is delete $params->{"CLONED_FROM:$_"}, "https://bar/tests/$_", "CLONED_FROM set ($_)" for 41, 42, 43;
+        is scalar keys %$params, 0, 'exactly 3 jobs posted, so no further settings';
+    } or diag explain \@post_args;
 };
 
 done_testing();

script/openqa-clone-job
@@ -202,7 +202,7 @@ sub parse_options() {
 sub main() {
     my ($jobid, $host) = parse_options();
     return unless $jobid;
-    clone_job($jobid, \%options);
+    clone_jobs($jobid, \%options);
 }
 
 main;

t/09-job_clone.t
@@ -76,14 +76,14 @@ subtest 'get job' => sub {
     my $temp_assetdir = tempdir;
     my %options = (@common_options, dir => $temp_assetdir);
     my $job_id = 4321;
-    my ($ua, $local, $local_url, $remote, $remote_url) = create_url_handler(\%options);
+    my $url_handler = create_url_handler(\%options);
     throws_ok {
-        clone_job_get_job($job_id, $remote, $remote_url, \%options)
+        clone_job_get_job($job_id, $url_handler, \%options)
     }
     qr/failed to get job '$job_id'/, 'invalid job id results in error';
 
     $job_id = 99937;
-    combined_like { clone_job_get_job($job_id, $remote, $remote_url, \%options) } qr/^$/, 'got job';
+    combined_like { clone_job_get_job($job_id, $url_handler, \%options) } qr/^$/, 'got job';
 };
 
 subtest 'get job with verbose output' => sub {
@@ -94,12 +94,11 @@ subtest 'get job with verbose output' => sub {
 
     my $temp_assetdir = tempdir;
     my %options = (@common_options, dir => $temp_assetdir, verbose => 1);
-    my ($ua, $local, $local_url, $remote, $remote_url);
-    combined_like { ($ua, $local, $local_url, $remote, $remote_url) = create_url_handler(\%options); } qr/^$/,
+    my $url_handler;
+    combined_like { $url_handler = create_url_handler(\%options); } qr/^$/,
       'Configured user agent without unexpected output';
     my $job_id = 99937;
-    combined_like { clone_job_get_job($job_id, $remote, $remote_url, \%options) } qr/"id" : $job_id/,
-      'Job settings logged';
+    combined_like { clone_job_get_job($job_id, $url_handler, \%options) } qr/"id" : $job_id/, 'Job settings logged';
 };
 
 done_testing();

t/api/04-jobs.t
@@ -979,6 +979,7 @@ subtest 'handle settings when posting job' => sub {
 subtest 'do not re-generate settings when cloning job' => sub {
     my $job_settings = $jobs->search({test => 'autoupgrade'})->first->settings_hash;
     clone_job_apply_settings([qw(BUILD_SDK= ISO_MAXSIZE=)], 0, $job_settings, {});
+    $job_settings->{is_clone_job} = 1;
     $t->post_ok('/api/v1/jobs', form => $job_settings)->status_is(200);
     my $new_job_settings = $jobs->find($t->tx->res->json->{id})->settings_hash;
     delete $job_settings->{is_clone_job};

commit e370aecf7de259886016178dfa562d493dc9198e

openqa-clone-job: Fix adding parameters from CLI to job settings
This was broken by e591fd06c52e7 and 927a01292ab9. This change adds tests
and checks for whether job settings are passed correctly (to children by
default and by parents when `--parental-inheritance` is specified).

lib/OpenQA/Script/CloneJob.pm
@@ -43,7 +43,7 @@ sub clone_job_apply_settings ($argv, $depth, $settings, $options) {
         }
         my ($key, $value) = ($1, $2);
 
-        next unless (is_global_setting($key) or $depth == 0 or $options->{'parental-inheritance'});
+        next unless is_global_setting($key) || $depth <= 1 || $options->{'parental-inheritance'};
 
         # delete key if value empty
         if (!defined $value || $value eq '') {
@@ -233,7 +233,7 @@ sub clone_jobs ($jobid, $options) {
     handle_tx($tx, $url_handler, $options, $jobs);
 }
 
-sub clone_job ($jobid, $url_handler, $options, $post_params = {}, $jobs = {}, $depth = 1) {
+sub clone_job ($jobid, $url_handler, $options, $post_params = {}, $jobs = {}, $depth = 1, $relation = '') {
     return $post_params if defined $post_params->{$jobid};
 
     my $job = $jobs->{$jobid} = clone_job_get_job($jobid, $url_handler, $options);
@@ -253,7 +253,7 @@ sub clone_job ($jobid, $url_handler, $options, $post_params = {}, $jobs = {}, $d
                 next if $max_depth && $depth > $max_depth;
                 next unless $clone_children || $dependencies == $parallel;
             }
-            clone_job($_, $url_handler, $options, $post_params, $jobs, $depth + 1) for @$dependencies;
+            clone_job($_, $url_handler, $options, $post_params, $jobs, $depth + 1, $job_type) for @$dependencies;
         }
         if ($job_type ne 'children') {
             $settings->{_PARALLEL} = join(',', @$parallel) if @$parallel;
@@ -266,7 +266,7 @@ sub clone_job ($jobid, $url_handler, $options, $post_params = {}, $jobs = {}, $d
 
     $settings->{CLONED_FROM} = $url_handler->{remote_url}->clone->path("/tests/$jobid")->to_string;
     if (my $group_id = $job->{group_id}) { $settings->{_GROUP_ID} = $group_id }
-    clone_job_apply_settings($options->{args}, $depth, $settings, $options);
+    clone_job_apply_settings($options->{args}, $relation eq 'children' ? 0 : $depth, $settings, $options);
 }
 
 sub post_jobs ($post_params, $url_handler, $options) {

t/35-script_clone_job.t
@@ -62,13 +62,13 @@ subtest 'clone job apply settings tests' => sub {
     $test_settings{HDDSIZEGB} = 40;
     $test_settings{WORKER_CLASS} = 'local';
     delete $test_settings{NAME};
-    clone_job_apply_settings(\@argv, 0, \%child_settings, \%options);
+    clone_job_apply_settings(\@argv, 1, \%child_settings, \%options);
     is_deeply(\%child_settings, \%test_settings, 'cloned child job with correct global setting and new settings');
 
     %test_settings = %parent_settings;
     $test_settings{WORKER_CLASS} = 'local';
     delete $test_settings{NAME};
-    clone_job_apply_settings(\@argv, 1, \%parent_settings, \%options);
+    clone_job_apply_settings(\@argv, 2, \%parent_settings, \%options);
     is_deeply(\%parent_settings, \%test_settings, 'cloned parent job only take global setting');
 };
 
@@ -253,7 +253,8 @@ subtest 'overall cloning with parallel and chained dependencies' => sub {
     );
     $clone_mock->redefine(clone_job_get_job => sub ($job_id, @args) { $fake_jobs{$job_id} });
 
-    my %options = (host => 'foo', from => 'bar', 'clone-children' => 1, 'skip-download' => 1, verbose => 1, args => []);
+    my %options
+      = (host => 'foo', from => 'bar', 'clone-children' => 1, 'skip-download' => 1, verbose => 1, args => ['FOO=bar']);
     throws_ok { OpenQA::Script::CloneJob::clone_jobs(42, \%options) } qr|API key/secret missing|,
       'dies on missing API credentials';
 
@@ -274,19 +275,37 @@ subtest 'overall cloning with parallel and chained dependencies' => sub {
     };
     subtest 'post args' => sub {
         my $params = $check_common_post_args->() or return;
+        is delete $params->{'FOO:42'}, 'bar', 'setting passed to main job';
         is delete $params->{'TEST:43'}, 'child', 'child job 43 cloned';
+        is delete $params->{'FOO:43'}, 'bar', 'setting passed to child job';
         is delete $params->{'_START_AFTER:43'}, '42', 'child job cloned to start after main job 42';
         is delete $params->{"CLONED_FROM:$_"}, "https://bar/tests/$_", "CLONED_FROM set ($_)" for 41, 42, 43;
         is scalar keys %$params, 0, 'exactly 3 jobs posted, so no further settings';
     } or diag explain \@post_args;
 
+    subtest 'clone with parential inheritance' => sub {
+        @post_args = ();
+        $options{'clone-children'} = undef;
+        $options{'parental-inheritance'} = 1;
+        combined_like { OpenQA::Script::CloneJob::clone_jobs(42, \%options) } qr/cloning/i, 'output logged';
+        subtest 'post args' => sub {
+            my $params = $check_common_post_args->() or return;
+            is delete $params->{'FOO:41'}, 'bar', 'setting passed to parent job';
+            is delete $params->{'FOO:42'}, 'bar', 'setting passed to main job';
+            is delete $params->{"CLONED_FROM:$_"}, "https://bar/tests/$_", "CLONED_FROM set ($_)" for 41, 42;
+            is scalar keys %$params, 0, 'exactly 2 jobs posted, so no further settings';
+        } or diag explain \@post_args;
+    };
+
     subtest 'clone only parallel children' => sub {
         @post_args = ();
         $fake_jobs{41}->{children}->{Chained} = [7];
-        $options{'clone-children'} = undef;
-        OpenQA::Script::CloneJob::clone_jobs(41, \%options);
+        $options{'parental-inheritance'} = undef;
+        combined_like { OpenQA::Script::CloneJob::clone_jobs(41, \%options) } qr/cloning/i, 'output logged';
         subtest 'post args' => sub {
             my $params = $check_common_post_args->() or return;
+            is delete $params->{'FOO:41'}, 'bar', 'setting passed to main job';
+            is delete $params->{'FOO:42'}, 'bar', 'setting passed to child job';
             is delete $params->{"CLONED_FROM:$_"}, "https://bar/tests/$_", "CLONED_FROM set ($_)" for 41, 42;
             is scalar keys %$params, 0, 'exactly 2 jobs posted, so no further settings';
         } or diag explain \@post_args;