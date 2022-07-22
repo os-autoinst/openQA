366b892a10ab8c1393f30d3b9f5830d8121b9805
Oliver Kurz <okurz@suse.de>
2022-07-22 12:29:54 +0200

    Use new Leap repository name format

--- tools/ci/build_dependencies.sh
+++ tools/ci/build_dependencies.sh
@@ -17,8 +17,8 @@ listdeps() {
 
 listdeps > $DEPS_BEFORE
 
-sudo zypper ar --priority 91 -f https://download.opensuse.org/repositories/devel:openQA/openSUSE_Leap_15.4 devel_openQA
-sudo zypper ar --priority 90 -f https://download.opensuse.org/repositories/devel:/openQA:/Leap:/15.4/openSUSE_Leap_15.4 devel_openQA_Leap
+sudo zypper ar --priority 91 -f https://download.opensuse.org/repositories/devel:openQA/15.4 devel_openQA
+sudo zypper ar --priority 90 -f https://download.opensuse.org/repositories/devel:/openQA:/Leap:/15.4/15.4 devel_openQA_Leap
 tools/retry sudo sh -c 'zypper --gpg-auto-import-keys ref && sudo zypper --no-refresh -n install openQA-devel perl-TAP-Harness-JUnit'
 
 listdeps > $DEPS_AFTER
