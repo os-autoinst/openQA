c275422bb9455a3a3f5c680a46af8665ca0b99ab
Cris Dywan <cdywan@suse.de>
2022-10-28 18:40:59 +0200

    Use signatures in OpenQA::Scheduler
    Including Client, Controller::API and Model::Jobs

--- lib/OpenQA/Scheduler/Client.pm
+++ lib/OpenQA/Scheduler/Client.pm
@@ -16,15 +16,13 @@ my $IS_SCHEDULER_ITSELF;
 sub mark_current_process_as_scheduler { $IS_SCHEDULER_ITSELF = 1; }
 sub is_current_process_the_scheduler { return $IS_SCHEDULER_ITSELF; }
 
-sub new {
-    my $class = shift;
+sub new ($class, @args) {
     die 'creating an OpenQA::Scheduler::Client from the scheduler itself is forbidden'
       if is_current_process_the_scheduler;
-    $class->SUPER::new(@_);
+    $class->SUPER::new(@args);
 }
 
-sub wakeup {
-    my ($self, $worker_id) = @_;
+sub wakeup ($self) {
     return if $self->{wakeup};
     $self->{wakeup}++;
     $self->client->max_connections(0)->request_timeout(5)->get_p($self->_api('wakeup'))
--- lib/OpenQA/Scheduler/Controller/API.pm
+++ lib/OpenQA/Scheduler/Controller/API.pm
@@ -2,12 +2,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 package OpenQA::Scheduler::Controller::API;
-use Mojo::Base 'Mojolicious::Controller';
+use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 use OpenQA::Scheduler;
 
-sub wakeup {
-    my $self = shift;
+sub wakeup ($self) {
     OpenQA::Scheduler::wakeup();
     $self->render(text => 'ok');
 }
