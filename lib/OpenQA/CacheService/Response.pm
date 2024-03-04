d27d56db1eff5d3d6ae1ae39d694a1d575fe67a5
Marius Kittler <mkittler@suse.de>
2024-03-04 18:10:18 +0100

    Make it less likely for jobs incompleting with `Cache … queue … full`
    * Introduce a hard-limit for inactive Minion jobs
    * Make the hard-limit slightly higher than the normal limit
    * Enforce only the hard-limit on openQA job setup; so when the normal
      Minion queue limit was reached after a job had already been picked-up it
      can still continue to create Minion jobs instead of ending up as
      incomplete
    * See https://progress.opensuse.org/issues/128276

--- lib/OpenQA/CacheService/Response.pm
+++ lib/OpenQA/CacheService/Response.pm
@@ -5,7 +5,14 @@ package OpenQA::CacheService::Response;
 use Mojo::Base -base, -signatures;
 
 has [qw(data error)];
+
+# define soft-limit for inactive Minion jobs (enforced when determining idle worker slots availability)
 has max_inactive_jobs => $ENV{OPENQA_CACHE_MAX_INACTIVE_JOBS} // 5;
+
+# define hard-limit for inactive Minion jobs (enforced on setup of already started openQA job)
+has max_inactive_jobs_hard_limit => sub ($self) {
+    $ENV{OPENQA_CACHE_MAX_INACTIVE_JOBS_HARD_LIMIT} // ($self->max_inactive_jobs + 40);
+};
 
 sub has_error ($self) { !!$self->error }
 
