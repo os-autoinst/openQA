94e5f9fcf66a16151bd63280c690210928634236
Marius Kittler <mkittler@suse.de>
2024-04-08 16:17:56 +0200

    Add explicit ESLint config for compatibility with ESLint 9.0.0
    * Avoid running into `Error: Could not find config file.` with ESLint 9.0.0
      by adding an explicit config file
    * Configure Prettier plugin/config as before

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+];
