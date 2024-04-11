2b11ce52cecd749dc6747f184fdb1133a43949af
Marius Kittler <mkittler@suse.de>
2024-04-11 16:58:12 +0200

    Fix warnings about using undefined values when computing needle details
    Apparently the name is not always set. Considering this warning was always
    ignored anyway we can also silence it completely. The only bad consequence
    is that the redirection won't go to the right step but it looks like this
    was never guaranteed to happen anyway.
    
    I also changed the code to use `any` instead of `grep` because that makes
    more sense for a check like this.
    
    Related ticket: https://progress.opensuse.org/issues/158808

--- lib/OpenQA/WebAPI/Controller/Admin/Needle.pm
+++ lib/OpenQA/WebAPI/Controller/Admin/Needle.pm
@@ -9,6 +9,7 @@ use OpenQA::Utils;
 use OpenQA::WebAPI::ServerSideDataTable;
 use Date::Format 'time2str';
 use DateTime::Format::Pg;
+use List::Util qw(any);
 use Time::Seconds;
 
 sub index ($self) { $self->render('admin/needle/index') }
@@ -109,8 +110,8 @@ sub module ($self) {
 
     my $index = 1;
     for my $detail (@{$module->results->{details}}) {
-        last if $detail->{needle} eq $needle;
-        last if grep { $needle eq $_->{name} } @{$detail->{needles} || []};
+        last if $needle eq ($detail->{needle} // '');
+        last if any { $needle eq ($_->{name} // '') } @{$detail->{needles} || []};
         $index++;
     }
     $self->redirect_to('step', testid => $module->job_id, moduleid => $module->name(), stepid => $index);
