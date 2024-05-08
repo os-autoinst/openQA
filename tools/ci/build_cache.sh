d1b7633f705f3aa9a3a547f662a8b0ab204d8f99
Oliver Kurz <okurz@suse.de>
2024-05-08 20:35:05 +0200

    Update to Leap 15.6 where applicable
    Related progress issue: https://progress.opensuse.org/issues/157984

--- tools/ci/build_cache.sh
+++ tools/ci/build_cache.sh
@@ -5,8 +5,8 @@
 
 set -ex
 
-sudo zypper ar -f -p 90 https://download.opensuse.org/repositories/devel:/openQA:/Leap:/15.5/15.5 openQA
-sudo zypper ar -f -p 95 http://download.opensuse.org/repositories/devel:openQA/15.5 devel
+sudo zypper ar -f -p 90 https://download.opensuse.org/repositories/devel:/openQA:/Leap:/15.6/15.6 openQA
+sudo zypper ar -f -p 95 http://download.opensuse.org/repositories/devel:openQA/15.6 devel
 tools/retry sudo zypper --gpg-auto-import-keys ref
 sudo zypper -n install --download-only $(sed -e 's/\r//' < tools/ci/ci-packages.txt)
 sudo rpm -i -f $(find /var/cache/zypp/packages/ | grep '.rpm$')
