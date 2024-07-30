42267c3e3a4924b3233b5897b2f5a39ffd164ea9
Marius Kittler <mkittler@suse.de>
2024-07-30 13:10:52 +0200

    Avoid incomplete jobs when Git cloning fails
    * Retry Git processing instead of immediately failing the Minion job (and
      thus also setting the related openQA job to incomplete)
    * See https://progress.opensuse.org/issues/163784

--- lib/OpenQA/Task/Git/Clone.pm
+++ lib/OpenQA/Task/Git/Clone.pm
@@ -27,10 +27,11 @@ sub _git_clone_all ($job, $clones) {
 
     # Prevent multiple git clone tasks for the same path to run in parallel
     my @guards;
+    my $retry_delay = {delay => 30 + int(rand(10))};
     for my $path (sort keys %$clones) {
         $path = Mojo::File->new($path)->realpath if (-e $path);    # resolve symlinks
         my $guard = $app->minion->guard("git_clone_${path}_task", 2 * ONE_HOUR);
-        return $job->retry({delay => 30 + int(rand(10))}) unless $guard;
+        return $job->retry($retry_delay) unless $guard;
         push(@guards, $guard);
     }
 
@@ -41,7 +42,11 @@ sub _git_clone_all ($job, $clones) {
     for my $path (sort { length($a) <=> length($b) } keys %$clones) {
         my $url = $clones->{$path};
         die "Don't even think about putting '..' into '$path'." if $path =~ /\.\./;
-        _git_clone($job, $ctx, $path, $url);
+        eval { _git_clone($job, $ctx, $path, $url) };
+        next unless my $error = $@;
+        my $max_retries = $ENV{OPENQA_GIT_CLONE_RETRIES} // 10;
+        return $job->retry($retry_delay) if $job->retries < $max_retries;
+        return $job->fail($error);
     }
 }
 
