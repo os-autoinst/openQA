e7eb938d30e36167d40f51848d928b387e9ede65
robert.richardson <robert.richardson@suse.com>
2024-08-16 15:30:50 +0200

    Avoid array recreation with _run_cmd helper method
    - Minor fixes

--- lib/OpenQA/Task/Git/Clone.pm
+++ lib/OpenQA/Task/Git/Clone.pm
@@ -1,5 +1,6 @@
 # Copyright SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
+
 package OpenQA::Task::Git::Clone;
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 use OpenQA::Git;
@@ -12,10 +13,16 @@ sub register ($self, $app, @) {
     $app->minion->add_task(git_clone => \&_git_clone_all);
 }
 
+# $clones is a hashref with paths as keys and urls to git repos as values.
+# The urls may also refer to a branch via the url fragment.
+# If no branch is set, the default branch of the remote (if target path doesn't exist yet)
+# or local checkout is used.
+# If the target path already exists, an update of the specified branch is performed,
+# if the url matches the origin remote.
+
 sub _git_clone_all ($job, $clones) {
     my $app = $job->app;
     my $job_id = $job->id;
-
 
     # Prevent multiple git clone tasks for the same path to run in parallel
     my @guards;
@@ -32,10 +39,9 @@ sub _git_clone_all ($job, $clones) {
 
     # iterate clones sorted by path length to ensure that a needle dir is always cloned after the corresponding casedir
     for my $path (sort { length($a) <=> length($b) } keys %$clones) {
-        my $git = OpenQA::Git->new(app => $app, dir => $path);
         my $url = $clones->{$path};
         die "Don't even think about putting '..' into '$path'." if $path =~ /\.\./;
-        eval { _git_clone($git, $job, $ctx, $path, $url) };
+        eval { _git_clone($app, $job, $ctx, $path, $url) };
         next unless my $error = $@;
         my $max_retries = $ENV{OPENQA_GIT_CLONE_RETRIES} // 10;
         return $job->retry($retry_delay) if $job->retries < $max_retries;
@@ -43,7 +49,8 @@ sub _git_clone_all ($job, $clones) {
     }
 }
 
-sub _git_clone ($git, $job, $ctx, $path, $url) {
+sub _git_clone ($app, $job, $ctx, $path, $url) {
+    my $git = OpenQA::Git->new(app => $app, dir => $path);
     $ctx->debug(qq{Updating $path to $url});
     $url = Mojo::URL->new($url);
     my $requested_branch = $url->fragment;
@@ -54,7 +61,7 @@ sub _git_clone ($git, $job, $ctx, $path, $url) {
     die "Unable to detect remote default branch for '$url'" unless $remote_default_branch;
 
     if (!-d $path) {
-        $git->git_clone_url_to_path($url, $path);
+        $git->git_clone_url($url);
         # update local branch to latest remote branch version
         $git->git_fetch("$requested_branch:$requested_branch")
           if ($requested_branch ne $remote_default_branch);
