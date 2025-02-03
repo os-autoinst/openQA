7c2e4a95c6dff95490048da8f35d793de71c841f
Wabri <12409541+Wabri@users.noreply.github.com>
2025-02-03 17:09:57 +0100

    Use needles from correct ref of NEEDLES_DIR
    If the NEEDLES_DIR (or CASEDIR as a backup) variable is a git repo URL
    with a fragment specifying the ref of the repository, we attempt to use
    it as the source of needles when viewing needle diffs in test results.
    
    If the NEEDLES_GIT_SHA variable is set, we use that instead as it should
    be the most accurate way to determine exactly which commit was used

--- lib/OpenQA/Setup.pm
+++ lib/OpenQA/Setup.pm
@@ -69,8 +69,9 @@ sub read_config ($app) {
             do_push => 'no',
             do_cleanup => 'no',
             git_auto_clone => 'yes',
-            git_auto_update => 'no',
             git_auto_update_method => 'best-effort',
+            git_auto_update => 'yes',
+            checkout_needles_sha => 'no',
         },
         scheduler => {
             max_job_scheduled_time => 7,
