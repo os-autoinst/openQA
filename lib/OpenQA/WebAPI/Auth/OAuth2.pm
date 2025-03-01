os-autoinst/openQA

commit fb002508a3ac27304637308a1a524247cf1e8546

Support OpenID Connect better in the OAuth2 'custom' provider
The OpenID Connect standard specifies that `sub` contains the user ID.
Until now, openQA only looked for a field named `id`.

When using Keycloak as identity provider there is no such field, it
returns only `sub` as expected in OpenID Connect.

To avoid breaking any existing configs, this patch adds a new `id_from`
config field which defaults to `id`, so existing behaviour is preserved.
Set `id_from = sub` in the OAuth2 provider config to get the new
behaviour.

Fixes: https://github.com/os-autoinst/openQA/issues/5771

lib/OpenQA/WebAPI/Auth/OAuth2.pm
@@ -44,6 +44,7 @@ sub auth_setup ($server) {
                 user_url => $config->{user_url},
                 token_scope => $config->{token_scope},
                 token_label => $config->{token_label},
+                id_from => $config->{id_from},
                 nickname_from => $config->{nickname_from},
                 unique_name => $config->{unique_name},
             },
@@ -69,16 +70,18 @@ sub update_user ($controller, $main_config, $provider_config, $data) {
         return $controller->render(text => $msg, status => 403);    # return always 403 for consistency
     }
     my $details = $tx->res->json;
-    if (ref $details ne 'HASH' || !$details->{id} || !$details->{$provider_config->{nickname_from}}) {
+    my $id_field = $provider_config->{id_from};
+    my $nickname_field = $provider_config->{nickname_from};
+    if (ref $details ne 'HASH' || !$details->{$id_field} || !$details->{$nickname_field}) {
         log_debug('OAuth2 user provider returned: ' . dumper($details));
         return $controller->render(text => 'User data returned by OAuth2 provider is insufficient', status => 403);
     }
     my $provider_name = $main_config->{provider};
     $provider_name = $provider_config->{unique_name} || $provider_name if $provider_name eq 'custom';
     my $user = $controller->schema->resultset('Users')->create_user(
-        $details->{id},
+        $details->{$id_field},
         provider => "oauth2\@$provider_name",
-        nickname => $details->{$provider_config->{nickname_from}},
+        nickname => $details->{$nickname_field},
         fullname => $details->{name},
         email => $details->{email});
 

lib/OpenQA/Setup.pm
@@ -96,6 +96,7 @@ sub read_config ($app) {
             token_label => '',
             nickname_from => '',
             unique_name => '',
+            id_from => 'id',
         },
         hypnotoad => {
             listen => ['http://localhost:9526/'],

t/03-auth.t
@@ -116,7 +116,7 @@ subtest OAuth2 => sub {
     $ua_mock->redefine(get => sub { shift; push @get_args, [@_]; $get_tx });
 
     my %main_cfg = (provider => 'custom');
-    my %provider_cfg = (user_url => 'http://does-not-exist', token_label => 'bar', nickname_from => 'login');
+    my %provider_cfg = (user_url => 'http://does-not-exist', token_label => 'bar', id_from => 'id', nickname_from => 'login');
     my %data = (access_token => 'some-token');
     my %expected_user = (username => 42, provider => 'oauth2@custom', nickname => 'Demo');
     my $users = $t->app->schema->resultset('Users');

t/config.t
@@ -87,6 +87,7 @@ subtest 'Test configuration default modes' => sub {
             user_url => '',
             token_scope => '',
             token_label => '',
+            id_from => 'id',
             nickname_from => '',
             unique_name => '',
         },

commit fe1484e9072c8f6899005366c125bf42721c48be

Fix non-custom OAuth2 providers
The fb002508a commit introduced usage of 'id_from' config parameter. And
while the commit messsage says it defaults to the old value 'id', in
fact the parameter is set only for the custom provider, and in all the
other cases it isn't included in $provider_config at all.

Fix this by having default/fallback value also where the variable is
actually used, thus avoiding the need to set it in every supported
provider.

This un-breaks authentication via Github, among other things.

Fixes: fb002508a "Support OpenID Connect better in the OAuth2 'custom' provider"

lib/OpenQA/WebAPI/Auth/OAuth2.pm
@@ -70,7 +70,7 @@ sub update_user ($controller, $main_config, $provider_config, $data) {
         return $controller->render(text => $msg, status => 403);    # return always 403 for consistency
     }
     my $details = $tx->res->json;
-    my $id_field = $provider_config->{id_from};
+    my $id_field = $provider_config->{id_from} // 'id';
     my $nickname_field = $provider_config->{nickname_from};
     if (ref $details ne 'HASH' || !$details->{$id_field} || !$details->{$nickname_field}) {
         log_debug('OAuth2 user provider returned: ' . dumper($details));