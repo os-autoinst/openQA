95206c9d4a7b148b91fc5f1af6b564653f462cc4
Marius Kittler <mkittler@suse.de>
2025-03-20 13:26:44 +0100

    Show job ID only in AMQP log messages when actually present
    Otherwise it can be confusing if the event ID is actually a job ID like for
    certain job events.
    
    Related ticket: https://progress.opensuse.org/issues/178147

--- lib/OpenQA/WebAPI/Plugin/AMQP.pm
+++ lib/OpenQA/WebAPI/Plugin/AMQP.pm
@@ -73,8 +73,9 @@ sub publish_amqp ($self, $topic, $event_data, $headers = {}, $remaining_attempts
         sub ($error) {
             my $left = looks_like_number $remaining_attempts && $remaining_attempts > 1 ? $remaining_attempts - 1 : 0;
             my $delay = $retry_delay * $config->{publish_retry_delay_factor};
-            my ($event_id, $job_id) = ($event_data->{id} // 'none', $event_data->{job_id} // 'none');
-            log_error "Publishing $topic failed: $error (event ID: $event_id, job ID: $job_id, $left attempts left)";
+            my ($event_id, $job_id) = ($event_data->{id} // 'none', $event_data->{job_id});
+            my $additional_info = $job_id ? ", job ID: $job_id" : '';
+            log_error "Publishing $topic failed: $error (event ID: $event_id$additional_info, $left attempts left)";
             my $retry_function = sub ($loop) { $self->publish_amqp($topic, $event_data, $headers, $left, $delay) };
             Mojo::IOLoop->timer($retry_delay => $retry_function) if $left;
         })->finally(sub { undef $publisher });
