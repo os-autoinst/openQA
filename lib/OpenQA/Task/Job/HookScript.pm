c8df6ddffd0b0943d5d5ac6f5764036d6cfe81e2
Ioannis Bonatakis <ybonatakis@suse.com>
2025-05-02 06:28:22 +0200

    Add linear backoff in hook script
    Use the _delay_ which comes from FinalizeResults to override the default Minion
    delay behavior and force a linear backoff calculation based on the retries in
    case of job failure and until it reaches the max retries. The default
    _retries_ also reduced to achieve approximately one day before give up .This
    should offer a less agressive pattern, giving the Minion tasks more time to
    recover from various issues and prevent overloading. Linear backoff is
    selected over exponential as the later is already applied to the Minion tasks
    in general.
    
    https://progress.opensuse.org/issues/181427
    
    Co-authored-by: perlpunk  <tina.mueller@suse.com>

--- lib/OpenQA/Task/Job/HookScript.pm
+++ lib/OpenQA/Task/Job/HookScript.pm
@@ -13,16 +13,17 @@ sub _hook_script ($job, $hook, $openqa_job_id, $options) {
     my $ensure_task_retry_on_termination_signal_guard = OpenQA::Task::SignalGuard->new($job);
     my $timeout = $options->{timeout};
     my $kill_timeout = $options->{kill_timeout};
-    my $delay = $options->{delay};
     my $retries = $options->{retries};
+    my $delay = $options->{delay} * ($job->retries + 1);
     my $skip_rc = $options->{skip_rc};
 
     $ensure_task_retry_on_termination_signal_guard->abort(1);
     my ($rc, $out) = _run_hook($hook, $openqa_job_id, $timeout, $kill_timeout);
     $job->note(hook_cmd => $hook, hook_result => $out, hook_rc => $rc);
 
-    if ($retries && $skip_rc) {
-        $job->retry($delay ? {delay => $delay} : {}) if defined $rc && $rc == $skip_rc && $job->retries < $retries;
+    if ($retries && $skip_rc && defined $rc && $rc == $skip_rc && $job->retries < $retries) {
+        $job->app->log->debug("Job @{[$job->id]}: Retrying @{[$job->retries]} times with linear delay: ${delay}s");
+        $job->retry($delay ? {delay => $delay} : {});
     }
 }
 
