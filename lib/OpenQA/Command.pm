os-autoinst/openQA

commit 47c3c7bec49310c945f30bbbf9e2c3f068611028

Deduplicate openqa-cli options and help

lib/OpenQA/Command.pm
@@ -96,19 +96,15 @@ sub parse_params ($self, $args, $param_file) {
 
 sub run ($self, @args) {
     my %options = (pretty => 0, quiet => 0, links => 0, verbose => 0);
-    getopt \@args, ['pass_through'],
-      'apibase=s' => sub { $self->apibase($_[1]) },
-      'apikey=s' => sub { $self->apikey($_[1]) },
-      'apisecret=s' => sub { $self->apisecret($_[1]) },
-      'host=s' => sub { $self->host($_[1] =~ m!^/|://! ? $_[1] : "https://$_[1]") },
-      'o3' => sub { $self->host('https://openqa.opensuse.org') },
-      'osd' => sub { $self->host('http://openqa.suse.de') },
-      'odn' => sub { $self->host('https://openqa.debian.net') },
-      'L|links' => \$options{links},
-      'name=s' => sub { $self->name($_[1]) },
-      'p|pretty' => \$options{pretty},
-      'q|quiet' => \$options{quiet},
-      'v|verbose' => \$options{verbose};
+    OpenQA::CLI::get_opt(global => \@args, ['pass_through'], \%options);
+    for (qw(apibase apikey apisecret name)) {
+        $self->$_($options{$_}) if $options{$_};
+    }
+    my $host = $options{host};
+    $self->host($host =~ m!^/|://! ? $host : "https://$host") if $host;
+    $self->host('https://openqa.opensuse.org') if $options{o3};
+    $self->host('http://openqa.suse.de') if $options{osd};
+    $self->host('https://openqa.debian.net') if $options{odn};
 
     return $self->options(\%options)->command(@args);
 }

dist/rpm/openQA.spec
@@ -404,6 +404,9 @@ ln -s %{_datadir}/openqa/script/dump-db %{buildroot}%{_bindir}/openqa-dump-db
 ln -s %{_datadir}/openqa/script/openqa-label-all %{buildroot}%{_bindir}/openqa-label-all
 %endif
 
+install -d -m 755 %{buildroot}%{_datadir}/openqa/client
+install -m 755 public/openqa-cli.yaml %{buildroot}%{_datadir}/openqa/client/openqa-cli.yaml
+
 # munin
 %if %{with munin_package}
 install -d -m 755 %{buildroot}/%{_prefix}/lib/munin/plugins
@@ -767,6 +770,8 @@ fi
 
 %files client
 %dir %{_datadir}/openqa
+%dir %{_datadir}/openqa/client
+%{_datadir}/openqa/client/openqa-cli.yaml
 %dir %{_datadir}/openqa/script
 %{_datadir}/openqa/script/client
 %{_datadir}/openqa/script/clone_job.pl

lib/OpenQA/CLI.pm
@@ -2,63 +2,92 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 package OpenQA::CLI;
-use Mojo::Base 'Mojolicious::Commands';
+use Mojo::Base 'Mojolicious::Commands', -signatures;
+use Mojo::Util qw(getopt encode);
+use YAML::PP;
+use FindBin '$RealBin';
 
 has hint => <<EOF;
 
 See 'openqa-cli COMMAND --help' for more information on a specific command.
 EOF
-has message => sub { shift->extract_usage . "\nCommands:\n" };
+has message => sub { OpenQA::CLI->_help() };
 has namespaces => sub { ['OpenQA::CLI'] };
 
+my $specfile = "$RealBin/../public/openqa-cli.yaml";
+# packaged location
+$specfile = "$RealBin/../client/openqa-cli.yaml" unless -f $specfile;
+
+my $app_spec;
+sub _get_global_options { $app_spec->{options} }
+
+sub _get_options ($name) { $app_spec->{subcommands}->{$name}->{options}; }
+
+sub run ($self, @args) {
+    $app_spec = YAML::PP::LoadFile($specfile);
+    return $self->SUPER::run(@args);
+}
+
+sub get_opt ($type, $args, $p, $result) {
+    my $opts = $type eq 'global' ? _get_global_options() : _get_options($type);
+    my %getopt;
+    for my $option (@$opts) {
+        my ($spec, $name);
+        if (ref $option) {
+            $name = $option->{name};
+            $spec = _option_hash_to_spec($option);
+        }
+        else {
+            $spec = $option =~ s/ .*//rs;
+            ($name) = $spec =~ m/^([\w-]+)/;
+        }
+        $getopt{$spec} = \$result->{$name};
+    }
+    getopt $args, $p, %getopt;
+}
+
+sub _help ($self, $name = undef) {
+    my $help = '';
+    my $global_opts = _print_options('Options (for all commands):', _get_global_options());
+    if ($name) {
+        my $cmd = $app_spec->{subcommands}->{$name};
+        $help .= "$cmd->{description}\n" if $cmd->{description};
+        $help .= $global_opts;
+        $help .= _print_options("Options for $name:", _get_options($name));
+    }
+    else {
+        $help = "$global_opts\n";
+    }
+    return encode 'UTF-8' => $help;
+}
+
+sub _print_options ($label, $options) {
+    my @rows;
+    my @getopts;
+    for my $option (@$options) {
+        my ($spec, $desc);
+        my $getopt = '';
+        if (ref $option) {
+            $spec = _option_hash_to_spec($option);
+            $desc = $option->{summary};
+        }
+        else {
+            ($spec, $desc) = split / +--/, $option;
+        }
+        push @getopts, [$spec, $desc =~ s/\s+/ /gr];
+    }
+    require Getopt::Long::Descriptive;
+    my (undef, $usage) = Getopt::Long::Descriptive::describe_options('', @getopts);
+    return $label . $usage;
+}
+
+sub _option_hash_to_spec ($opt) {
+    my $name = $opt->{name};
+    my $spec = $name;
+    my $aliases = $opt->{aliases};
+    $spec .= '|' . join '|', @{$opt->{aliases}} if $opt->{aliases};
+    $spec .= '=s' if $opt->{type} // 'string' ne 'flag';
+    return $spec;
+}
+
 1;
-
-=encoding utf8
-
-=head1 NAME
-
-openqa-cli - provides command-line access to the openQA API
-
-=head1 SYNOPSIS
-
-  Usage: openqa-cli COMMAND [OPTIONS]
-
-    # Show api command help with all available options and more examples
-    openqa-cli api --help
-
-    # Show details for job from localhost
-    openqa-cli api jobs/4160811
-
-    # Show details for job from arbitrary host
-    openqa-cli api --host http://openqa.example.com jobs/408
-
-    # Show details for OSD job (prettified JSON)
-    openqa-cli api --osd --pretty jobs/4160811
-
-    # Archive job from O3
-    openqa-cli archive --o3 408 /tmp/job_408
-
-  Options (for all commands):
-        --apibase <path>        API base, defaults to /api/v1
-        --apikey <key>          API key
-        --apisecret <secret>    API secret
-        --host <host>           Target host, defaults to http://localhost
-    -h, --help                  Get more information on a specific command
-        --osd                   Set target host to http://openqa.suse.de
-        --o3                    Set target host to https://openqa.opensuse.org
-        --odn                   Set target host to https://openqa.debian.net
-
-  Configuration:
-    API key and secret are read from "client.conf" if not specified via CLI
-    arguments. The config file is checked for under "$OPENQA_CONFIG",
-    "~/.config/openqa" and "/etc/openqa" in this order. It must look like
-    this:
-
-      [openqa.opensuse.org]
-      key = 45ABCEB4562ACB04
-      secret = 4BA0003086C4CB95
-      [another.host]
-      key = D7345DA7B9D86B3B
-      secret = A98CDBA9C8DB87BD
-
-=cut

lib/OpenQA/CLI/api.pm
@@ -6,144 +6,34 @@ use Mojo::Base 'OpenQA::Command', -signatures;
 
 use Mojo::File 'path';
 use Mojo::JSON qw(decode_json);
-use Mojo::Util qw(getopt);
 use OpenQA::Constants qw(JOBS_OVERVIEW_SEARCH_CRITERIA);
 
 has description => 'Issue an arbitrary request to the API';
 has usage => sub {
     my $search_criteria = join(', ', JOBS_OVERVIEW_SEARCH_CRITERIA);
-    shift->extract_usage =~ s/\$search_criteria/$search_criteria/r;
+    OpenQA::CLI->_help('api') =~ s/\$search_criteria/$search_criteria/r;
 };
 
 sub command ($self, @args) {
-    die $self->usage
-      unless getopt \@args,
-      'a|header=s' => \my @headers,
-      'D|data-file=s' => \my $data_file,
-      'd|data=s' => \(my $data = ''),
-      'f|form' => \my $form,
-      'j|json' => \my $json,
-      'param-file=s' => \my @param_file,
-      'r|retries=i' => \my $retries,
-      'X|method=s' => \(my $method = 'GET');
+    die $self->usage unless OpenQA::CLI::get_opt(api => \@args, [], \my %options);
 
     @args = $self->decode_args(@args);
     die $self->usage unless my $path = shift @args;
+    my $data_file = $options{'data-file'};
+    my $data = $options{data} // '';
     $data = $data_file eq '-' ? $self->data_from_stdin : path($data_file)->slurp if $data_file;
 
-    my $params = $form ? decode_json($data) : $self->parse_params(\@args, \@param_file);
+    my $params = $options{form} ? decode_json($data) : $self->parse_params(\@args, $options{'param-file'});
     my @data = keys %$params ? (form => $params) : ($data);
 
-    my $headers = $self->parse_headers(@headers);
+    my $headers = $self->parse_headers(@{$options{header}});
     $headers->{Accept} //= 'application/json';
-    $headers->{'Content-Type'} = 'application/json' if $json;
+    $headers->{'Content-Type'} = 'application/json' if $options{json};
 
     my $url = $self->url_for($path);
     my $client = $self->client($url);
-    my $tx = $client->build_tx($method, $url, $headers, @data);
-    $self->retry_tx($client, $tx, $retries);
+    my $tx = $client->build_tx($options{method} // 'GET', $url, $headers, @data);
+    $self->retry_tx($client, $tx, $options{retries});
 }
 
 1;
-
-=encoding utf8
-
-=head1 SYNOPSIS
-
-  Usage: openqa-cli api [OPTIONS] PATH [PARAMS]
-
-    # Show details for job from localhost
-    openqa-cli api jobs/4160811
-
-    # Show details for job from arbitrary host
-    openqa-cli api --host http://openqa.example.com jobs/408
-
-    # Show details for job from OSD (prettified JSON)
-    openqa-cli api --osd --pretty jobs/4160811
-
-    # List all jobs (CAUTION: this might time out for a large instance)
-    openqa-cli api --host openqa.example.com jobs
-
-    # List all jobs matching the search criteria
-    openqa-cli api --osd jobs groupid=135 distri=caasp version=3.0 latest=1
-
-    # List the latest jobs matching the search criteria
-    # supported search criteria: $search_criteria
-    openqa-cli api --osd jobs/overview groupid=135 distri=caasp version=3.0
-
-    # Restart a job
-    openqa-cli api -X POST jobs/16/restart
-
-    # Delete job (CAUTION: destructive operation)
-    openqa-cli api --host openqa.example.com -X DELETE jobs/1
-
-    # Trigger a single job
-    openqa-cli api -X POST jobs ISO=foo.iso DISTRI=my-distri \
-      FLAVOR=my-flavor VERSION=42 BUILD=42 TEST=my-test
-
-    # Trigger a single set of jobs (see
-    # https://open.qa/docs/#_spawning_single_new_jobs_jobs_post for details)
-    openqa-cli api -X POST jobs \
-      TEST:0=first-job TEST:1=second-job _START_AFTER:1=0
-
-    # Trigger jobs on ISO "foo.iso" creating a "scheduled product" (see
-    # https://open.qa/docs/#_spawning_multiple_jobs_based_on_templates_isos_post
-    # for details, e.g for considering to use the `async` flag)
-    openqa-cli api --o3 -X POST isos ISO=foo.iso \
-      DISTRI=my-distri FLAVOR=my-flavor ARCH=my-arch VERSION=42 BUILD=1234
-
-    # Track scheduled product
-    openqa-cli api --o3 isos/1234
-
-    # Change group id for job
-    openqa-cli api --json --data '{"group_id":1}' -X PUT jobs/639172
-
-    # Change group id for job (pipe JSON data)
-    echo '{"group_id":1}' | openqa-cli api --json -X PUT jobs/639172
-
-    # Post job template
-    openqa-cli api -X POST job_templates_scheduling/1 \
-      schema=JobTemplates-01.yaml preview=0 template="$(cat foo.yaml)"
-
-    # Post job template (from file)
-    openqa-cli api -X POST job_templates_scheduling/1 \
-      schema=JobTemplates-01.yaml preview=0 --param-file template=foo.yaml
-
-    # Post job template (from JSON file)
-    openqa-cli api --data-file form.json -X POST job_templates_scheduling/1
-
-  Options:
-        --apibase <path>          API base, defaults to /api/v1
-        --apikey <key>            API key
-        --apisecret <secret>      API secret
-    -a, --header <name:value>     One or more additional HTTP headers
-    -D, --data-file <path>        Load content to send with request from file
-                                  or from stdin by specifying "-" as path
-    -d, --data <string>           Content to send with request
-    -f, --form                    Turn JSON object into form parameters
-        --host <host>             Target host, defaults to http://localhost
-    -h, --help                    Show this summary of available options
-    -j, --json                    Request content is JSON
-        --osd                     Set target host to http://openqa.suse.de
-        --o3                      Set target host to https://openqa.opensuse.org
-        --odn                     Set target host to https://openqa.debian.net
-        --param-file <param=file> Load content of params from files instead of
-                                  from command line arguments. Multiple params
-                                  may be specified by adding the option
-                                  multiple times
-    -L, --links                   Print pagination links to STDERR
-        --name <name>             Name of this client, used by openQA to
-                                  identify different clients via User-Agent
-                                  header, defaults to "openqa-cli"
-    -p, --pretty                  Pretty print JSON content
-    -q, --quiet                   Do not print error messages to STDERR
-    -r, --retries <retries>       Retry up to the specified value on some
-                                  errors. Retries can also be set by the
-                                  environment variable 'OPENQA_CLI_RETRIES',
-                                  defaults to no retry.
-                                  Set 'OPENQA_CLI_RETRY_SLEEP_TIME_S' to
-                                  configure the sleep time between retries.
-    -X, --method <method>         HTTP method to use, defaults to GET
-    -v, --verbose                 Print HTTP response headers
-
-=cut

lib/OpenQA/CLI/archive.pm
@@ -4,17 +4,11 @@
 package OpenQA::CLI::archive;
 use Mojo::Base 'OpenQA::Command', -signatures;
 
-use Mojo::Util qw(getopt);
-
 has description => 'Download assets and test results from a job';
-has usage => sub { shift->extract_usage };
+has usage => sub { OpenQA::CLI->_help('archive') };
 
 sub command ($self, @args) {
-
-    die $self->usage
-      unless getopt \@args,
-      'l|asset-size-limit=i' => \(my $limit),
-      't|with-thumbnails' => \my $thumbnails;
+    die $self->usage unless OpenQA::CLI::get_opt(archive => \@args, [], \my %options);
 
     @args = $self->decode_args(@args);
     die $self->usage unless my $job = shift @args;
@@ -23,42 +17,13 @@ sub command ($self, @args) {
     my $url = $self->url_for("jobs/$job/details");
     my $client = $self->client($url);
     $client->archive->run(
-        {url => $url, archive => $path, 'with-thumbnails' => $thumbnails, 'asset-size-limit' => $limit});
+        {
+            url => $url,
+            archive => $path,
+            'with-thumbnails' => $options{'with-thumbnails'},
+            'asset-size-limit' => $options{'asset-size-limit'}});
 
     return 0;
 }
 
 1;
-
-=encoding utf8
-
-=head1 SYNOPSIS
-
-  Usage: openqa-cli archive [OPTIONS] JOB PATH
-
-    # Download assets and test results from OSD to /tmp/job_416081
-    openqa-cli archive --osd 416081 /tmp/job_416081
-
-    # Download assets and test results from arbitrary host
-    openqa-cli archive --host http://openqa.example.com 407 /tmp/foo
-
-    # Download assets and test results from localhost (including thumbnails and
-    # very large assets)
-    openqa-cli archive -l 1048576000 -t 408 /tmp/bar
-
-  Options:
-        --apibase <path>           API base, defaults to /api/v1
-        --apikey <key>             API key
-        --apisecret <secret>       API secret
-        --host <host>              Target host, defaults to http://localhost
-    -h, --help                     Show this summary of available options
-    -l, --asset-size-limit <num>   Asset size limit in bytes
-        --name <name>              Name of this client, used by openQA to
-                                   identify different clients via User-Agent
-                                   header, defaults to "openqa-cli"
-        --osd                      Set target host to http://openqa.suse.de
-        --o3                       Set target host to https://openqa.opensuse.org
-        --odn                      Set target host to https://openqa.debian.net
-    -t, --with-thumbnails          Download thumbnails as well
-
-=cut

lib/OpenQA/CLI/monitor.pm
@@ -6,10 +6,10 @@ use Mojo::Base 'OpenQA::Command', -signatures;
 
 use OpenQA::Jobs::Constants;
 use List::Util qw(all);
-use Mojo::Util qw(encode getopt);
+use Mojo::Util qw(encode);
 
 has description => 'Monitors a set of jobs';
-has usage => sub { shift->extract_usage };
+has usage => sub { OpenQA::CLI->_help('monitor') };
 
 sub _monitor_jobs ($self, $client, $follow, $poll_interval, $job_ids, $job_results) {
     my $start = time;
@@ -45,39 +45,11 @@ sub _monitor_and_return ($self, $client, $follow, $poll_interval, $job_ids) {
 }
 
 sub command ($self, @args) {
-    die $self->usage unless getopt \@args,
-      'f|follow' => \my $follow,
-      'i|poll-interval=i' => \my $poll_interval;
+    die $self->usage unless OpenQA::CLI::get_opt(monitor => \@args, [], \my %options);
+
     @args = $self->decode_args(@args);
-    $self->_monitor_and_return($self->client($self->url_for('tests')), $follow, $poll_interval, \@args);
+    $self->_monitor_and_return($self->client($self->url_for('tests')),
+        $options{follow}, $options{'poll-interval'}, \@args);
 }
 
 1;
-
-=encoding utf8
-
-=head1 SYNOPSIS
-
-  Usage: openqa-cli monitor [OPTIONS] [JOB_IDS]
-
-  Wait until all specified jobs have reached a final state and return a non-zero
-  exit code if at least one job is not ok (is not passed or softfailed).
-
-  Options:
-        --apibase <path>           API base, defaults to /api/v1
-        --apikey <key>             API key
-        --apisecret <secret>       API secret
-        --host <host>              Target host, defaults to http://localhost
-    -h, --help                     Show this summary of available options
-        --osd                      Set target host to http://openqa.suse.de
-        --o3                       Set target host to https://openqa.opensuse.org
-        --name <name>              Name of this client, used by openQA to
-                                   identify different clients via User-Agent
-                                   header, defaults to "openqa-cli"
-    -f, --follow                   Use the newest clone of each monitored job
-    -i, --poll-interval <seconds>  Specifies the poll interval
-    -p, --pretty                   Pretty print JSON content
-    -q, --quiet                    Do not print error messages to STDERR
-    -v, --verbose                  Print HTTP response headers
-
-=cut

lib/OpenQA/CLI/schedule.pm
@@ -3,11 +3,10 @@
 
 package OpenQA::CLI::schedule;
 use Mojo::Base 'OpenQA::CLI::monitor', -signatures;
-use Mojo::Util qw(encode getopt);
 use Term::ANSIColor qw(colored);
 
 has description => 'Schedules a set of jobs (via "isos post" creating a schedule product)';
-has usage => sub { shift->extract_usage };
+has usage => sub { OpenQA::CLI->_help('schedule') };
 has post_url => sub { shift->url_for('isos') };
 
 sub _create_jobs ($self, $client, $args, $param_file, $job_ids) {
@@ -29,54 +28,15 @@ sub _create_jobs ($self, $client, $args, $param_file, $job_ids) {
 }
 
 sub command ($self, @args) {
-    die $self->usage
-      unless getopt \@args,
-      'param-file=s' => \my @param_file,
-      'm|monitor' => \my $monitor,
-      'f|follow' => \my $follow,
-      'i|poll-interval=i' => \my $poll_interval,
-      ;
+    die $self->usage unless OpenQA::CLI::get_opt(schedule => \@args, [], \my %options);
+
     @args = $self->decode_args(@args);
     my $client = $self->client($self->post_url);
 
     my @job_ids;
-    my $create_res = $self->_create_jobs($client, \@args, \@param_file, \@job_ids);
-    return $create_res if $create_res != 0 || !$monitor;
-    return $self->_monitor_and_return($client, $follow, $poll_interval, \@job_ids);
+    my $create_res = $self->_create_jobs($client, \@args, $options{'param-file'}, \@job_ids);
+    return $create_res if $create_res != 0 || !$options{monitor};
+    return $self->_monitor_and_return($client, $options{follow}, $options{'poll-interval'}, \@job_ids);
 }
 
 1;
-
-=encoding utf8
-
-=head1 SYNOPSIS
-
-  Usage: openqa-cli schedule [OPTIONS] DISTRI=… VERSION=… FLAVOR=… ARCH=… [ISO=… …]
-
-  Schedule a set of jobs creating a scheduled product on the web UI ("posts an ISO").
-
-  Options:
-        --apibase <path>           API base, defaults to /api/v1
-        --apikey <key>             API key
-        --apisecret <secret>       API secret
-        --host <host>              Target host, defaults to http://localhost
-    -h, --help                     Show this summary of available options
-        --osd                      Set target host to http://openqa.suse.de
-        --o3                       Set target host to https://openqa.opensuse.org
-        --param-file <param=file>  Load content of params from files instead of
-                                   from command line arguments. Multiple params
-                                   may be specified by adding the option
-                                   multiple times
-    -m, --monitor                  Wait until all jobs are done/cancelled and return
-                                   non-zero exit code if at least on job has not
-                                   passed/softfailed
-    -f, --follow                   Use the newest clone of each monitored job
-        --name <name>              Name of this client, used by openQA to
-                                   identify different clients via User-Agent
-                                   header, defaults to "openqa-cli"
-    -i, --poll-interval <seconds>  Specifies the poll interval used with --monitor
-    -p, --pretty                   Pretty print JSON content
-    -q, --quiet                    Do not print error messages to STDERR
-    -v, --verbose                  Print HTTP response headers
-
-=cut

commit 792f4d86cd2f7d2d07ea29b14b5157c7eef368d0

Allow empty values in openqa-cli parameters
    openqa-cli --apibase ''

did not set the parameter as expected

Issue: https://progress.opensuse.org/issues/186146

Bug introduced in 47c3c7bec

lib/OpenQA/Command.pm
@@ -98,7 +98,7 @@ sub run ($self, @args) {
     my %options = (pretty => 0, quiet => 0, links => 0, verbose => 0);
     OpenQA::CLI::get_opt(global => \@args, ['pass_through'], \%options);
     for (qw(apibase apikey apisecret name)) {
-        $self->$_($options{$_}) if $options{$_};
+        $self->$_($options{$_}) if defined $options{$_};
     }
     my $host = $options{host};
     $self->host($host =~ m!^/|://! ? $host : "https://$host") if $host;

t/43-cli-api.t
@@ -110,6 +110,10 @@ subtest 'API' => sub {
     throws_ok { $api->run(@auth) } qr/Usage: openqa-cli api/, 'usage';
     is $api->apikey, 'ARTHURKEY01', 'apikey';
     is $api->apisecret, 'EXCALIBUR', 'apisecret';
+
+    $api = OpenQA::CLI::api->new;
+    throws_ok { $api->run('--apibase', '') } qr/Usage: openqa-cli api/, 'usage';
+    is $api->apibase, '', 'set apibase to an empty string';
 };
 
 subtest 'Client' => sub {