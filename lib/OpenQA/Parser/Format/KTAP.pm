aca3f6bcd24512c5ef3ad634d6899518d2ff6801
Sebastian Chlad <sebastian.chlad@suse.com>
2025-08-11 08:23:17 +0200

    parser: ktap: Refactor to use internal state
    Refactor the initial implementation which was passing all needed variables
    as references, and instead store them in an object attribute. This
    simplifies method signatures and improves readability.

--- lib/OpenQA/Parser/Format/KTAP.pm
+++ lib/OpenQA/Parser/Format/KTAP.pm
@@ -5,99 +5,115 @@ package OpenQA::Parser::Format::KTAP;
 use Mojo::Base 'OpenQA::Parser::Format::Base', -signatures;
 
 # Translates KTAP kernel selftests format -> OpenQA internal representation
-use Carp qw(croak confess);
+use Carp qw(confess);
 use OpenQA::Parser::Result::OpenQA;
 use TAP::Parser;
 
 has [qw(test)];
+has state => sub { return { steps => undef, m => 0 } };
 
-sub _testgroup_init ($self, $line, $test_ref, $steps_ref, $m_ref) {
-    if ($$steps_ref && $self->test) {
-        $$steps_ref->{name} = $self->test->{name};
-        $$steps_ref->{test} = $self->test;
-        $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($$steps_ref));
-    }
-    my ($group_name) = $line =~ /^#\s*selftests:\s+(.*)/;
-    my $sanitized_group_name = "selftests: $group_name";
-    $sanitized_group_name =~ s/[\/.]/_/g;
-    $$test_ref = {
-        flags => {},
-        category => 'KTAP',
-        name => $sanitized_group_name,
-    };
-    $self->test(OpenQA::Parser::Result->new($$test_ref));
-    $self->_add_test($self->test);
-    $$steps_ref = OpenQA::Parser::Result->new(
-        {
-            details => [],
-            dents => 0,
-            result => 'passed'
-        });
-
-    $$m_ref = 0;
-}
-
-sub _parse_subtest ($self, $result, $test_ref, $steps_ref, $m_ref) {
-    return unless $$steps_ref && $self->test;
-    my $line = $result->as_string;
-    return unless $line =~ /^#\s*(ok|not ok)\s+\d+\s+(.+)/;
-    my ($status, $subtest_name) = ($1, $2);
-    my $filename = "KTAP-@{[$$test_ref->{name}]}-$$m_ref.txt";
-    push @{$$steps_ref->{details}},
-      {
-        text => $filename,
-        title => $subtest_name,
-        result => $status eq 'ok' ? 'ok' : 'fail',
-      };
-    $$steps_ref->{result} = 'fail' if $status ne 'ok';
-    $self->_add_output({file => $filename, content => $line});
-    $$m_ref++;
-}
-
-sub _testgroup_finalize ($self, $steps_ref, $result) {
-    return unless $$steps_ref && $self->test;
-
-    my $line = $result ? $result->as_string : '';
-    my $group_failed = 0;    # it is possible the subtests do not report 'not ok' but only the group
-    if ($line =~ /^not ok\b/i) {
-        $$steps_ref->{result} = 'fail';
-        $group_failed = 1;
-    }
-    elsif ($line =~ /#\s*SKIP\b/i) {
-        $$steps_ref->{result} = 'skip';
-    }
-    $$steps_ref->{name} = $self->test->{name};
-    $$steps_ref->{test} = $self->test;
-    $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($$steps_ref));
-    $self->test(undef);    # clear the group
-    $$steps_ref = undef;    # clear the subtests
-}
-
-sub parse ($self, $KTAP) {
-    my $parser = TAP::Parser->new({tap => $KTAP});
-    confess 'Failed ' . $parser->parse_errors if $parser->parse_errors;
-    my ($test, $steps, $m) = ({}, undef, 0);
-
-    while (my $result = $parser->next) {
-        next if $result->type eq 'version' || $result->type eq 'plan';
-        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*selftests:\s+(.*)/) {
-            $self->_testgroup_init($result->as_string, \$test, \$steps, \$m);
-            next;
-        }
-        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*(ok|not ok)\s+\d+\s+(.+)/) {
-            $self->_parse_subtest($result, \$test, \$steps, \$m);
-            next;
-        }
-        if ($result->type eq 'test' && $result->description =~ /^selftests: /) {
-            $self->_testgroup_finalize(\$steps, $result);
-            next;
-        }
-    }
-
-    if ($steps && $self->test) {
+sub _testgroup_init ($self, $line) {
+    # flush previous group if any
+    if ($self->state->{steps} && $self->test) {
+        my $steps = $self->state->{steps};
         $steps->{name} = $self->test->{name};
         $steps->{test} = $self->test;
         $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($steps));
+    }
+
+    my ($group_name) = $line =~ /^#\s*selftests:\s+(.*)/;
+    my $sanitized_group_name = "selftests: $group_name";
+    $sanitized_group_name =~ s/[\/.]/_/g;
+
+    my $group = {
+        flags    => {},
+        category => 'KTAP',
+        name     => $sanitized_group_name,
+    };
+
+    $self->test(OpenQA::Parser::Result->new($group));
+    $self->_add_test($self->test);
+
+    $self->state->{steps} = OpenQA::Parser::Result->new({
+        details => [],
+        dents   => 0,
+        result  => 'ok',
+    });
+    $self->state->{m} = 0;
+}
+
+sub _parse_subtest ($self, $result) {
+    my $steps = $self->state->{steps} or return;
+    $self->test or return;
+
+    my $line = $result->as_string;
+    return unless $line =~ /^#\s*(ok|not ok)\s+\d+\s+(.+)/;
+    my ($status, $subtest_name) = ($1, $2);
+
+    my $m        = $self->state->{m};
+    my $filename = "KTAP-@{[$self->test->{name}]}-$m.txt";
+
+    push @{$steps->{details}}, {
+        text   => $filename,
+        title  => $subtest_name,
+        result => $status eq 'ok' ? 'ok' : 'fail',
+    };
+    $steps->{result} = 'fail' if $status ne 'ok';
+
+    $self->_add_output({ file => $filename, content => $line });
+    $self->state->{m} = $m + 1;
+}
+
+sub _testgroup_finalize ($self, $result) {
+    my $steps = $self->state->{steps} or return;
+    $self->test or return;
+
+    my $line = $result ? $result->as_string : '';
+    if ($line =~ /^not ok\b/i) {
+        $steps->{result} = 'fail';
+    }
+    elsif ($line =~ /#\s*SKIP\b/i) {
+        $steps->{result} = 'skip';
+    }
+
+    $steps->{name} = $self->test->{name};
+    $steps->{test} = $self->test;
+
+    $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($steps));
+
+    $self->test(undef);
+    $self->state->{steps} = undef;
+    $self->state->{m}     = 0;
+}
+
+sub parse ($self, $KTAP) {
+    my $parser = TAP::Parser->new({ tap => $KTAP });
+    confess 'Fail to parse KTAP:' . $parser->parse_errors if $parser->parse_errors;
+
+    while (my $result = $parser->next) {
+        next if $result->type eq 'version' || $result->type eq 'plan';
+
+        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*selftests:\s+/) {
+            $self->_testgroup_init($result->as_string);
+            next;
+        }
+        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*(ok|not ok)\s+\d+\s+/) {
+            $self->_parse_subtest($result);
+            next;
+        }
+        if ($result->type eq 'test' && $result->description =~ /^selftests: /) {
+            $self->_testgroup_finalize($result);
+            next;
+        }
+    }
+
+    if (my $steps = $self->state->{steps} and $self->test) {
+        $steps->{name} = $self->test->{name};
+        $steps->{test} = $self->test;
+        $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($steps));
+        $self->test(undef);
+        $self->state->{steps} = undef;
+        $self->state->{m}     = 0;
     }
 }
 
@@ -148,23 +164,28 @@ Inherits from L<OpenQA::Parser::Format::Base>. Additional attributes include:
 
 An instance of L<OpenQA::Parser::Result> representing the current test group being parsed.
 
-=head2 steps
+=head2 state
 
-A temporary result object accumulating the individual subtest results for the current test group.
+A hashref used to accumulate transient parse state for the current test group:
+`steps` (L<OpenQA::Parser::Result>) and `m` (subtest index).
 
 =head1 METHODS
 
-=head2 _testgroup_init($line, $test_ref, $steps_ref, $m_ref)
+=head2 _testgroup_init($line)
 
 Initializes a new test group when a C<# selftests: groupname> comment is encountered.
 
-=head2 _parse_subtest($result, $test_ref, $steps_ref, $m_ref)
+=head2 _parse_subtest($result)
 
 Parses a subtest line of the form C<# ok 1 testname> and appends the result to the current steps.
 
-=head2 _testgroup_finalize($steps_ref)
+=head2 _testgroup_finalize($result)
 
 Finalizes the current test group and stores the accumulated results.
+
+=head2 parse($KTAP)
+
+Main entry point. Parses KTAP-formatted text and populates the parser’s result, test, and output collections.
 
 =cut
 
