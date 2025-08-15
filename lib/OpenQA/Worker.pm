os-autoinst/openQA

commit 512e43c4a1bf8c22defd4a1a03916ce446801478

Exit worker with non-zero return code in error cases
So systemd tries to restart the worker (instead of keeping it "inactive").

lib/OpenQA/Worker.pm
@@ -59,6 +59,7 @@ sub main {
     my $lockfd = lockit();
     my $dir;
     my $shared_cache;
+    my $return_code = 0;
     clean_pool();
 
     # register error handler
@@ -66,10 +67,11 @@ sub main {
         error => sub {
             my ($reactor, $err) = @_;
 
+            $return_code = 1;
             try {
                 # log error using print because logging utils might have caused the exception
                 # (no need to repeat $err, it is printed anyways)
-                print("stopping because a critical error occurred.\n");
+                log_error('stopping because a critical error occurred');
 
                 # try to stop the job nicely
                 stop('exception');
@@ -108,7 +110,7 @@ sub main {
     # start event loop - this will block until stop is called
     Mojo::IOLoop->start;
 
-    return 0;
+    return $return_code;
 }
 
 sub prepare_cache_directory {

script/worker
@@ -134,6 +134,6 @@ $ENV{QEMUPORT} = ($options{instance}) * 10 + 20002;
 $ENV{VNC}      = ($options{instance}) + 90;
 
 OpenQA::Worker::init($host_settings, \%options);
-OpenQA::Worker::main($host_settings);
+exit OpenQA::Worker::main($host_settings);
 
 # vim: set sw=4 sts=4 et:

commit 6694496eec877668ba51e1e80904fa46a466ac75

Fix return code of worker in error case
This restores the change introduced by 512e43c4a1b after it was broken
by 535f874433 when splitting the worker `main` function.

lib/OpenQA/Worker.pm
@@ -351,7 +351,7 @@ sub init ($self) {
             });
     }
 
-    return $return_code;
+    return \$return_code;
 }
 
 sub configure_cache_client ($self) {
@@ -367,7 +367,7 @@ sub configure_cache_client ($self) {
 sub exec ($self) {
     my $return_code = $self->init;
     Mojo::IOLoop->singleton->start;
-    return $return_code;
+    return $$return_code;
 }
 
 sub _prepare_cache_directory ($webui_host, $cachedirectory) {

t/24-worker-overall.t
@@ -142,9 +142,9 @@ subtest 'worker load' => sub {
     ok $worker->_check_system_utilization(10, [11, 12, 13]), 'load still exceeded on falling flank but high';
 };
 
-subtest 'delay and exec' => sub {
+subtest 'passing return code' => sub {
     my $worker_mock = Test::MockModule->new('OpenQA::Worker');
-    $worker_mock->redefine(init => 42);
+    $worker_mock->redefine(init => \42);
     is $worker->exec, 42, 'return code passed from init';
 };
 