os-autoinst/openQA

commit 8e91645e143b24092de2a2c3c40858212a12fd42

Use signatures in Shared::Controller::Running

lib/OpenQA/Shared/Controller/Running.pm
@@ -112,14 +112,14 @@ sub streamtext ($self, $file_name, $start_hook = undef, $close_hook = undef) {
     # Check for new lines from the logfile using recurring timer
     # Setup utility function to close the connection if something goes wrong
     my $timer_id;
-    my $close_connection = sub {
+    my $close_connection = sub ($self) {
         Mojo::IOLoop->remove($timer_id);
         $close_hook->();
         $self->finish;
         close $log;
     };
     $timer_id = Mojo::IOLoop->recurring(
-        TEXT_STREAMING_INTERVAL() => sub {
+        TEXT_STREAMING_INTERVAL() => sub (@) {
             if (!$ino) {
                 # log file was not yet opened
                 return unless open($log, '<', $logfile);
@@ -147,7 +147,7 @@ sub streamtext ($self, $file_name, $start_hook = undef, $close_hook = undef) {
 
     # Stop monitoring the logfile when the connection closes
     $self->on(
-        finish => sub {
+        finish => sub (@) {
             Mojo::IOLoop->remove($timer_id);
             $close_hook->($worker, $job);
         });
@@ -158,8 +158,7 @@ sub livelog ($self) {
     $self->streamtext('autoinst-log-live.txt');
 }
 
-sub liveterminal {
-    my ($self) = @_;
+sub liveterminal ($self) {
     return 0 unless $self->init();
     $self->streamtext('serial-terminal-live.txt');
 }

commit c5189391c046ee567f647761b45033b79535fd6c

Fix Too few arguments for subroutine error on close_connection
To prevent `Timer failed: Too few arguments for subroutine`, the
function needs to be called with $self. This fixes regression from
changes introduced in 8e91645.

poo: https://progress.opensuse.org/issues/187014

Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

lib/OpenQA/Shared/Controller/Running.pm
@@ -130,7 +130,7 @@ sub streamtext ($self, $file_name, $start_hook = undef, $close_hook = undef) {
             # Zero tolerance for any shenanigans with the logfile, such as
             # truncation, rotation, etc.
             my @st = stat $logfile;
-            return $close_connection->() unless @st && $st[1] == $ino && $st[3] > 0 && $st[7] >= $size;
+            return $close_connection->($self) unless @st && $st[1] == $ino && $st[3] > 0 && $st[7] >= $size;
 
             # If there's new data, read it all and send it out. Then
             # seek to the current position to reset EOF.