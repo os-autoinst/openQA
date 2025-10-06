os-autoinst/openQA

commit d222601a2998902a4aeb9a7fc96183c3ea54781c

parser: ktap: Don't add skipped subtests to output

lib/OpenQA/Parser/Format/KTAP.pm
@@ -70,6 +70,7 @@ sub _parse_subtest ($self, $result) {
         $steps->{result} = 'softfail';
     }
 
+    return if $line =~ /#\s*SKIP\b/i;
     $self->_add_output({file => $filename, content => $line});
     $self->state->{m} = $m + 1;
 }

commit c7b74bd3bd453555e9ad846ca2a15c7c11507fd4

parser: ktap: Return earlier if subtest result is SKIP
This way, nothing gets pushed to $self->state->{steps}->{details} and is
left hanging in the UI.

Fixes: d222601a2998 ("parser: ktap: Don't add skipped subtests to output")

lib/OpenQA/Parser/Format/KTAP.pm
@@ -49,6 +49,7 @@ sub _parse_subtest ($self, $result) {
 
     my $line = $result->as_string;
     return unless $line =~ /^#\s*(?<status>ok|not ok)\s+(?<index>\d+)\s+(?<name>[^#]*)/;
+    return if $line =~ /#\s*SKIP\b/i;
     my ($status, $index, $subtest_name) = @+{qw(status index name)};
 
     my $has_todo = $line =~ /#\s*TODO\b/i;
@@ -70,7 +71,6 @@ sub _parse_subtest ($self, $result) {
         $steps->{result} = 'softfail';
     }
 
-    return if $line =~ /#\s*SKIP\b/i;
     $self->_add_output({file => $filename, content => $line});
     $self->state->{m} = $m + 1;
 }

t/30-test_parser.t
@@ -967,7 +967,7 @@ subtest ktap_parse => sub {
     my $parser = OpenQA::Parser::Format::KTAP->new;
     $parser->load($ktap_file);
 
-    is $parser->results->size, 4, 'Expected four test groups';
+    is $parser->results->size, 5, 'Expected five test groups';
 
     my $result0 = $parser->results->get(0);
     is $result0->{name}, 'selftests:cgroup:test_core', 'The first test group has expected name';
@@ -982,6 +982,9 @@ subtest ktap_parse => sub {
 
     my $result3 = $parser->results->get(3);
     is $result3->result, 'skip', 'Group result is skipped';
+
+    my $result4 = $parser->results->get(4);
+    is scalar @{$result4->details}, 10, 'Has 10 subtest details';
 };
 
 subtest 'ktap_parse_incorrect_file' => sub {

t/data/ktap_correct.tap
@@ -44,3 +44,22 @@ not ok 10 selftests: cgroup: test_zswap # exit=1
 # selftests: cgroup: test_hugetlb_memcg
 # 1..0 # SKIP memory hugetlb accounting is disabled
 ok 5 selftests: cgroup: test_hugetlb_memcg # SKIP
+# selftests: cgroup: test_freezer
+# ok 1 test_cgfreezer_simple
+# ok 2 test_cgfreezer_tree
+# ok 3 test_cgfreezer_forkbomb
+# ok 4 test_cgfreezer_mkdir
+# ok 5 test_cgfreezer_rmdir
+# ok 6 test_cgfreezer_migrate
+# ok 7 test_cgfreezer_ptrace
+# ok 8 test_cgfreezer_stopped
+# ok 9 test_cgfreezer_ptraced
+# ok 10 test_cgfreezer_vfork
+# ok 11 # SKIP test_cgfreezer_time_empty
+# ok 12 # SKIP test_cgfreezer_time_simple
+# ok 13 # SKIP test_cgfreezer_time_populate
+# ok 14 # SKIP test_cgfreezer_time_migrate
+# ok 15 # SKIP test_cgfreezer_time_parent
+# ok 16 # SKIP test_cgfreezer_time_child
+# ok 17 # SKIP test_cgfreezer_time_nested
+ok 1 selftests: cgroup: test_freezer
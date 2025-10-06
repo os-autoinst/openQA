c7b74bd3bd453555e9ad846ca2a15c7c11507fd4
Ricardo B. Marlière <rbm@opensuse.org>
2025-10-06 12:52:29 -0300

    parser: ktap: Return earlier if subtest result is SKIP
    This way, nothing gets pushed to $self->state->{steps}->{details} and is
    left hanging in the UI.
    
    Fixes: d222601a2998 ("parser: ktap: Don't add skipped subtests to output")

--- lib/OpenQA/Parser/Format/KTAP.pm
+++ lib/OpenQA/Parser/Format/KTAP.pm
@@ -49,6 +49,7 @@ sub _parse_subtest ($self, $result) {
 
     my $line = $result->as_string;
     return unless $line =~ /^#\s*(?<status>ok|not ok)\s+(?<index>\d+)\s+(?<name>[^#]*)/;
+    return if $line =~ /#\s*SKIP\b/i;
     my ($status, $index, $subtest_name) = @+{qw(status index name)};
 
     my $has_todo = $line =~ /#\s*TODO\b/i;
@@ -70,7 +71,6 @@ sub _parse_subtest ($self, $result) {
         $steps->{result} = 'softfail';
     }
 
-    return if $line =~ /#\s*SKIP\b/i;
     $self->_add_output({file => $filename, content => $line});
     $self->state->{m} = $m + 1;
 }
