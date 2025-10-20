437a8903bdda1ca3c827842ee2b9ddb2e001cd8c
Tina Müller <tina.mueller@suse.com>
2025-10-20 15:12:36 +0200

    Revert "Don't redirect to asset domain via /needles/ID/(image|json) route"
    This reverts commit a007fe1466d28dc3451f4192b341c2c283a8d991.

--- lib/OpenQA/WebAPI/Controller/File.pm
+++ lib/OpenQA/WebAPI/Controller/File.pm
@@ -75,7 +75,7 @@ sub _needle_by_id_and_extension ($self, $extension) {
     my $needle_filename = $needle->name . $extension;
 
     push @{$self->static->paths}, $needle_dir;
-    return $self->_serve_static($needle_filename, 1);
+    return $self->_serve_static($needle_filename);
 }
 
 sub needle_image_by_id ($self) {
