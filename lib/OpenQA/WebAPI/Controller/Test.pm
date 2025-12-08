os-autoinst/openQA

commit 8d898b5767bdaf3ce4330b6d286fe2c389e5edba

Fix possibly excessive memory use when computer test result overview
When accessing the test results overview page (especially without
parameters, e.g. http://localhost:9526/tests/overview) a very huge set of
jobs is considered. This leads to a very excessive memory usage, e.g. with
current OSD data the RSS rises over 7 GiB and the request takes very long.
There's already a limit of 2000 jobs which is also effective but
unfortunately not passed to `complex_query` and only used later on. I
tracked the memory usage while running the code with debug printing it it
was very clear that `complex_query` is the culprit.

This change uses the limit from the start. This means additional
filtering done in Perl code also needs to happen as part of the now
limited initial query (using a sub query, see the added comment). So this
change replaces these Perl loops/grep with passing the filter parameters
directly to the database. This slightly changes the order of things but
should lead to the same end result.

With this change the "preferred" machine (basically the most frequent
machine per architecture) is only determined based on the jobs that are
actually displayed. That means jobs will no longer show as e.g. `kde@uefi`
when filtering for `machine=uefi` because with this filter the machine
`uefi` becomes the preferred (most frequent) machine (as now all displayed
jobs have that machine). I think this change is acceptable. It means that
some tests had to be adjusted accordingly.

Related ticket: https://progress.opensuse.org/issues/192448

lib/OpenQA/WebAPI/Controller/Test.pm
@@ -685,10 +685,9 @@ sub _calculate_preferred_machines ($jobs) {
 }
 
 # Take an job objects arrayref and prepare data structures for 'overview'
-sub _prepare_job_results ($self, $all_jobs, $limit) {
+sub _prepare_job_results ($self, $jobs, $job_ids) {
     my %archs;
     my %results;
-    my @job_ids;
     my $aggregated = {
         none => 0,
         passed => 0,
@@ -699,25 +698,8 @@ sub _prepare_job_results ($self, $all_jobs, $limit) {
         running => 0,
         unknown => 0
     };
-    my $preferred_machines = _calculate_preferred_machines($all_jobs);
-
-    # read parameter for additional filtering
-    my $failed_modules = $self->param_hash('failed_modules');
-    my $states = $self->param_hash('state');
-    my $results = $self->param_hash('result');
-    my $archs = $self->param_hash('arch');
-    my $machines = $self->param_hash('machine');
-
-    my @jobs = grep {
-              (not $states or $states->{$_->state})
-          and (not $results or $results->{$_->result})
-          and (not $archs or $archs->{$_->ARCH})
-          and (not $machines or $machines->{$_->MACHINE})
-          and (not $failed_modules or $_->result eq OpenQA::Jobs::Constants::FAILED)
-    } @$all_jobs;
-    my $limit_exceeded = @jobs >= $limit;
-    @jobs = @jobs[0 .. ($limit - 1)] if $limit_exceeded;
-    my @jobids = map { $_->id } @jobs;
+    my @jobs = $jobs->all;
+    my $preferred_machines = _calculate_preferred_machines(\@jobs);
 
     # prefetch the number of available labels for those jobs
     my $schema = $self->schema;
@@ -726,7 +708,7 @@ sub _prepare_job_results ($self, $all_jobs, $limit) {
     # prefetch test suite names from job settings
     my $job_settings
       = $schema->resultset('JobSettings')
-      ->search({job_id => {-in => [map { $_->id } @jobs]}, key => {-in => [qw(JOB_DESCRIPTION TEST_SUITE_NAME)]}});
+      ->search({job_id => {-in => $job_ids}, key => {-in => [qw(JOB_DESCRIPTION TEST_SUITE_NAME)]}});
     my %settings_by_job_id;
     for my $js ($job_settings->all) {
         $settings_by_job_id{$js->job_id}->{$js->key} = $js->value;
@@ -740,12 +722,13 @@ sub _prepare_job_results ($self, $all_jobs, $limit) {
       = $schema->resultset('TestSuites')->search({name => \%desc_args}, {columns => [qw(name description)]});
     my %descriptions = map { $_->name => $_->description } @descriptions;
 
-    my $failed_modules_by_job = $self->_fetch_failed_modules_by_jobs(\@jobids);
-    my ($children_by_job, $parents_by_job) = $self->_fetch_dependencies_by_jobs(\@jobids);
+    my $failed_modules_by_job = $self->_fetch_failed_modules_by_jobs($job_ids);
+    my ($children_by_job, $parents_by_job) = $self->_fetch_dependencies_by_jobs($job_ids);
     foreach my $job (@jobs) {
         my $id = $job->id;
         my $result = $job->overview_result(
-            $comment_data, $aggregated, $failed_modules,
+            $comment_data, $aggregated,
+            $self->param_hash('failed_modules'),
             $failed_modules_by_job->{$id} || [],
             $self->param('todo')) or next;
         my $test = $job->TEST;
@@ -780,14 +763,11 @@ sub _prepare_job_results ($self, $all_jobs, $limit) {
         $results{$distri}{$version}{$flavor}{$test} //= {};
         $results{$distri}{$version}{$flavor}{$test}{$arch} = $result;
 
-        # populate job IDs for adding multiple comments
-        push @job_ids, $id;
-
         # add description
         my $description = $settings_by_job_id{$id}->{JOB_DESCRIPTION} // $descriptions{$test_suite_names{$id}};
         $results{$distri}{$version}{$flavor}{$test}{description} //= $description;
     }
-    return ($limit_exceeded, \%archs, \%results, \@job_ids, $aggregated);
+    return (\%archs, \%results, $aggregated);
 }
 
 sub _prepare_groupids ($self) {
@@ -857,32 +837,26 @@ sub overview ($self) {
     my $validation = $self->validation;
     $validation->optional('t')->datetime;
     my $until = $validation->param('t');
-    my %stash = (
-        # build, version, distri are not mandatory and therefore not
-        # necessarily come from the search args so they can be undefined.
-        build => ref $search_args->{build} eq 'ARRAY' ? join(',', @{$search_args->{build}}) : $search_args->{build},
-        version => $search_args->{version},
-        distri => $search_args->{distri},
-        groups => $groups,
-        until => $until,
-        parallel_children_collapsable_results_sel => $config->{global}->{parallel_children_collapsable_results_sel},
-    );
-    my @jobs = $self->schema->resultset('Jobs')->complex_query(%$search_args)->latest_jobs($until);
-
-    my $limit = $config->{misc_limits}->{tests_overview_max_jobs};
-    (my $limit_exceeded, $stash{archs}, $stash{results}, $stash{job_ids}, $stash{aggregated})
-      = $self->_prepare_job_results(\@jobs, $limit);
+    $search_args->{until} = $until;
+    my $distri = $search_args->{distri};
+    my $version = $search_args->{version};
+    my $jobs_rs = $self->schema->resultset('Jobs');
+    my $latest_job_ids = $jobs_rs->complex_query_latest_ids(%$search_args);
+    my $limit = $search_args->{limit};    # one more than actual limit so we can check whether the limit was exceeded
+    my $exceeded_limit = @$latest_job_ids >= $limit ? $limit - 1 : 0;
+    my $jobs = $jobs_rs->latest_jobs_from_ids($latest_job_ids, $limit);
+    my ($archs, $results, $aggregated) = $self->_prepare_job_results($jobs, $latest_job_ids);
 
     # determine distri/version from job results if not explicitly specified via search args
-    my @distris = keys %{$stash{results}};
+    my @distris = keys %$results;
     my $formatted_distri;
     my $formatted_version;
     my $only_distri = scalar @distris == 1;
-    if (!defined $stash{distri} && $only_distri) {
+    if (!defined $distri && $only_distri) {
         $formatted_distri = $distris[0];
-        if (!defined $stash{version}) {
-            my @versions = keys %{$stash{results}->{$formatted_distri}};
-            $formatted_version = $versions[0] if (scalar @versions == 1);
+        if (!defined $version) {
+            my @versions = keys %{$results->{$formatted_distri}};
+            $formatted_version = $versions[0] if scalar @versions == 1;
         }
     }
 
@@ -899,18 +873,28 @@ sub overview ($self) {
         _add_distri_and_version_to_summary(\@summary_parts, $formatted_distri, $formatted_version, 1);
 
         # add distri and version from query parameters as regular strings
-        _add_distri_and_version_to_summary(\@summary_parts, $stash{distri}, $stash{version}, 0);
+        _add_distri_and_version_to_summary(\@summary_parts, $distri, $version, 0);
     }
 
-    $self->stash(
-        %stash,
+    my %stash = (
+        # build, version, distri are not mandatory and therefore not
+        # necessarily come from the search args so they can be undefined.
+        build => ref $search_args->{build} eq 'ARRAY' ? join(',', @{$search_args->{build}}) : $search_args->{build},
+        distri => $distri,
+        version => $version,
+        groups => $groups,
+        archs => $archs,
+        results => $results,
+        aggregated => $aggregated,
+        job_ids => $latest_job_ids,
+        until => $until,
+        parallel_children_collapsable_results_sel => $config->{global}->{parallel_children_collapsable_results_sel},
         summary_parts => \@summary_parts,
         only_distri => $only_distri,
-        limit_exceeded => $limit_exceeded ? $limit : undef
+        limit_exceeded => $exceeded_limit,
     );
-    $self->respond_to(
-        json => {json => \%stash},
-        html => {template => 'test/overview'});
+    $self->stash(\%stash);
+    $self->respond_to(json => {json => \%stash}, html => {template => 'test/overview'});
 }
 
 sub _get_latest_job ($self) {

lib/OpenQA/Schema/ResultSet/Jobs.pm
@@ -353,16 +353,53 @@ sub _prepare_complex_query_search_args ($self, $args) {
     return (\@conds, \%attrs);
 }
 
-sub complex_query ($self, %args) {
-    # For args where we accept a list of values, allow passing either an
-    # array ref or a comma-separated list
+sub _accept_comma_separated_arg_values ($args) {
     for my $arg (qw(state ids result modules modules_result)) {
-        next unless $args{$arg};
-        $args{$arg} = [split(',', $args{$arg})] unless (ref($args{$arg}) eq 'ARRAY');
+        next unless my $value = $args->{$arg};
+        $args->{$arg} = [split ',', $value] unless ref $value eq 'ARRAY';
     }
+}
+
+sub complex_query ($self, %args) {
+    _accept_comma_separated_arg_values(\%args);
     my ($conds, $attrs) = $self->_prepare_complex_query_search_args(\%args);
-    my $jobs = $self->search({-and => $conds}, $attrs);
-    return $jobs;
+    return $self->search({-and => $conds}, $attrs);
+}
+
+sub complex_query_latest_ids ($self, %args) {
+    # prepare basic search conditions and attributes
+    _accept_comma_separated_arg_values(\%args);
+    my ($conds, $attrs) = $self->_prepare_complex_query_search_args(\%args);
+    my $filters = $args{filters};
+    my $has_filters = $filters && @$filters > 0;
+    my $rows = $has_filters ? delete $attrs->{rows} : undef;  # when filtering, limit rows only in outer/filtering query
+    if (my $until = $args{until}) { push @$conds, {'me.t_created' => {'<=' => $until}} }
+
+    # set attributes to return only the latest job IDs for a certain combination of TEST, DISTRI, VERSION, …
+    $attrs->{order_by} = \['max(me.id) DESC'];
+    $attrs->{select} = ['max(me.id)'];
+    $attrs->{as} = ['id'];
+    $attrs->{group_by} = [qw(TEST DISTRI VERSION BUILD FLAVOR ARCH MACHINE)];
+
+    # execute the search; use a sub query if filtering is enabled
+    my $search = $self->search({-and => $conds}, $attrs);
+    if ($has_filters) {
+        # add another layer of querying for filters
+        # note: The filtering cannot be applied in the same query we do the grouping to return only the latest job IDs.
+        #       Otherwise adding filter parameters would lead to old jobs showing up. That is not wanted (and we have
+        #       therefore the test "filtering does not reveal old jobs" in `10-tests_overview.t` to test this).
+        my %filter_attrs = %$attrs;
+        $filter_attrs{rows} = $rows;
+        push @$filters, {'me.id' => {-in => $search->as_query}};
+        $search = $self->search({-and => $filters}, \%filter_attrs);
+    }
+    return [map { $_->id } $search->all];
+}
+
+sub latest_jobs_from_ids ($self, $latest_job_ids, $limit_from_initial_search) {
+    my %search_args = (id => {-in => $latest_job_ids});
+    my %options = (order_by => {-desc => 'id'}, rows => $limit_from_initial_search - 1);
+    return $self->search(\%search_args, \%options);
 }
 
 sub cancel_by_settings (

lib/OpenQA/WebAPI/Controller/API/V1/Job.pm
@@ -226,29 +226,15 @@ So this works in the same way as the test results overview in the GUI.
 sub overview ($self) {
     my ($search_args, $groups) = $self->compose_job_overview_search_args;
     my $failed_modules = $self->param_hash('failed_modules');
-    my $states = $self->param_hash('state');
-    my $results = $self->param_hash('result');
-    my $archs = $self->param_hash('arch');
-    my $machines = $self->param_hash('machine');
-
-    my @jobs = $self->schema->resultset('Jobs')->complex_query(%$search_args)->latest_jobs;
+    my $jobs_rs = $self->schema->resultset('Jobs');
+    my $latest_job_ids = $jobs_rs->complex_query_latest_ids(%$search_args);
+    my @jobs = $jobs_rs->latest_jobs_from_ids($latest_job_ids, $search_args->{limit})->all;
 
     my @job_hashes;
     for my $job (@jobs) {
-        next if $states && !$states->{$job->state};
-        next if $results && !$results->{$job->result};
-        next if $archs && !$archs->{$job->ARCH};
-        next if $machines && !$machines->{$job->MACHINE};
-        if ($failed_modules) {
-            next if $job->result ne OpenQA::Jobs::Constants::FAILED;
-            next unless OpenQA::Utils::any_array_item_contained_by_hash($job->failed_modules, $failed_modules);
-        }
-        push(
-            @job_hashes,
-            {
-                id => $job->id,
-                name => $job->name,
-            });
+        next
+          if $failed_modules && !OpenQA::Utils::any_array_item_contained_by_hash($job->failed_modules, $failed_modules);
+        push @job_hashes, {id => $job->id, name => $job->name};
     }
     $self->render(json => \@job_hashes);
 }

lib/OpenQA/WebAPI/Plugin/Helpers.pm
@@ -11,7 +11,7 @@ use OpenQA::Utils qw(bugurl human_readable_size render_escaped_refs href_to_bugr
 use OpenQA::Events;
 use OpenQA::Jobs::Constants qw(EXECUTION_STATES PRE_EXECUTION_STATES ABORTED_RESULTS FAILED NOT_COMPLETE_RESULTS);
 use Text::Glob qw(glob_to_regex_string);
-use List::Util qw(any);
+use List::Util qw(any min);
 use Feature::Compat::Try;
 
 sub register ($self, $app, $config) {
@@ -338,6 +338,9 @@ sub register ($self, $app, $config) {
     $app->helper('reply.validation_error' => \&_validation_error);
 
     $app->helper(compose_job_overview_search_args => \&_compose_job_overview_search_args);
+    $app->helper(every_non_empty_param => \&_every_non_empty_param);
+    $app->helper(compute_overview_filtering_params => \&_compute_overview_filtering_params);
+
     $app->helper(groups_for_globs => \&_groups_for_globs);
     $app->helper(param_hash => \&_param_hash);
     $app->helper(
@@ -407,10 +410,9 @@ sub _compose_job_overview_search_args ($c) {
     $v->optional('groupid')->num(0, undef);
     $v->optional('modules', 'comma_separated');
     $v->optional('flavor', 'comma_separated');
-    $v->optional('limit', 'not_empty')->num(0, undef);
+    $v->optional('limit', 'not_empty')->num(1, undef);
 
     # add simple query params to search args
-    $search_args{limit} = $v->param('limit') if $v->is_valid('limit');
     for my $arg (qw(distri version flavor test)) {
         next unless $v->is_valid($arg);
         my @params = @{$v->every_param($arg) // []};
@@ -480,6 +482,10 @@ sub _compose_job_overview_search_args ($c) {
         }
     }
 
+    # limit results to return one row more than the configured/specified limit so we know when the limit is exceeded
+    my $configured_limit = $c->app->config->{misc_limits}->{tests_overview_max_jobs};
+    $search_args{limit} = ($v->is_valid('limit') ? min($configured_limit, $v->param('limit')) : $configured_limit) + 1;
+
     # exclude jobs which are already cloned by setting scope for OpenQA::Jobs::complex_query()
     $search_args{scope} = 'current';
 
@@ -495,7 +501,29 @@ sub _compose_job_overview_search_args ($c) {
     # allow filtering by comment text
     if (my $c = $v->param('comment')) { $search_args{comment_text} = $c }
 
+    # allow filtering by several job settings
+    $search_args{filters} = $c->compute_overview_filtering_params;
+
     return (\%search_args, \@groups);
+}
+
+sub _every_non_empty_param ($c, $param_key) {
+    [map { split ',', $_ } @{$c->every_param($param_key)}]
+}
+
+sub _compute_overview_filtering_params ($c) {
+    my $states = $c->every_non_empty_param('state');
+    my $results = $c->every_non_empty_param('result');
+    my $archs = $c->every_non_empty_param('arch');
+    my $machines = $c->every_non_empty_param('machine');
+    my $failed_modules = $c->every_non_empty_param('failed_modules');
+    my %filters;
+    $filters{state} = {-in => $states} if @$states;
+    $filters{result} = {-in => $results} if @$results;
+    $filters{result} = FAILED if @$failed_modules;
+    $filters{ARCH} = {-in => $archs} if @$archs;
+    $filters{MACHINE} = {-in => $machines} if @$machines;
+    return [\%filters];
 }
 
 sub _groups_for_globs ($c) {

t/10-jobs.t
@@ -81,13 +81,17 @@ subtest 'latest jobs' => sub {
     is $jobs->latest_build(version => 'Factory', distri => 'opensuse'), '0048@0815', 'latest for non-integer build';
     is $jobs->latest_build(version => '13.1', distri => 'opensuse'), '0091', 'latest for different version differs';
 
-    my @latest = $jobs->latest_jobs;
-    my @ids = map { $_->id } @latest;
+    my $ids = $jobs->complex_query_latest_ids;
     # These two jobs have later clones in the fixture set, so should not appear
-    ok(grep(!/^(99962|99945)$/, @ids), 'jobs with later clones do not show up in latest jobs');
+    ok grep(!/^(99962|99945)$/, @$ids), 'jobs with later clones do not show up in latest jobs';
     # These are the later clones, they should appear
-    ok(grep(/^99963$/, @ids), 'cloned jobs appear as latest job');
-    ok(grep(/^99946$/, @ids), 'cloned jobs appear as latest job (2nd)');
+    ok grep(/^99963$/, @$ids), 'cloned jobs appear as latest job';
+    ok grep(/^99946$/, @$ids), 'cloned jobs appear as latest job (2nd)';
+
+    my $filtered_ids = $jobs->complex_query_latest_ids(filters => [{result => {-in => [FAILED, PASSED]}}]);
+    ok grep(!/^(99962|99945)$/, @$filtered_ids), 'jobs with later clones still not present';
+    ok grep(/^99946$/, @$filtered_ids), 'failed/passed job still returned';
+    ok grep(!/^99963$/, @$filtered_ids), 'running job filtered out';
 };
 
 

t/10-tests_overview.t
@@ -151,10 +151,8 @@ subtest 'time parameter' => sub {
 };
 
 subtest 'limit parameter' => sub {
-    $t->get_ok(
-        '/tests/overview?distri=opensuse&version=Factory&limit=2&limit=2',
-        'no database error when specifying more than one limit'
-    );
+    $t->get_ok('/tests/overview?distri=opensuse&version=Factory&build=0048&limit=2&limit=2',
+        'no database error when specifying more than one limit');
     is $t->tx->res->dom->find('table.overview td.name')->size, 2, 'number of jobs limited';
 };
 

t/api/04-jobs.t
@@ -278,7 +278,8 @@ subtest 'job overview' => sub {
     subtest 'limit parameter' => sub {
         $query->query(build => '0048', limit => 1);
         $t->get_ok($query->path_query)->status_is(200);
-        is(scalar(@{$t->tx->res->json}), 1, 'Expect only one job entry');
+        my $json = $t->tx->res->json;
+        is scalar @$json, 1, 'Expect only one job entry' or always_explain $json;
         $t->json_is('/0/id' => 99939, 'Check correct order');
     };
 };

t/ui/10-tests_overview.t
@@ -487,8 +487,9 @@ subtest 'filtering by machine' => sub {
         element_not_present("#$_") for qw(flavor_DVD_arch_i586 flavor_GNOME-Live_arch_i686 flavor_NET_arch_x86_64);
         is element_prop('filter-machine'), 'uefi', 'machine filter still visible in form';
 
-        my @job_rows = map { $_->get_text } @{$driver->find_elements('#content tbody tr')};
-        is_deeply \@job_rows, ['kde@uefi'], 'only the job with machine uefi is shown' or always_explain \@job_rows;
+        my $get_job_ids = qq|return Array.from(document.querySelectorAll('span[id^="res-"]')).map(e => e.id.substr(4))|;
+        my $job_ids = $driver->execute_script($get_job_ids);
+        is_deeply $job_ids, ['99982'], 'only the job with machine uefi is shown' or always_explain $job_ids;
 
         $driver->find_element('#filter-panel .card-header')->click();
         $driver->find_element('#filter-machine')->clear();

commit b95d539879116f5ad6f4e3fb960172661e857ac2

Fix job IDs that are considered for mass-commenting on overview page
* Fix regression introduced by 8d898b5767bdaf3ce4330b6d286fe2c389e5edba
* Create another array of job IDs that contains only IDs which are actually
  shown on the page
* See https://progress.opensuse.org/issues/193453

lib/OpenQA/WebAPI/Controller/Test.pm
@@ -688,6 +688,7 @@ sub _calculate_preferred_machines ($jobs) {
 sub _prepare_job_results ($self, $jobs, $job_ids) {
     my %archs;
     my %results;
+    my @job_ids;
     my $aggregated = {
         none => 0,
         passed => 0,
@@ -762,12 +763,13 @@ sub _prepare_job_results ($self, $jobs, $job_ids) {
         $results{$distri}{$version}{$flavor} //= {};
         $results{$distri}{$version}{$flavor}{$test} //= {};
         $results{$distri}{$version}{$flavor}{$test}{$arch} = $result;
+        push @job_ids, $id;
 
         # add description
         my $description = $settings_by_job_id{$id}->{JOB_DESCRIPTION} // $descriptions{$test_suite_names{$id}};
         $results{$distri}{$version}{$flavor}{$test}{description} //= $description;
     }
-    return (\%archs, \%results, $aggregated);
+    return (\%archs, \%results, $aggregated, \@job_ids);
 }
 
 sub _prepare_groupids ($self) {
@@ -845,7 +847,7 @@ sub overview ($self) {
     my $limit = $search_args->{limit};    # one more than actual limit so we can check whether the limit was exceeded
     my $exceeded_limit = @$latest_job_ids >= $limit ? $limit - 1 : 0;
     my $jobs = $jobs_rs->latest_jobs_from_ids($latest_job_ids, $limit);
-    my ($archs, $results, $aggregated) = $self->_prepare_job_results($jobs, $latest_job_ids);
+    my ($archs, $results, $aggregated, $job_ids) = $self->_prepare_job_results($jobs, $latest_job_ids);
 
     # determine distri/version from job results if not explicitly specified via search args
     my @distris = keys %$results;
@@ -886,7 +888,7 @@ sub overview ($self) {
         archs => $archs,
         results => $results,
         aggregated => $aggregated,
-        job_ids => $latest_job_ids,
+        job_ids => $job_ids,
         until => $until,
         parallel_children_collapsable_results_sel => $config->{global}->{parallel_children_collapsable_results_sel},
         summary_parts => \@summary_parts,