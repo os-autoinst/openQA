os-autoinst/openQA

commit 7b3f46b88025dc476251f4a2921cd0f9c7aa9ded

parser: ktap: Show full output by default if no line was parsed
In the case where a subtest don't have proper KTAP output, nothing will be
shown in the UI. This is not ideal since it forces the user to open the
individual log asset file and also prevent them from easily opening bugs
from the helper buttons.

lib/OpenQA/Parser/Format/KTAP.pm
@@ -48,7 +48,11 @@ sub _parse_subtest ($self, $result) {
     $self->test or return;
 
     my $line = $result->as_string;
-    return unless $line =~ /^#\s*(?<status>ok|not ok)\s+(?<index>\d+)\s+(?<name>[^#]*)/;
+    unless ($line =~ /^#\s*(?<status>ok|not ok)\s+(?<index>\d+)\s+(?<name>[^#]*)/) {
+        push @{$steps->{unparsed_lines}}, $line;
+        return;
+    }
+    $steps->{parsed_lines_count}++;
     return if $line =~ /#\s*SKIP\b/i;
     my ($status, $index, $subtest_name) = @+{qw(status index name)};
 
@@ -92,6 +96,22 @@ sub _testgroup_finalize ($self, $result) {
 
     $steps->{name} = $self->test->{name};
     $steps->{test} = $self->test;
+
+    # If there are no parsed lines, default to simply show the whole log.
+    # This is better than not showing anything, impeding the user to see
+    # the buttons of adding product or test bugs and requiring them to
+    # search and open individual log assets.
+    if (!$steps->{parsed_lines_count}) {
+        my $filename = "KTAP-@{[$steps->{name}]}.txt";
+        push @{$steps->{details}},
+          {
+            text => $filename,
+            title => $steps->{name},
+            result => 'ok',
+          };
+        $self->_add_output({file => $filename, content => join("\n", @{$steps->{unparsed_lines}})});
+    }
+
     $self->generated_tests_results->add(OpenQA::Parser::Result::OpenQA->new($steps));
 
     $self->test(undef);
@@ -106,11 +126,11 @@ sub parse ($self, $KTAP) {
     while (my $result = $parser->next) {
         next if $result->type eq 'version' || $result->type eq 'plan';
 
-        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*selftests:\s+/) {
-            $self->_testgroup_init($result->as_string);
-            next;
-        }
-        if ($result->type eq 'comment' && $result->as_string =~ /^#\s*(ok|not ok)\s+\d+\s+/) {
+        if ($result->type eq 'comment') {
+            if ($result->as_string =~ /^#\s*selftests:\s+/) {
+                $self->_testgroup_init($result->as_string);
+                next;
+            }
             $self->_parse_subtest($result);
             next;
         }

commit 45f6f1c098ea1cef5179aaeb649a6198164de6b9

KTAP: Don't increment parsed_lines_count in "SKIP" lines
Lines containing "SKIP" are not parsed, so the counter must not be
incremented otherwise they will be shown in the UI.

Fixes: 7b3f46b ("parser: ktap: Show full output by default if no line was parsed")

lib/OpenQA/Parser/Format/KTAP.pm
@@ -54,8 +54,8 @@ sub _parse_subtest ($self, $result) {
         push @{$steps->{unparsed_lines}}, $line;
         return;
     }
-    $steps->{parsed_lines_count}++;
     return if $line =~ /#\s*SKIP\b/i;
+    $steps->{parsed_lines_count}++;
     my ($status, $index, $subtest_name) = @+{qw(status index name)};
 
     my $has_todo = $line =~ /#\s*TODO\b/i;