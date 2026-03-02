d620f1c91157346f69b50545b8e108b6761427e7
Oliver Kurz <okurz@suse.de>
2026-03-02 13:24:20 +0100

    feat: limit job results on group overview and dashboard
    Implement a limit on the number of jobs fetched for job group overview,
    parent group overview, and the dashboard (consistent with the tests
    overview page). This prevents performance issues and 502 errors when
    accessing job groups with a huge number of jobs (e.g. >10k).
    
    A warning message is displayed to the user when results are truncated.
    
    Related progress issue: https://progress.opensuse.org/issues/196913

--- lib/OpenQA/WebAPI/Controller/API/V1/JobGroup.pm
+++ lib/OpenQA/WebAPI/Controller/API/V1/JobGroup.pm
@@ -400,10 +400,11 @@ sub build_results ($self) {
     my $show_tags = $validation->param('show_tags') // $only_tagged;
 
     my $tags = $show_tags ? $group->tags : undef;
+    my $max_jobs_limit = $self->app->config->{misc_limits}->{job_groups_overview_max_jobs};
     my $cbr
       = OpenQA::BuildResults::compute_build_results($group, $limit_builds,
         $time_limit_days, $only_tagged ? $tags : undef,
-        [], $tags);
+        [], $tags, $max_jobs_limit);
     $self->render(json => $cbr);
 }
 
