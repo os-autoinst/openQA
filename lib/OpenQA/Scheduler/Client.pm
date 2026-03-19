56d85b63d0beafc4251be4daa8900ba8383291d5
Oliver Kurz <okurz@suse.de>
2026-03-19 17:44:34 +0100

    refactor(Scheduler): convert remaining modules to signatures

--- lib/OpenQA/Scheduler/Client.pm
+++ lib/OpenQA/Scheduler/Client.pm
@@ -13,8 +13,8 @@ has client => sub { OpenQA::Client->new(api => shift->host // 'localhost') };
 has port => sub { service_port('scheduler') };
 
 my $IS_SCHEDULER_ITSELF;
-sub mark_current_process_as_scheduler { $IS_SCHEDULER_ITSELF = 1; }
-sub is_current_process_the_scheduler { return $IS_SCHEDULER_ITSELF; }
+sub mark_current_process_as_scheduler () { $IS_SCHEDULER_ITSELF = 1; }
+sub is_current_process_the_scheduler () { return $IS_SCHEDULER_ITSELF; }
 
 sub new ($class, @args) {
     die 'creating an OpenQA::Scheduler::Client from the scheduler itself is forbidden'
@@ -30,7 +30,7 @@ sub wakeup ($self) {
       ->finally(sub { delete $self->{wakeup} })->wait;
 }
 
-sub singleton { state $client ||= __PACKAGE__->new }
+sub singleton ($class = undef) { state $client ||= ($class // __PACKAGE__)->new }
 
 sub _api ($self, $method) {
     my ($host, $port) = ($self->host // '127.0.0.1', $self->port);
