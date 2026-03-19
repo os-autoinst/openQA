b5924bee5be2c661bf4880058cabd860c4698d33
Ioannis Bonatakis <ybonatakis@suse.com>
2026-03-19 16:10:04 +0100

    fix(influxdb): prevent empty tags populated to telegraf
    Address the missing tag which produces
    
    `2026-03-13T09:54:06Z E! [inputs.http] Error in plugin:
    [url=https://openqa.suse.de/admin/influxdb/jobs]: parsing metrics failed:
    metric parse error: expected tag at 27:53:
    "openqa_jobs_by_arch,url=https://openqa.suse.de,arch= scheduled=1i"`
    
    by skipping the assignment for any empty or undef _arch_
    
    for the testing, instead of modifying 01-jobs.pl, create the job directly
    in the test itself in order to avoid modify fixtures which could break
    other tests.
    
    Issue: https://progress.opensuse.org/issues/197858
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- lib/OpenQA/WebAPI/Controller/Admin/Influxdb.pm
+++ lib/OpenQA/WebAPI/Controller/Admin/Influxdb.pm
@@ -16,7 +16,8 @@ sub _queue_sub_stats ($query, $state, $result) {
     }
     my $archs = $query->search({}, {select => ['ARCH', {count => 'id'}], as => [qw(ARCH count)], group_by => 'ARCH'});
     while (my $c = $archs->next) {
-        $result->{openqa_jobs_by_arch}->{'arch=' . $c->get_column('ARCH')}->{$state} = $c->get_column('count');
+        next unless my $arch = $c->get_column('ARCH');
+        $result->{openqa_jobs_by_arch}->{'arch=' . $arch}->{$state} = $c->get_column('count');
     }
 }
 
