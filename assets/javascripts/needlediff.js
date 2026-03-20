os-autoinst/openQA

commit dc1a3709e12f76f1f10ac3525508e85346663968

chore: adapt openQA for jQuery 4.0.0
Motivation:
The jQuery dependency was upgraded to 4.0.0 which removed several deprecated
APIs and changed behavior for unitless CSS properties, causing UI tests to
hang and various components to fail.

Design Choices:
- Added compatibility shims in openqa.js for removed APIs ($.trim,
  $.isFunction, $.isArray, $.isPlainObject, $.isEmptyObject, $.inArray,
  $.active) to support 3rd-party plugins like chosen and timeago.
- Refactored admin_groups.js to use native JS event listeners and validation
  logic for better Selenium test compatibility.
- Appended 'px' to unitless CSS properties in test_result.js and navbar
  script.
- Replaced raw CSS strings with object literals in needlediff.js.
- Updated job_templates.js to use $.ajax with serialize().
- Isolated third-party code overrides locally to maintain upgradeability.
- Added documentation comments explaining each shim.

User Benefits:
- Fixed UI hanging issues in test results page.
- Restored admin group and user management functionality.
- Ensured 3rd-party plugins continue working with updated library.
- Cleaner, more maintainable codebase with documented compatibility layer.

Related issue: https://github.com/os-autoinst/openQA/pull/6984

assets/javascripts/needlediff.js
@@ -7,7 +7,7 @@ function NeedleDiff(id, width, height) {
   var container = $('#' + id);
   var divide = 0.5;
 
-  canvas.css('position", "absolute');
+  canvas.css('position', 'absolute');
   this.ctx = canvas[0].getContext('2d');
   this.screenshotImg = null;
   this.needleImg = null;
@@ -39,9 +39,13 @@ function NeedleDiff(id, width, height) {
   // Draw canvas into its container
   canvas.attr('width', width);
   canvas.attr('height', height);
-  container.css(
-    'border: 1px solid black; margin: 0px; position: relative; width: ' + width + 'px; height: ' + height + 'px;'
-  );
+  container.css({
+    border: '1px solid black',
+    margin: '0px',
+    position: 'relative',
+    width: width + 'px',
+    height: height + 'px'
+  });
   container.append(canvas);
 
   Object.defineProperty(this, 'container', {

assets/3rdparty/bootstrap-4-multi-dropdown-navbar/beta2.0/js/bootstrap-4-navbar.js
@@ -1,31 +1,31 @@
-
 /*!
  * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
  * Copyright 2017.
  * Licensed under the GPL license
  */
 
+$(document).ready(function () {
+  $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    var $el = $(this);
+    var $parent = $(this).offsetParent('.dropdown-menu');
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
 
-$( document ).ready( function () {
-    $( '.dropdown-menu a.dropdown-toggle' ).on( 'click', function ( e ) {
-        var $el = $( this );
-        var $parent = $( this ).offsetParent( ".dropdown-menu" );
-        if ( !$( this ).next().hasClass( 'show' ) ) {
-            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
-        }
-        var $subMenu = $( this ).next( ".dropdown-menu" );
-        $subMenu.toggleClass( 'show' );
-        
-        $( this ).parent( "li" ).toggleClass( 'show' );
+    $(this).parent('li').toggleClass('show');
 
-        $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
-            $( '.dropdown-menu .show' ).removeClass( "show" );
-        } );
-        
-         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
-            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
-        }
+    $(this)
+      .parents('li.nav-item.dropdown.show')
+      .on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-menu .show').removeClass('show');
+      });
 
-        return false;
-    } );
-} );
+    if (!$parent.parent().hasClass('navbar-nav')) {
+      $el.next().css({top: $el[0].offsetTop, left: $parent.outerWidth() - 4});
+    }
+
+    return false;
+  });
+});

assets/assetpack.def
@@ -147,7 +147,6 @@
 < javascripts/fullscreen.js
 < javascripts/obs_rsync.js
 < javascripts/parent_group_overview.js
-< 3rdparty/bootstrap-4-multi-dropdown-navbar/beta2.0/js/bootstrap-4-navbar.js
 < ../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 < ../node_modules/chosen-js/chosen.jquery.js
 < ../node_modules/jquery-ujs/src/rails.js

assets/javascripts/admin_groups.js
@@ -58,31 +58,25 @@ function fetchHtmlEntry(url, targetElement) {
 
 function countEmptyInputs(form) {
   return Array.from(form.querySelectorAll('input')).reduce(
-    (count, e) => count + (e.type !== 'number' && !jQuery.trim(e.value).length),
+    (count, e) => count + (e.type !== 'number' && !e.value.trim().length),
     0
   );
 }
 
 function validateJobGroupForm(form) {
-  const button = form.querySelector('button[type=submit]');
-  let emptyInputs = countEmptyInputs(form);
-  button.disabled = emptyInputs > 0;
-  if (form.dataset.eventHandlersInitialized) {
-    return;
-  }
-  $('input', form).on('keyup change', function () {
+  const $form = $(form);
+  const $button = $form.find('button[type=submit]');
+  const disableSubmit = function () {
+    $button.prop('disabled', countEmptyInputs(form) > 0);
+  };
+  $form.off('.val').on('keyup.val change.val input.val', 'input', function () {
     if (this.type === 'number') {
       return;
     }
-    if (!jQuery.trim(this.value).length) {
-      this.classList.add('is-invalid');
-      button.disabled = ++emptyInputs;
-    } else {
-      this.classList.remove('is-invalid');
-      button.disabled = --emptyInputs > 0;
-    }
+    $(this).toggleClass('is-invalid', !this.value.trim().length);
+    disableSubmit();
   });
-  form.dataset.eventHandlersInitialized = true;
+  disableSubmit();
 }
 
 function createGroup(form) {

assets/javascripts/admin_user.js
@@ -17,7 +17,7 @@ function setup_admin_user() {
     }
 
     var form = $(this).parent('form');
-    if (!confirm('Are you sure to put ' + username + ' into role: ' + $.trim(role) + '?')) {
+    if (!confirm('Are you sure to put ' + username + ' into role: ' + role.trim() + '?')) {
       rollback(form);
       return;
     }

assets/javascripts/job_templates.js
@@ -568,7 +568,10 @@ function submitProperties(form) {
   var editorForm = $(form);
   editorForm.find('.buttons').hide();
   editorForm.find('.progress-indication').show();
-  fetchWithCSRF(editorForm.data('put-url'), {method: 'PUT', body: new FormData(form)})
+  fetchWithCSRF(editorForm.data('put-url'), {
+    method: 'PUT',
+    body: new FormData(form)
+  })
     .then(response => {
       return response
         .json()
@@ -584,7 +587,13 @@ function submitProperties(form) {
     .then(({response, json}) => {
       showAdvancedFieldsIfJsonRefersToThem(json);
       const overallError = updateValidation(form, json);
-      if (overallError) throw overallError;
+      if (overallError) {
+        showSubmitResults(
+          editorForm,
+          `<i class="fa fa-exclamation-circle"></i> Unable to apply changes: <strong>${overallError}</strong>`
+        );
+        return;
+      }
       if (!response.ok) throw `Server returned ${response.status}: ${response.statusText}`;
       const warnings = json?.warnings_by_field;
       const remark =
@@ -608,6 +617,10 @@ function submitProperties(form) {
         editorForm,
         `<i class="fa fa-exclamation-circle"></i> Unable to apply changes: <strong>${error}</strong>`
       );
+    })
+    .finally(() => {
+      editorForm.find('.buttons').show();
+      editorForm.find('.progress-indication').hide();
     });
 
   return false;

assets/javascripts/openqa.js
@@ -645,3 +645,103 @@ function renderHttpUrlAsLink(value, oneUrlPerLine = false) {
 function getXhrError(jqXHR, textStatus, errorThrown) {
   return jqXHR.responseJSON?.error || jqXHR.responseText || errorThrown || textStatus;
 }
+
+if (typeof window !== 'undefined' && window.jQuery) {
+  (function ($) {
+    // jQuery 4.0.0 removed several deprecated APIs. These shims maintain compatibility
+    // with 3rd-party plugins (timeago, chosen) that still rely on these methods.
+    // See: https://github.com/jquery/jquery/issues/4884
+    //
+    // NOTE: timeago declares "jquery": ">=1.5.0 <4.0" in its package.json, meaning it is
+    // officially incompatible with jQuery 4. The shims above ($.trim, $.isFunction) cover
+    // the current usage. If timeago is upgraded or other removed APIs are hit, this may break.
+    // Consider replacing timeago with a jQuery 4-compatible alternative (e.g. vanilla JS
+    // relative time formatting) to remove this dependency.
+
+    // $.active: Removed in jQuery 4. Required by Selenium wait_for_ajax in t/lib/OpenQA/SeleniumTest.pm.
+    // Note: Only tracks jQuery $.ajax calls, not fetch(). SeleniumTest.pm checks runningFetchRequests separately.
+    if ($.active === undefined) {
+      $.active = 0;
+      if (typeof document !== 'undefined') {
+        $(document).on('ajaxSend', function () {
+          $.active = ($.active || 0) + 1;
+        });
+        $(document).on('ajaxComplete', function () {
+          $.active = Math.max(0, ($.active || 1) - 1);
+        });
+      }
+    }
+
+    // $.trim: Removed in jQuery 4. Used by timeago/jquery.timeago.js and chosen-js/chosen.jquery.js
+    if ($.trim === undefined) {
+      $.trim = function (str) {
+        return String(str).trim();
+      };
+    }
+
+    // $.isFunction: Removed in jQuery 4. Used by timeago/jquery.timeago.js
+    if ($.isFunction === undefined) {
+      $.isFunction = function (fn) {
+        return typeof fn === 'function';
+      };
+    }
+
+    // $.isArray: Removed in jQuery 4. Used by chosen-js/chosen.jquery.js
+    if ($.isArray === undefined) {
+      $.isArray = Array.isArray;
+    }
+
+    // $.isPlainObject: Removed in jQuery 4. Used by chosen-js/chosen.jquery.js
+    if ($.isPlainObject === undefined) {
+      $.isPlainObject = function (obj) {
+        return obj !== null && typeof obj === 'object' && Object.getPrototypeOf(obj) === Object.prototype;
+      };
+    }
+
+    // $.isEmptyObject: Removed in jQuery 4. Used by chosen-js/chosen.jquery.js
+    if ($.isEmptyObject === undefined) {
+      $.isEmptyObject = function (obj) {
+        return Object.keys(obj).length === 0;
+      };
+    }
+
+    // $.inArray: Behavior changed in jQuery 4 (now delegates to Array.prototype.indexOf).
+    // Used by chosen-js/chosen.jquery.js. Shim for compatibility.
+    if ($.inArray === undefined) {
+      $.inArray = function (elem, arr, i) {
+        return arr ? Array.prototype.indexOf.call(arr, elem, i) : -1;
+      };
+    }
+  })(window.jQuery);
+
+  $(document).ready(function () {
+    setTimeout(function () {
+      var $dropdownToggle = $('.dropdown-menu a.dropdown-toggle');
+      if ($dropdownToggle.length) {
+        $dropdownToggle.off('click');
+        $dropdownToggle.on('click', function (e) {
+          var $el = $(this);
+          var $parent = $(this).offsetParent('.dropdown-menu');
+          if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+          }
+          var $subMenu = $(this).next('.dropdown-menu');
+          $subMenu.toggleClass('show');
+          $(this).parent('li').toggleClass('show');
+
+          if (!$parent.parent().hasClass('navbar-nav')) {
+            $el.next().css({top: $el[0].offsetTop + 'px', left: $parent.outerWidth() - 4 + 'px'});
+          }
+
+          return false;
+        });
+      }
+
+      var $dropdown = $('.nav-item.dropdown');
+      $dropdown.off('hidden.bs.dropdown');
+      $dropdown.on('hidden.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu .show').removeClass('show');
+      });
+    }, 0);
+  });
+}

assets/javascripts/test_result.js
@@ -94,7 +94,7 @@ function checkPreviewVisible(stepPreviewContainer, preview) {
   var endOfRow = rrow.height() + rrow.offset().top;
   if (endOfPreview > endOfRow) {
     // only enlarge the margin - otherwise the page scrolls back
-    rrow.css('margin-bottom', endOfPreview - endOfRow + extraMargin);
+    rrow.css('margin-bottom', endOfPreview - endOfRow + extraMargin + 'px');
   }
 }
 
@@ -123,7 +123,7 @@ function previewSuccess(stepPreviewContainer, data, force) {
     }
     setDiffScreenshot(imageSource);
   }
-  pin.css('left', -($('.result').width() + $('.component').width() + 2 * 16));
+  pin.css('left', -($('.result').width() + $('.component').width() + 2 * 16) + 'px');
   var tdWidth = $('.current_preview').parents('td').width();
   pout
     .width(tdWidth)

commit 22a15cc5a79725d6b31f2e2783a1d81e65ec47be

fix: needlediff view alignment (regression from dc1a3709e)
This fixes an alignment issue in the needlediff view which was a
regression from dc1a3709e.

The Root Cause:
Before dc1a3709e needlediff.js contained malformed .css() calls:
1. canvas.css('position", "absolute'); (had a typo in the quotes)

Because of the typo and passing a single long string of properties to jQuery's
.css() it is assumed that these inline styles were silently failing and never
actually applied for years. As a result, the screenshot view correctly relied
on the stylesheet (assets/stylesheets/result_preview.scss), which gave the
<canvas> a responsive max-width: 100% and a light border.

Simply removing the redundant style fixes the issue.

assets/javascripts/needlediff.js
@@ -7,7 +7,6 @@ function NeedleDiff(id, width, height) {
   var container = $('#' + id);
   var divide = 0.5;
 
-  canvas.css('position', 'absolute');
   this.ctx = canvas[0].getContext('2d');
   this.screenshotImg = null;
   this.needleImg = null;