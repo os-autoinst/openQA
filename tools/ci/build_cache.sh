38de9afd1055086e3a5d59a4ea21a1ff1a3d6acc
Marius Kittler <mkittler@suse.de>
2026-04-24 14:12:39 +0200

    fix(ci): Avoid conflicts when building cache
    Remove the conflicting `crypto-policies-scripts-*` from `ci-packages.txt`. The
    nightly job will add the correct version back if it is still required.
    
    Replace separate `zypper … install --download-only … && rpm -i …` invocations
    with simply using `zypper install …`. This will still populate the cache as
    before and fix errors when building the cache, e.g.:
    
    ```
    file /usr/share/crypto-policies/BSI/opensslcnf.txt from install of
    crypto-policies-20250714.cd6043a-160000.1.1.noarch conflicts with file from
    package crypto-policies-20250124.4d262e7-160000.2.1.noarch
    ```
    
    To keep the installed packages, this change configures the `--keep-packages`
    option for all repositories. To verify that this actually works, this change
    also adds a final check for whether the zypper cache has been populated.

--- tools/ci/build_cache.sh
+++ tools/ci/build_cache.sh
@@ -7,7 +7,11 @@ set -ex
 
 sudo zypper ar -f -p 90 https://download.opensuse.org/repositories/devel:/openQA:/Leap:/16.0/16.0 openQA
 sudo zypper ar -f -p 95 http://download.opensuse.org/repositories/devel:openQA/16.0 devel
+sudo zypper mr --keep-packages --all
 tools/retry sudo zypper --gpg-auto-import-keys ref
-sudo zypper -n install --download-only $(sed -e 's/\r//' < tools/ci/ci-packages.txt)
-sudo rpm -i -f $(find /var/cache/zypp/packages/ | grep '.rpm$')
+sudo zypper -n install $(sed -e 's/\r//' < tools/ci/ci-packages.txt)
+if [[ "$(find /var/cache/zypp/packages/ | wc -l)" == 0 ]]; then
+    echo "no packages cached"
+    exit 1
+fi
 echo "build_cache.sh done"
