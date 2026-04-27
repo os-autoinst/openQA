c7f5fa4d3627e5340f20ba9bdea56b8d1c4b29fe
Marius Kittler <mkittler@suse.de>
2026-04-27 10:55:26 +0200

    fix(ci): Do not refresh repos when installing from cache
    Otherwise we can't install the possibly outdated packages from the cache ending
    up with errors like `Package 'perl-Perl-Tidy-20260204.0.0' not found.`.

--- tools/ci/build_or_install_from_cache.sh
+++ tools/ci/build_or_install_from_cache.sh
@@ -4,6 +4,8 @@ cache=/var/cache/zypp/packages
 if [ -z "$CIRCLE_WORKFLOW_ID" ]; then
     bash "$(dirname "$0")"/build_cache.sh
 elif [[ -f $cache/.keep_packages ]] && [[ "$(find "$cache" -name '*.rpm' | wc -l)" -gt 0 ]]; then
+    sudo zypper ms --no-refresh --all
+    sudo zypper mr --no-refresh --all
     sudo zypper -n in $(sed -e 's/\r//' < tools/ci/ci-packages.txt)
 else
     echo "Zypper cache is not populated. Please trigger a cache.fullstack job manually to ensure a cache is available for the latest version."
