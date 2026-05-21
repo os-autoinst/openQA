os-autoinst/openQA

commit cd76f31f34cb167c404d3786939cc2a8802c64c0

test: Move author tests to xt/ and separate compile checks
Motivation:
Following Perl ecosystem best practices, author tests (static analysis,
style checks, POD, etc.) should be separated from functional/dynamic
tests. This allows build environments and package maintainers to focus
exclusively on tests that verify the correctness of the software in its
runtime environment, while avoiding dependencies and runtime overhead
associated with developer-only quality gates.

Furthermore, compiling the entire codebase via `01-compile-check-all.t`
takes exceptionally long when running alongside other tests because
it gets instrumented by `Devel::Cover`. Separating it into its own
directory allows us to isolate it and bypass the coverage overhead.

Design Choices:
- Moved 00-tidy.t, 01-style.t, 02-pod.t, and 45-make-update-deps.t from
  t/ to xt/.
- Moved 01-compile-check-all.t from t/ to t/compile/.
- Renamed the Makefile target 'test-tidy-compile-style' to 'test-author'
  to match industry-standard naming and ensure consistency with the
  'os-autoinst-common' sub-module.
- Added a new 'test-compile' Makefile target for the compile checks.
- Set COVEROPT="" on both 'test-author' and 'test-compile' targets to
  bypass Devel::Cover instrumentation, drastically speeding up execution.
- Updated t/01-compile-check-all.t to include the new xt/ directory in
  its syntax checks.
- Updated xt/01-style.t to include xt/ in style checks and correctly
  exclude itself from self-referential style rules.
- Simplified Makefile targets (test-t, test-ui, test-api) by removing
  GLOBIGNORE exclusions for moved tests, as they are naturally isolated.
- Updated t/testrules.yml to reflect the new test locations.
- Simplified dist/rpm/openQA.spec as author tests can be ignored
  completely.
- Updated `prove_wrapper` in CI tools to support new directories.

User Benefit:
- Developers get much faster feedback via 'make test-author' and
  'make test-compile' due to bypassing Devel::Cover.

Makefile
@@ -238,13 +238,12 @@ ifeq ($(TESTS),)
 test: test-helm-chart
 endif
 endif
-
 .PHONY: test-checkstyle
-test-checkstyle: test-checkstyle-standalone test-tidy-compile-style ## Run checkstyle and tidy-compile-style tests
+test-checkstyle: test-checkstyle-standalone test-author test-compile ## Run checkstyle, author, and compile tests
 
 .PHONY: test-t
 test-t: node_modules ## Run standard Perl tests
-	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:t/*style*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/*.t" GLOBIGNORE="$(unstables)"
 
 .PHONY: test-heavy
 test-heavy: node_modules ## Run heavy tests
@@ -252,11 +251,12 @@ test-heavy: node_modules ## Run heavy tests
 
 .PHONY: test-ui
 test-ui: node_modules ## Run UI tests
-	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:t/*style*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=25 PROVE_ARGS="$$HARNESS t/ui/*.t" GLOBIGNORE="$(unstables)"
 
 .PHONY: test-api
 test-api: node_modules ## Run API tests
-	$(MAKE) test-with-database TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="t/*tidy*:t/*compile*:t/*style*:$(unstables)"
+	$(MAKE) test-with-database TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/api/*.t" GLOBIGNORE="$(unstables)"
+
 
 # put unstable tests in tools/unstable_tests.txt and uncomment in circle CI config to handle unstables with retries
 .PHONY: test-unstable
@@ -371,18 +371,18 @@ setup-hooks: ## Install pre-commit git hooks
 
 # all additional checks not called by prove
 .PHONY: test-checkstyle-standalone
-test-checkstyle-standalone: test-shellcheck test-yaml test-critic test-shfmt test-gitlint ## Run all style and static analysis checks
+test-checkstyle-standalone: test-shellcheck test-yaml test-shfmt test-gitlint ## Run all style and static analysis checks
 ifeq ($(CONTAINER_TEST),1)
 test-checkstyle-standalone: test-check-containers
 endif
 
-.PHONY: test-critic
-test-critic: ## Run Perl Critic
-	prove xt/02-perlcritic.t
+.PHONY: test-author
+test-author: ## Run author tests (tidy, style, critic, pod, etc.)
+	$(MAKE) test-unit-and-integration TIMEOUT_M=20 PROVE_ARGS="$$HARNESS xt/*.t" GLOBIGNORE="$(unstables)"
 
-.PHONY: test-tidy-compile-style
-test-tidy-compile-style: ## Run tidy, compile and style tests
-	$(MAKE) test-unit-and-integration TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/*{tidy,compile,style}*.t" GLOBIGNORE="$(unstables)"
+.PHONY: test-compile
+test-compile: ## Run compile tests
+	$(MAKE) test-unit-and-integration TIMEOUT_M=20 PROVE_ARGS="$$HARNESS t/compile/*.t" GLOBIGNORE="$(unstables)"
 
 .PHONY: test-shellcheck
 test-shellcheck: ## Run shellcheck on scripts

dist/rpm/openQA.spec
@@ -346,9 +346,6 @@ sed -i '/Perl::Tidy/d' cpanfile
 cpanm -n --mirror http://no.where/ --installdeps --with-feature=test .
 %endif
 
-# we don't really need the tidy test
-rm -f t/00-tidy.t
-
 %if %{with tests}
 rm -rf %{buildroot}/DB
 export LC_ALL=en_US.UTF-8

t/compile/01-compile-check-all.t
@@ -8,7 +8,7 @@ use Test::Warnings qw(:no_end_test :report_warnings);
 use Test::Compile;
 use File::Which;
 use FindBin;
-use lib "$FindBin::Bin/lib", "$FindBin::Bin/../external/os-autoinst-common/lib";
+use lib "$FindBin::Bin/../lib", "$FindBin::Bin/../../external/os-autoinst-common/lib";
 use OpenQA::Test::TimeLimit '400';
 
 
@@ -37,12 +37,12 @@ if (-d '.git' and which('git')) {
     chomp @all_git_files;
     my %skip = map { $_ => undef } @$SKIP;
     @files = map { $root . $_ }
-      grep { !-l $_ && !exists $skip{$_} && $_ !~ /^(external|t)\// } @all_git_files;    # Exclude files to skip
+      grep { !-l $_ && !exists $skip{$_} && $_ !~ /^(external|t|xt)\// } @all_git_files;    # Exclude files to skip
 }
 else {
     @files = ($test->all_pm_files('lib'), $test->all_pl_files('script'));
     my %skip = map { $_ => undef } @$SKIP;
-    @files = grep { my $f = s{^\./}{}r; !exists $skip{$f} && $f !~ /^t\// } @files;
+    @files = grep { my $f = s{^\./}{}r; !exists $skip{$f} && $f !~ /^(external|t|xt)\// } @files;
 }
 
 # Only check perl files and skip test scripts (already executed)

t/testrules.yml
@@ -3,9 +3,7 @@
 # test not listed here is run afterwards
 seq:
   - par:
-      - ./t/00-tidy.t
-      - ./t/01-compile-check-all.t
-      - ./t/02-pod.t
+      - ./t/compile/01-compile-check-all.t
       - ./t/04-scheduler.t
       - ./t/05-scheduler-cancel.t
       - ./t/05-scheduler-capabilities.t
@@ -40,3 +38,5 @@ seq:
       - ./t/basic.t
       - ./t/deploy.t
       - ./t/full-stack.t
+      - ./xt/00-tidy.t
+      - ./xt/02-pod.t

xt/01-style.t
@@ -8,20 +8,20 @@ ok system(qq{git grep -I -l 'Copyright \((C)\|(c)\|©\)' ':!COPYING' ':!external
   'No redundant copyright character';
 ok
   system(
-qq{git grep -I -l 'This program is free software.*if not, see <http://www.gnu.org/licenses/' ':!COPYING' ':!external/' ':!t/01-style.t'}
+qq{git grep -I -l 'This program is free software.*if not, see <http://www.gnu.org/licenses/' ':!COPYING' ':!external/' ':!xt/01-style.t'}
   ) != 0, 'No verbatim licenses in source files';
-ok system(qq{git grep -I -l '[#/ ]*SPDX-License-Identifier ' ':!COPYING' ':!external/' ':!t/01-style.t'}) != 0,
+ok system(qq{git grep -I -l '[#/ ]*SPDX-License-Identifier ' ':!COPYING' ':!external/' ':!xt/01-style.t'}) != 0,
   'SPDX-License-Identifier correctly terminated';
-is qx{git grep -I -L '^use Test::Most' t/**.t}, '', 'All tests use Test::Most';
+is qx{git grep -I -L '^use Test::Most' {t,xt}/**.t}, '', 'All tests use Test::Most';
 is qx{git grep --all-match -e '^use Mojo::Base' -e 'use base'}, '', 'No redundant Mojo::Base+base';
 is qx{git grep -I --all-match -e '^use Mojo::Base' -e 'use \\(strict\\|warnings\\);' ':!docs' ':!external'}, '',
   'Only combined Mojo::Base+strict+warnings';
-is qx{git grep -I -L '^use Test::Warnings' t/**.t ':!t/01-style.t'}, '', 'All tests use Test::Warnings';
-is qx{git grep -I -l '^use Test::\\(Exception\\|Fatal\\)' t/**.t}, '',
+is qx{git grep -I -L '^use Test::Warnings' {t,xt}/**.t ':!xt/01-style.t'}, '', 'All tests use Test::Warnings';
+is qx{git grep -I -l '^use Test::\\(Exception\\|Fatal\\)' {t,xt}/**.t}, '',
   'Test::Most already includes Test::Exception, no need to use Test::Exception or Test::Fatal';
-is qx{git grep -I -l '^\\(throws\\|dies\\|lives\\)_ok.*\<sub\>' t/**.t}, '',
+is qx{git grep -I -l '^\\(throws\\|dies\\|lives\\)_ok.*\<sub\>' {t,xt}/**.t}, '',
   'Only use simplified prototyped Test::Exception functions';
-is qx{git grep -I -l 'like.*\$\@' t/**.t}, '', 'Use throws_ok instead of manual checks of exceptions';
+is qx{git grep -I -l 'like.*\$\@' {t,xt}/**.t}, '', 'Use throws_ok instead of manual checks of exceptions';
 is qx{git grep -I -l ' \\(if\\|unless\\) \$\@'}, '', 'Use try/catch instead of manual \$\@ checks';
 is qx{git grep -I -l '^use \\(Try::Tiny\\|TryCatch\\)'}, '',
   'No Try::Tiny or TryCatch necessary, use Feature::Compat::Try and later native Perl';
@@ -31,7 +31,7 @@ qx{git grep -l -e '^sub \\S\\+ [^(]\\+' --and --not -e 'sub [(\{]' --and --not -
 is qx{git grep -I -l 'sub [a-z_A-Z0-9]\\+()' ':!docs/'}, '',
   'Consistent space before function signatures (this is not ensured by perltidy)';
 is
-qx{git grep -Pr "(?<!->)(?<!sub )\\b(ok|is|isnt|like|unlike|cmp_ok|can_ok|isa_ok|subtest|diag|note|explain|pass|fail|new_ok|is_deeply)\\s*\\(" t/ | grep -vE "t/(lib|testresults)/"},
+qx{git grep -Pr "(?<!->)(?<!sub )\\b(ok|is|isnt|like|unlike|cmp_ok|can_ok|isa_ok|subtest|diag|note|explain|pass|fail|new_ok|is_deeply)\\s*\\(" {t,xt}/ | grep -vE "(t|xt)/(lib|testresults)/"},
   '',
   'Consistent Test::More call format (no parentheses)';
 

commit 3599347ce90019724bbaf4f80c3974116f947903

fix: make TESTS selection work again (regression from cd76f31f3)
Motivation:
Specifying individual tests via 'make test TESTS=...' should ideally
isolate execution and avoid the overhead of unrelated checks. A recent
modification (commit cd76f31f3) unconditionally added 'test-compile' to
the main 'test' target, causing it to run even when a user explicitly
requested only specific test files.

Design Choices:
- Introduced a 'COMPILE_TEST' variable to control the 'test-compile'
  target, similar to the existing 'CHECKSTYLE' variable.
- Added logic to implicitly set 'COMPILE_TEST=0' whenever 'TESTS' is
  specified.
- Updated the 'test' target to use the 'compile_tests' variable instead
  of a hardcoded 'test-compile' dependency.

Makefile
@@ -25,6 +25,7 @@ ifeq ($(TESTS),)
 PROVE_ARGS ?= --trap -r ${EXTRA_PROVE_ARGS} t
 else
 CHECKSTYLE ?= 0
+COMPILE_TEST ?= 0
 PROVE_ARGS ?= --trap ${EXTRA_PROVE_ARGS} $(TESTS)
 endif
 PROVE_LIB_ARGS ?= -l
@@ -234,7 +235,14 @@ checkstyle_tests =
 else
 checkstyle_tests = test-checkstyle-standalone
 endif
-test: $(checkstyle_tests) test-compile test-with-database ## Run all tests (including checkstyle, compile test and database tests)
+
+ifeq ($(COMPILE_TEST),0)
+compile_tests =
+else
+compile_tests = test-compile
+endif
+
+test: $(checkstyle_tests) $(compile_tests) test-with-database ## Run all tests (including checkstyle, compile test and database tests)
 ifeq ($(CONTAINER_TEST),1)
 ifeq ($(TESTS),)
 test: test-containers-compose