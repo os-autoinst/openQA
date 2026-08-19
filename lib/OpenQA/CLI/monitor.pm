b4a1a53e6f7f82562f64f457da85428cb13a71df
Oliver Kurz <okurz@suse.de>
2026-08-19 13:21:24 +0200

    feat: support native scheduled product monitoring
    Motivation:
    Avoid custom polling downstream scripts (such as monitor-openqa_job) when
    triggering and monitoring are decoupled.
    
    Design Choices:
    Refactor OpenQA::CLI::schedule to move _wait_for_jobs and helper
    subroutines into OpenQA::CLI::monitor. Add scheduled-product-id to the
    monitor subcommand natively.
    
    Benefits:
    Reduces code duplication across repositories, improves maintainability,
    and provides clean integration CLI commands for separate CI pipeline jobs.

--- lib/OpenQA/CLI/monitor.pm
+++ lib/OpenQA/CLI/monitor.pm
@@ -7,9 +7,41 @@ use Mojo::Base 'OpenQA::Command', -signatures;
 use OpenQA::Jobs::Constants;
 use List::Util qw(all);
 use Mojo::Util qw(encode);
+use Term::ANSIColor qw(colored);
 
 has description => 'Monitors a set of jobs';
 has usage => sub { OpenQA::CLI->_help('monitor') };
+
+sub _error_from_json ($self, $json) {
+    return $json->{error} // join "\n", map { $_->{error_message} } @{$json->{failed}};
+}
+
+sub _populate_job_ids ($self, $results, $job_ids) {
+    return 'No results present' unless ref $results eq 'HASH';
+    my $ids = $results->{successful_job_ids};
+    return 'No successful job IDs present' unless ref $ids eq 'ARRAY';
+    push @$job_ids, @$ids;
+    return 0;
+}
+
+sub _wait_for_jobs ($self, $client, $poll_interval, $scheduled_product_id, $job_ids) {
+    return undef unless $scheduled_product_id;
+    my %pending_statuses = (added => 1, scheduling => 1);
+    my $status = 'added';
+    while (exists $pending_statuses{$status}) {
+        my $tx = $client->build_tx(GET => $self->url_for("isos/$scheduled_product_id"));
+        my $res = $self->retry_tx($client, $tx);
+        return $res if $res != 0;
+        my $json = $tx->res->json;
+        $status = $json->{status} // '?';
+        my $results = $json->{results};
+        my $error = $self->_error_from_json($results // $json);
+        return $error if $error;
+        return $self->_populate_job_ids($results, $job_ids) if $status eq 'scheduled';
+        sleep $poll_interval;
+    }
+    return "Scheduled product $scheduled_product_id ended up $status";
+}
 
 sub _monitor_jobs ($self, $client, $follow, $poll_interval, $job_ids, $job_results) {
     my $start = time;
@@ -48,8 +80,20 @@ sub command ($self, @args) {
     die $self->usage unless OpenQA::CLI::get_opt(monitor => \@args, [], \my %options);
 
     @args = $self->decode_args(@args);
-    $self->_monitor_and_return($self->client($self->url_for('tests')),
-        $options{follow}, $options{'poll-interval'}, \@args);
+    my $client = $self->client($self->url_for('tests'));
+    my $poll_interval = $options{'poll-interval'} // 10;
+    my @job_ids = @args;
+
+    if (my $sp_id = $options{'scheduled-product-id'}) {
+        my $error = $self->_wait_for_jobs($client, $poll_interval, $sp_id, \@job_ids);
+        if ($error) {
+            print STDERR colored(['red'], $error, "\n");
+            return 1;
+        }
+    }
+
+    die $self->usage unless @job_ids;
+    $self->_monitor_and_return($client, $options{follow}, $poll_interval, \@job_ids);
 }
 
 1;
