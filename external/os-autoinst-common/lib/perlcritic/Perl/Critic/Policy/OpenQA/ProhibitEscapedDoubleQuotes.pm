94474ae83a30a8c03875f97a4c81d7676f92dd79
Oliver Kurz <okurz@suse.de>
2026-09-22 13:09:52 +0200

    feat(style): enable strict escaped double quotes policy globally
    Motivation:
    Clean up and enforce the custom OpenQA::ProhibitEscapedDoubleQuotes
    policy strictly
    across the entire codebase now that all existing violations have been resolved.
    
    Design Choices:
    Remove the temporary ignore list of files from ProhibitEscapedDoubleQuotes.pm.
    
    Benefits:
    Ensures complete consistency of idiomatic quoting across the entire repository
    moving forward.

--- external/os-autoinst-common/lib/perlcritic/Perl/Critic/Policy/OpenQA/ProhibitEscapedDoubleQuotes.pm
+++ external/os-autoinst-common/lib/perlcritic/Perl/Critic/Policy/OpenQA/ProhibitEscapedDoubleQuotes.pm
@@ -19,52 +19,7 @@ sub default_severity { return $SEVERITY_MEDIUM }
 sub default_themes { return qw(openqa) }
 sub applies_to { return qw(PPI::Token::Quote::Double) }
 
-# Existing files with violations to be ignored to keep the build green while
-# preventing any further/new violations from being introduced in other files.
-my %ignored_files = map { $_ => 1 } qw(
-    lib/OpenQA/Downloader.pm
-    lib/OpenQA/Markdown.pm
-    lib/OpenQA/LiveHandler/Controller/LiveViewHandler.pm
-    lib/OpenQA/Schema.pm
-    lib/OpenQA/Shared/Controller/Running.pm
-    lib/OpenQA/Task/Needle/Delete.pm
-    lib/OpenQA/Shared/Controller/Auth.pm
-    lib/OpenQA/WebSockets.pm
-    lib/OpenQA/Worker.pm
-    lib/OpenQA/WebAPI/Controller/Test.pm
-    t/10-tests_overview.t
-    t/25-bugs.t
-    t/10-jobs.t
-    t/33-developer_mode.t
-    t/34-developer_mode-unit.t
-    t/40-script_load_dump_templates.t
-    t/44-scripts-initdb.t
-    t/35-script_clone_job.t
-    t/43-cli-api.t
-    t/api/09-comments.t
-    t/lib/OpenQA/Test/Database.pm
-    t/full-stack.t
-    t/ui/16-activity-view.t
-    t/ui/15-comments.t
-    t/ui/12-needle-edit.t
-    t/ui/16-tests_job_next_previous.t
-    t/ui/13-admin.t
-    t/ui/10-tests_overview.t
-    tools/generate-cli-completions
-    t/ui/25-developer_mode.t
-    t/ui/18-tests-details.t
-);
-
 sub violates ($self, $elem, $doc) {
-    my $filename = $doc->filename();
-    if ($filename) {
-        for my $ignored (keys %ignored_files) {
-            if ($filename =~ /\Q$ignored\E$/) {
-                return ();
-            }
-        }
-    }
-
     my $content = $elem->content;
 
     # Strip the enclosing double quotes of PPI::Token::Quote::Double
